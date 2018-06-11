a8b86c3f8173b7f2126e80372fa0dddb0cda9e7f
gc <30398469+gach@users.noreply.github.com>
2018-06-12 07:42:02 +1000

    small change

--- commands/RuneScape - Fun/trivia.js
+++ commands/RuneScape - Fun/trivia.js
@@ -13,12 +13,12 @@ module.exports = class extends Command {
 
 	async run(msg) {
 		const item = triviaQuestions[Math.floor(Math.random() * triviaQuestions.length)];
-		await msg.channel.send(item.q);
+		await msg.send(item.q);
 		try {
 			const collected = await msg.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
-			const winnerMessage = collected.first();
+			const winner = collected.first();
 			return msg.channel.send(
-				`<:RSTickBox:381462594734522372> ${winnerMessage.author} had the right answer with \`${winnerMessage.content}\`!`
+				`<:RSTickBox:381462594734522372> ${winner.author} had the right answer with \`${winner.content}\`!`
 			);
 		} catch (err) {
 			return msg.channel.send('<:RSXBox:381462594961014794> Nobody answered correctly.');
