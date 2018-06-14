c691fdbdf1e1caf376af35f4bc83bcd846c88a42
gc <30398469+gach@users.noreply.github.com>
2018-06-15 06:26:49 +1000

    update cachecleaner

--- tasks/cachecleanup.js
+++ tasks/cachecleanup.js
@@ -1,6 +1,29 @@
-// Copyright kyranet @ https://gist.github.com/kyranet/4542edf4679ff1a6a8fe6bf94909ac9d
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Kyra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-const { Task } = require('klasa');
+const { Task } = require('../index');
 const { util: { binaryToID } } = require('discord.js');
 
 // THRESHOLD equals to 30 minutes in milliseconds:
@@ -14,19 +37,16 @@ const THRESHOLD = 1000 * 60 * 30,
 // The header with the console colours
 const HEADER = `\u001B[39m\u001B[94m[CACHE CLEANUP]\u001B[39m\u001B[90m`;
 
-module.exports = class extends Task {
-
-	constructor(...args) {
-		super(...args, { enabled: true });
-	}
+/**
+ * @since 3.0.0
+ * @version 2.0.0
+ * @extends {Task}
+ */
+module.exports = class MemorySweeper extends Task {
 
 	async run() {
-		const OLD_SNOWFLAKE = binaryToID((Date.now() - THRESHOLD - EPOCH).toString(2).padStart(42, '0') + EMPTY);
-		let presences = 0,
-			guildMembers = 0,
-			emojis = 0,
-			lastMessage = 0,
-			users = 0;
+		const OLD_SNOWFLAKE = binaryToID(((Date.now() - THRESHOLD) - EPOCH).toString(2).padStart(42, '0') + EMPTY);
+		let presences = 0, guildMembers = 0, emojis = 0, lastMessages = 0, users = 0;
 
 		// Per-Guild sweeper
 		for (const guild of this.client.guilds.values()) {
@@ -38,7 +58,8 @@ module.exports = class extends Task {
 			const { me } = guild;
 			for (const [id, member] of guild.members) {
 				if (member === me) continue;
-				if (member.lastMessageID && member.lastMessageID < OLD_SNOWFLAKE) continue;
+				if (member.voiceChannelID) continue;
+				if (member.lastMessageID && member.lastMessageID > OLD_SNOWFLAKE) continue;
 				guildMembers++;
 				guild.members.delete(id);
 			}
@@ -52,36 +73,37 @@ module.exports = class extends Task {
 		for (const channel of this.client.channels.values()) {
 			if (channel.lastMessageID) {
 				channel.lastMessageID = null;
-				lastMessage++;
+				lastMessages++;
 			}
 		}
 
 		// Per-User sweeper
 		for (const user of this.client.users.values()) {
-			if (user.lastMessageID && user.lastMessageID < OLD_SNOWFLAKE) continue;
-			user.lastMessageID = null;
+			if (user.lastMessageID && user.lastMessageID > OLD_SNOWFLAKE) continue;
 			this.client.users.delete(user.id);
-			lastMessage++;
+			this.client.gateways.users.cache.delete(user.id);
 			users++;
 		}
 
 		// Emit a log
-		this.client.console.log(
-			`${HEADER} ${this.setColor(presences)} [Presence]s | ${this.setColor(guildMembers)} [GuildMember]s | ${this.setColor(
-				users
-			)} [User]s | ${this.setColor(emojis)} [Emoji]s | ${this.setColor(lastMessage)} [Last Message]s.`
-		);
+		this.client.emit('verbose',
+			`${HEADER} ${
+				this.setColor(presences)} [Presence]s | ${
+				this.setColor(guildMembers)} [GuildMember]s | ${
+				this.setColor(users)} [User]s | ${
+				this.setColor(emojis)} [Emoji]s | ${
+				this.setColor(lastMessages)} [Last Message]s.`);
 	}
 
 	/**
-   * Set a colour depending on the amount:
-   * > 1000 : Light Red colour
-   * > 100  : Light Yellow colour
-   * < 100  : Green colour
-   * @since 3.0.0
-   * @param {number} number The number to colourise
-   * @returns {number}
-   */
+	 * Set a colour depending on the amount:
+	 * > 1000 : Light Red colour
+	 * > 100  : Light Yellow colour
+	 * < 100  : Green colour
+	 * @since 3.0.0
+	 * @param {number} number The number to colourise
+	 * @returns {number}
+	 */
 	setColor(number) {
 		const text = String(number).padStart(5, ' ');
 		// Light Red color
