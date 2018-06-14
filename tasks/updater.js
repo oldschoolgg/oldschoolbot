a269a26fd3b42812f08e095fcbd8defd9c96b367
gc <30398469+gach@users.noreply.github.com>
2018-06-15 06:36:10 +1000

    use old updater

--- tasks/updater.js
+++ tasks/updater.js
@@ -5,12 +5,14 @@ const delay = time => new Promise(res => setTimeout(() => res(), time));
 module.exports = class extends Task {
 
 	async run() {
-		const chunks = chunk(this.client.gateways.users.cache
-		    .filter(config => config.autoupdate)
-		    .map(config => `{"type": "update", "player": "${config.RSN}"}`), 5);
-		for (const userChunk of chunks) {
-		    snekfetch.get(`https://crystalmathlabs.com/tracker/api.php?multiquery=[${userChunk.join(',')}]`).catch(() => null);
-		    await sleep(500);
+		const userArray = [];
+		this.client.gateways.users.cache.filter(config => config.autoupdate).forEach(config => {
+			userArray.push(`{"type": "update", "player": "${config.RSN}"}`);
+		});
+		for (let i = 0; i < userArray.length / 5; i++) {
+			const usernames = userArray.slice(i, i + 5).join(', ');
+			snekfetch.get(`https://crystalmathlabs.com/tracker/api.php?multiquery=[${usernames}]`).catch(() => null);
+			await delay(500);
 		}
 	}
 
