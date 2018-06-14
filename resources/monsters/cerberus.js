1d06fef279ffb4ba54914e61af61b9e6c7c0f60d
gc <30398469+gach@users.noreply.github.com>
2018-06-14 12:12:30 +1000

    use classes for the monsters

--- resources/monsters/cerberus.js
+++ resources/monsters/cerberus.js
@@ -1,19 +1,27 @@
-module.exports = {
-	drops: {
-		crystals: [{
-			smoulderingStone: '<:Smouldering_stone:403380366657978400>',
-			eternalCrystal: '<:Eternal_crystal:403380366808973312>',
-			pegasianCrystal: '<:Pegasian_crystal:403380366611578891>',
-			primordialCrystal: '<:Primordial_crystal:403380366888665088>'
-		}],
-		jarOfSouls: '<:Jar_of_souls:403383744771391490>',
-		hellpuppy: '<:Hellpuppy:324127376185491458>'
-	},
+const { Monster } = require('./Monster');
+
+class Cerberus extends Monster {
+
+	constructor() {
+		super();
+		this.drops = {
+			crystals: [{
+				smoulderingStone: '<:Smouldering_stone:403380366657978400>',
+				eternalCrystal: '<:Eternal_crystal:403380366808973312>',
+				pegasianCrystal: '<:Pegasian_crystal:403380366611578891>',
+				primordialCrystal: '<:Primordial_crystal:403380366888665088>'
+			}],
+			jarOfSouls: '<:Jar_of_souls:403383744771391490>',
+			hellpuppy: '<:Hellpuppy:324127376185491458>'
+		};
+	}
+
 	randomCrystal() {
 		const keys = Object.keys(this.drops.crystals);
 		const randomKey = keys[Math.floor(Math.random() * keys.length)];
 		return this.drops.crystals[randomKey];
-	},
+	}
+
 	kill(quantity) {
 		const loot = [];
 		for (let i = 0; i < quantity; i++) {
@@ -23,4 +31,7 @@ module.exports = {
 		}
 		return loot.join(' ');
 	}
-};
+
+}
+
+module.exports = Cerberus;
