1f9a45217b2fff6e5b3ae7d504db07986fbe102c
gc <30398469+gach@users.noreply.github.com>
2018-06-28 20:06:14 +1000

    Update info command links

--- commands/Utility/info.js
+++ commands/Utility/info.js
@@ -15,11 +15,13 @@ Old School Bot is an open-source Discord Bot based on Old School RuneScape.
 
 	- Github/Code: <https://github.com/gc/oldschoolbot/> ⭐
 
+	- Bot Invite Link: <http://bit.ly/2lDw8e7>
+
 	- Old School Bot uses Klasa, a Discord Bot framework.
 
 	- If you have any other enquiries, join the support server!
 
-	- Support Server: https://discord.gg/ypZbV7c
+	- Support Server: http://bit.ly/oldschoolbotsupport
 `);
 	}
 
