1d840d4d6ba3b8ffdcb7691c7e44804b13cbec14
gc <30398469+gach@users.noreply.github.com>
2018-06-28 20:37:23 +1000

    add emote-servers command

---
+++ commands/Utility/emotes.js
@@ -0,0 +1,31 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['emoteservers'],
+			description: 'Sends the invite link for the bot.'
+		});
+	}
+
+	async run(msg) {
+		const embed = new MessageEmbed()
+			.setTitle('Emote Servers')
+			.setColor(14981973)
+			.setThumbnail(this.client.user.displayAvatarURL())
+			.setDescription(servers);
+
+		return msg.send({ embed });
+	}
+
+};
+
+const servers = `
+https://discord.gg/SB8edn6
+https://discord.gg/hBAySb3
+https://discord.gg/9DtWEXH
+https://discord.gg/8GR2fZM
+https://discord.gg/j7s22Mc
+https://discord.gg/JWGsNRy`;
