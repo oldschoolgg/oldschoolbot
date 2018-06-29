3228b0dfeb8133b811694a25bec2da7a80d97d0f
gc <30398469+gach@users.noreply.github.com>
2018-06-29 20:11:25 +1000

    add final hour command

---
+++ commands/Utility/final.js
@@ -0,0 +1,25 @@
+const { Command } = require('klasa');
+const moment = require('moment');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['dmt', 'tourney'],
+			description: 'Information on the final hour of DMM.'
+		});
+	}
+
+	async run(msg) {
+		const timeUntil = moment(new Date(1529778600 * 1000)).format('HH [hours] [and] mm [minutes]');
+		return msg.send(`
+**Permadeath Stage:** ${timeUntil}
+**Stream:** <http://www.twitch.tv/oldschoolrs>
+**Seasonals:** Immediately after the tournament finishes
+**Total Prize:** $32,000
+**First Place Prize:** $20,000
+**News Post:** <http://services.runescape.com/m=news/deadman-summer-finals-2018-live?oldschool=1>
+`);
+	}
+
+};
