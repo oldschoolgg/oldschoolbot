0480a43bec94c16ef6d0b4005ec9bd5e7ff00990
gc <30398469+gach@users.noreply.github.com>
2018-07-17 13:28:08 +1000

    Small bug

--- commands/RuneScape - Fun/casket.js
+++ commands/RuneScape - Fun/casket.js
@@ -1,20 +1,22 @@
 const { Command } = require('klasa');
-const master = require('../../resources/clues/master');
-const medium = require('../../resources/clues/medium');
+const clues = {
+	master: require('../../resources/clues/master'),
+	medium: require('../../resources/clues/medium')
+};
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
 			description: 'Simulate rare drops from clue scrolls',
-			usage: '<easy|medium|elite|master> <quantity:int>',
+			usage: '<medium|master> <quantity:int>',
 			usageDelim: ' '
 		});
 	}
 
 	async run(msg, [tier, quantity]) {
 		if (quantity > 100) return msg.send('I can only do 100 Clues at a time!');
-		const loot = [tier].open(quantity);
+		const loot = clues[tier].open(quantity);
 		return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 	}
 
