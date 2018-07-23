17f41b18c356fd77ed66e6cc24b26c97f6f33d0e
gc <30398469+gach@users.noreply.github.com>
2018-07-23 14:46:44 +1000

    format the amount

--- tasks/vote.js
+++ tasks/vote.js
@@ -5,7 +5,7 @@ module.exports = class extends Task {
 	async run(vote) {
 		const user = await this.client.users.fetch(vote.user);
 		const amount = Math.floor(Math.random() * 5000000) + 1;
-		this.client.voteLogs.send(`${user} just voted for Old School Bot and received ${amount} GP! Thank you <:Smiley:420283725469974529>`);
+		this.client.voteLogs.send(`${user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`);
 		user.configs.update('GP', amount);
 	}
 
