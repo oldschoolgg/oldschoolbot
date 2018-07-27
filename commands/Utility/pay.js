2cff74698c37bf5ec93fc2d0e41548705083d1c5
gc <30398469+gach@users.noreply.github.com>
2018-07-27 23:47:03 +1000

    dont let people send money to bots

--- commands/Utility/pay.js
+++ commands/Utility/pay.js
@@ -14,6 +14,7 @@ module.exports = class extends Command {
 		const { GP } = msg.author.configs;
 		if (GP < amount) throw `You don't have enough GP.`;
 		if (user.id === msg.author.id) throw `You can't send money to yourself.`;
+		if (user.bot) throw `You can't send money to a bot.`;
 		msg.author.configs.update('GP', GP - amount);
 		user.configs.update('GP', user.configs.GP + amount);
 		return msg.send(`You sent ${amount.toLocaleString()} GP to ${user}.`);
