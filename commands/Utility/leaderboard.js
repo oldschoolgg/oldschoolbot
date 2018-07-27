02de1295e5b9a0c514b1d250b5f168c6f9b4db56
gc <30398469+gach@users.noreply.github.com>
2018-07-27 23:34:47 +1000

    add leaderboard command

---
+++ commands/Utility/leaderboard.js
@@ -0,0 +1,17 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Shows the people with the most virtual GP.'
+		});
+	}
+
+	async run(msg) {
+		const users = this.client.users.filter(user => user.configs.GP > 0);
+		const leaderboard = users.map(user => `**${user.username}** has ${user.configs.GP.toLocaleString()} GP `);
+		return msg.send(leaderboard.join('\n'));
+	}
+
+};
