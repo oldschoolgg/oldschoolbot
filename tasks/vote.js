09981a9cc20bfab21dab7795cd0c1198cebcb7f6
gc <30398469+gach@users.noreply.github.com>
2018-07-28 00:06:12 +1000

    double GP on weekends, increase minimum amount for voting to 500k

--- tasks/vote.js
+++ tasks/vote.js
@@ -4,7 +4,8 @@ module.exports = class extends Task {
 
 	async run(vote) {
 		const user = await this.client.users.fetch(vote.user);
-		const amount = Math.floor(Math.random() * 5000000) + 1;
+		let amount = Math.floor(Math.random() * 5000000) + 500000;
+		if (vote.isWeekend) amount *= 2;
 		this.client.voteLogs.send(`${user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`);
 		user.configs.update('GP', amount);
 	}
