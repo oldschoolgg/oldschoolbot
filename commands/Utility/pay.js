83e6cfabab1882f4e3b52492ea496df33f68cbb2
gc <30398469+gach@users.noreply.github.com>
2018-07-27 23:42:35 +1000

    add pay command

---
+++ commands/Utility/pay.js
@@ -0,0 +1,19 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Send virtual GP to people (not real GP).',
+			usage: '<user:user> <amount:int{1,2147483647}>'
+		});
+	}
+
+	async run(msg, [user, amount]) {
+		const { GP } = msg.author.configs;
+		if (GP < amount) throw `You don't have enough GP.`;
+		msg.author.configs.update('GP', GP - amount);
+		user.configs.update('GP', user.configs.GP + amount);
+	}
+
+};
