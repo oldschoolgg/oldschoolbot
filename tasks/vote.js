3e72e182fe94ed12a51f761f0517b4900dbe1c0b
gc <30398469+gach@users.noreply.github.com>
2018-07-28 15:01:35 +1000

    oops

--- tasks/vote.js
+++ tasks/vote.js
@@ -7,7 +7,7 @@ module.exports = class extends Task {
 		let amount = Math.floor(Math.random() * 5000000) + 500000;
 		if (vote.isWeekend) amount *= 2;
 		this.client.voteLogs.send(`${user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`);
-		user.configs.update('GP', amount);
+		user.configs.update('GP', user.configs.GP + amount);
 	}
 
 };
