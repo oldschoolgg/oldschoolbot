3b12d7bf2333a6a1a6bf2063aeef26c695f8dd98
gc <30398469+gach@users.noreply.github.com>
2018-08-03 19:59:50 +1000

    test

--- commands/Utility/biggify.js
+++ commands/Utility/biggify.js
@@ -10,18 +10,17 @@ module.exports = class extends Command {
 
 	async run(msg) {
 		const { url } = await this.checkChatForImage(msg);
-		Jimp.read(url).then(async img => {
-			img.resize(400, Jimp.AUTO);
-			const chunkHeight = img.bitmap.height / 4;
-			for (let i = 0; i <= 3; i++) {
-				const tempImg = img.clone().crop(0, chunkHeight * i, 400, chunkHeight);
-				await sleep(100);
-				tempImg.getBuffer('image/png', (err, buffer) => {
-					if (err) throw err;
-					return msg.send(new MessageAttachment(buffer, 'image.png'));
-				});
-			}
-		});
+		const img = await Jimp.read(url);
+		img.resize(400, Jimp.AUTO);
+		const chunkHeight = img.bitmap.height / 4;
+		for (let i = 0; i <= 3; i++) {
+			const tempImg = img.clone().crop(0, chunkHeight * i, 400, chunkHeight);
+			await sleep(100);
+			tempImg.getBuffer('image/png', (err, buffer) => {
+				if (err) throw err;
+				return msg.send(new MessageAttachment(buffer, 'image.png'));
+			});
+		}
 	}
 
 };
