650208ce5c5a22577a0c29f3c8eed7c48e2377fc
gc <30398469+gach@users.noreply.github.com>
2018-08-25 18:23:16 +1000

    fix

--- commands/RuneScape - Utility/players.js
+++ commands/RuneScape - Utility/players.js
@@ -10,38 +10,15 @@ module.exports = class extends Command {
 		});
 	}
 
-	getPlayerCount() {
-		return get('http://oldschool.runescape.com/slu').then(
+	async run(msg) {
+		const playerCount = await get('http://oldschool.runescape.com/slu').then(
 			res =>
 				res.body
 					.toString()
 					.split("<p class='player-count'>")[1]
 					.split('</p>')[0]
-					.replace(/\D/g, '')
 		);
-	}
-
-	getOsbPlayerCount() {
-		return get('https://rsbuddy.com/stats.json').then(res => res.body.inGame);
-	}
-
-	getRunelitePlayerCount() {
-		return get('https://api.runelite.net/runelite-1.4.10.2/session/count').then(res => res.body.toString());
-	}
-
-	async run(msg) {
-		const [
-			total,
-			osb,
-			runelite
-		] = await Promise.all([this.getPlayerCount(), this.getOsbPlayerCount(), this.getRunelitePlayerCount()]);
-
-		return msg.send(`
-There are ${parseInt(total).toLocaleString()} players ingame!
-
-**RuneLite:** ${parseInt(runelite).toLocaleString()} clients open
-**OSBuddy:** ${osb.toLocaleString()} clients open
-`);
+		return msg.send(playerCount);
 	}
 
 };
