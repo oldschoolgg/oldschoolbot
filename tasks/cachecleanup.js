57997ef3d38c4bfe388be8b312b2b6f450bc8217
gc <30398469+gach@users.noreply.github.com>
2018-09-25 19:59:56 +1000

    refactor cachecleanup

--- tasks/cachecleanup.js
+++ tasks/cachecleanup.js
@@ -1,89 +1,65 @@
-/**
- * @license
- * MIT License
- *
- * Copyright (c) 2017-2018 Kyra
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-const { Task } = require('klasa');
-const { util: { binaryToID } } = require('discord.js');
+const { Task, Colors } = require('klasa');
 
 // THRESHOLD equals to 30 minutes in milliseconds:
 //     - 1000 milliseconds = 1 second
 //     - 60 seconds        = 1 minute
 //     - 30 minutes
-const THRESHOLD = 1000 * 60 * 30,
-	EPOCH = 1420070400000,
-	EMPTY = '0000100000000000000000';
 
-// The header with the console colours
-const HEADER = `\u001B[39m\u001B[94m[CACHE CLEANUP]\u001B[39m\u001B[90m`;
-
-/**
- * @since 3.0.0
- * @version 2.0.0
- * @extends {Task}
- */
 module.exports = class MemorySweeper extends Task {
 
+	constructor(...args) {
+		super(...args);
+
+		// The colors to stylise the console's logs
+		this.colors = {
+			red: new Colors({ text: 'lightred' }),
+			yellow: new Colors({ text: 'lightyellow' }),
+			green: new Colors({ text: 'green' })
+		};
+
+		// The header with the console colors
+		this.header = new Colors({ text: 'lightblue' }).format('[CACHE CLEANUP]');
+	}
+
 	async run() {
-		const OLD_SNOWFLAKE = binaryToID(((Date.now() - THRESHOLD) - EPOCH).toString(2).padStart(42, '0') + EMPTY);
-		let presences = 0, guildMembers = 0, emojis = 0, lastMessages = 0, users = 0;
+		let presences = 0, guildMembers = 0, emojis = 0, lastMessages = 0;
 
 		// Per-Guild sweeper
 		for (const guild of this.client.guilds.values()) {
+			// Don't wipe the Old School Bot guild
+			if (guild.id === '342983479501389826') continue;
+
 			// Clear presences
 			presences += guild.presences.size;
 			guild.presences.clear();
 
-			// Clear members that haven't send a message in the last 30 minutes
+			// Clear emojis
+			emojis += guild.emojis.size;
+			guild.emojis.clear();
+
+			// Clear members
 			const { me } = guild;
 			for (const [id, member] of guild.members) {
 				if (member === me) continue;
-				if (member.voiceChannelID) continue;
-				if (member.lastMessageID && member.lastMessageID > OLD_SNOWFLAKE) continue;
 				guildMembers++;
 				guild.members.delete(id);
 			}
 
+			// Completely delete all voice channels, delete lastMessageID of channels
 			for (const channel of guild.channels.values()) {
-				if (channel.type !== 'voice') continue;
-				guild.channels.delete(channel.id);
-				this.client.channels.delete(channel.id);
-			}
-
-			// Clear emojis
-			emojis += guild.emojis.size;
-			guild.emojis.clear();
-		}
-
-		// Per-Channel sweeper
-		for (const channel of this.client.channels.values()) {
-			if (channel.lastMessageID) {
-				channel.lastMessageID = null;
-				lastMessages++;
+				if (channel.type === 'voice') {
+					guild.channels.delete(channel.id);
+					this.client.channels.delete(channel.id);
+				} else if (channel.type === 'text') {
+					if (!channel.lastMessageID) continue;
+					channel.lastMessageID = null;
+					lastMessages++;
+				}
 			}
 		}
 
-		/* DISABLED
+		/*
+		DISABLED
 		// Per-User sweeper
 		for (const user of this.client.users.values()) {
 			if (user.lastMessageID && user.lastMessageID > OLD_SNOWFLAKE) continue;
@@ -95,31 +71,21 @@ module.exports = class MemorySweeper extends Task {
 
 		// Emit a log
 		this.client.emit('verbose',
-			`${HEADER} ${
+			`${this.header} ${
 				this.setColor(presences)} [Presence]s | ${
 				this.setColor(guildMembers)} [GuildMember]s | ${
-				this.setColor(users)} [User]s | ${
 				this.setColor(emojis)} [Emoji]s | ${
 				this.setColor(lastMessages)} [Last Message]s.`);
 	}
 
-	/**
-	 * Set a colour depending on the amount:
-	 * > 1000 : Light Red colour
-	 * > 100  : Light Yellow colour
-	 * < 100  : Green colour
-	 * @since 3.0.0
-	 * @param {number} number The number to colourise
-	 * @returns {number}
-	 */
 	setColor(number) {
 		const text = String(number).padStart(5, ' ');
 		// Light Red color
-		if (number > 1000) return `\u001B[39m\u001B[91m${text}\u001B[39m\u001B[90m`;
+		if (number > 1000) return this.colors.red.format(text);
 		// Light Yellow color
-		if (number > 100) return `\u001B[39m\u001B[93m${text}\u001B[39m\u001B[90m`;
+		if (number > 100) return this.colors.yellow.format(text);
 		// Green color
-		return `\u001B[39m\u001B[32m${text}\u001B[39m\u001B[90m`;
+		return this.colors.green.format(text);
 	}
 
 };
