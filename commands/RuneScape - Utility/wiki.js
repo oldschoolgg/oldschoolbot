bcbf1ed72d99e0687f2a0beb18c7d934a1c8cfea
gc <30398469+gach@users.noreply.github.com>
2018-10-03 04:26:14 +1000

    use new wiki

--- commands/RuneScape - Utility/wiki.js
+++ commands/RuneScape - Utility/wiki.js
@@ -1,7 +1,8 @@
 const { Command } = require('klasa');
-const snekfetch = require('snekfetch');
-const base = 'http://oldschoolrunescape.wikia.com';
 const { MessageEmbed } = require('discord.js');
+const snekfetch = require('snekfetch');
+
+const getURL = (query) => `https://oldschool.runescape.wiki/api.php?action=query&format=json&prop=extracts%7Cpageimages%7Cinfo&iwurl=1&generator=search&formatversion=2&exsentences=5&exintro=1&explaintext=1&piprop=original&inprop=url&gsrsearch=${query}&gsrlimit=1`;
 
 module.exports = class extends Command {
 
@@ -10,32 +11,29 @@ module.exports = class extends Command {
 			cooldown: 3,
 			aliases: ['w'],
 			description: 'Search the OSRS Wikipedia for an article.',
-			usage: '<query:str>'
+			usage: '[query:str]'
 		});
 	}
 
 	async run(msg, [query]) {
-		const { id } = await snekfetch
-			.get(`${base}/api/v1/Search/List?query=${query}`)
-			.then(res => res.body.items[0])
-			.catch(() => {
-				throw "I couldn't find anything with that query!";
-			});
+		if (!query) {
+			return msg.send('https://oldschool.runescape.wiki');
+		}
 
-		const item = await snekfetch
-			.get(`${base}/api/v1/Articles/Details?ids=${id}&abstract=250`)
-			.then(res => res.body.items[id])
+		const { title, extract, thumbnail, fullurl } = await snekfetch
+			.get(getURL(query))
+			.then(res => res.body.query.pages[0])
 			.catch(() => {
 				throw "I couldn't find anything with that query!";
 			});
 
 		const embed = new MessageEmbed()
 			.setColor(52224)
-			.setThumbnail(item.thumbnail)
-			.setURL(base + item.url)
-			.setTitle(item.title)
-			.setDescription(item.abstract)
-			.setFooter('Old School RuneScape Wiki', 'https://i.imgur.com/GMs5my3.png');
+			.setThumbnail(thumbnail)
+			.setURL(fullurl)
+			.setTitle(title)
+			.setDescription(extract)
+			.setFooter('[Old School RuneScape Wiki](https://oldschool.runescape.wiki)', 'https://i.imgur.com/GMs5my3.png');
 		return msg.send({ embed });
 	}
 
