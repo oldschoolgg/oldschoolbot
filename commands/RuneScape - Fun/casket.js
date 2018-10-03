8de6065eb7f3c559c02e1691df983a5179476c25
gc <30398469+gach@users.noreply.github.com>
2018-10-03 14:21:59 +1000

    update casket command

--- commands/RuneScape - Fun/casket.js
+++ commands/RuneScape - Fun/casket.js
@@ -10,14 +10,15 @@ module.exports = class extends Command {
 		super(...args, {
 			description: 'Simulate rare drops from clue scrolls',
 			usage: '<medium|master> <quantity:int>',
+			cooldown: 1,
 			usageDelim: ' '
 		});
 	}
 
 	async run(msg, [tier, quantity]) {
-		if (quantity > 100) return msg.send('I can only do 100 Clues at a time!');
+		if (quantity > 100) return msg.send(`I can only do 100 ${tier} clues at a time!`);
 		const loot = clues[tier].open(quantity);
-		return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+		return msg.send(loot.length > 0 ? loot : "You didn't get any rares.");
 	}
 
 };
