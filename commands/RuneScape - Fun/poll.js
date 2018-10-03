b5d5746e94eff0c081b3b460cc9e58791400c9b8
gc <30398469+gach@users.noreply.github.com>
2018-10-03 14:22:04 +1000

    add poll command

---
+++ commands/RuneScape - Fun/poll.js
@@ -0,0 +1,20 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Creates a simple reaction poll for people to vote on.',
+			cooldown: 2
+		});
+	}
+
+	async run(msg) {
+		return msg.react('420283725469974529')
+			.then(react => react.message.react('421822898316115969'))
+			.catch(() => {
+				throw `There was an error <:Sad:421822898316115969> Do I have permissions to react to messages?`;
+			});
+	}
+
+};
