b283cace01191138f8ead88d240a4925ad634a74
gc <30398469+gach@users.noreply.github.com>
2018-10-08 05:32:25 +1100

    fix bug

--- extendables/combatLevel.js
+++ extendables/combatLevel.js
@@ -10,7 +10,7 @@ class combatLevel extends Extendable {
 		});
 	}
 
-	extend({ Defence, Attack, Ranged, Magic, Prayer, Hitpoints }) {
+	extend({ Defence, Strength, Attack, Ranged, Magic, Prayer, Hitpoints }) {
 		const base = 0.25 * (Defence.level + Hitpoints.level + Math.floor(Prayer.level / 2));
 		const melee = 0.325 * (Attack.level + Strength.level);
 		const range = 0.325 * (Math.floor(Ranged.level / 2) + Ranged.level);
