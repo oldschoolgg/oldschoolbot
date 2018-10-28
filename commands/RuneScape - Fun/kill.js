c8d342165e3be5fda45c8ba0ed5139f52e6f50e9
devin8 <40153548+devin8@users.noreply.github.com>
2018-10-28 13:00:54 -0400

    +kill/finish update
    Added arguments 
    Added bosses/monsters
    - Ancient wyverns
    - dks (+kill)
    - dag prime/supreme (+kill)
    - adamant/mithril/rune dragons (+kill)
    - crazy arch (+kill)
    - chaos fanatic (+kill)
    - bryophyta (+kill)

--- commands/RuneScape - Fun/kill.js
+++ commands/RuneScape - Fun/kill.js
@@ -1,36 +1,5 @@
 /* eslint-disable */
 const { Command } = require('klasa');
-const corp = require('../../resources/monsters/corp');
-const raidsOne = require('../../resources/monsters/raids');
-const tob = require('../../resources/monsters/tob');
-const barrows = require('../../resources/monsters/barrows');
-const bandos = require('../../resources/monsters/bandos');
-const saradomin = require('../../resources/monsters/saradomin');
-const zulrah = require('../../resources/monsters/zulrah');
-const vorkath = require('../../resources/monsters/vorkath');
-const cerberus = require('../../resources/monsters/cerberus');
-const abbyDemon = require('../../resources/monsters/abbyDemon');
-const abyssalSire = require('../../resources/monsters/abyssalSire');
-const giantMole = require('../../resources/monsters/giantMole');
-const grotesqueGuardians = require('../../resources/monsters/grotesqueGuardians');
-const demonicGorilla = require('../../resources/monsters/demonicGorilla');
-const thermy = require('../../resources/monsters/thermy');
-const skotizo = require('../../resources/monsters/skotizo');
-const kraken = require('../../resources/monsters/kraken');
-const kingBlackDragon = require('../../resources/monsters/kingBlackDragon');
-const venenatis = require('../../resources/monsters/venenatis');
-const callisto = require('../../resources/monsters/callisto');
-const scorpia = require('../../resources/monsters/scorpia');
-const dagannothRex = require('../../resources/monsters/dagannothRex');
-const obor = require('../../resources/monsters/obor');
-const wintertodt = require('../../resources/monsters/wintertodt');
-const chaosElemental = require('../../resources/monsters/chaosElemental');
-const wyvern = require('../../resources/monsters/wyvern');
-const vetion = require('../../resources/monsters/vetion');
-const lizardmanShaman = require('../../resources/monsters/lizardmanShaman');
-const armadyl = require('../../resources/monsters/armadyl');
-const zamorak = require('../../resources/monsters/zamorak');
-const kalphiteQueen = require('../../resources/monsters/kalphiteQueen');
 
 module.exports = class extends Command {
 
@@ -45,186 +14,340 @@ module.exports = class extends Command {
 
 	async run(msg, [quantity, ...BossName]) {
 		switch (BossName.join('').toUpperCase()) {
-			case 'CORP': {
+			case 'CORP':
+			case 'CORPOREALBEAST':
+			case 'CORPBEAST': {
 				if (quantity > 5000) return msg.send('I can only kill a maximum of 5000 Corp beasts at a time!');
+				const corp = require('../../resources/monsters/corp');
 				const loot = corp.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'RAIDS':
-			case 'OLM': {
+			case 'OLM':
+			case 'RAIDS1':
+			case 'COX':
+			case 'CHAMBERSOFXERIC': {
 				if (quantity > 100000) return msg.send('I can only do a maximum of 100,000 Raids at a time!');
-				const loot = raidsOne.kill(quantity);
+				const raids = require('../../resources/monsters/raids');
+				const loot = raids.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'RAIDS2':
 			case 'THEATREOFBLOOD':
 			case 'TOB': {
 				if (quantity > 10000) return msg.send("I can only do a maximum of 10,000 Theatres of Blood at a time! I'm not Woox!");
-				const loot = tob.kill(quantity);
+				const raids = require('../../resources/monsters/tob');
+				const loot = raids.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'BARROWS': {
 				if (quantity > 300) return msg.send('I can only do a maximum of 300 Barrows Chests at a time!');
+				const barrows = require('../../resources/monsters/barrows');
 				const loot = barrows.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'BANDOS': {
+			case 'BANDOS':
+			case 'GENERALGRAARDOR':
+			case 'GRAARDOR': {
 				if (quantity > 100000) return msg.send('I can only do a maximum of 100,000 Bandos kills at a time!');
+				const bandos = require('../../resources/monsters/bandos');
 				const loot = bandos.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'SARA': {
+			case 'SARA':
+			case 'SARADOMIN':
+			case 'ZILY':
+			case 'ZILYANA':
+			case 'COMMANDERZILYANA': {
 				if (quantity > 500) return msg.send('I can only do a maximum of 500 Sara kills at a time!');
+				const saradomin = require('../../resources/monsters/saradomin');
 				const loot = saradomin.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'ZULRAH': {
 				if (quantity > 500) return msg.send('I can only do a maximum of 500 Zulrah kills at a time!');
+				const zulrah = require('../../resources/monsters/zulrah');
 				const loot = zulrah.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'VORKATH': {
 				if (quantity > 500) return msg.send('I can only do a maximum of 500 Vorkath kills at a time!');
+				const vorkath = require('../../resources/monsters/vorkath');
 				const loot = vorkath.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'CERB':
 			case 'CERBERUS': {
 				if (quantity > 500) return msg.send('I can only do a maximum of 500 Cerberus kills at a time!');
+				const cerberus = require('../../resources/monsters/cerberus');
 				const loot = cerberus.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'ABYSSALDEMON':
+			case 'ABYSSALDEMONS':
 			case 'ABBYDEMON':
-			case 'ABBY': {
+			case 'ABBYDEMONS': {
 				if (quantity > 5000) return msg.send('I can only do a maximum of 5000 Abyssal Demon kills at a time!');
+				const abbyDemon = require('../../resources/monsters/abbyDemon');
 				const loot = abbyDemon.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'DEMONICGORILLA':
+			case 'GORILLAS':
+			case 'DGS':
 			case 'DEMONICGORILLAS':
-			case 'DG':
-			case 'GORILLAS': {
+			case 'DEMONICGORILLA':
+			case 'DEMONICS': {
 				if (quantity > 500) return msg.send("I can only kill 500 Demonic Gorilla's at a time!");
+				const demonicGorilla = require('../../resources/monsters/demonicGorilla');
 				const loot = demonicGorilla.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'KQ':
 			case 'KALPHITEQUEEN': {
 				if (quantity > 500) return msg.send("I can only kill 500 Kalphite Queen's at a time!");
+				const kalphiteQueen = require('../../resources/monsters/kalphiteQueen');
 				const loot = kalphiteQueen.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
+			case 'ZAMMY':
 			case 'ZAMORAK':
 			case 'KRIL':
-			case 'ZAMMY': {
+			case 'KRILTSUTSAROTH': {
 				if (quantity > 500) return msg.send("I can only kill 500 K'ril Tsutsaroth's at a time!");
+				const zamorak = require('../../resources/monsters/zamorak');
 				const loot = zamorak.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
+			case 'ARMA':
+			case 'ARMADYL':
 			case 'KREE':
-			case 'ARMA': {
+			case 'KREEARRA': {
 				if (quantity > 500) return msg.send("I can only kill 500 Kree'arra's at a time!");
+				const armadyl = require('../../resources/monsters/armadyl');
 				const loot = armadyl.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'LIZARDMANSHAMAN':
-			case 'LIZARDMAN':
 			case 'SHAMANS':
-			case 'SHAMAN': {
+			case 'LIZARDMANSHAMANS':
+			case 'LIZARDMANSHAMAN':
+			case 'LIZARDMEN': {
 				if (quantity > 1000) return msg.send("I can only kill 1000 Lizardman Shaman's at a time!");
+				const lizardmanShaman = require('../../resources/monsters/lizardmanShaman');
 				const loot = lizardmanShaman.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'CALLISTO': {
 				if (quantity > 500) return msg.send("I can only kill 500 Callisto's at a time!");
+				const callisto = require('../../resources/monsters/callisto');
 				const loot = callisto.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'VETION': {
 				if (quantity > 500) return msg.send("I can only kill 500 Vet'ions at a time!");
+				const vetion = require('../../resources/monsters/vetion');
 				const loot = vetion.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'VENENATIS': {
-				if (quantity > 500) return msg.send('I can only kill 100 Venenatis at a time!');
+				if (quantity > 500) return msg.send('I can only kill 500 Venenatis at a time!');
+				const venenatis = require('../../resources/monsters/venenatis');
 				const loot = venenatis.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'WYVERN':
+			case 'SKELETALWYVERN':
+			case 'SKELETALWYVERNS':
 			case 'WYVERNS': {
-				if (quantity > 1000) return msg.send('I can only kill 500 Skeletal Wyverns at a time!');
+				if (quantity > 1000) return msg.send('I can only kill 1000 Skeletal Wyverns at a time!');
+				const wyvern = require('../../resources/monsters/wyvern');
 				const loot = wyvern.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
+			case 'ANCIENTWYVERN':
+			case 'ANCIENTWYVERNS':
+			case 'FOSSILISLANDWYVERNS': {
+				if (quantity > 1000) return msg.send('I can only kill 1000 Ancient Wyverns at a time!');
+				const ancientWyvern = require('../../resources/monsters/ancientWyvern');
+				const loot = ancientWyvern.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
 			case 'GROTESQUEGUARDIANS':
-			case 'GG':
+			case 'GGS':
 			case 'DUSK':
 			case 'DAWN': {
 				if (quantity > 500) return msg.send('I can only kill 500 Grotesque Guardians at a time!');
+				const grotesqueGuardians = require('../../resources/monsters/grotesqueGuardians');
 				const loot = grotesqueGuardians.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'KBD':
 			case 'KINGBLACKDRAGON': {
-				if (quantity > 500) return msg.send('I can only kill 100 King Black Dragons at a time!');
+				if (quantity > 500) return msg.send('I can only kill 500 King Black Dragons at a time!');
+				const kingBlackDragon = require('../../resources/monsters/kingBlackDragon');
 				const loot = kingBlackDragon.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'SIRE':
-			case 'ABYSSALSIRE': {
+			case 'ABYSSALSIRE':
+			case 'ABBYSIRE':
+			case 'SIRE': {
 				if (quantity > 500) return msg.send('I can only kill 500 Abyssal Sires at a time!');
+				const abyssalSire = require('../../resources/monsters/abyssalSire');
 				const loot = abyssalSire.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'THERMY': {
+			case 'THERMY':
+			case 'THERMONUCLEARSMOKEDEVIL': {
 				if (quantity > 500) return msg.send("I can only kill 500 Thermonuclear smoke devil's at a time!");
+				const thermy = require('../../resources/monsters/thermy');
 				const loot = thermy.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'GIANTMOLE':
 			case 'MOLE': {
 				if (quantity > 5000) return msg.send("I can only kill 5000 Giant Mole's at a time!");
+				const giantMole = require('../../resources/monsters/giantMole');
 				const loot = giantMole.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'SKOTIZO': {
-				if (quantity > 500) return msg.send('I can only kill 100 Skotizo at a time!');
+				if (quantity > 500) return msg.send('I can only kill 500 Skotizo at a time!');
+				const skotizo = require('../../resources/monsters/skotizo');
 				const loot = skotizo.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'SCORPIA': {
-				if (quantity > 500) return msg.send('I can only kill 100 Scorpia at a time!');
+				if (quantity > 500) return msg.send('I can only kill 500 Scorpia at a time!');
+				const scorpia = require('../../resources/monsters/scorpia');
 				const loot = scorpia.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'CHAOSELE':
 			case 'CHAOSELEMENTAL': {
 				if (quantity > 500) return msg.send('I can only kill 500 Chaos Elementals at a time!');
+				const chaosElemental = require('../../resources/monsters/chaosElemental');
 				const loot = chaosElemental.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'WINTERTODT':
-			case 'TODT': {
+			case 'WINTERTOAD':
+			case 'TODT':
+			case 'WT': {
 				if (quantity > 500) return msg.send("I can only kill 500 Wintertodt's at a time!");
+				const wintertodt = require('../../resources/monsters/wintertodt');
 				const loot = wintertodt.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
 			case 'KRAKEN': {
 				if (quantity > 500) return msg.send("I can only kill 500 Kraken's at a time!");
+				const kraken = require('../../resources/monsters/kraken');
 				const loot = kraken.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'OBOR': {
+			case 'OBOR':
+			case 'HILLGIANTBOSS': {
 				if (quantity > 500) return msg.send("I can only kill 500 Obor's at a time!");
+				const obor = require('../../resources/monsters/obor');
 				const loot = obor.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
-			case 'REX': {
+			case 'REX':
+			case 'DAGANNOTHREX': {
 				if (quantity > 500) return msg.send("I can only kill 500 Dagannoth Rex's at a time!");
+				const dagannothRex = require('../../resources/monsters/dagannothRex');
 				const loot = dagannothRex.kill(quantity);
 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
 			}
+			case 'SUPREME':
+			case 'DAGANNOTHSUPREME': {
+				if (quantity > 500) return msg.send("I can only kill 500 Dagannoth Supremes at a time!");
+				const dagannothSupreme = require('../../resources/monsters/dagannothSupreme');
+				const loot = dagannothSupreme.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+			case 'PRIME':
+			case 'DAGANNOTHPRIME': {
+				if (quantity > 500) return msg.send("I can only kill 500 Dagannoth Primes at a time!");
+				const dagannothPrime = require('../../resources/monsters/dagannothPrime');
+				const loot = dagannothPrime.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'DAGKINGS':
+			case 'DAGANNOTHKINGS':
+			case 'DKS': {
+				let quantityRex = 0;
+				let quantitySupreme = 0;
+				let quantityPrime = 0;
+				if (quantity > 1000) return msg.send("I can only kill 1000 Dagannoth Kings at a time!");
+				const dagannothRex = require('../../resources/monsters/dagannothRex');
+				const dagannothSupreme = require('../../resources/monsters/dagannothSupreme');
+				const dagannothPrime = require('../../resources/monsters/dagannothPrime');
+				if(quantity % 3 === 2){
+					quantitySupreme = Math.floor(quantity/3) + 1;
+					quantityRex = quantitySupreme;
+					quantityPrime = quantityRex - 1;
+				}else if(quantity % 3 === 1){
+					quantitySupreme = Math.floor(quantity/3) + 1;
+					quantityRex = quantitySupreme - 1;
+					quantityPrime = quantityRex;
+				}else{
+					quantitySupreme = quantity/3;
+					quantityRex = quantity/3;
+					quantityPrime = quantity/3;
+				}
+				const loot = dagannothRex.kill(quantityRex) + " " + dagannothSupreme.kill(quantitySupreme) + " " + dagannothPrime.kill(quantityPrime);
+ 				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+			case 'CHAOSFANATIC': {
+				if (quantity > 500) return msg.send("I can only kill 500 Chaos Fanatics at a time!");
+				const chaosFanatic = require('../../resources/monsters/chaosFanatic');
+				const loot = chaosFanatic.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'CRAZYARCH':
+			case 'CRAZYARCHAEOLOGIST': {
+				if (quantity > 500) return msg.send("I can only kill 500 Crazy Archaeologists at a time!");
+				const crazyArchaeologist = require('../../resources/monsters/crazyArchaeologist');
+				const loot = crazyArchaeologist.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'MITHRILDRAGONS':
+			case 'MITHRILDRAGS':
+			case 'MITHDRAGS':
+			case 'MITHDRAGONS':  {
+				if (quantity > 1000) return msg.send("I can only kill 1000 Mithril Dragons at a time!");
+				const mithrilDragon = require('../../resources/monsters/mithrilDragon');
+				const loot = mithrilDragon.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'ADAMANTDRAGONS':
+			case 'ADAMANTDRAGS':
+			case 'ADDYDRAGS':
+			case 'ADDYDRAGONS': {
+				if (quantity > 1000) return msg.send("I can only kill 1000 Adamant Dragons at a time!");
+				const adamantDragon = require('../../resources/monsters/adamantDragon');
+				const loot = adamantDragon.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'RUNEDRAGONS':
+			case 'RUNEDRAGS': {
+				if (quantity > 1000) return msg.send("I can only kill 1000 Rune Dragons at a time!");
+				const runeDragon = require('../../resources/monsters/runeDragon');
+				const loot = runeDragon.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
+			case 'BRYOPHYTA':
+			case 'MOSSGIANTBOSS': {
+				if (quantity > 500) return msg.send("I can only kill 500 Bryophyta's at a time!");
+				const bryophyta = require('../../resources/monsters/bryophyta');
+				const loot = bryophyta.kill(quantity);
+				return msg.send(loot.length > 0 ? loot : 'You got nothing.');
+			}
+
 			default:
 				return msg.send("I don't have that boss yet.");
 		}
---
+++ resources/monsters/mithrilDragon.js
@@ -0,0 +1,23 @@
+const mithrilDragon = {
+	drops: {
+		draconicVisage: '<:Draconic_visage:403378090979491840>',
+		dragonFullHelm: '<:Dragon_full_helm:456177009639424020>',
+		chewedBones: 'chewedbones',
+		clueScroll: '<:Clue_scroll:365003979840552960>'
+	},
+	kill(quantity) {
+		const loot = [];
+		for (let i = 0; i < quantity; i++) {
+			if (this.roll(32768)) loot.push(this.drops.dragonFullHelm);
+			if (this.roll(42)) loot.push(this.drops.chewedBones);
+			if (this.roll(10000)) loot.push(this.drops.draconicVisage);
+			if (this.roll(350)) loot.push(this.drops.clueScroll);
+		}
+		return loot.join(' ');
+	},
+	roll(max) {
+		return Math.floor((Math.random() * max) + 1) === 1;
+	}
+};
+
+module.exports = mithrilDragon;
