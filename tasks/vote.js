3693233c13ff474fd10057f6b18646c5ba3eb115
gc <30398469+gach@users.noreply.github.com>
2018-10-31 14:46:29 +1100

    no decimals in vote rewards

--- tasks/vote.js
+++ tasks/vote.js
@@ -4,8 +4,10 @@ module.exports = class extends Task {
 
 	async run({ user, isWeekend }) {
 		const _user = await this.client.users.fetch(user);
-		const member = await this.client.guilds.get('342983479501389826')
-			.members.fetch(user).catch(() => null);
+		const member = await this.client.guilds
+			.get('342983479501389826')
+			.members.fetch(user)
+			.catch(() => null);
 
 		let amount = Math.floor(Math.random() * 5000000) + 500000;
 		let bonuses = '';
@@ -16,12 +18,13 @@ module.exports = class extends Task {
 		}
 
 		if (member) {
-			amount *= 1.5;
+			amount = Math.floor(amount * 1.5);
 			bonuses += ' <:OSRSBot:363583286192111616>';
 		}
 
-		this.client.voteLogs
-			.send(`${bonuses} ${_user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`);
+		this.client.voteLogs.send(
+			`${bonuses} ${_user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`
+		);
 		_user.configs.update('GP', _user.configs.GP + amount);
 	}
 
