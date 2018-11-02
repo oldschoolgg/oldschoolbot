245b174a7924781d90278793f4c7661fb2927ebb
devin8 <40153548+devin8@users.noreply.github.com>
2018-11-02 10:52:11 -0400

    Add files via upload

--- resources/monsters/saradomin.js
+++ resources/monsters/saradomin.js
@@ -9,7 +9,31 @@ const saradomin = {
 		saradominSword: '<:Saradomin_sword:403052160822280192>',
 		saradominsLight: '<:Saradomins_light:403052160977338378>'
 	},
+	nameMap: {
+		saradominSword: 'Saradomin Sword',
+		saradominsLight: 'Saradomin Light',
+		armadylCrossbow: 'Armadyl Crossbow',
+		godswordShard: 'Godsword Shard',
+		saradominHilt: 'Saradomin Hilt',
+		pet: 'Pet'
+	},
+	// prices as of 11/1/2018
+	priceMap: {
+		saradominSword: 1200000,
+		saradominsLight: 78000,
+		armadylCrossbow: 31000000,
+		godswordShard: 150000,
+		saradominHilt: 42600000,
+		pet: 0
+	},
 	kill(quantity) {
+		if (quantity <= 500) {
+			return this.smallKill(quantity);
+		} else {
+			return this.bigKill(quantity);
+		}
+	},
+	smallKill(quantity) {
 		const loot = [];
 		for (let i = 0; i < quantity; i++) {
 			if (this.roll(5000)) loot.push(this.drops.pet);
@@ -20,6 +44,36 @@ const saradomin = {
 			if (this.roll(86)) loot.push(this.drops.shards[Math.floor(Math.random() * this.drops.shards.length)]);
 		}
 		return loot.join(' ');
+	},
+	bigKill(quantity) {
+		const loot = {
+			saradominSword: 0,
+			saradominsLight: 0,
+			armadylCrossbow: 0,
+			godswordShard: 0,
+			saradominHilt: 0,
+			pet: 0
+		};
+		const displayLoot = [];
+		let totalValue = 0;
+
+		for (let i = 0; i < quantity; i++) {
+			if (this.roll(5000)) loot.pet++;
+			if (this.roll(508)) loot.armadylCrossbow++;
+			if (this.roll(508)) loot.saradominHilt++;
+			if (this.roll(254)) loot.saradominsLight++;
+			if (this.roll(127)) loot.saradominSword++;
+			if (this.roll(86)) loot.godswordShard++;
+		}
+
+		for (const key in loot) {
+			displayLoot.push(`**${this.nameMap[key]}**: ${loot[key].toLocaleString()} `);
+			totalValue += this.priceMap[key] * loot[key];
+		}
+
+		displayLoot.push(`\n**Total Value:** ${totalValue.toLocaleString()} GP`);
+		displayLoot.push(`**GP per Kill:** ${Math.round(totalValue / quantity).toLocaleString()} GP`);
+		return displayLoot.join('\n');
 	},
 	roll(max) {
 		return Math.floor((Math.random() * max) + 1) === 1;
--- resources/monsters/zamorak.js
+++ resources/monsters/zamorak.js
@@ -5,23 +5,75 @@ const zamorak = {
 			'<:Godsword_shard_1:403049018764165121>'],
 		pet: '<:Pet_kril_tsutsaroth:324127377527406594>',
 		staffOfTheDead: '<:Staff_of_the_dead:405251862695116801>',
-		runeSword: '<:Rune_sword:405251862674014209>',
 		zamorakianSpear: '<:Zamorakian_spear:405251862883729418>',
 		steamBattlestaff: '<:Steam_battlestaff:405251862451716097>',
 		zamorakHilt: '<:Zamorak_hilt:405251862489595905>'
 	},
+	nameMap: {
+		staffOfTheDead: 'Staff of the Dead',
+		zamorakianSpear: 'Zamorakian Spear',
+		steamBattlestaff: 'Steam Battlestaff',
+		godswordShard: 'Godsword Shard',
+		zamorakHilt: 'Zamorak Hilt',
+		pet: 'Pet'
+	},
+	// prices as of 11/1/2018
+	priceMap: {
+		staffOfTheDead: 6300000,
+		zamorakianSpear: 11700000,
+		steamBattlestaff: 600000,
+		godswordShard: 150000,
+		zamorakHilt: 4200000,
+		pet: 0
+	},
 	kill(quantity) {
+		if (quantity <= 500) {
+			return this.smallKill(quantity);
+		} else {
+			return this.bigKill(quantity);
+		}
+	},
+	smallKill(quantity) {
 		const loot = [];
 		for (let i = 0; i < quantity; i++) {
 			if (this.roll(5000)) loot.push(this.drops.pet);
 			if (this.roll(508)) loot.push(this.drops.staffOfTheDead);
 			if (this.roll(508)) loot.push(this.drops.zamorakHilt);
-			if (this.roll(406)) loot.push(this.drops.runeSword);
 			if (this.roll(128)) loot.push(this.drops.zamorakianSpear);
 			if (this.roll(128)) loot.push(this.drops.steamBattlestaff);
 			if (this.roll(86)) loot.push(this.drops.shards[Math.floor(Math.random() * this.drops.shards.length)]);
 		}
 		return loot.join(' ');
+	},
+	bigKill(quantity) {
+		const loot = {
+			staffOfTheDead: 0,
+			zamorakianSpear: 0,
+			steamBattlestaff: 0,
+			godswordShard: 0,
+			zamorakHilt: 0,
+			pet: 0
+		};
+		const displayLoot = [];
+		let totalValue = 0;
+
+		for (let i = 0; i < quantity; i++) {
+			if (this.roll(5000)) loot.pet++;
+			if (this.roll(508)) loot.staffOfTheDead++;
+			if (this.roll(508)) loot.zamorakHilt++;
+			if (this.roll(128)) loot.zamorakianSpear++;
+			if (this.roll(128)) loot.steamBattlestaff++;
+			if (this.roll(86)) loot.godswordShard++;
+		}
+
+		for (const key in loot) {
+			displayLoot.push(`**${this.nameMap[key]}**: ${loot[key].toLocaleString()} `);
+			totalValue += this.priceMap[key] * loot[key];
+		}
+
+		displayLoot.push(`\n**Total Value:** ${totalValue.toLocaleString()} GP`);
+		displayLoot.push(`**GP per Kill:** ${Math.round(totalValue / quantity).toLocaleString()} GP`);
+		return displayLoot.join('\n');
 	},
 	roll(max) {
 		return Math.floor((Math.random() * max) + 1) === 1;
