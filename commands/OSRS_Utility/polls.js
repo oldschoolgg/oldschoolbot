189403c641f487df778ea4042a10a4dc2b23e330
gc <30398469+gach@users.noreply.github.com>
2018-11-05 13:09:07 +1100

    add forums, polls cmd

---
+++ commands/OSRS_Utility/polls.js
@@ -0,0 +1,15 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Shows the link for the OSRS Polls.'
+		});
+	}
+
+	async run(msg) {
+		return msg.send('http://services.runescape.com/m=poll/oldschool/index.ws');
+	}
+
+};
