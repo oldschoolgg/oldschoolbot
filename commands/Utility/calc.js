d80305a129a72ac0c5120a34ba0ec14940993541
devin8 <40153548+devin8@users.noreply.github.com>
2018-11-09 14:33:13 -0500

    Added a calculator command using math.js
    mathjs needs installing into ../node_modules from http://mathjs.org/download.html

---
+++ commands/Utility/calc.js
@@ -0,0 +1,31 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const math = require('mathjs');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['c'],
+			description: 'Performs mathematical calculations.',
+			usage: '<expression:str>'
+		});
+	}
+	async run(msg, [expression]) {
+		let ans;
+		try {
+			ans = math.eval(expression);
+		} catch (error) {
+			return msg.send('Please input a valid calculation.');
+		}
+
+		const embed = new MessageEmbed()
+			.setColor(0xffffff)
+			.setTitle('Calculation')
+			.addField('Input', `\n${expression}`)
+			.addField('Output', `\n${ans}`);
+
+		return msg.send({ embed });
+	}
+
+};
