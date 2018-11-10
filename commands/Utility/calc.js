910966e2d1f1ea77c3f8c6e9fb980ae12fd53e4e
devin8 <40153548+devin8@users.noreply.github.com>
2018-11-10 17:58:42 -0500

    Fixed all issues with calc

--- commands/Utility/calc.js
+++ commands/Utility/calc.js
@@ -12,20 +12,66 @@ module.exports = class extends Command {
 		});
 	}
 	async run(msg, [expression]) {
+		const expressionFixed = this.numberFormatter(expression);
 		let ans;
 		try {
-			ans = math.eval(expression);
+			ans = math.eval(expressionFixed);
 		} catch (error) {
 			return msg.send('Please input a valid calculation.');
 		}
+
+		const ansFixed = this.abbreviateNumber(ans);
 
 		const embed = new MessageEmbed()
 			.setColor(0xffffff)
 			.setTitle('Calculation')
 			.addField('Input', `\n${expression}`)
-			.addField('Output', `\n${ans}`);
+			.addField('Output', `\n${ansFixed}`);
 
 		return msg.send({ embed });
+	}
+
+	numberFormatter(exp) {
+		if (exp.includes('k')) {
+			exp = exp.replace('k', '* 1000');
+		}
+		if (exp.includes('K')) {
+			exp = exp.replace('K', '* 1000');
+		}
+		if (exp.includes('m')) {
+			exp = exp.replace('m', '* 1000000');
+		}
+		if (exp.includes('M')) {
+			exp = exp.replace('M', '* 1000000');
+		}
+		if (exp.includes('b')) {
+			exp = exp.replace('b', '* 1000000000');
+		}
+		if (exp.includes('B')) {
+			exp = exp.replace('B', '* 1000000000');
+		}
+		return exp;
+	}
+
+	//	credit to https://stackoverflow.com/questions/2685911/is-there-a-way-to-round-numbers-into-a-reader-friendly-format-e-g-1-1k
+	abbreviateNumber(number) {
+		let decPlaces = 3;
+		decPlaces = Math.pow(10, decPlaces);
+		const abbrev = ['K', 'M', 'B'];
+		for (let i = abbrev.length - 1; i >= 0; i--) {
+			const size = Math.pow(10, (i + 1) * 3);
+			if (size <= number) {
+				number = Math.round(number * decPlaces / size) / decPlaces;
+				if ((number === 1000) && (i < abbrev.length - 1)) {
+					number = 1;
+					i++;
+				}
+				number += abbrev[i];
+				break;
+			}
+		}
+
+		return number;
 	}
 
 };
