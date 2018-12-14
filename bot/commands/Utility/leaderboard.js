ab8d8cc495343ee7d149b7a76eceae5e34f2788f
gc <30398469+gach@users.noreply.github.com>
2018-12-14 21:55:11 +1100

    lots

--- bot/commands/Utility/leaderboard.js
+++ bot/commands/Utility/leaderboard.js
@@ -11,9 +11,9 @@ module.exports = class extends Command {
 	async run(msg) {
 		const users = this.client.users
 			.filter(user => user.settings.get('GP') > 0)
-			.sort((a, b) => b.settings.get('GP') - a.configs.get('GP')).first(10);
+			.sort((a, b) => b.settings.get('GP') - a.settings.get('GP')).first(10);
 
-		const leaderboard = users.map(({ username, configs }) => `**${username}** has ${configs.GP.toLocaleString()} GP `);
+		const leaderboard = users.map(({ username, settings }) => `**${username}** has ${settings.get('GP').toLocaleString()} GP `);
 
 		return msg.send(leaderboard.join('\n') || 'Nobody has any GP!');
 	}
