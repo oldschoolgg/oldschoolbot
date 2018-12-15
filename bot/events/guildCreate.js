2b875352405adb86e795b463f1c3f0589d88b990
gc <30398469+gach@users.noreply.github.com>
2018-12-15 13:21:01 +1100

    fix webhooks

--- bot/events/guildCreate.js
+++ bot/events/guildCreate.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run({ name, memberCount }) {
-		this.client.guildLogs
+		this.client.channels.get(this.client.guildLogs)
 			.send(`\`${name}\` with \`${memberCount.toLocaleString()}\` Members added.`);
 	}
 
