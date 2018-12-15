b7231d6425d5a229b8785c429973b02c6c2b0cb7
gc <30398469+gach@users.noreply.github.com>
2018-12-15 13:44:16 +1100

    config > settings

--- bot/tasks/vote.js
+++ bot/tasks/vote.js
@@ -25,7 +25,7 @@ module.exports = class extends Task {
 		this.client.channels.get(this.client.voteLogs).send(
 			`${bonuses} ${_user} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`
 		);
-		_user.settings.update('GP', _user.configs.GP + amount);
+		_user.settings.update('GP', _user.settings.get('GP') + amount);
 	}
 
 };
