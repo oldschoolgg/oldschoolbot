d1f29d08e340b0bba589e66405b410d7e27aba74
gc <30398469+gach@users.noreply.github.com>
2019-01-17 23:05:35 +1100

    remove rand command

--- bot/commands/Fun/rand.js
+++ bot/commands/Fun/rand.js
@@ -1,34 +1,17 @@
 const { Command } = require('klasa');
-const fetch = require('node-fetch');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			cooldown: 3,
-			description: 'Returns a random reddit post on a given subreddit.',
-			usage: '<subreddit:str>'
+			description: '[REMOVED]'
 		});
 	}
 
-	error() {
-		throw `There was an error. Reddit may be down, or the subreddit doesnt exist.`;
-	}
-
-	async run(msg, [subreddit]) {
-		const { kind, data } = await fetch(`https://www.reddit.com/r/${subreddit}/.json?limit=30`)
-			.then(res => res.json())
-			.catch(this.error);
-
-		if (!kind || !data || data.children.length === 0) this.error();
-
-		const post = data.children[Math.floor(Math.random() * data.children.length)].data;
-
-		if (post.over_18 && !msg.channel.nsfw) {
-			throw 'I cant post a NSFW image in this channel unless you mark it as NSFW!';
-		}
-
-		return msg.send(post.url);
+	async run(msg) {
+		return msg.send('This command has sadly been removed from Old School Bot permanently, as it has the potential ' +
+			"to post NSFW content which isn't something we want in a family-friendly bot. If you'd still like to use this " +
+		'command, you can add this bot which has the exact same command: <https://invite.ordobot.me>.');
 	}
 
 };
