3ac7ec40a0fac980fd07b2cea01808a84a62cfa6
gc <30398469+gach@users.noreply.github.com>
2019-01-30 00:20:01 +1100

    reduce to 1 in 5

--- bot/tasks/vote.js
+++ bot/tasks/vote.js
@@ -26,7 +26,7 @@ module.exports = class extends Task {
 		let chStr = `${bonuses} ${_user.username} just voted for Old School Bot and received ${amount.toLocaleString()} GP! Thank you <:Smiley:420283725469974529>`;
 		let dmStr = `${bonuses} Thank you for voting for Old School Bot! You received ${amount.toLocaleString()} GP.`;
 
-		if (roll(6)) {
+		if (roll(5)) {
 			const pet = pet[Math.floor(Math.random() * pet.length)];
 			const userPets = _user.settings.get('pets');
 			if (!userPets[pet.id]) userPets[pet.id] = 1;
