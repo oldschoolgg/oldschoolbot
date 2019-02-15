8e7be8c603a1b8e88a51abd5897f1bc434f84108
devin8 <40153548+devin8@users.noreply.github.com>
2019-02-15 09:48:39 -0500

    Added 5 new monsters to +kill/finish from kebos release (#41)
    * Added 5 new monsters to +kill/finish from kebos release
    
    Five new monsters to +kill (Alchemical Hydra*, hydras, drakes, wyrms, Hespori)
    
    Two new monsters to +finish (Alchemical Hydra, Hespori)
    
    *Has the addition of larger quantity of kills
    
    * add hydra emotes
    
    * add drake emotes
    
    * add emotes
    
    * add emotes
    
    * add emotes

---
+++ data/monsters/drake.js
@@ -0,0 +1,24 @@
+const drake = {
+	drops: {
+		drakeTooth: '<:Drakes_tooth:545977597079715840>',
+		drakeClaw: '<:Drakes_claw:545977596714811393>',
+		dragonWeaponry: ['<:Dragon_knife:545975448446828584>',
+			'<:Dragon_thrownaxe:403018313187328010>'],
+		clueScroll: '<:Clue_scroll:365003979840552960>'
+	},
+	kill(quantity) {
+		const loot = [];
+		for (let i = 0; i < quantity; i++) {
+			if (this.roll(512)) loot.push(this.drops.drakeTooth);
+			if (this.roll(512)) loot.push(this.drops.drakeClaw);
+			if (this.roll(2000)) loot.push(this.drops.dragonWeaponry[Math.floor(Math.random() * this.drops.dragonWeaponry.length)]);
+			if (this.roll(128)) loot.push(this.drops.clueScroll);
+		}
+		return loot.join(' ');
+	},
+	roll(max) {
+		return Math.floor((Math.random() * max) + 1) === 1;
+	}
+};
+
+module.exports = drake;
