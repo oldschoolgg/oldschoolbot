bba54ccb81e2bbd331137bc95e1be4347a0300d6
gc <30398469+gach@users.noreply.github.com>
2019-05-14 23:26:25 +1000

    fix tag finalizer bug

--- bot/finalizers/commandStats.js
+++ bot/finalizers/commandStats.js
@@ -7,6 +7,7 @@ module.exports = class extends Finalizer {
 	}
 
 	async run(msg) {
+		if (!msg.command) return;
 		const { name } = msg.command;
 		const commandStats = this.client.settings.get('commandStats');
 
