b223a1e84ca8298967354ba001c7250ea5fe28ba
gc <30398469+gach@users.noreply.github.com>
2019-05-18 07:03:53 +1000

    improvements

--- bot/tasks/updater.js
+++ bot/tasks/updater.js
@@ -5,16 +5,16 @@ module.exports = class extends Task {
 
 	async run() {
 		const rsnList = this.client.gateways.users.cache
-			.filter(config => config.autoupdate)
-			.map(config => config.RSN);
+			.filter(user => user.settings.autoupdate)
+			.map(user => user.settings.RSN);
 
 		// Only update 5 users per request
 		const userArray = chunk(rsnList, 5);
 
 		userArray.forEach(async list => {
-			const multiQuery = list.map(rsn => `{"type":"update","player": "${rsn}"}`).join(',');
+			const multiQuery = list.map(rsn => JSON.stringify({ type: 'update', player: rsn })).join(',');
 			await fetch(`https://crystalmathlabs.com/tracker/api.php?multiquery=[${multiQuery}]`);
-			// Wait 5 seconds between each request
+
 			await sleep(300);
 		});
 	}
