9d7cac6eb4b46dd903f362c4a2adf190a3604afc
gc <30398469+gach@users.noreply.github.com>
2019-05-18 07:17:15 +1000

    pets leaderboard

--- bot/commands/Utility/leaderboard.js
+++ bot/commands/Utility/leaderboard.js
@@ -1,21 +1,33 @@
 const { Command } = require('klasa');
+const { util: { escapeMarkdown } } = require('discord.js');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			description: 'Shows the people with the most virtual GP.'
+			description: 'Shows the people with the most virtual GP.',
+			usage: '[pets]'
 		});
 	}
 
-	async run(msg) {
-		const users = this.client.users
-			.filter(user => user.settings.get('GP') > 0)
-			.sort((a, b) => b.settings.get('GP') - a.settings.get('GP')).first(10);
+	async run(msg, [board = 'gp']) {
+		if (board === 'gp') {
+			const users = this.client.users
+				.filter(user => user.settings.get('GP') > 0)
+				.sort((a, b) => Object.keys(b.settings.get('pets')).length - a.settings.get('GP')).first(10);
 
-		const leaderboard = users.map(({ username, settings }) => `**${username}** has ${settings.get('GP').toLocaleString()} GP `);
+			const leaderboard = users.map(({ username, settings }) => `**${escapeMarkdown(username)}** has ${settings.get('GP').toLocaleString()} GP `);
 
-		return msg.send(leaderboard.join('\n') || 'Nobody has any GP!');
+			return msg.send(leaderboard.join('\n') || 'Nobody has any GP!');
+		} else {
+			const users = this.client.users
+				.filter(user => Object.keys(user.settings.get('pets')).length > 0)
+				.sort((a, b) => b.settings.get('GP') - a.settings.get('GP')).first(10);
+
+			const leaderboard = users.map(({ username, settings }) => `**${escapeMarkdown(username)}** has ${Object.keys(settings.get('pets')).length} GP `);
+
+			return msg.send(leaderboard.join('\n') || 'Nobody has any pets!');
+		}
 	}
 
 };
