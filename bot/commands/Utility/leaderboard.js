d722d9da3487bfb712ab7e710f9c5ff0f7b9f33f
gc <30398469+gach@users.noreply.github.com>
2019-05-18 07:26:18 +1000

    fix leaderboard

--- bot/commands/Utility/leaderboard.js
+++ bot/commands/Utility/leaderboard.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 		if (board === 'gp') {
 			const users = this.client.users
 				.filter(user => user.settings.get('GP') > 0)
-				.sort((a, b) => Object.keys(b.settings.get('pets')).length - a.settings.get('GP')).first(10);
+				.sort((a, b) => b.settings.get('GP') - a.settings.get('GP')).first(10);
 
 			const leaderboard = users.map(({ username, settings }) => `**${escapeMarkdown(username)}** has ${settings.get('GP').toLocaleString()} GP `);
 
@@ -22,9 +22,9 @@ module.exports = class extends Command {
 		} else {
 			const users = this.client.users
 				.filter(user => Object.keys(user.settings.get('pets')).length > 0)
-				.sort((a, b) => b.settings.get('GP') - a.settings.get('GP')).first(10);
+				.sort((a, b) => Object.keys(b.settings.get('pets')).length - Object.keys(a.settings.get('pets')).length).first(10);
 
-			const leaderboard = users.map(({ username, settings }) => `**${escapeMarkdown(username)}** has ${Object.keys(settings.get('pets')).length} GP `);
+			const leaderboard = users.map(({ username, settings }) => `**${escapeMarkdown(username)}** has ${Object.keys(settings.get('pets')).length} pets `);
 
 			return msg.send(leaderboard.join('\n') || 'Nobody has any pets!');
 		}
