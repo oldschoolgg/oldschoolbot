56e11699ee4c3b30a454dc878eddb3184a156116
gc <30398469+gach@users.noreply.github.com>
2019-05-22 08:47:15 +1000

    fix dm bug

--- bot/inhibitors/onlyStaffCanUseCommands.js
+++ bot/inhibitors/onlyStaffCanUseCommands.js
@@ -3,6 +3,7 @@ const { Inhibitor } = require('klasa');
 module.exports = class extends Inhibitor {
 
 	async run(msg) {
+		if (!msg.guild) return;
 		if (msg.channel.settings.get('onlyStaffCanUseCommands')) {
 			const hasPerm = await msg.hasAtLeastPermissionLevel(6);
 			if (!hasPerm) throw true;
