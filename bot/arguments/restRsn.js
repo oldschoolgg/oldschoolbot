14f9832efdeafcc3785ef09f00589d5855d9f21c
gc <30398469+gach@users.noreply.github.com>
2019-05-24 13:07:00 +1000

    add rsn args

---
+++ bot/arguments/restRsn.js
@@ -0,0 +1,21 @@
+const { Argument } = require('klasa');
+
+module.exports = class extends Argument {
+
+	constructor(...args) {
+		super(...args, { name: '...rsn', aliases: ['...rsn'] });
+	}
+
+	get rsnArg() {
+		return this.store.get('rsn');
+	}
+
+	run(arg, possible, message) {
+		if (!arg) return this.rsnArg.run(arg, possible, message);
+		const { args, usage: { usageDelim } } = message.prompter;
+		const index = args.indexOf(arg);
+		const rest = args.splice(index, args.length - index).join(usageDelim);
+		return this.rsnArg.run(rest, possible, message);
+	}
+
+};
