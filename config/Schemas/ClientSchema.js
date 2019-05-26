5904d418343e91d5a7aa78a1a01fc389d9522a31
gc <30398469+gach@users.noreply.github.com>
2019-05-27 00:00:42 +1000

    paginate polls and news

--- config/Schemas/ClientSchema.js
+++ config/Schemas/ClientSchema.js
@@ -3,4 +3,5 @@ const { Client } = require('klasa');
 Client.defaultClientSchema
 	.add('commandStats', 'any', { default: {} })
 	.add('totalCommandsUsed', 'integer', { default: 0 })
-	.add('prices', 'any', { default: {} });
+	.add('prices', 'any', { default: {} })
+	.add('pollQuestions', 'any', { default: {} });
