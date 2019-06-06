94d0bd390964fe1c4d918b6a7a380eb255e99694
unknown <gc@users.noreply.github.com>
2019-06-07 06:26:59 +1000

    linting

--- bot/arguments/restRsn.js
+++ bot/arguments/restRsn.js
@@ -12,7 +12,10 @@ module.exports = class extends Argument {
 
 	run(arg, possible, message) {
 		if (!arg) return this.rsnArg.run(arg, possible, message);
-		const { args, usage: { usageDelim } } = message.prompter;
+		const {
+			args,
+			usage: { usageDelim }
+		} = message.prompter;
 		const index = args.indexOf(arg);
 		const rest = args.splice(index, args.length - index).join(usageDelim);
 		return this.rsnArg.run(rest, possible, message);
