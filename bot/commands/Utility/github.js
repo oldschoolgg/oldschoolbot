83ffeafa927408044a9259a3d018ae7fbbb3010a
gc <gc@users.noreply.github.com>
2019-07-19 23:35:56 +1000

    change bot emote

--- bot/commands/Utility/github.js
+++ bot/commands/Utility/github.js
@@ -1,13 +1,13 @@
 const { Command } = require('klasa');
 
 module.exports = class extends Command {
-
 	constructor(...args) {
 		super(...args, { description: 'Shows the Github link.' });
 	}
 
 	async run(msg) {
-		return msg.send(`<:OSRSBot:363583286192111616> Old School Bot is now open-source! <https://github.oldschool.gg/>`);
+		return msg.send(
+			`<:OSBot:601768469905801226> Old School Bot is now open-source! <https://github.oldschool.gg/>`
+		);
 	}
-
 };
