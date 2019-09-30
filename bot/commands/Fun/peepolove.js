8cf0c52a2d230245c0ba6bbe3dd864ab1317adf8
gc <gc@users.noreply.github.com>
2019-10-01 03:55:50 +1000

    enable people to peepoLove any images

--- bot/commands/Fun/peepolove.js
+++ bot/commands/Fun/peepolove.js
@@ -2,7 +2,6 @@ const { Command } = require('klasa');
 const { MessageAttachment } = require('discord.js');
 const { Canvas } = require('canvas-constructor');
 const fs = require('fs');
-const fetch = require('node-fetch');
 const peepoBody = fs.readFileSync('./resources/images/peepoBody.png');
 const peepoHands = fs.readFileSync('./resources/images/peepoHands.png');
 
@@ -11,23 +10,16 @@ module.exports = class extends Command {
 		super(...args, {
 			description: 'Generates a peepoLove image from a given image/avatar.',
 			cooldown: 7,
-			usage: '<person:user>',
+			usage: '<image:image>',
 			requiredPermissions: ['ATTACH_FILES']
 		});
 	}
 
-	async parseURL(imageURL) {
-		const imageBuffer = await fetch(imageURL).then(result => result.buffer());
-		return imageBuffer;
-	}
-
-	async run(msg, [user]) {
-		const userImage = await this.parseURL(user.displayAvatarURL({ size: 512, format: 'png' }));
-
+	async run(msg, [[imageBuffer]]) {
 		const imageBuff = new Canvas(512, 512)
 			.addImage(peepoBody, 0, 0)
 			.rotate(-0.4)
-			.addImage(userImage, -210, 512 - 241, 330, 330, {
+			.addImage(imageBuffer, -210, 512 - 241, 330, 330, {
 				type: 'round',
 				radius: 330 / 2,
 				restore: true
