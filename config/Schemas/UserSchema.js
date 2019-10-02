405b807f2b76e9d899fe56122c4f975f0f99fe4c
gc <gc@users.noreply.github.com>
2019-10-03 06:22:52 +1000

    add badges feature

--- config/Schemas/UserSchema.js
+++ config/Schemas/UserSchema.js
@@ -5,4 +5,5 @@ Client.defaultUserSchema
 	.add('GP', 'integer', { default: 0 })
 	.add('autoupdate', 'boolean', { default: false })
 	.add('RSN', 'string', { min: 1, max: 12, default: null })
-	.add('pets', 'any', { default: {} });
+	.add('pets', 'any', { default: {} })
+	.add('badges', 'integer', { array: true, default: [] });
---
+++ config/badges.js
@@ -0,0 +1,3 @@
+module.exports = {
+	0: '<:settings_rs:586330616539381760>'
+};
