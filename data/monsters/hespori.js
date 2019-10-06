695b6be35975722a37b3bd2791b410fcd9e4c1af
gc <gc@users.noreply.github.com>
2019-10-07 01:17:14 +1100

    remove seeds from hespori drop

--- data/monsters/hespori.js
+++ data/monsters/hespori.js
@@ -1,15 +1,13 @@
 const hespori = {
 	drops: {
 		pet: '<:Tangleroot:324127378978635778>',
-		bucket: '<:Bottomless_compost_bucket:545978484078411777>',
-		seeds: 'White lily seeds'
+		bucket: '<:Bottomless_compost_bucket:545978484078411777>'
 	},
 	kill(quantity) {
 		const loot = [];
 		for (let i = 0; i < quantity; i++) {
 			if (this.roll(7000)) loot.push(this.drops.pet);
 			if (this.roll(35)) loot.push(this.drops.bucket);
-			if (this.roll(20)) loot.push(this.drops.seeds);
 		}
 		return loot.join(' ');
 	},
@@ -24,10 +22,6 @@ const hespori = {
 				lootMSG.push(
 					`**Bottomless Compost Bucket:** ${kc.toLocaleString()} KC ${this.drops.bucket}`
 				);
-			}
-			if (!loot.includes('WLS') && this.roll(20)) {
-				loot.push('WLS');
-				lootMSG.push(`**White Lily Seeds:** ${kc.toLocaleString()} KC ${this.drops.seeds}`);
 			}
 		}
 		return [kc, lootMSG.join('\n')];
