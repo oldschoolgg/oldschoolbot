dfb0226f0da0a3e22ebae9cb49c38e504d329fac
gc <gc@users.noreply.github.com>
2019-12-21 14:59:18 +1100

    remove them... :O

--- bot/commands/OSRS_Fun/kill.js
+++ bot/commands/OSRS_Fun/kill.js
@@ -1,7 +1,6 @@
 const { Command } = require('klasa');
 const { MessageAttachment } = require('discord.js');
 
-const { roll } = require('../../../config/util');
 const generateBankImage = require('../../../data/new_monsters/utils/generateBankImage');
 
 module.exports = class extends Command {
@@ -16,19 +15,10 @@ module.exports = class extends Command {
 	async run(msg, [quantity, bossName]) {
 		if (!this.client.killWorkerThread) return;
 
-		const donationMessage = `Old School Bot is in huge need for donations for server costs. Please consider joining the support server (${
-			msg.guild ? msg.guild.settings.prefix : '+'
-		}support) if you're able to help.\n`;
-
-		const shouldShow = roll(3);
-
 		const result = await this.client.killWorkerThread(quantity, bossName);
 		if (typeof result === 'string') {
-			return msg.send(shouldShow ? donationMessage + result : result);
+			return msg.send(result);
 		}
-		return msg.send(
-			shouldShow ? donationMessage : '',
-			new MessageAttachment(Buffer.from(generateBankImage(result)), 'osbot.png')
-		);
+		return msg.send(new MessageAttachment(Buffer.from(generateBankImage(result)), 'osbot.png'));
 	}
 };
