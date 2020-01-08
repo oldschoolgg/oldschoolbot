504e9b6b9d30057b8a3ab66df1067162fb011ea8
gc <gc@users.noreply.github.com>
2020-01-08 13:12:45 +1100

    add logging for minions completion

--- src/tasks/monsterActivity.ts
+++ src/tasks/monsterActivity.ts
@@ -1,18 +1,19 @@
 import { Task } from 'klasa';
-import { KillableMonsters } from '../lib/constants';
+import { KillableMonsters, Events } from '../lib/constants';
 import { MonsterActivityTaskOptions, Bank } from '../lib/types';
 import { addBankToBank, getMinionName } from '../lib/util';
-import { Permissions, TextChannel, ClientUser, MessageAttachment } from 'discord.js';
+import { TextChannel, MessageAttachment } from 'discord.js';
 
 export default class extends Task {
 	async run({ monsterID, userID, channelID, quantity }: MonsterActivityTaskOptions) {
 		const monster = KillableMonsters.find(mon => mon.id === monsterID);
 		const user = await this.client.users.fetch(userID);
 
+		const logInfo = `MonsterID[${monsterID}] userID[${userID}] channelID[${channelID}] quantity[${quantity}]`;
+
 		if (!monster || !user) {
-			console.log(
-				`Missing user or monster... MonsterID[${monsterID}] userID[${userID}] channelID[${channelID}] quantity[${quantity}] `
-			);
+			this.client.emit(Events.Wtf, `Missing user or monster - ${logInfo}`);
+
 			return;
 		}
 
@@ -29,17 +30,16 @@ export default class extends Task {
 
 		await user.settings.update('bank', addBankToBank(loot, newBank));
 
+		this.client.emit(Events.Log, `User received Minion Loot - ${logInfo}`);
+
 		const str = `${user}, ${getMinionName(user)} finished killing ${quantity} ${monster.name}.`;
 
-		if (
-			!channel ||
-			!(channel as TextChannel)
-				.permissionsFor(this.client.user as ClientUser)!
-				.has(Permissions.FLAGS.SEND_MESSAGES)
-		) {
+		if (!channel) {
 			user.send(str, new MessageAttachment(image));
 		} else {
-			(channel as TextChannel).send(str, new MessageAttachment(image));
+			(channel as TextChannel).send(str, new MessageAttachment(image)).catch(() => {
+				user.send(str, new MessageAttachment(image));
+			});
 		}
 	}
 }
