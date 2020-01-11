6687b7b5f9a5e46c4eca3df910f1459eaba8add1
Hoffi Coffi <hofficoffirs@gmail.com>
2020-01-11 00:19:17 +0000

    adding cash command to show just the GP in your bank

---
+++ src/commands/Utility/cash.ts
@@ -0,0 +1,30 @@
+import { KlasaClient, CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from "../../lib/BotCommand";
+import { Emoji } from '../../lib/constants.js';
+
+export default class extends BotCommand {
+    public constructor(
+        client: KlasaClient,
+        store: CommandStore,
+        file: string[],
+        directory: string
+    ) {
+        super(client, store, file, directory, {
+            cooldown: 3,
+            aliases: ['bal', 'gp'],
+            description: 'Shows how much virtual GP you own.'
+        });
+    }
+
+    async run(msg: KlasaMessage) {
+        await msg.author.settings.sync(true);
+        const coins: number = msg.author.settings.get('GP');
+        
+        if (coins === 0) {
+			throw `You have no GP yet <:Sad:421822898316115969> You can get some GP by using the +daily command.`;
+        }
+        
+        return msg.channel.send(`${Emoji.MoneyBag} You have ${coins.toLocaleString()} GP!`);
+    }
+}
