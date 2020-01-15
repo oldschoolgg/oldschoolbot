f2dc8b7a5b6e4221e49896ceb900ade7fbd2671b
gc <gc@users.noreply.github.com>
2020-01-16 01:51:09 +1100

    run item fetching on init

--- src/tasks/prices.ts
+++ src/tasks/prices.ts
@@ -2,6 +2,10 @@ import { Task } from 'klasa';
 import { Items } from 'oldschooljs';
 
 export default class extends Task {
+	async init() {
+		this.run();
+	}
+
 	async run() {
 		this.syncItems();
 	}
