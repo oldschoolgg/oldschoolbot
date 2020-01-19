079e6a8e5c03e61c38629940b5dcf391e7feb97b
gc <gc@users.noreply.github.com>
2020-01-19 18:54:04 +1100

    fix hot-reloading

--- src/tasks/reloadOnChanges.ts
+++ src/tasks/reloadOnChanges.ts
@@ -1,16 +1,20 @@
-const { Task, Stopwatch } = require('klasa');
-const { watch } = require('chokidar');
-const { extname, basename, sep } = require('path');
+import { sep, extname, basename, join } from 'path';
+import { Task, KlasaClient, TaskStore, Stopwatch, Piece } from 'klasa';
+import { watch } from 'chokidar';
 
 const nodeModules = `${sep}node_modules${sep}`;
 
-module.exports = class extends Task {
-	constructor(...args) {
-		super(...args);
+export default class extends Task {
+	private _running = false;
+
+	public constructor(client: KlasaClient, store: TaskStore, file: string[], directory: string) {
+		super(client, store, file, directory);
 		this.enabled = !this.client.production;
 	}
 
-	async run(name, _path, piece) {
+	async run() {}
+
+	async reloadPiece(name: string, _path: string, piece?: Piece) {
 		const timer = new Stopwatch();
 
 		for (const module of Object.keys(require.cache)) {
@@ -23,28 +27,31 @@ module.exports = class extends Task {
 		const reload = this.client.commands.get('reload');
 		if (!reload) return;
 		if (piece) {
+			// @ts-ignore
 			await reload.run({ sendLocale: () => null, sendMessage: () => null }, [piece]);
 			log = `Reloaded it in ${timer}`;
 		} else {
-			await reload.everything({ sendLocale: () => null, sendMessage: () => null });
+			// @ts-ignore
+			await reload.everything({
+				sendLocale: () => null,
+				sendMessage: () => null
+			});
 			log = `Reloaded everything in ${timer}.`;
 		}
 
 		timer.stop();
-		return this.client.emit('log', `${name} was updated. ${log}`);
+		this.client.emit('log', `${name} was updated. ${log}`);
+		return null;
 	}
 
 	async init() {
 		if (this.client._fileChangeWatcher) return;
-
-		this.client._fileChangeWatcher = watch(process.cwd(), {
-			ignored: ['**/node_modules/**/*', '**/bwd/provider/**/*', '**/.git/**/*'],
+		this.client._fileChangeWatcher = watch(join(process.cwd(), 'dist/**/*.js'), {
 			persistent: true,
-			ignoreInitial: true,
-			cwd: process.cwd()
+			ignoreInitial: true
 		});
 
-		const reloadStore = async _path => {
+		const reloadStore = async (_path: string) => {
 			const store = _path.split(sep).find(dir => this.client.pieceStores.has(dir));
 
 			const name = basename(_path);
@@ -52,18 +59,20 @@ module.exports = class extends Task {
 			if (!store) {
 				if (this._running) return;
 				this._running = true;
-				await this.run(name, _path);
+				await this.reloadPiece(name, _path);
 				this._running = false;
 				return;
 			}
 
 			const piece = this.client.pieceStores.get(store).get(name.replace(extname(name), ''));
 
-			await this.run(name, _path, piece);
+			await this.reloadPiece(name, _path, piece);
 		};
 
 		for (const event of ['add', 'change', 'unlink']) {
-			this.client._fileChangeWatcher.on(event, reloadStore);
+			if (this.client._fileChangeWatcher) {
+				this.client._fileChangeWatcher.on(event, reloadStore);
+			}
 		}
 	}
-};
+}
