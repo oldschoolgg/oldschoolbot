c4ff31abb0c023b505a0d3569b9ca18ad89eaf57
Anthony <49297058+SomeRangerOSRS@users.noreply.github.com>
2020-01-20 23:23:53 -0400

    Updated cmd.js
    Updated the Cmd.Js to require permission level 6 rather than 7 as per https://klasa.js.org/#/docs/klasa/stable/Tutorials/UnderstandingPermissionLevels

--- src/commands/Configuration/cmd.js
+++ src/commands/Configuration/cmd.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			description: 'Enable and Disable certain bot commands in your guild. Admins only.',
 			usage: '<enable|disable> <command:cmd>',
 			usageDelim: ' ',
-			permissionLevel: 7
+			permissionLevel: 6
 		});
 	}
 
