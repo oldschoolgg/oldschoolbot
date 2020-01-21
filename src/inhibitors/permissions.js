2f27726bbcfd2fef4b50a38f0ef5ea25dc2db215
gc <gc@users.noreply.github.com>
2020-01-22 07:24:54 +1100

    fix error

--- src/inhibitors/permissions.js
+++ src/inhibitors/permissions.js
@@ -1,18 +1,19 @@
 const { Inhibitor } = require('klasa');
 
 module.exports = class extends Inhibitor {
-	async run(message, command) {
+	async run(msg, command) {
 		const { broke, permission } = await this.client.permissionLevels.run(
-			message,
+			msg,
 			command.permissionLevel
 		);
 
 		if (!permission) {
-			const isStaff = await message.hasAtLeastPermissionLevel(6);
-			const channelDisabled = message.channel.settings.onlyStaffCanUseCommands;
+			const isStaff = await msg.hasAtLeastPermissionLevel(6);
+			const channelDisabled =
+				msg.guild && msg.channel.settings.get('onlyStaffCanUseCommands');
 
 			if (!channelDisabled || (isStaff && channelDisabled)) {
-				throw broke ? message.language.get('INHIBITOR_PERMISSIONS') : true;
+				throw broke ? msg.language.get('INHIBITOR_PERMISSIONS') : true;
 			}
 			throw true;
 		}
