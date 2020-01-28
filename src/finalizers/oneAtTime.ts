f217c74da924eeeea2278588b9db2d900899a0db
gc <gc@users.noreply.github.com>
2020-01-29 04:18:26 +1100

    do oneAtTime handling directly in commandHandler

--- src/finalizers/oneAtTime.ts
+++
@@ -1,9 +0,0 @@
-import { Finalizer, Command, KlasaMessage } from 'klasa';
-
-export default class extends Finalizer {
-	run(msg: KlasaMessage, command: Command) {
-		if (command.oneAtTime && this.client.oneCommandAtATimeCache.has(msg.author.id)) {
-			this.client.oneCommandAtATimeCache.delete(msg.author.id);
-		}
-	}
-}
