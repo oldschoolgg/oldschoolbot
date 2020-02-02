768a3e6da4f2cbf69531e18c5d03d5553bea344c
gc <gc@users.noreply.github.com>
2020-02-03 01:44:21 +1100

    update to new vanity url

--- src/commands/Utility/info.js
+++ src/commands/Utility/info.js
@@ -20,7 +20,7 @@ Old School Bot is an open-source Discord Bot based on Old School RuneScape.
 
 	- If you have any other enquiries, join the support server!
 
-	- Support Server: <https://support.oldschool.gg/>
+	- Support Server: discord.gg/osb
 `);
 	}
 };
