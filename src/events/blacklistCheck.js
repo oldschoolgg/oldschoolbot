92217a332b85488432bee13f84f50f8d08ad6bf2
gc <gc@users.noreply.github.com>
2020-02-06 13:49:28 +1100

    use new settings .get syntax

--- src/events/blacklistCheck.js
+++ src/events/blacklistCheck.js
@@ -9,8 +9,8 @@ module.exports = class extends Event {
 	run(guild) {
 		if (!guild.available) return;
 		if (
-			this.client.settings.guildBlacklist.includes(guild.id) ||
-			(guild.owner && this.client.settings.guildBlacklist.includes(guild.owner.id))
+			this.client.settings.get('guildBlacklist').includes(guild.id) ||
+			(guild.owner && this.client.settings.get('guildBlacklist').includes(guild.owner.id))
 		) {
 			guild.leave();
 			this.client.emit('warn', `Blacklisted guild detected: ${guild.name} [${guild.id}]`);
