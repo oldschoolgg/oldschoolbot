c89a4323eb0dbbd0e8ba70af761adcc916c99804
gc <gc@users.noreply.github.com>
2020-02-19 00:53:03 +1100

    optimize addArrayOfBanks

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -10,6 +10,10 @@ export interface Bank {
 	[key: number]: number;
 }
 
+export interface StringKeyedBank {
+	[key: string]: number;
+}
+
 export interface SettingsEntry {
 	id: string;
 	RSN?: string;
