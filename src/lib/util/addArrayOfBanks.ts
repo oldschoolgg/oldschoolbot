335ce9b8b72c5ba20655add1f33c7a746298dfdc
gc <gc@users.noreply.github.com>
2020-02-19 00:53:03 +1100

    optimize addArrayOfBanks

--- src/lib/util/addArrayOfBanks.ts
+++ src/lib/util/addArrayOfBanks.ts
@@ -1,11 +1,13 @@
-import { Bank } from '../types';
-import { addBankToBank } from '../util';
+import { Bank, StringKeyedBank } from '../types';
 
 export default function addArrayOfBanks(arrayOfBanks: Bank[]) {
-	let newBank = {};
+	const newBank: StringKeyedBank = {};
 
 	for (const bank of arrayOfBanks) {
-		newBank = addBankToBank(bank, newBank);
+		for (const [itemID, quantity] of Object.entries(bank)) {
+			if (newBank[itemID]) newBank[itemID] += quantity;
+			else newBank[itemID] = quantity;
+		}
 	}
 
 	return newBank;
