636f39b934b476162a9bdf417bc32389c3bece53
gc <gc@users.noreply.github.com>
2020-02-25 23:23:28 +1100

    getItemPrice no longer used

--- src/lib/util/getItemPrice.ts
+++
@@ -1,7 +0,0 @@
-import { KlasaClient } from 'klasa';
-
-import { ClientSettings } from '../ClientSettings';
-
-export default function getItemPrice(client: KlasaClient, itemID: number | string) {
-	return client.settings.get(ClientSettings.Prices)[itemID].price;
-}
