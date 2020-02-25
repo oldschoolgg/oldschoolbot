43a0f77c14b90c68cc24390d37f5ce7d8fac9bbe
gc <gc@users.noreply.github.com>
2020-02-26 00:46:37 +1100

    fix wrong paths

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-const { twitchClientID } = require('../config/private');
+const { twitchClientID } = require('../private.json');
 
 const convertLVLtoXP = lvl => {
 	let points = 0;
