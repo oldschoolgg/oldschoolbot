ce2c08a8c6c7bcce9ce3aabf15f3ccd1de70e769
wyattos <59992174+wyattos@users.noreply.github.com>
2020-03-12 21:01:14 -0700

    dfs + dragonfire ward (#162)
    * dfs + dragonfire ward
    
    * infernal pick
    
    * wildy wards
    
    * crystal key

--- src/lib/craftables.ts
+++ src/lib/craftables.ts
@@ -64,6 +64,72 @@ const Craftables: Craftable[] = [
 			[itemID('Zamorak godsword')]: 1
 		},
 		smithingLevel: 80
+	},
+	{
+		name: 'Dragonfire shield',
+		inputItems: {
+			[itemID('Draconic visage')]: 1
+		},
+		outputItems: {
+			[itemID('Dragonfire shield')]: 1
+		},
+		smithingLevel: 90
+	},
+	{
+		name: 'Dragonfire ward',
+		inputItems: {
+			[itemID('Skeletal visage')]: 1
+		},
+		outputItems: {
+			[itemID('Dragonfire ward')]: 1
+		},
+		smithingLevel: 90
+	},
+	{
+		name: 'Infernal pickaxe',
+		inputItems: {
+			[itemID('Dragon pickaxe')]: 1,
+			[itemID('Smouldering stone')]: 1
+		},
+		outputItems: {
+			[itemID('Infernal pickaxe')]: 1
+		},
+		smithingLevel: 85
+	},
+	{
+		name: 'Malediction ward',
+		inputItems: {
+			[itemID('Malediction shard 1')]: 1,
+			[itemID('Malediction shard 2')]: 1,
+			[itemID('Malediction shard 3')]: 1
+		},
+		outputItems: {
+			[itemID('Malediction ward')]: 1
+		},
+		smithingLevel: 1
+	},
+	{
+		name: 'Odium ward',
+		inputItems: {
+			[itemID('Odium shard 1')]: 1,
+			[itemID('Odium shard 2')]: 1,
+			[itemID('Odium shard 3')]: 1
+		},
+		outputItems: {
+			[itemID('Odium ward')]: 1
+		},
+		smithingLevel: 1
+	},
+	{
+		name: 'Crystal key',
+		inputItems: {
+			[itemID('Loop half of key')]: 1,
+			[itemID('Tooth half of key')]: 1
+		},
+		outputItems: {
+			[itemID('Crystal key')]: 1
+		},
+		smithingLevel: 1
 	}
 ];
 
