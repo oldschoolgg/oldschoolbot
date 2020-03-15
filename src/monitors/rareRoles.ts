c739467256260c55d7770c6a81b0b7cbd2a78663
gc <gc@users.noreply.github.com>
2020-03-15 12:54:52 +1100

    add more rng roles

--- src/monitors/rareRoles.ts
+++ src/monitors/rareRoles.ts
@@ -3,12 +3,18 @@ import { Monitor, MonitorStore, KlasaMessage } from 'klasa';
 import { SupportServer, Emoji } from '../lib/constants';
 import { roll } from '../lib/util';
 
-const rareRoles: [string, number][] = [
-	['670211706907000842', 250 / 10],
-	['670211798091300864', 2500 / 10],
-	['670212713258942467', 25_000 / 10],
-	['670212821484568577', 250_000 / 10],
-	['670212876832735244', 10_000_000 / 10]
+// The actual rates are these numbers divided by 10.
+const rareRoles: [string, number, string][] = [
+	['670211706907000842', 250, 'Bronze'],
+	['670211798091300864', 2500, 'Iron'],
+	['688563471096348771', 5000, 'Steel'],
+	['688563333464457304', 10_000, 'Mithril'],
+	['688563389185917072', 15_000, 'Adamant'],
+	['670212713258942467', 25_000, 'Rune'],
+	['670212821484568577', 50_000, 'Dragon'],
+	['670212821484568577', 75_000, 'Barrows'],
+	['688563780686446649', 100_000, 'Justiciar'],
+	['670212876832735244', 10_000_000, 'Third Age']
 ];
 
 export default class extends Monitor {
@@ -28,11 +34,15 @@ export default class extends Monitor {
 
 		if (!roll(10)) return;
 
-		for (const [roleID, chance] of rareRoles) {
-			if (roll(chance)) {
+		for (const [roleID, chance, name] of rareRoles) {
+			if (roll(chance / 10)) {
 				if (msg.member?.roles.has(roleID)) continue;
+				if (!this.client.production) {
+					return msg.send(`${msg.author}, you would've gotten the **${name}** role.`);
+				}
 				msg.member?.roles.add(roleID);
 				msg.react(Emoji.Gift);
+				break;
 			}
 		}
 	}
