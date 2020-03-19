a846a27ac789a3eea7f3463e033fb21c24974d31
gc <gc@users.noreply.github.com>
2020-03-20 00:10:05 +1100

    fix up some things in questingActivity task

--- src/tasks/minions/questingActivity.ts
+++ src/tasks/minions/questingActivity.ts
@@ -1,7 +1,7 @@
 import { Task, KlasaMessage } from 'klasa';
 
-import { saidYes, noOp } from '../../lib/util';
-import { Time } from '../../lib/constants';
+import { saidYes, noOp, rand } from '../../lib/util';
+import { Time, MAX_QP } from '../../lib/constants';
 import { QuestingActivityTaskOptions } from '../../lib/types/minions';
 import getUsersPerkTier from '../../lib/util/getUsersPerkTier';
 import { channelIsSendable } from '../../lib/util/channelIsSendable';
@@ -13,12 +13,16 @@ export default class extends Task {
 		const currentQP = user.settings.get(UserSettings.QP);
 
 		// This assumes you do quests in order of scaling difficulty, ~115 hours for max qp
-		let qpRecieved = 4;
-		if (currentQP >= 84) {
-			qpRecieved = 3;
-			if (currentQP >= 182) {
-				qpRecieved = Math.floor(Math.random() * 3);
-			}
+		let qpRecieved = rand(3, 5);
+		if (currentQP >= 182) {
+			qpRecieved = rand(1, 3);
+		} else if (currentQP >= 84) {
+			qpRecieved = rand(2, 4);
+		}
+
+		// The minion could be at (MAX_QP - 1) QP, but gain 4 QP here, so we'll trim that down from 4 to 1.
+		if (currentQP + qpRecieved > MAX_QP) {
+			qpRecieved -= currentQP + qpRecieved - MAX_QP;
 		}
 
 		let str = `${user}, ${
@@ -27,37 +31,34 @@ export default class extends Task {
 			user.minionName
 		} asks if you'd like them to do another of the same trip.`;
 
-		if (currentQP + qpRecieved >= 275) {
-			str += `You have achieved the maximum amount of 275 Quest Points!`;
+		if (currentQP + qpRecieved >= MAX_QP) {
+			str += `You have achieved the maximum amount of ${MAX_QP} Quest Points!`;
 		}
 
-		// Add QP and make sure they don't have over the max of 275
 		await user.addQP(qpRecieved);
-		if (user.settings.get(UserSettings.QP) > 275) {
-			user.settings.update('QP', 275);
-		}
 
 		const channel = this.client.channels.get(channelID);
 		if (!channelIsSendable(channel)) return;
 
-		channel.send(str).catch(noOp);
+		this.client.queuePromise(() => {
+			channel.send(str).catch(noOp);
+			channel
+				.awaitMessages(mes => mes.author === user && saidYes(mes.content), {
+					time: getUsersPerkTier(user) > 1 ? Time.Minute * 10 : Time.Minute * 2,
+					max: 1
+				})
+				.then(messages => {
+					const response = messages.first();
 
-		channel
-			.awaitMessages(mes => mes.author === user && saidYes(mes.content), {
-				time: getUsersPerkTier(user) > 1 ? Time.Minute * 10 : Time.Minute * 2,
-				max: 1
-			})
-			.then(messages => {
-				const response = messages.first();
+					if (response) {
+						if (response.author.minionIsBusy) return;
 
-				if (response) {
-					if (response.author.minionIsBusy) return;
+						user.log(`continued trip of Questing.`);
 
-					user.log(`continued trip of Questing.`);
-
-					this.client.commands.get('goquest')!.run(response as KlasaMessage, []);
-				}
-			})
-			.catch(noOp);
+						this.client.commands.get('quest')!.run(response as KlasaMessage, []);
+					}
+				})
+				.catch(noOp);
+		});
 	}
 }
