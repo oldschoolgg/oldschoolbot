f9b3deab1c8df692dd499d01778671b326dd35f7
coolbop32 <48160358+coolbop32@users.noreply.github.com>
2020-03-26 09:14:17 -0400

    Add Woodcutting Skill (#185)
    * Woodcutting
    
    * changed how boost and pet worked
    
    * updated xp rate, added skillcape
    
    * changed w, changed mushroom, added +chop to +m
    
    * changed spelling of trimed woodcut cape
    
    * .

--- src/lib/skills/index.ts
+++ src/lib/skills/index.ts
@@ -2,13 +2,15 @@ import { Collection } from 'discord.js';
 
 import Mining from './mining';
 import Smithing from './smithing';
+import Woodcutting from './woodcutting';
 import { SkillsEnum } from '../types';
 
-export type Skill = typeof Mining | typeof Smithing;
+export type Skill = typeof Mining | typeof Smithing | typeof Woodcutting;
 
 const Skills: Collection<string, Skill> = new Collection([
 	[SkillsEnum.Mining, Mining as Skill],
-	[SkillsEnum.Smithing, Smithing as Skill]
+	[SkillsEnum.Smithing, Smithing as Skill],
+	[SkillsEnum.Woodcutting, Woodcutting as Skill]
 ]);
 
 export default Skills;
