172d29532b2ec57a17770a6c757b71c04f3e6737
gc <gc@users.noreply.github.com>
2020-04-02 14:57:52 +1100

    change 1 month = 30 days instead of 31

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ export const enum Time {
 	Minute = 1000 * 60,
 	Hour = 1000 * 60 * 60,
 	Day = 1000 * 60 * 60 * 24,
-	Month = 1000 * 60 * 60 * 24 * 31,
+	Month = 1000 * 60 * 60 * 24 * 30,
 	Year = 1000 * 60 * 60 * 24 * 365
 }
 
