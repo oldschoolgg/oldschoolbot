ec53ca6406fc61904f7ad896ad3ef4f59302ca5e
gc <gc@users.noreply.github.com>
2020-04-03 21:36:11 +1100

    add daily reminder patron perk

---
+++ src/tasks/dailyReminder.ts
@@ -0,0 +1,24 @@
+import { Task } from 'klasa';
+
+import getUsersPerkTier from '../lib/util/getUsersPerkTier';
+import { PerkTier, Time } from '../lib/constants';
+import { UserSettings } from '../lib/UserSettings';
+import { noOp } from '../lib/util';
+
+export default class extends Task {
+	async run() {
+		const currentDate = Date.now();
+
+		for (const user of this.client.users.values()) {
+			if (getUsersPerkTier(user) < PerkTier.Two) continue;
+			const lastVoteDate = user.settings.get(UserSettings.LastDailyTimestamp);
+			if (lastVoteDate === -1) continue;
+
+			const difference = currentDate - lastVoteDate;
+			if (difference >= Time.Hour * 12) {
+				await user.settings.update(UserSettings.LastDailyTimestamp, -1);
+				await user.send(`Your daily is ready!`).catch(noOp);
+			}
+		}
+	}
+}
