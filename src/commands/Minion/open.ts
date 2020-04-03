fc97543e6d016a800ab32ce4056960e4669f01b4
gc <gc@users.noreply.github.com>
2020-04-03 22:36:31 +1100

    notification on: gilded, 3a, bloodhound

--- src/commands/Minion/open.ts
+++ src/commands/Minion/open.ts
@@ -8,6 +8,13 @@ import Openables from '../../lib/openables';
 import { stringMatches, itemNameFromID } from '../../lib/util';
 import bankFromLootTableOutput from '../../lib/util/bankFromLootTableOutput';
 import createReadableItemListFromBank from '../../lib/util/createReadableItemListFromTuple';
+import { cluesRares } from '../../lib/collectionLog';
+import { UserSettings } from '../../lib/UserSettings';
+import { formatOrdinal } from '../../lib/util/formatOrdinal';
+import itemID from '../../lib/util/itemID';
+
+const itemsToNotifyOf: number[] = Object.values(cluesRares).flat(Infinity);
+itemsToNotifyOf.push(itemID('Bloodhound'));
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -36,7 +43,24 @@ export default class extends BotCommand {
 
 		const opened = `You opened one of your ${clueTier.name} Clue Caskets`;
 
-		if (Object.keys(loot).length === 0) {
+		// Here we check if the loot has any ultra-rares (3rd age, gilded, bloodhound),
+		// and send a notification if they got one.
+		const keys = Object.keys(loot);
+		if (keys.some(key => itemsToNotifyOf.includes(parseInt(key)))) {
+			const lootStr = await createReadableItemListFromBank(this.client, loot);
+			const nthCasket = msg.author.settings.get(UserSettings.ClueScores)[clueTier.id] ?? 1;
+
+			this.client.emit(
+				Events.ServerNotification,
+				`**${msg.author.username}'s** minion, ${
+					msg.author.minionName
+				}, just opened their ${formatOrdinal(nthCasket)} ${
+					clueTier.name
+				} casket and received **${lootStr}**!`
+			);
+		}
+
+		if (keys.length === 0) {
 			return msg.send(`${opened} and got nothing :(`);
 		}
 
