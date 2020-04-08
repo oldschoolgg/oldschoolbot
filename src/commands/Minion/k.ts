92fcbab7d9bf93868f5d47c187d78d05ef5a4e84
gc <gc@users.noreply.github.com>
2020-04-09 04:07:01 +1000

    add +k cmd alias for minion killing

---
+++ src/commands/Minion/k.ts
@@ -0,0 +1,24 @@
+import { KlasaMessage, CommandStore } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			altProtection: true,
+			oneAtTime: true,
+			cooldown: 1,
+			usage: '[quantity:int{1}|name:...string] [name:...string]',
+			usageDelim: ' '
+		});
+	}
+
+	async run(msg: KlasaMessage, [quantity, name = '']: [string | number, string]) {
+		return this.client.commands
+			.get('minion')!
+			.kill(msg, [quantity, name])
+			.catch(err => {
+				return msg.send(err);
+			});
+	}
+}
