4fff53e37a67db68a7d504265b4ea0a5b8fed533
coolbop32 <48160358+coolbop32@users.noreply.github.com>
2020-04-15 17:04:05 -0400

    moved the different tiers of bars to own files

--- src/config.ts
+++ src/config.ts
@@ -50,7 +50,7 @@ const clientOptions: KlasaClientOptions = {
 	},
 	/* Klasa Options */
 	createPiecesFolders: false,
-	prefix: '^',
+	prefix: '+',
 	providers: {
 		default: 'postgres',
 		postgres: privateConfig?.postgresConfig
