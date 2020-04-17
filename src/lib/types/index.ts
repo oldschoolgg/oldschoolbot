52494545fedd7e5486e09dac14e975f87bcffc47
gc <gc@users.noreply.github.com>
2020-04-18 06:18:24 +1000

    types fix

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -107,7 +107,8 @@ export type RedditAppConfig = null | {
 export type TwitterAppConfig = null | {
 	consumer_key: string;
 	consumer_secret: string;
-	access_token_key: string;
+	access_token: string;
+	access_token_key?: string;
 	access_token_secret: string;
 };
 
