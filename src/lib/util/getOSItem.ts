e38c2be0ed2a88909973786cdb909afb8dedec43
Alexsuperfly <alexcsumner@gmail.com>
2020-04-25 18:10:35 -0400

    maybe fix bad items idk

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -3,7 +3,16 @@ import { Item } from 'oldschooljs/dist/meta/types';
 
 import cleanItemName from './cleanItemName';
 
+const overrideItems = {
+	19532: ['Zenyte bracelet', 19492],
+	11072: ['Sapphire bracelet', 11071],
+	11069: ['Gold bracelet', 11068]
+};
+
 export default function getOSItem(itemName: string | number): Item {
+	const item = Object.entries(overrideItems).find(i => i[1].includes(itemName));
+	itemName = !item ? itemName : item[0];
+
 	const osItem = Items.get(typeof itemName === 'number' ? itemName : cleanItemName(itemName)) as
 		| Item
 		| undefined;
