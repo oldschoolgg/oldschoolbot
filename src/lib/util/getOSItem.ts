6ec3df5378f5c395824e402cd3e418661b23519a
Alexsuperfly <alexcsumner@gmail.com>
2020-04-25 23:04:06 -0400

    more fix idk

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -4,18 +4,18 @@ import { Item } from 'oldschooljs/dist/meta/types';
 import cleanItemName from './cleanItemName';
 
 const overrideItems = {
-	19532: ['Zenyte bracelet', 19492],
-	11072: ['Sapphire bracelet', 11071],
-	11069: ['Gold bracelet', 11068]
+	19532: ['Zenyte bracelet', 'zenyte bracelet', 19492],
+	11072: ['Sapphire bracelet', 'sapphire bracelet', 11071],
+	11069: ['Gold bracelet', 'gold bracelet', 11068]
 };
 
 export default function getOSItem(itemName: string | number): Item {
-	const item = Object.entries(overrideItems).find(i => i[1].includes(itemName));
-	itemName = !item ? itemName : item[0];
+	itemName = typeof itemName === 'number' ? itemName : cleanItemName(itemName);
 
-	const osItem = Items.get(typeof itemName === 'number' ? itemName : cleanItemName(itemName)) as
-		| Item
-		| undefined;
+	const item = Object.entries(overrideItems).find(i => i[1].includes(itemName));
+	itemName = !item ? itemName : parseInt(item[0]);
+
+	const osItem = Items.get(itemName) as Item | undefined;
 	if (!osItem) throw `${itemName} doesnt exist.`;
 	return osItem;
 }
