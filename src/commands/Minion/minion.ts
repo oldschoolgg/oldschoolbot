dd9350351185ae5dbc823178b49fba2915557237
gc <gc@users.noreply.github.com>
2020-04-26 19:31:35 +1000

    reset sacrifice amount on become ironman

--- src/commands/Minion/minion.ts
+++ src/commands/Minion/minion.ts
@@ -140,6 +140,7 @@ Type \`confirm\` if you understand the above information, and want to become an 
 				UserSettings.MonsterScores,
 				UserSettings.ClueScores,
 				UserSettings.BankBackground,
+				UserSettings.SacrificedValue,
 				'gear',
 				'stats',
 				'skills'
