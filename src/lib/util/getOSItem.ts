9a0930460c9b374177b9dc5dfd3d20748a5eb196
gc <gc@users.noreply.github.com>
2020-04-26 19:13:36 +1000

    revert fully

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -4,9 +4,9 @@ import { Item } from 'oldschooljs/dist/meta/types';
 import cleanItemName from './cleanItemName';
 
 export default function getOSItem(itemName: string | number): Item {
-	itemName = typeof itemName === 'number' ? itemName : cleanItemName(itemName);
-
-	const osItem = Items.get(itemName) as Item | undefined;
+	const osItem = Items.get(typeof itemName === 'number' ? itemName : cleanItemName(itemName)) as
+		| Item
+		| undefined;
 	if (!osItem) throw `${itemName} doesnt exist.`;
 	return osItem;
 }
