38dbe9f8e5d21cc3a0182740d3b8fa4b6441bf19
gc <gc@users.noreply.github.com>
2020-05-07 00:56:28 +1000

    add botstats command

---
+++ src/commands/Patrons/botstats.ts
@@ -0,0 +1,131 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Monsters } from 'oldschooljs';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { PerkTier } from '../../lib/constants';
+import backgroundImages from '../../lib/minions/data/bankBackgrounds';
+import { StringKeyedBank } from '../../lib/types';
+import ClueTiers from '../../lib/minions/data/clueTiers';
+
+type BankQueryResult = { bankBackground: number; count: string }[];
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['bstats'],
+			perkTier: PerkTier.Four,
+			subcommands: true,
+			usage: '<servers|minions|ironmen|sacrificed|bankbg|monsters|clues>',
+			oneAtTime: true,
+			cooldown: 60
+		});
+	}
+
+	async _query(str: string): Promise<any> {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+		// @ts-ignore
+		return this.client.providers.default.runAll(str);
+	}
+
+	async servers(msg: KlasaMessage) {
+		return msg.send(`Old School Bot is in ${this.client.guilds.size} servers.`);
+	}
+
+	async minions(msg: KlasaMessage) {
+		const result = await this._query(
+			`SELECT COUNT(*) FROM users WHERE "minion.hasBought" = true;`
+		);
+
+		return msg.send(`There are ${result[0].count.toLocaleString()} minions!`);
+	}
+
+	async ironmen(msg: KlasaMessage) {
+		const result = await this._query(
+			`SELECT COUNT(*) FROM users WHERE "minion.ironman" = true;`
+		);
+
+		return msg.send(`There are ${parseInt(result[0].count).toLocaleString()} ironman minions!`);
+	}
+
+	async sacrificed(msg: KlasaMessage) {
+		const result = await this._query(`SELECT SUM ("sacrificedValue") AS total FROM users;`);
+
+		return msg.send(
+			`There has been ${parseInt(
+				result[0].total
+			).toLocaleString()} GP worth of items sacrificed!`
+		);
+	}
+
+	async bankbg(msg: KlasaMessage) {
+		const result: BankQueryResult = await this._query(`SELECT "bankBackground", COUNT(*) 
+FROM users
+WHERE "bankBackground" <> 1
+GROUP BY "bankBackground";`);
+
+		return msg.send(
+			result
+				.map(
+					res =>
+						`**${backgroundImages[res.bankBackground].name}:** ${parseInt(
+							res.count
+						).toLocaleString()}`
+				)
+				.join('\n')
+		);
+	}
+
+	async monsters(msg: KlasaMessage) {
+		const totalBank: { [key: string]: number } = {};
+
+		const res: any = await this._query(
+			`SELECT ARRAY(SELECT "monsterScores" FROM users WHERE "monsterScores"::text <> '{}'::text);`
+		);
+
+		const banks: StringKeyedBank[] = res[0].array;
+
+		banks.map(bank => {
+			for (const [id, qty] of Object.entries(bank)) {
+				if (!totalBank[id]) totalBank[id] = qty;
+				else totalBank[id] += qty;
+			}
+		});
+
+		return msg.send(
+			Object.entries(totalBank)
+				.sort(([, qty1], [, qty2]) => qty2 - qty1)
+				.map(([monID, qty]) => {
+					return `**${Monsters.get(parseInt(monID))?.name}:** ${qty.toLocaleString()}`;
+				})
+				.join('\n')
+		);
+	}
+
+	async clues(msg: KlasaMessage) {
+		const totalBank: { [key: string]: number } = {};
+
+		const res: any = await this._query(
+			`SELECT ARRAY(SELECT "clueScores" FROM users WHERE "clueScores"::text <> '{}'::text);`
+		);
+
+		const banks: StringKeyedBank[] = res[0].array;
+
+		banks.map(bank => {
+			for (const [id, qty] of Object.entries(bank)) {
+				if (!totalBank[id]) totalBank[id] = qty;
+				else totalBank[id] += qty;
+			}
+		});
+
+		return msg.send(
+			Object.entries(totalBank)
+				.map(
+					([clueID, qty]) =>
+						`**${
+							ClueTiers.find(t => t.id === parseInt(clueID))?.name
+						}:** ${qty.toLocaleString()}`
+				)
+				.join('\n')
+		);
+	}
+}
