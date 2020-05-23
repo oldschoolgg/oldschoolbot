9db64d4cb92aa38dbf02d0cfb3fe811c87d81b6c
gc <gc@users.noreply.github.com>
2020-05-23 16:38:36 +1000

    search all monsters for kc lb (to show jad lb)

--- src/commands/Minion/leaderboard.ts
+++ src/commands/Minion/leaderboard.ts
@@ -8,7 +8,7 @@ import { stringMatches, toTitleCase, convertXPtoLVL } from '../../lib/util';
 import { collectionLogTypes } from '../../lib/collectionLog';
 import { UserRichDisplay } from '../../lib/structures/UserRichDisplay';
 import Skills from '../../lib/skilling/skills';
-import findMonster from '../../lib/minions/functions/findMonster';
+import { Monsters } from 'oldschooljs';
 
 const CACHE_TIME = Time.Minute * 5;
 
@@ -294,7 +294,13 @@ ORDER BY u.petcount DESC LIMIT 2000;`
 		if (!name) {
 			throw `Please specify which monster, for example \`${msg.cmdPrefix}leaderboard kc bandos\``;
 		}
-		const monster = findMonster(name);
+
+		const monster = Monsters.find(
+			mon =>
+				stringMatches(mon.name, name) ||
+				mon.aliases.some(alias => stringMatches(alias, name))
+		);
+
 		if (!monster) throw `That's not a valid monster!`;
 
 		const onlyForGuild = msg.flagArgs.server;
