71f3741f516d024eebdb48321824ee3b17b6f3d0
gc <gc@users.noreply.github.com>
2020-06-01 22:15:12 +1000

    run patreon task when a user gets patron role

---
+++ src/events/guildMemberUpdate.ts
@@ -0,0 +1,20 @@
+import { Event, EventStore } from 'klasa';
+import { GuildMember } from 'discord.js';
+
+import { Roles, SupportServer } from '../lib/constants';
+
+export default class extends Event {
+	public constructor(store: EventStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			once: false,
+			event: 'guildMemberUpdate'
+		});
+	}
+
+	async run(oldMember: GuildMember, newMember: GuildMember) {
+		if (oldMember.guild.id !== SupportServer) return;
+		if (!oldMember.roles.has(Roles.Patron) && newMember.roles.has(Roles.Patron)) {
+			this.client.tasks.get('patreon')?.run();
+		}
+	}
+}
