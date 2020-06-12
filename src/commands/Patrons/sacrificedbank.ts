09d35c311067136c3c7f44276eab4a2aa1875cc9
gc <gc@users.noreply.github.com>
2020-06-13 02:57:37 +1000

    add sacrificed bank command

---
+++ src/commands/Patrons/sacrificedbank.ts
@@ -0,0 +1,24 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { PerkTier } from '../../lib/constants';
+import { ClientSettings } from '../../lib/settings/types/ClientSettings';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['sbank'],
+			perkTier: PerkTier.Two,
+			oneAtTime: true,
+			cooldown: 60 * 10
+		});
+	}
+
+	async run(msg: KlasaMessage) {
+		return msg.channel.sendBankImage({
+			bank: this.client.settings.get(ClientSettings.EconomyStats.SacrificedBank),
+			title: `Sacrificed Items Of All Players`,
+			content: `Note: Approximately 130b worth of items are not shown, as they were sacrificed before items were starting to be tracked.`
+		});
+	}
+}
