6dac8b3f1cfbf7bc4b403931c22ec4949ebaefea
gc <gc@users.noreply.github.com>
2020-07-05 01:58:35 +1000

    improve disconnection logging

--- src/events/disconnect.ts
+++ src/events/disconnect.ts
@@ -1,8 +1,8 @@
 import { Event } from 'klasa';
+import { Events } from '../lib/constants';
 
 export default class extends Event {
 	public run(err: { code: number; reason: string }) {
-		this.client.console.error(`Disconnected | ${err.code}: ${err.reason}`);
-		this.client.console.log(`Disconnected | ${err.code}: ${err.reason}`);
+		this.client.emit(Events.Log, `Disconnected | ${err.code}: ${err.reason}`);
 	}
 }
