2c68c011648460d8ef858470dcae8102fbb0d7dc
gc <gc@users.noreply.github.com>
2020-07-08 22:11:34 +1000

    move eatable foods from wt to own file

--- src/commands/Minion/wt.ts
+++ src/commands/Minion/wt.ts
@@ -12,7 +12,6 @@ import {
 import { WintertodtActivityTaskOptions } from '../../lib/types/minions';
 import addSubTaskToActivityTask from '../../lib/util/addSubTaskToActivityTask';
 import { SkillsEnum } from '../../lib/skilling/types';
-import { Cookables } from '../../lib/skilling/skills/cooking';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import bankHasItem from '../../lib/util/bankHasItem';
 import hasItemEquipped from '../../lib/gear/functions/hasItemEquipped';
@@ -20,8 +19,7 @@ import resolveItems from '../../lib/util/resolveItems';
 import { MinigameIDsEnum } from '../../lib/minions/data/minigames';
 import { ClientSettings } from '../../lib/settings/types/ClientSettings';
 import { minionNotBusy, requiresMinion } from '../../lib/minions/decorators';
-
-const healingFoods = Cookables.filter(item => item.healAmount).reverse();
+import { Eatables } from '../../lib/minions/data/Eatables';
 
 const pyroPieces = resolveItems([
 	'Pyromancer hood',
@@ -84,13 +82,13 @@ export default class extends BotCommand {
 		const quantity = Math.floor(msg.author.maxTripLength / durationPerTodt);
 
 		const bank = msg.author.settings.get(UserSettings.Bank);
-		for (const food of healingFoods) {
+		for (const food of Eatables) {
 			const amountNeeded = Math.ceil(healAmountNeeded / food.healAmount!) * quantity;
 			if (!bankHasItem(bank, food.id, amountNeeded)) {
-				if (healingFoods.indexOf(food) === healingFoods.length - 1) {
-					throw `You don't have enough food to do Wintertodt! You can use these food items: ${healingFoods
-						.map(i => i.name)
-						.join(', ')}.`;
+				if (Eatables.indexOf(food) === Eatables.length - 1) {
+					throw `You don't have enough food to do Wintertodt! You can use these food items: ${Eatables.map(
+						i => i.name
+					).join(', ')}.`;
 				}
 				continue;
 			}
