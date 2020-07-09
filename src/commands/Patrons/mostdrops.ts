f275176cf5c88d7b38e28827540c50bc3ea5fd7c
gc <gc@users.noreply.github.com>
2020-07-09 20:11:06 +1000

    remove monster from mostdrops cmd, fix title

--- src/commands/Patrons/mostdrops.ts
+++ src/commands/Patrons/mostdrops.ts
@@ -1,29 +1,21 @@
 import { CommandStore, KlasaMessage } from 'klasa';
-import { Monsters } from 'oldschooljs';
 
 import { BotCommand } from '../../lib/BotCommand';
 import { PerkTier } from '../../lib/constants';
-import { stringMatches } from '../../lib/util';
 import getOSItem from '../../lib/util/getOSItem';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			perkTier: PerkTier.Four,
-			usage: '<monsterName:str> <itemName:str>',
+			usage: '<itemName:str>',
 			oneAtTime: true,
 			cooldown: 120,
 			usageDelim: ','
 		});
 	}
 
-	async run(msg: KlasaMessage, [monName, itemName]: [string, string]) {
-		const mon = Monsters.find(mon => mon.aliases.some(alias => stringMatches(alias, monName)));
-
-		if (!mon) {
-			throw `That's not a valid monster.`;
-		}
-
+	async run(msg: KlasaMessage, [itemName]: [string]) {
 		const item = getOSItem(itemName);
 
 		const query = `SELECT "id", "collectionLogBank"->>'${item.id}' AS "qty" FROM users WHERE "collectionLogBank"->>'${item.id}' IS NOT NULL ORDER BY ("collectionLogBank"->>'${item.id}')::int DESC LIMIT 10;`;
@@ -40,7 +32,7 @@ export default class extends BotCommand {
 		const command = this.client.commands.get('leaderboard');
 
 		return msg.send(
-			`**Dry Streaks for ${item.name} from ${mon.name}:**\n${result
+			`**Most '${item.name}' received:**\n${result
 				.map(
 					({ id, qty }) =>
 						// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
