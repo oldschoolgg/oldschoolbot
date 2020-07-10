ae0d10c88f350f5547766fd6e070505c9a8ca12e
BrandonEnglish848 <btoplafayette@gmail.com>
2020-07-10 08:13:33 -0500

    This will generate an item or multiples of an item
    <-- THIS SHOULD **NEVER** BE PUSHED TO THE STANDARD USER COMMANDS-->
    This is to help with local instance testing and is ONLY for *development purposes*.

---
+++ src/commands/Owner/spawn.ts
@@ -0,0 +1,18 @@
+import { KlasaMessage, CommandStore } from 'klasa';
+import { BotCommand } from '../../lib/BotCommand';
+import getOSItem from '../../lib/util/getOSItem';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			usage: '[number:int{1,10000000}] <itemname:...string>',
+			usageDelim: ' ',
+		});
+	}
+
+	async run(msg: KlasaMessage, [qty = 1, itemName]: [number, string]) {
+		const osItem = getOSItem(itemName);
+		await msg.author.addItemsToBank({ [osItem.id]: qty });
+		return msg.send(`Gave you ${qty}x ${osItem.name}.`);
+	}
+}
