15fcf425655e3d107220ace79cfe5382c5c3307c
BrandonEnglish848 <btoplafayette@gmail.com>
2020-07-10 08:23:50 -0500

    DO NOT ADD TO BOT
    This will allow a Dev to increase or decrease their exp in any skill.

---
+++ src/commands/Owner/enhance.ts
@@ -0,0 +1,27 @@
+import { KlasaMessage, CommandStore } from 'klasa';
+import { BotCommand } from '../../lib/BotCommand';
+import { convertXPtoLVL } from '../../lib/util';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['enhance','exp','xp'],
+			usage: '<amount:int{-200000000,200000000}> <skillName:string>',
+			usageDelim: ' '
+		});
+	}
+
+	async run(msg: KlasaMessage, [amount, skillName]:[number, string]) {
+		await msg.author.settings.sync(true);
+		
+		const currentXP=msg.author.settings.get(`skills.${skillName}`) as number;
+		
+		var newXP = await Math.min(200_000_000, currentXP + amount);
+		if (newXP < 0) { 
+			await msg.author.settings.update(`skills.${skillName}`, 0);
+		} else {
+			await msg.author.settings.update(`skills.${skillName}`, Math.floor(newXP));
+			await msg.reply( 'Currently Level: '+convertXPtoLVL(msg.author.settings.get(`skills.${skillName}`) as number));
+		}
+	}
+}
