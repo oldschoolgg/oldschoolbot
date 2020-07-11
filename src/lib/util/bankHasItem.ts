cfbbdc955a0def92c18aa78a2a0e9863979a110e
gc <gc@users.noreply.github.com>
2020-07-11 23:51:22 +1000

    delete most of the bank methods

--- src/lib/util/bankHasItem.ts
+++
@@ -1,5 +0,0 @@
-import { Bank } from '../types';
-
-export default function bankHasItem(itemBank: Bank, itemID: number, quantity = 1) {
-	return typeof itemBank[itemID] === 'number' && itemBank[itemID] >= quantity;
-}
