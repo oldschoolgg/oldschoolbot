23b771f2c1c08c59ffa5a4122de973532227b212
gc <gc@users.noreply.github.com>
2020-07-18 01:48:12 +1000

    fix

--- src/lib/util/createTupleOfItemsFromBank.ts
+++ src/lib/util/createTupleOfItemsFromBank.ts
@@ -8,6 +8,10 @@ export default async function createTupleOfItemsFromBank(client: KlasaClient, ba
 
 	for (const [itemID, qty] of Object.entries(bank)) {
 		const item = Items.get(parseInt(itemID));
+		if (!item) {
+			readableTuple.push([parseInt(itemID), qty, 73]);
+			continue;
+		}
 		readableTuple.push([item!.id, qty, (await client.fetchItemPrice(item!.id)) * qty]);
 	}
 
