7b60f9517c00cdc03424170f4a9649ce59509a2a
gc <gc@users.noreply.github.com>
2020-07-25 02:27:42 +1000

    only random tradeables

--- src/lib/simulation/mysteryBox.ts
+++ src/lib/simulation/mysteryBox.ts
@@ -1,11 +1,12 @@
 import Loot from 'oldschooljs/dist/structures/Loot';
 import { Items } from 'oldschooljs';
+import { Item } from 'oldschooljs/dist/meta/types';
 
 export const mysteryBox = (qty = 1) => {
 	const loot = new Loot();
 
 	for (let i = 0; i < qty; i++) {
-		loot.add(Items.random().id, 1);
+		loot.add(Items.filter(i => (i as Item).tradeable_on_ge).random().id, 1);
 	}
 
 	return loot.values();
