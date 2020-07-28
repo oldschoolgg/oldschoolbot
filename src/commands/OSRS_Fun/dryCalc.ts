af7aa1429431031ac3e52c9c28829fd0a02819d1
Lajnux <lajnuxxx@gmail.com>
2020-07-28 15:57:04 +0200

    fixed few changes to limit the usage input.

--- src/commands/OSRS_Fun/dryCalc.ts
+++ src/commands/OSRS_Fun/dryCalc.ts
@@ -1,27 +1,17 @@
 import { Command, CommandStore, KlasaMessage } from 'klasa';
 
-/* 
-  Witty Switch Comments Pulled From:
-  https://oldschool.runescape.wiki/w/Calculator:Dry_calc
-*/
 export default class extends Command {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			cooldown: 1,
 			description:
 				'Calculates the drop chance of getting an item within a set amount of rolls.',
-			usage: '<dropRate:int> <rolls:int>',
+			usage: '<dropRate:int{1,10000000}> <rolls:int{1,1000000}>',
 			usageDelim: ' '
 		});
 	}
 
-	async run(msg: KlasaMessage, [dropRate = 1, rolls = 1]: [number, number]) {
-		if (dropRate > 100000000 || rolls > 100000000) {
-			throw "I can't process a number higher than 100 million! enneUni has already claimed the only uncut onyx!";
-		}
-		if (dropRate < 1 || rolls < 1) {
-			throw "I can't calculate with zeros nor negative numbers. Do more rolls or update the drop values.";
-		}
+	async run(msg: KlasaMessage, [dropRate, rolls]: [number, number]) {
 		const noDropChance = Math.pow(1 - 1 / dropRate, rolls);
 		const dropChance = 100 * (1 - noDropChance);
 		const output = `${rolls} rolls were done for a drop with a 1/${dropRate} (${round(
