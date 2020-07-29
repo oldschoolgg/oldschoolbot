9ebec9175d9e512a475c2e0bad5addade9846ccf
GC <30398469+gc@users.noreply.github.com>
2020-07-29 21:27:12 +1000

    Command(Bank): Add support for passing multiple item names, runelite bank tags (#441)
    * add support for passing multiple item names, runelite bank tags
    
    * tests
    
    * improve tests and cache getOSItem
    
    Co-authored-by: gc <gc@users.noreply.github.com>

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,6 +1,20 @@
 import ava from 'ava';
-import { stripEmojis } from '../src/lib/util';
 
-ava('foo', test => {
-	test.deepEqual(stripEmojis('b👏r👏u👏h'), 'bruh');
+import { stripEmojis } from '../src/lib/util';
+import getOSItem from '../src/lib/util/getOSItem';
+
+ava('stripEmojis', test => {
+	test.is(stripEmojis('b👏r👏u👏h'), 'bruh');
+});
+
+ava('getOSItem', test => {
+	test.is(getOSItem('Twisted bow').id, 20997);
+	test.is(getOSItem(20997).id, 20997);
+	test.is(getOSItem('20997').id, 20997);
+
+	try {
+		getOSItem('Non-existant item');
+	} catch (err) {
+		test.is(err, 'That item doesnt exist.');
+	}
 });
