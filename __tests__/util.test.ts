b1a745e879a2f69d3c57c72c720ec4a3598c5d8e
gc <gc@users.noreply.github.com>
2020-07-29 20:45:38 +1000

    tests

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,6 +1,13 @@
 import ava from 'ava';
 import { stripEmojis } from '../src/lib/util';
+import getOSItem from '../src/lib/util/getOSItem';
 
-ava('foo', test => {
+ava('stripEmojis', test => {
 	test.deepEqual(stripEmojis('b👏r👏u👏h'), 'bruh');
+});
+
+ava('getOSItem', test => {
+	test.deepEqual(getOSItem('Twisted bow').id, 20997);
+	test.deepEqual(getOSItem(20997).id, 20997);
+	test.deepEqual(getOSItem('20997').id, 20997);
 });
