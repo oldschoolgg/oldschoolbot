0c6bea7caea118e7e5a4836727e5bd8a32091f55
gc <gc@users.noreply.github.com>
2020-07-29 20:41:10 +1000

    add support for passing multiple item names, runelite bank tags

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -4,9 +4,15 @@ import { Item } from 'oldschooljs/dist/meta/types';
 import cleanItemName from './cleanItemName';
 
 export default function getOSItem(itemName: string | number): Item {
-	const osItem = Items.get(typeof itemName === 'number' ? itemName : cleanItemName(itemName)) as
-		| Item
-		| undefined;
+	let identifier: string | number | undefined;
+	if (typeof itemName === 'number') {
+		identifier = itemName;
+	} else {
+		const parsed = parseInt(itemName);
+		identifier = isNaN(parsed) ? cleanItemName(itemName) : parsed;
+	}
+
+	const osItem = Items.get(identifier) as Item | undefined;
 	if (!osItem) throw `That item doesnt exist.`;
 	return osItem;
 }
