e9cfd70c33b6daced6e2f1552384eb6a0adb180c
coolbop32 <48160358+coolbop32@users.noreply.github.com>
2020-08-02 13:16:49 -0400

    fix

--- src/monitors/stagingGitHandler.ts
+++ src/monitors/stagingGitHandler.ts
@@ -1,6 +1,7 @@
 import { Monitor, MonitorStore, KlasaMessage, util } from 'klasa';
 
 import { Channel } from '../lib/constants';
+import { staging } from '../config';
 
 export default class extends Monitor {
 	public constructor(store: MonitorStore, file: string[], directory: string) {
@@ -11,6 +12,7 @@ export default class extends Monitor {
 			ignoreEdits: true,
 			ignoreSelf: true
 		});
+		this.enabled = staging;
 	}
 
 	async run(msg: KlasaMessage) {
