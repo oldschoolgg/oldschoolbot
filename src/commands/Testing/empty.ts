cc5245d32879245456514304c0060c8cbd376eb4
James Wallen-Jones <j.wallen.jones@googlemail.com>
2020-08-05 22:41:30 +0100

    move empty command

--- src/commands/Testing/empty.ts
+++ src/commands/Testing/empty.ts
@@ -8,9 +8,19 @@ export default class extends BotCommand {
 			aliases: ['clear', 'emptybank'],
 			usage: ''
 		});
+		this.enabled = !this.client.production;
 	}
 
 	async run(msg: KlasaMessage) {
+		// Make 100% sure this command can never be used in prod
+		if (
+			this.client.production ||
+			!this.client.user ||
+			this.client.user.id === '303730326692429825'
+		) {
+			return;
+		}
+
 		await msg.author.removeGP(msg.author.settings.get(UserSettings.GP));
 		for (const [itemID, qty] of Object.entries(msg.author.settings.get(UserSettings.Bank))) {
 			await msg.author.removeItemFromBank(parseInt(itemID), qty);
