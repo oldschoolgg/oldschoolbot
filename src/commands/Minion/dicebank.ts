b40912c35ace6146fff805dfe92edf3f9684ceb2
gc <gc@users.noreply.github.com>
2020-08-05 22:27:52 +1000

    add confirm, increase win chance

--- src/commands/Minion/dicebank.ts
+++ src/commands/Minion/dicebank.ts
@@ -3,6 +3,7 @@ import { CommandStore, KlasaMessage } from 'klasa';
 import { BotCommand } from '../../lib/BotCommand';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import { rand, sleep, multiplyBank } from '../../lib/util';
+import { Time } from '../../lib/constants';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -13,6 +14,25 @@ export default class extends BotCommand {
 	}
 
 	async run(msg: KlasaMessage) {
+		await msg.channel.send(
+			`${msg.author} please type \`confirm\` to confirm you want to dice your bank. If you lose, your entire bank and gp will be deleted. If you win, both will be doubled. There is a 55% chance of losing, 45% chance of winning.`
+		);
+		try {
+			await msg.channel.awaitMessages(
+				_msg =>
+					_msg.author.id === msg.author.id && _msg.content.toLowerCase() === 'confirm',
+				{
+					max: 1,
+					time: Time.Second * 15,
+					errors: ['time']
+				}
+			);
+		} catch (err) {
+			return msg.channel.send(
+				`${msg.author} has chickened out, and didn't confirm dicebank.`
+			);
+		}
+
 		const number = rand(1, 100);
 		await msg.author.settings.sync(true);
 		await msg.channel.send(
@@ -21,7 +41,7 @@ export default class extends BotCommand {
 		await sleep(5000);
 
 		const bank = msg.author.settings.get(UserSettings.Bank);
-		if (number < 60) {
+		if (number < 56) {
 			await msg.author.settings.reset(['bank', 'GP']);
 			return msg.channel.sendBankImage({
 				content: `You lose your entire bank! Heres an image for you to remember what you lost...`,
