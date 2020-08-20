a50a60675298bfab9e412022cfae1e67256df594
Lajnux <lajnuxxx@gmail.com>
2020-08-20 18:16:06 +0200

    getRandomItem now only returns tradeable_on_ge ite

--- src/lib/openables.ts
+++ src/lib/openables.ts
@@ -274,8 +274,8 @@ function getRandomItem(tradeables: boolean): number {
 		if (allItemsIDs.includes(i.id) || cantBeDropped.includes(i.id)) {
 			return false;
 		}
-
-		return (i as Item).tradeable === tradeables;
+		
+		return (i as Item).tradeable_on_ge === tradeables;
 	}).random().id;
 }
 
