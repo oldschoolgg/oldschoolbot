37d9ad0636336cd98e7d4a64d1c431ac9d0062a9
gc <gc@users.noreply.github.com>
2020-08-27 21:22:48 +1000

    t

--- src/tasks/minions/fishingActivity.ts
+++ src/tasks/minions/fishingActivity.ts
@@ -157,7 +157,9 @@ export default class extends Task {
 
 		const minutesInTrip = Math.ceil(duration / 1000 / 60);
 		for (let i = 0; i < minutesInTrip; i++) {
-			if (roll(6000)) {
+			const chance = user.hasItemEquippedAnywhere(itemID('Shelldon')) ? 10_000 : 7000;
+
+			if (roll(chance)) {
 				loot[itemID('Shelldon')] = 1;
 				str += `\n<:shelldon:748496988407988244> A crab steals your fish just as you catch it! After some talking, the crab, called Sheldon, decides to join you on your fishing adventures. You can equip Shelldon and he will help you fish!`;
 				break;
