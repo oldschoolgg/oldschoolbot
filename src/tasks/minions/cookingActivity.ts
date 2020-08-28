ab5b9b9117ce6d02cb0d7e29ec9d340539d0396a
gc <gc@users.noreply.github.com>
2020-08-28 22:30:24 +1000

    make remy/shelldon 2.5x more common

--- src/tasks/minions/cookingActivity.ts
+++ src/tasks/minions/cookingActivity.ts
@@ -72,9 +72,7 @@ export default class extends Task {
 
 		const minutesInTrip = Math.ceil(duration / 1000 / 60);
 		for (let i = 0; i < minutesInTrip; i++) {
-			const chance = user.hasItemEquippedAnywhere(itemID('Remy')) ? 10_000 : 7000;
-
-			if (roll(chance)) {
+			if (roll(3000)) {
 				loot[itemID('Remy')] = 1;
 				str += `\n<:remy:748491189925183638> A small rat notices you cooking, and tells you you're cooking it all wrong! He crawls into your bank to help you with your cooking. You can equip Remy for a boost to your cooking skills.`;
 				break;
--- src/tasks/minions/fishingActivity.ts
+++ src/tasks/minions/fishingActivity.ts
@@ -157,9 +157,7 @@ export default class extends Task {
 
 		const minutesInTrip = Math.ceil(duration / 1000 / 60);
 		for (let i = 0; i < minutesInTrip; i++) {
-			const chance = user.hasItemEquippedAnywhere(itemID('Shelldon')) ? 10_000 : 7000;
-
-			if (roll(chance)) {
+			if (roll(3000)) {
 				loot[itemID('Shelldon')] = 1;
 				str += `\n<:shelldon:748496988407988244> A crab steals your fish just as you catch it! After some talking, the crab, called Sheldon, decides to join you on your fishing adventures. You can equip Shelldon and he will help you fish!`;
 				break;
