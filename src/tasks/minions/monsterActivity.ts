19f50909f256d915727d2c17d9bbfcc4fdea88ce
gc <gc@users.noreply.github.com>
2020-09-03 23:52:02 +1000

    fix

--- src/tasks/minions/monsterActivity.ts
+++ src/tasks/minions/monsterActivity.ts
@@ -37,7 +37,7 @@ export default class extends Task {
 		const minutes = Math.ceil(duration / Time.Minute);
 		if (fullMonster.data.attributes.includes(MonsterAttribute.Dragon)) {
 			for (let i = 0; i < minutes; i++) {
-				if (roll(1500)) {
+				if (roll(2500)) {
 					gotKlik = true;
 					loot[itemID('Klik')] = 1;
 					break;
