ed969e98a4493c3f89033d232f88494c6987e070
Alexsuperfly <alexcsumner@gmail.com>
2020-09-04 12:07:34 -0400

    Add last man standing command (#499)
    * add last man standing fun simulation command
    
    * some work
    
    * add kill tracking
    
    * bold player names
    
    * update
    
    * maybe done
    
    * dumb comma
    
    * address review
    
    * stuff
    
    * parse contestants manually cuz klasa wont???
    
    * more text lines
    
    * shit eslint
    
    * change a death  message
    
    * add "last man standing" to header, remove underline
    
    * refactoring/fixes
    
    * change some messages
    
    * have to use 'auto'
    
    Co-authored-by: gc <gc@users.noreply.github.com>
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

--- src/tasks/minions/agilityActivity.ts
+++ src/tasks/minions/agilityActivity.ts
@@ -122,38 +122,9 @@ export default class extends Task {
 
 		await user.addItemsToBank(loot, true);
 
-<<<<<<< HEAD
 		handleTripFinish(this.client, user, channelID, str, res => {
 			user.log(`continued trip of ${quantity}x ${course.name} laps`);
 			return this.client.commands.get('laps')!.run(res, [quantity, course.aliases[0]]);
-=======
-		const channel = this.client.channels.get(channelID);
-		if (!channelIsSendable(channel)) return;
-
-		this.client.queuePromise(() => {
-			channel.send(str);
-
-			channel
-				.awaitMessages(mes => mes.author === user && saidYes(mes.content), {
-					time: getUsersPerkTier(user) > 1 ? Time.Minute * 10 : Time.Minute * 2,
-					max: 1
-				})
-				.then(messages => {
-					const response = messages.first();
-
-					if (response) {
-						if (response.author.minionIsBusy) return;
-
-						user.log(`continued trip of ${quantity}x ${course.name} laps`);
-
-						this.client.commands
-							.get('laps')!
-							.run(response as KlasaMessage, [quantity, course.aliases[0]])
-							.catch(err => channel.send(err));
-					}
-				})
-				.catch(noOp);
->>>>>>> b6851c1... Misc updates (#555)
 		});
 	}
 }
--- src/tasks/minions/questingActivity.ts
+++ src/tasks/minions/questingActivity.ts
@@ -46,20 +46,8 @@ export default class extends Task {
 				? undefined
 				: res => {
 						user.log(`continued trip of Questing.`);
-<<<<<<< HEAD
 						return this.client.commands.get('quest')!.run(res as KlasaMessage, []);
 				  }
 		);
-=======
-
-						this.client.commands
-							.get('quest')!
-							.run(response as KlasaMessage, [])
-							.catch(err => channel.send(err));
-					}
-				})
-				.catch(noOp);
-		});
->>>>>>> b6851c1... Misc updates (#555)
 	}
 }
