4e21a015ebcf37c472c5cc82355fc22fda7b8cda
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-09-04 10:45:45 -0300

    - Fixed 3rd age items not working for skilling, like 3rd age axe and pickaxe.

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -9,15 +9,12 @@ export default function getOSItem(itemName: string | number): Item {
 	if (cache.has(itemName)) {
 		return cache.get(itemName);
 	}
-
 	let identifier: string | number | undefined;
-	if (typeof itemName === 'number') {
-		identifier = itemName;
+	if (!Number(itemName)) {
+		identifier = cleanItemName(itemName as string);
 	} else {
-		const parsed = parseInt(itemName);
-		identifier = isNaN(parsed) ? cleanItemName(itemName) : parsed;
+		identifier = Number(itemName);
 	}
-
 	const osItem = Items.get(identifier) as Item | undefined;
 	if (!osItem) throw `That item doesnt exist.`;
 	cache.set(itemName, osItem);
