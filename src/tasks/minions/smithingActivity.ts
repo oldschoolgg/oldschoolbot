59dda4e174d351ac47e628011eaf2cfaab628b96
Lajnux <68188574+Lajnux@users.noreply.github.com>
2020-09-12 18:13:51 +0200

    Fix smithing continuation (#611)

--- src/tasks/minions/smithingActivity.ts
+++ src/tasks/minions/smithingActivity.ts
@@ -73,7 +73,7 @@ export default class extends Task {
 
 					this.client.commands
 						.get('smith')!
-						.run(response as KlasaMessage, [SmithedBar.name])
+						.run(response as KlasaMessage, [quantity, SmithedBar.name])
 						.catch(err => channel.send(err));
 				}
 			})
