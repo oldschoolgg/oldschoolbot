442af66f1eda2a3fd94b5b7ac8c7d75f1fa5540b
Kevin S. [Gidedin] <mbr.ark@gmail.com>
2020-09-15 13:56:54 -0300

    [BSO] Changed all occurrences of '+command' to '=command' (#635)
    * - Fixed 3rd age items not working for skilling, like 3rd age axe and pickaxe.
    
    * - Changed '+' to '=' whenever the bot suggests a command.
    
    * - Reverted unwanted changes.

--- src/tasks/minions/clueActivity.ts
+++ src/tasks/minions/clueActivity.ts
@@ -42,7 +42,7 @@ export default class extends Task {
 			clueTier.name
 		} clues. ${user.minionName} carefully places the reward casket${
 			quantity > 1 ? 's' : ''
-		} in your bank. You can open this casket using \`+open ${clueTier.name}\``;
+		} in your bank. You can open this casket using \`=open ${clueTier.name}\``;
 
 		let loot = { [clueTier.id]: quantity };
 		if (roll(10)) {
