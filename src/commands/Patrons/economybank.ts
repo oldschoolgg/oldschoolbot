66b0a52efa9a9cf2f89423b5b24962fd1e6f807f
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-09-15 12:48:46 -0300

    - Improved eBank.

--- src/commands/Patrons/economybank.ts
+++ src/commands/Patrons/economybank.ts
@@ -2,6 +2,7 @@ import { CommandStore, KlasaMessage } from 'klasa';
 
 import { BotCommand } from '../../lib/BotCommand';
 import { PerkTier, Time } from '../../lib/constants';
+import { ItemBank } from '../../lib/types';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -14,24 +15,19 @@ export default class extends BotCommand {
 	}
 
 	async run(msg: KlasaMessage) {
-		const banks: { [key: string]: number } = {};
-		let lastID = 0;
-		while (true) {
-			const res = (await this.client.query(
-				msg.flagArgs.im
-					? `SELECT id, bank FROM users WHERE id::BIGINT > ${lastID} AND "minion.ironman" = true order BY id::bigint asc limit 5000;`
-					: `SELECT id, bank FROM users WHERE id::BIGINT > ${lastID} order BY id::bigint asc limit 5000;`
-			)) as any[];
-			if (res.length === 0) {
-				break;
-			}
-			res.forEach(data => {
-				lastID = data.id;
-				Object.entries(data.bank).forEach((i: any) => {
-					banks[i[0]] = (banks[i[0]] ?? 0) + Number(i[1]);
-				});
-			});
-		}
-		return msg.channel.sendBankImage({ bank: banks, title: `Entire Economy Bank` });
+		const query = `select
+				json_object_agg(itemID, itemQTY)::jsonb as banks
+			 from (
+				select key as itemID, sum(value::int) as itemQTY
+				from users
+				cross join json_each_text(bank)
+				${msg.flagArgs.im ? ` where users."minion.ironman" = true ` : ``}
+				group by key
+			 ) s`;
+		const queryBank = await this.client.query<{ banks: ItemBank }[]>(query);
+		return msg.channel.sendBankImage({
+			bank: queryBank[0].banks,
+			title: `Entire Economy Bank`
+		});
 	}
 }
