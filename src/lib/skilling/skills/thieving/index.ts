a2404525a3be716e0c061d2e2628823339232271
gc <gc@users.noreply.github.com>
2020-09-19 02:46:33 +1000

    commit current work

---
+++ src/lib/skilling/skills/thieving/index.ts
@@ -0,0 +1,12 @@
+import { Emoji } from '../../../constants';
+import { SkillsEnum } from '../../types';
+import Pickpocketables from './pickpocketables';
+
+const Thieving = {
+	aliases: ['thieving', 'stealing'],
+	id: SkillsEnum.Agility,
+	emoji: Emoji.Agility,
+	Pickpocketables
+};
+
+export default Thieving;
---
+++ src/lib/skilling/skills/thieving/pickpocketables/index.ts
@@ -0,0 +1,155 @@
+import LootTable from 'oldschooljs/dist/structures/LootTable';
+
+import { ItemBank } from '../../../../types';
+
+interface Pickpocketable {
+	name: string;
+	level: number;
+	xp: number;
+	qpRequired?: number;
+	itemsRequired?: ItemBank;
+	table?: LootTable;
+}
+
+const HamTable =new LootTable()
+.add('Bronze arrow', 1–13, 3/100)
+.add('Bronze axe', 1, 3/100)
+.add('Bronze dagger', 1, 3/100)
+.add('Bronze pickaxe', 1, 3/100)
+.add('Iron axe', 1, 3/100)
+.add('Iron dagger', 1, 3/100)
+.add('Iron pickaxe', 1, 3/100)
+.add('Leather body', 1, 3/100)
+.add('Steel arrow', 1–13, 2/100)
+.add('Steel axe', 1, 2/100)
+.add('Steel dagger', 1, 2/100)
+.add('Steel pickaxe', 1, 2/100)
+.add('Ham boots', 1, 1/100)
+.add('Ham cloak', 1, 1/100)
+.add('Ham gloves', 1, 1/100)
+.add('Ham hood', 1, 1/100)
+.add('Ham logo', 1, 1/100)
+.add('Ham robe', 1, 1/100)
+.add('Ham shirt', 1, 1/100)
+
+const Pickpocketables: Pickpocketable[] = [
+	{
+		name: 'Man',
+		level: 1,
+		xp: 8,
+		table: new LootTable().add('Coins', 3)
+	},
+	{
+		name: 'Woman',
+		level: 1,
+		xp: 8,
+		table: new LootTable().add('Coins', 3)
+	},
+	{
+		name: 'Female H.A.M. member',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Male H.A.M. member',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Warrior woman',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Al-Kharid warrior',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Rogue',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Cave goblin',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Master Farmer',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Guard',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Fremennik Citizen',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Bearded Pollnivnian Bandit',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Desert Bandit',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Knight of Ardougne',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Pollnivian Bandit',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Yanille Watchman',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Menaphite Thug',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Paladin',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Gnome',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Hero',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Vyre',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'Elf',
+		level: 1,
+		xp: 8
+	},
+	{
+		name: 'TzHaar-Hur',
+		level: 1,
+		xp: 8
+	}
+];
+
+export default Pickpocketables;
