d464d249b2f8ec9b5ba8a4f100fe93c967219a82
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-09-22 13:29:53 -0300

    Adds monitor to remove all non-mass/pingmass messages mass-bso

---
+++ src/monitors/mass.ts
@@ -0,0 +1,27 @@
+import { KlasaMessage, Monitor, MonitorStore } from 'klasa';
+
+import { SupportServer } from '../lib/constants';
+
+export default class extends Monitor {
+	public constructor(store: MonitorStore, file: string[], directory: string) {
+		super(store, file, directory, { enabled: true, ignoreOthers: false });
+		this.enabled = true;
+	}
+
+	async run(msg: KlasaMessage) {
+		if (
+			!msg.guild ||
+			msg.guild.id !== SupportServer ||
+			msg.channel.id !== '757683928751407224'
+		) {
+			return;
+		}
+		if (
+			['mass', 'pingmass'].every(
+				str => !msg.content.toLowerCase().includes(`${this.client.options.prefix}${str}`)
+			)
+		) {
+			await msg.delete();
+		}
+	}
+}
