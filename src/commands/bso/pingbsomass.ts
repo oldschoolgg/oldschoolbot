18e1ff0845932393a029b77b281381c6bf39f141
gc <gc@users.noreply.github.com>
2020-09-27 14:55:12 +1000

    add pingbsomass

---
+++ src/commands/bso/pingbsomass.ts
@@ -0,0 +1,24 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { Roles, SupportServer } from '../../lib/constants';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			cooldown: 60 * 5
+		});
+		this.enabled = this.client.production;
+	}
+
+	async run(msg: KlasaMessage) {
+		if (!msg.guild || msg.guild.id !== SupportServer) return;
+		if (!msg.member) return;
+		if (!msg.member.roles.has('759572886364225558') && !msg.member.roles.has(Roles.Moderator)) {
+			return;
+		}
+		return msg.send(
+			`<@&759573020464906242> - *Note: You can type \`.roles bso mass\` to remove, or add, this role to yourself.`
+		);
+	}
+}
