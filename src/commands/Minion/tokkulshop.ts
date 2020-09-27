361832c4c927a046601903bb00678ea5443e898c
Kevin S. [Gidedin] <mbr.ark@gmail.com>
2020-09-26 21:06:11 -0300

    Add Tokkul Shop (#654)
    * Adds Tokkul Shop (+tokkulshop/+tks)
    
    * Allows onyx bolt tips to be bought
    
    * Changed tokkulshop.ts to look for Jad KC instead of fire cape in collection
    Removed unnecessary property from tokkulBuyables.ts
    Added comment saying the prices are using karamja gloves 1
    
    * Fixes buy glitch
    Fixes aliases
    Adds more aliases
    
    * Fixes jad kc check
    
    * Fixes spelling error
    Changes prices to normal value
    
    * Fixes spelling errors
    Changes prices to normal value
    Changed TokkulShop const and type to TokkulShopItem
    
    * Enforces fire cape only for Obsidian Armour
    
    * Changes throw to return send.msg
    
    * Changes a to an on the fire cape error message
    
    * Fix item name when fire cape is required
    
    * Fix type of transaction when fire cape is needed

---
+++ src/commands/Minion/tokkulshop.ts
@@ -0,0 +1,131 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Monsters } from 'oldschooljs';
+import { addBanks, bankHasAllItemsFromBank, removeBankFromBank } from 'oldschooljs/dist/util';
+
+import { BotCommand } from '../../lib/BotCommand';
+import TokkulShopItem from '../../lib/buyables/tokkulBuyables';
+import { Time } from '../../lib/constants';
+import { UserSettings } from '../../lib/settings/types/UserSettings';
+import { Bank } from '../../lib/types';
+import { stringMatches } from '../../lib/util';
+import createReadableItemListFromBank from '../../lib/util/createReadableItemListFromTuple';
+import itemID from '../../lib/util/itemID';
+
+const { TzTokJad } = Monsters;
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			usage: '<buy|sell> [quantity:integer{1}] <item:...string>',
+			usageDelim: ' ',
+			oneAtTime: true,
+			cooldown: 5,
+			altProtection: true,
+			aliases: ['tks']
+		});
+	}
+
+	async run(msg: KlasaMessage, [type = 'buy', quantity, name]: ['buy' | 'sell', number, string]) {
+		await msg.author.settings.sync(true);
+
+		const userBank = msg.author.settings.get(UserSettings.Bank);
+
+		const shopInventory = TokkulShopItem.find(
+			item =>
+				stringMatches(name, item.name) ||
+				(item.aliases && item.aliases.some(alias => stringMatches(alias, name)))
+		);
+
+		if (!shopInventory) {
+			return msg.send(
+				`I don't recognize that item JalYt, here are my wares: ${TokkulShopItem.map(
+					item => {
+						return item.name;
+					}
+				).join(', ')}.`
+			);
+		}
+
+		if (shopInventory.requireFireCape && msg.author.getKC(TzTokJad) < 1) {
+			return msg.send(
+				`You are not worthy JalYt. Before you can ${type} an ${shopInventory.name}, you need to have defeated the might TzTok-Jad!`
+			);
+		}
+
+		if (!shopInventory.tokkulCost && type === 'buy') {
+			return msg.send(
+				`I am sorry JalYt, but I can't sell you that. Here are the items I can sell: ${TokkulShopItem.map(
+					item => {
+						if (item.tokkulReturn) return item.name;
+					}
+				).join(', ')}.`
+			);
+		}
+
+		if (quantity === undefined) {
+			quantity = type === 'sell' ? userBank[shopInventory.inputItem] : 1;
+		}
+
+		let outItems: Bank;
+		let inItems: Bank;
+		let itemString: string;
+		let inItemString: string;
+		if (type === 'buy') {
+			outItems = { [itemID('Tokkul')]: quantity * shopInventory.tokkulCost! };
+			inItems = { [shopInventory.inputItem]: quantity };
+			itemString = await createReadableItemListFromBank(this.client, outItems);
+			inItemString = await createReadableItemListFromBank(this.client, inItems);
+		} else {
+			outItems = { [shopInventory.inputItem]: quantity };
+			inItems = { [itemID('Tokkul')]: quantity * shopInventory.tokkulReturn };
+			itemString = await createReadableItemListFromBank(this.client, inItems);
+			inItemString = await createReadableItemListFromBank(this.client, outItems);
+		}
+
+		if (!bankHasAllItemsFromBank(userBank, outItems)) {
+			if (type === 'buy') {
+				return msg.send(
+					`I am sorry JalYt, but you don't have enough tokkul for that. You need **${itemString}** to buy **${inItemString}**.`
+				);
+			}
+			return msg.send(
+				`I am sorry JalYt, but you don't have enough items for that. You need **${itemString}** to sell for **${inItemString}**.`
+			);
+		}
+
+		if (!msg.flagArgs.cf && !msg.flagArgs.confirm) {
+			const sellMsg = await msg.channel.send(
+				`${msg.author}, JalYt, say \`confirm\` to confirm that you want to ${
+					type === 'buy' ? 'buy' : 'sell'
+				} **${inItemString}** for **${itemString}**.`
+			);
+
+			// Confirm the user wants to buy
+			try {
+				await msg.channel.awaitMessages(
+					_msg =>
+						_msg.author.id === msg.author.id &&
+						_msg.content.toLowerCase() === 'confirm',
+					{
+						max: 1,
+						time: Time.Second * 15,
+						errors: ['time']
+					}
+				);
+			} catch (err) {
+				return sellMsg.edit(
+					`Cancelling ${type === 'buy' ? 'purchase' : 'sale'} of **${inItemString}**.`
+				);
+			}
+		}
+
+		await msg.author.settings.update(
+			UserSettings.Bank,
+			addBanks([inItems, removeBankFromBank(userBank, outItems)])
+		);
+
+		return msg.send(
+			`You ${type === 'buy' ? 'bought' : 'sold'} **${inItemString}** for **${itemString}**.`
+		);
+	}
+}
