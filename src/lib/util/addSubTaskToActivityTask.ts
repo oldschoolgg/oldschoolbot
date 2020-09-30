a6f83c4c4ec44aa6877a15ed9a83371b9095fcaf
gc <gc@users.noreply.github.com>
2020-09-30 13:58:47 +1000

    undo

--- src/lib/util/addSubTaskToActivityTask.ts
+++ src/lib/util/addSubTaskToActivityTask.ts
@@ -29,7 +29,7 @@ export default function addSubTaskToActivityTask<T extends ActivityTaskOptions>(
 
 	const newSubtask: ActivityTaskOptions = {
 		...subTaskToAdd,
-		finishDate: Date.now() + 1,
+		finishDate: Date.now() + subTaskToAdd.duration,
 		id: uuid()
 	};
 
