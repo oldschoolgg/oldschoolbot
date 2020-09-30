496c476bdb3b847f03e8d0c60c6da90ef4d6af24
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-09-30 09:28:15 -0300

    Separate decant into its own function Add decant tests

--- src/commands/Minion/decant.ts
+++ src/commands/Minion/decant.ts
@@ -1,10 +1,8 @@
 import { CommandStore, KlasaMessage } from 'klasa';
 
 import { BotCommand } from '../../lib/BotCommand';
-import Potions from '../../lib/minions/data/potions';
+import decantPotionFromBank from '../../lib/minions/functions/decantPotionFromBank';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
-import { ItemBank } from '../../lib/types';
-import { addBanks, removeBankFromBank, stringMatches } from '../../lib/util';
 import createReadableItemListFromTuple from '../../lib/util/createReadableItemListFromTuple';
 
 export default class extends BotCommand {
@@ -18,47 +16,21 @@ export default class extends BotCommand {
 	}
 
 	async run(msg: KlasaMessage, [dose = 4, itemName]: [1 | 2 | 3 | 4, string]) {
-		const potionToDecant = Potions.find(pot => stringMatches(pot.name, itemName));
-		if (!potionToDecant) {
-			throw `You can't decant that!`;
-		}
-
 		await msg.author.settings.sync(true);
 		const userBank = msg.author.settings.get(UserSettings.Bank);
-		const potionsBank: ItemBank = {};
-		const newPotions: ItemBank = {};
-		let sumOfPots = 0;
-		let totalDosesToCreate = 0;
 
-		for (let i = 0; i < potionToDecant.items.length; i++) {
-			if (i === dose - 1) continue;
-			potionsBank[potionToDecant.items[i]] = userBank[potionToDecant.items[i]] ?? 0;
-			sumOfPots += potionsBank[potionToDecant.items[i]];
-			totalDosesToCreate += potionsBank[potionToDecant.items[i]] * (i + 1);
-		}
-
-		if (!totalDosesToCreate) {
-			throw `You don't have any **${potionToDecant.name}** to decant!`;
-		}
-
-		const newPotionDoseRequested = Math.floor(totalDosesToCreate / dose);
-		const leftOverDoses = totalDosesToCreate % dose;
-		if (newPotionDoseRequested) {
-			newPotions[potionToDecant.items[dose - 1]] = newPotionDoseRequested;
-		}
-		if (leftOverDoses) {
-			newPotions[potionToDecant.items[leftOverDoses - 1]] = 1;
-		}
-
-		const resultString = await createReadableItemListFromTuple(this.client, newPotions);
-
-		await msg.author.settings.update(
-			UserSettings.Bank,
-			addBanks([newPotions, removeBankFromBank(userBank, potionsBank)])
+		const { potionsToAdd, sumOfPots, potionName, finalUserBank } = decantPotionFromBank(
+			userBank,
+			itemName,
+			dose
 		);
 
+		const resultString = await createReadableItemListFromTuple(this.client, potionsToAdd);
+
+		await msg.author.settings.update(UserSettings.Bank, finalUserBank);
+
 		return msg.send(
-			`You decanted **${sumOfPots}x ${potionToDecant.name}${
+			`You decanted **${sumOfPots}x ${potionName}${
 				sumOfPots > 0 ? 's' : ''
 			}** into **${resultString}**.`
 		);
