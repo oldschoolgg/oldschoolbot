ff29f466c5ba5b087385b7c7206f3ab8bcb28b47
gc <gc@users.noreply.github.com>
2020-10-02 09:51:40 +1000

    add uniques to sarachnis

--- src/lib/minions/data/killableMonsters/index.ts
+++ src/lib/minions/data/killableMonsters/index.ts
@@ -328,6 +328,12 @@ const killableMonsters: KillableMonster[] = [
 		levelRequirements: {
 			prayer: 43
 		},
+		uniques: resolveItems([
+			'Sraracha',
+			'Jar of eyes',
+			'Giant egg sac(full)',
+			'Sarachnis cudgel'
+		]),
 		healAmountNeeded: 9 * 20,
 		attackStyleToUse: GearSetupTypes.Melee,
 		attackStylesUsed: [GearStat.AttackStab, GearStat.AttackRanged],
