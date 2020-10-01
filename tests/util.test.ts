4f7f6354c20b330edb98cc2c81ff4e3de09e70b0
Kevin S. [Gidedin] <mbr.ark@gmail.com>
2020-09-30 23:21:32 -0300

    Add getUserFoodFromBank test case (#762)

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,3 +1,6 @@
+import { itemID } from 'oldschooljs/dist/util';
+
+import getUserFoodFromBank from '../src/lib/minions/functions/getUserFoodFromBank';
 import { stripEmojis } from '../src/lib/util';
 import getOSItem from '../src/lib/util/getOSItem';
 
@@ -13,5 +16,25 @@ describe('util', () => {
 		expect(getOSItem('3rd age platebody').id).toEqual(10348);
 
 		expect(() => getOSItem('Non-existant item')).toThrowError('That item doesnt exist.');
+	});
+
+	test('getUserFoodFromBank', () => {
+		expect(getUserFoodFromBank({ [itemID('Shark')]: 1 }, 500)).toStrictEqual(false);
+		expect(getUserFoodFromBank({ [itemID('Shark')]: 100 }, 500)).toStrictEqual({
+			[itemID('Shark')]: 25
+		});
+		expect(
+			getUserFoodFromBank({ [itemID('Shark')]: 30, [itemID('Tuna')]: 20 }, 750)
+		).toStrictEqual({
+			[itemID('Tuna')]: 20,
+			[itemID('Shark')]: 28
+		});
+		// Shrimps is not an eatable so it is not used
+		expect(
+			getUserFoodFromBank(
+				{ [itemID('Shark')]: 100, [itemID('Lobster')]: 20, [itemID('Shrimps')]: 50 },
+				1600
+			)
+		).toStrictEqual({ [itemID('Lobster')]: 20, [itemID('Shark')]: 68 });
 	});
 });
