c15395f4d9f5ca1a94311cfe068391e7dab1d190
Lajnux <lajnuxxx@gmail.com>
2020-10-02 20:09:59 +0200

    activities

---
+++ src/tasks/minions/minigames/warriorsGuild/animatedArmourActivity.ts
@@ -0,0 +1,100 @@
+import { Task } from 'klasa';
+
+import { AnimatedArmourActivityTaskOptions } from '../../../lib/types/minions';
+import { channelIsSendable } from '../../../lib/util/channelIsSendable';
+import { noOp, roll } from '../../../lib/util';
+import { Armours } from '../../../commands/Minion/warriorsguild';
+import itemID from '../../../lib/util/itemID';
+
+export default class extends Task {
+	async run({
+		armourID,
+		userID,
+		channelID,
+		quantity,
+		duration
+	}: AnimatedArmourActivityTaskOptions) {
+		const user = await this.client.users.fetch(userID);
+		user.incrementMinionDailyDuration(duration);
+		const channel = await this.client.channels.fetch(channelID).catch(noOp);
+		const armour = Armours.find(armour => armour.name === armourID);
+		let str = '';
+
+		if (!armour) {
+			throw `WTF!`;
+		}
+
+		const fullhelm = armour.name.concat(' full helm');
+		const platelegs = armour.name.concat(' platelegs');
+		const platebody = armour.name.concat(' platebody');
+
+		if (armour.breakChance1) {
+			let killsBeforeBreak = 0;
+			for (let i = 0; i < quantity; i++) {
+				if (roll(armour.breakChance1)) {
+					break;
+				}
+				killsBeforeBreak++;
+			}
+			if (killsBeforeBreak !== quantity) {
+				str = `${user}, ${user.minionName} finished killing ${killsBeforeBreak}x animated ${
+					armour.name
+				} armour before a armour piece broke and received ${killsBeforeBreak *
+					armour.tokens}x Warrior guild tokens.`;
+				const loot = {
+					8851: killsBeforeBreak * armour.tokens
+				};
+
+				await user.addItemsToBank(loot, true);
+
+				str += `\nRemoved the following broken armour from bank: `;
+				if (armour.name === 'bronze') {
+					str += platebody;
+					await user.removeItemFromBank(itemID(platebody), 1);
+				}
+				if (armour.name === 'iron') {
+					str += platelegs;
+					await user.removeItemFromBank(itemID(platelegs), 1);
+				}
+				if (armour.name === 'steel') {
+					str += fullhelm;
+					await user.removeItemFromBank(itemID(fullhelm), 1);
+				}
+				if (armour.breakChance3) {
+					if (roll(armour.breakChance3)) {
+						str += `, ${fullhelm}`;
+						await user.removeItemFromBank(itemID(fullhelm), 1);
+					}
+					if (roll(armour.breakChance2)) {
+						str += `, ${platelegs}`;
+						await user.removeItemFromBank(itemID(platelegs), 1);
+					}
+				}
+			} else {
+				str = `${user}, ${user.minionName} finished killing ${quantity}x animated ${
+					armour.name
+				} armour and received ${killsBeforeBreak * armour.tokens}x Warrior guild tokens.`;
+				const loot = {
+					8851: killsBeforeBreak * armour.tokens
+				};
+
+				await user.addItemsToBank(loot, true);
+			}
+		} else {
+			str = `${user}, ${user.minionName} finished killing ${quantity}x animated ${
+				armour.name
+			} armour and received ${quantity * armour.tokens}x Warrior guild tokens.`;
+			const loot = {
+				8851: quantity * armour.tokens
+			};
+
+			await user.addItemsToBank(loot, true);
+		}
+
+		// TODO: Combat calculations in future depending on HP
+
+		if (!channelIsSendable(channel)) return;
+
+		return channel.send(str);
+	}
+}
---
+++ src/tasks/minions/minigames/warriorsGuild/cyclopsActivity.ts
@@ -0,0 +1,119 @@
+import { Task } from 'klasa';
+import { MessageAttachment } from 'discord.js';
+
+import { Events } from '../../../lib/constants';
+import { noOp, bankHasItem, roll, addItemToBank, removeItemFromBank } from '../../../lib/util';
+import itemID from '../../../lib/util/itemID';
+import killableMonsters from '../../../lib/minions/data/killableMonsters';
+import { CyclopsActivityTaskOptions } from '../../../lib/types/minions';
+import { UserSettings } from '../../../lib/settings/types/UserSettings';
+import { channelIsSendable } from '../../../lib/util/channelIsSendable';
+import announceLoot from '../../../lib/minions/functions/announceLoot';
+
+const defenders = [
+	{
+		itemID: itemID('Dragon defender'),
+		rollChance: 100
+	},
+	{
+		itemID: itemID('Rune defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Adamant defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Mithril defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Black defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Steel defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Iron defender'),
+		rollChance: 50
+	},
+	{
+		itemID: itemID('Bronze defender'),
+		rollChance: 50
+	}
+];
+
+export default class extends Task {
+	async run({ minigameID, userID, channelID, quantity, duration }: CyclopsActivityTaskOptions) {
+		const monster = killableMonsters.find(mon => mon.id === minigameID)!;
+		const user = await this.client.users.fetch(userID);
+		const userBank = user.settings.get(UserSettings.Bank);
+		const channel = await this.client.channels.fetch(channelID).catch(noOp);
+		user.incrementMinionDailyDuration(duration);
+
+		const logInfo = `MonsterID[${minigameID}] userID[${userID}] channelID[${channelID}] quantity[${quantity}]`;
+
+		let loot = monster.table.kill(quantity);
+		let position = 0;
+		for (const defender of defenders) {
+			if (bankHasItem(userBank, defender.itemID, 1)) {
+				for (let i = 0; i < quantity; i++) {
+					if (position > 0) {
+						if (roll(defenders[position - 1].rollChance)) {
+							loot = addItemToBank(loot, defenders[position - 1].itemID);
+						}
+					} else if (roll(defender.rollChance)) {
+						loot = addItemToBank(loot, defender.itemID);
+					}
+				}
+				break;
+			}
+			position++;
+		}
+		if (!bankHasItem(userBank, defenders[defenders.length - 1].itemID, 1)) {
+			for (let i = 0; i < quantity; i++) {
+				if (roll(defenders[defenders.length - 1].rollChance)) {
+					loot = addItemToBank(loot, defenders[defenders.length - 1].itemID);
+				}
+			}
+		}
+		// Remove Catacomb totem pieces
+		loot = removeItemFromBank(loot, 19677, user.numItemsInBankSync(19677));
+		loot = removeItemFromBank(loot, 19679, user.numItemsInBankSync(196779));
+		loot = removeItemFromBank(loot, 19681, user.numItemsInBankSync(19681));
+		loot = removeItemFromBank(loot, 19683, user.numItemsInBankSync(19683));
+
+		announceLoot(this.client, user, monster, quantity, loot);
+
+		await user.addItemsToBank(loot, true);
+
+		const image = await this.client.tasks
+			.get('bankImage')!
+			.generateBankImage(
+				loot,
+				`Loot From ${quantity} ${monster.name}:`,
+				true,
+				{ showNewCL: 1 },
+				user
+			);
+
+		this.client.emit(
+			Events.Log,
+			`${user.username}[${user.id}] received Minion Loot - ${logInfo}`
+		);
+
+		const str = `${user}, ${user.minionName} finished killing ${quantity} ${
+			monster.name
+		}. Your ${monster.name} KC is now ${(user.settings.get(UserSettings.MonsterScores)[
+			monster.id
+		] ?? 0) + quantity}.`;
+
+		user.incrementMonsterScore(minigameID, quantity);
+
+		if (!channelIsSendable(channel)) return;
+
+		return channel.send(str, new MessageAttachment(image));
+	}
+}
