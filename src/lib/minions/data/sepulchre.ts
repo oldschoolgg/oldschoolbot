c1309a57220e63540ce93a6d1309b19ac6cd1f4c
gc <gc@users.noreply.github.com>
2020-10-05 00:57:55 +1100

    add sepulchreFloors data

---
+++ src/lib/minions/data/sepulchre.ts
@@ -0,0 +1,39 @@
+import { Time } from '../../constants';
+
+export const sepulchreFloors = [
+	{
+		number: 1,
+		petChance: 35_000,
+		agilityLevel: 52,
+		xp: 500,
+		time: Time.Minute * 0.9
+	},
+	{
+		number: 2,
+		petChance: 16_000,
+		agilityLevel: 62,
+		xp: 850,
+		time: Time.Minute * 1.1
+	},
+	{
+		number: 3,
+		petChance: 8000,
+		agilityLevel: 72,
+		xp: 1425,
+		time: Time.Minute * 1.5
+	},
+	{
+		number: 4,
+		petChance: 4000,
+		agilityLevel: 82,
+		xp: 2625,
+		time: Time.Minute * 2
+	},
+	{
+		number: 5,
+		petChance: 2000,
+		agilityLevel: 92,
+		xp: 5850,
+		time: Time.Minute * 4
+	}
+];
