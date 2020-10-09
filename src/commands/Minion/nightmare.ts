82180767594b59e24c49d211c750ca1ad057ef0d
Andre Da Silva <ard35@njit.edu>
2020-10-09 09:47:38 -0400

    Allow Specific Mass Size for Nightmare
    - Add functionality for a specific mass size from 2 to 10 (including leader).
    - Add a timeout feature to start mass so it passes the debounce and processes users accordingly.

--- src/commands/Minion/nightmare.ts
+++ src/commands/Minion/nightmare.ts
@@ -47,7 +47,7 @@ const inquisitorItems = resolveItems([
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
-			usage: '<mass|solo>',
+			usage: '<mass|solo> [maximumSize:int{2,10}]',
 			usageDelim: ' ',
 			oneAtTime: true,
 			altProtection: true,
@@ -84,14 +84,17 @@ export default class extends BotCommand {
 
 	@minionNotBusy
 	@requiresMinion
-	async run(msg: KlasaMessage, [type]: ['mass' | 'solo']) {
+	async run(msg: KlasaMessage, [type, maximumSizeForParty]: ['mass' | 'solo', number]) {
 		this.checkReqs([msg.author], NightmareMonster, 2);
+
+		let maximumSize = 10;
+		if (type === 'mass' && maximumSizeForParty) maximumSize = maximumSizeForParty;
 
 		const partyOptions: MakePartyOptions = {
 			leader: msg.author,
-			minSize: 2,
-			maxSize: 10,
-			message: `${msg.author.username} is doing a ${NightmareMonster.name} mass! Anyone can click the ${Emoji.Join} reaction to join, click it again to leave.`,
+			minSize: 1,
+			maxSize: maximumSize - 1,
+			message: `${msg.author.username} is doing a ${NightmareMonster.name} mass! Anyone can click the ${Emoji.Join} reaction to join, click it again to leave. The maximum size for this mass is ${maximumSize}.`,
 			customDenier: user => {
 				if (!user.hasMinion) {
 					return [true, "you don't have a minion."];
