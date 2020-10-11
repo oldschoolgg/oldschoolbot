e25d21b12705372c37f7a8b41097f1cf7ae5ce54
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-10-11 09:42:07 -0300

    Fix merge changes

--- src/commands/Minion/sawmill.ts
+++ src/commands/Minion/sawmill.ts
@@ -2,7 +2,7 @@ import { CommandStore, KlasaMessage } from 'klasa';
 
 import { BotCommand } from '../../lib/BotCommand';
 import { Activity, Tasks, Time } from '../../lib/constants';
-import hasGracefulEquipped from '../../lib/gear/functions/hasGracefulEquipped';
+import { hasGracefulEquipped } from '../../lib/gear/functions/hasGracefulEquipped';
 import { Planks } from '../../lib/minions/data/planks';
 import { minionNotBusy, requiresMinion } from '../../lib/minions/decorators';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
