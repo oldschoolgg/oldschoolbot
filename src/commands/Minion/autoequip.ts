69ebb608270ba95f642aa02b0282987a351318cb
Kevin S. [Gidedin] <mbr.ark@gmail.com>
2020-10-11 01:57:49 -0300

    Add +autoequip (#711)
    * Working on autoequip
    
    * Add autoequip
    
    * Fix glitch that was removing more items than needed from user bank
    
    * Fix deepscan
    
    * Working on autoequip
    
    * Add an an extra sorting for gear score, in case both items have the same primary stat
    Add unit testing
    
    * Remove console.log, toRemoveFromGear and toRemoveFromBank from autoequip.ts
    Change getItemScore to handle all stats that are numbers
    Fix test case to avoid fetching items all the time
    
    * Remove some null item check where they are impossible to be null
    
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

---
+++ src/commands/Minion/autoequip.ts
@@ -0,0 +1,60 @@
+import { MessageAttachment } from 'discord.js';
+import { CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { GearTypes } from '../../lib/gear';
+import { generateGearImage } from '../../lib/gear/functions/generateGearImage';
+import resolveGearTypeSetting from '../../lib/gear/functions/resolveGearTypeSetting';
+import { requiresMinion } from '../../lib/minions/decorators';
+import minionNotBusy from '../../lib/minions/decorators/minionNotBusy';
+import getUserBestGearFromBank from '../../lib/minions/functions/getUserBestGearFromBank';
+import { UserSettings } from '../../lib/settings/types/UserSettings';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			altProtection: true,
+			oneAtTime: true,
+			cooldown: 1,
+			usage:
+				'<melee|mage|range> <attack|defence> <crush|slash|stab|ranged|magic> [prayer|strength]',
+			usageDelim: ' ',
+			aliases: ['aep', 'aequip']
+		});
+	}
+
+	@minionNotBusy
+	@requiresMinion
+	async run(
+		msg: KlasaMessage,
+		[gearType, type, style, extra = null]: [
+			GearTypes.GearSetupTypes,
+			string,
+			string,
+			string | null
+		]
+	) {
+		await msg.author.settings.sync(true);
+		const { gearToEquip, userFinalBank } = getUserBestGearFromBank(
+			msg.author.settings.get(UserSettings.Bank),
+			msg.author.rawGear()[gearType],
+			gearType,
+			type,
+			style,
+			extra
+		);
+		await msg.author.settings.update(UserSettings.Bank, userFinalBank);
+		await msg.author.settings.update(resolveGearTypeSetting(gearType), gearToEquip);
+		const image = await generateGearImage(
+			this.client,
+			msg.author,
+			msg.author.settings.get(resolveGearTypeSetting(gearType)),
+			gearType,
+			msg.author.settings.get(UserSettings.Minion.EquippedPet)
+		);
+		return msg.send(
+			`You auto-equipped your best ${style} stat gear for ${type} in your ${gearType} preset.`,
+			new MessageAttachment(image, 'osbot.png')
+		);
+	}
+}
