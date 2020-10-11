eed5930062866c98381f799f673f5f810e4eac31
Kevin Santiago Mendes <mbr.ark@gmail.com>
2020-10-11 09:36:38 -0300

    Fix missed import

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -6,7 +6,7 @@ import { Activity, Emoji, Time } from '../../lib/constants';
 import ClueTiers from '../../lib/minions/data/clueTiers';
 import killableMonsters from '../../lib/minions/data/killableMonsters';
 import { MinigameIDsEnum } from '../../lib/minions/data/minigames';
-import Planks from '../../lib/minions/data/planks';
+import { Planks } from '../../lib/minions/data/planks';
 import { GroupMonsterActivityTaskOptions } from '../../lib/minions/types';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import Agility from '../../lib/skilling/skills/agility';
