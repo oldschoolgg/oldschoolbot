d7cd54e64c1b5f3c6ada8272fe52c7fbc94701d5
gc <gc@users.noreply.github.com>
2020-10-13 00:57:22 +1100

    remove twitchnotifs/streamers

--- src/lib/settings/schemas/GuildSchema.ts
+++ src/lib/settings/schemas/GuildSchema.ts
@@ -6,9 +6,7 @@ Client.defaultGuildSchema
 	.add('joyReactions', 'textchannel', { default: null })
 	.add('totalCommandsUsed', 'integer', { default: 0 })
 	.add('petchannel', 'textchannel', { default: null })
-	.add('streamers', 'string', { array: true, default: [] })
 	.add('streamertweets', 'textchannel', { default: null })
 	.add('tweetchannel', 'textchannel', { default: null })
-	.add('twitchnotifs', 'textchannel', { default: null })
 	.add('levelUpMessages', 'textchannel', { default: null })
 	.add('staffOnlyChannels', 'textchannel', { array: true, default: [] });
