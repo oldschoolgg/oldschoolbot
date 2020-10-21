a05f19703095536ba040e1576e882947592dd07e
gc <gc@users.noreply.github.com>
2020-10-22 09:22:47 +1100

    fix

--- src/lib/minions/functions/tickerTaskFromActivity.ts
+++ src/lib/minions/functions/tickerTaskFromActivity.ts
@@ -33,6 +33,7 @@ export function tickerTaskFromActivity(type: Activity): Tasks {
 		case Activity.Nightmare:
 		case Activity.Sepulchre:
 		case Activity.TrickTreat:
+		case Activity.Zalcano:
 			return Tasks.MinigameTicker;
 		default: {
 			throw new Error(`Unrecognized activity`);
