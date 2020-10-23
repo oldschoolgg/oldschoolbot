88de69a2d986dd08e3489202d62055139b3f508d
gc <gc@users.noreply.github.com>
2020-10-23 11:15:37 +1100

    give rc/smithing/mining xp

--- src/tasks/minions/minigames/zalcanoActivity.ts
+++ src/tasks/minions/minigames/zalcanoActivity.ts
@@ -4,6 +4,7 @@ import { Bank, Misc } from 'oldschooljs';
 
 import { Events } from '../../../lib/constants';
 import { MinigameIDsEnum } from '../../../lib/minions/data/minigames';
+import { SkillsEnum } from '../../../lib/skilling/types';
 import { ZalcanoActivityTaskOptions } from '../../../lib/types/minions';
 import { handleTripFinish } from '../../../lib/util/handleTripFinish';
 
@@ -21,13 +22,25 @@ export default class extends Task {
 		user.incrementMinigameScore(MinigameIDsEnum.Zalcano, quantity);
 
 		const loot = new Bank();
+
+		let runecraftXP = 0;
+		let smithingXP = 0;
+		let miningXP = 0;
+
 		for (let i = 0; i < quantity; i++) {
 			loot.add(
 				Misc.Zalcano.kill({
 					team: [{ isMVP, performancePercentage: performance, id: '1' }]
 				})['1']
 			);
+			runecraftXP += randInt(100, 170);
+			smithingXP += randInt(250, 350);
+			miningXP += randInt(1100, 1400);
 		}
+
+		user.addXP(SkillsEnum.Mining, miningXP);
+		user.addXP(SkillsEnum.Smithing, smithingXP);
+		user.addXP(SkillsEnum.Runecraft, runecraftXP);
 
 		const kc = user.getMinigameScore(MinigameIDsEnum.Zalcano);
 
@@ -61,7 +74,8 @@ export default class extends Task {
 			channelID,
 			`${user}, ${
 				user.minionName
-			} finished killing ${quantity}x Zalcano. Your Zalcano KC is now ${kc + quantity}`,
+			} finished killing ${quantity}x Zalcano. Your Zalcano KC is now ${kc +
+				quantity}. You received ${runecraftXP} Runecraft XP, ${miningXP} Mining XP, ${smithingXP} Smithing XP.`,
 			res => {
 				user.log(`continued zalcano`);
 				return this.client.commands.get('zalcano')!.run(res, []);
