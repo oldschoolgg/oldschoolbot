7464eb8b2b43c02a3b46b15a480072160b21d804
gc <gc@users.noreply.github.com>
2020-10-26 16:11:51 +1100

    remove autoupdate

--- src/lib/settings/schemas/UserSchema.ts
+++ src/lib/settings/schemas/UserSchema.ts
@@ -7,7 +7,6 @@ Client.defaultUserSchema
 	.add('totalCommandsUsed', 'integer', { default: 0 })
 	.add('GP', 'integer', { default: 0 })
 	.add('QP', 'integer', { default: 0 })
-	.add('autoupdate', 'boolean', { default: false })
 	.add('RSN', 'string', { default: null })
 	.add('pets', 'any', { default: {} })
 	.add('badges', 'integer', { array: true, default: [] })
