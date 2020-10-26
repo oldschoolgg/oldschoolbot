c622e9ecbd96c702163deffa1e7b41835f32b188
gc <gc@users.noreply.github.com>
2020-10-26 16:43:49 +1100

    add examples/descs

--- src/commands/Fun/ask.ts
+++ src/commands/Fun/ask.ts
@@ -1,23 +1,36 @@
-import { Command, KlasaMessage } from 'klasa';
+import { CommandStore, KlasaMessage } from 'klasa';
 
+import { BotCommand } from '../../lib/BotCommand';
 import { randomItemFromArray } from '../../lib/util';
 
-export default class extends Command {
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			description: `Allows you to ask yes/no questions to the bot.`,
+			examples: [
+				'+ask Should I try to get an inferno cape?',
+				'+ask Should I become a patron?'
+			]
+		});
+	}
+
 	async run(msg: KlasaMessage) {
 		return msg.send(
 			randomItemFromArray([
 				'Yes.',
+				'Definitely.',
+				'Obviously yes.',
+				'Without a doubt.',
+				'I think so.',
+				'100%.',
+
+				"It's possible.",
+				'Maybe.',
+
 				'No.',
 				'No chance.',
-				'Definitely.',
-				'Maybe.',
-				"It's possible.",
 				'Unlikely.',
-				'Obviously yes.',
-				'I think so.',
 				'0 chance.',
-				'100%.',
-				'Without a doubt.',
 				'No way.'
 			])
 		);
