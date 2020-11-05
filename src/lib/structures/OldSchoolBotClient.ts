02fb37934c4efffe89a07a86f44811fcedc5f405
gc <gc@users.noreply.github.com>
2020-11-06 02:13:45 +1100

    fix client.production

--- src/lib/structures/OldSchoolBotClient.ts
+++ src/lib/structures/OldSchoolBotClient.ts
@@ -1,7 +1,9 @@
 import { Client as TagsClient } from '@kcp/tags';
+import { KlasaClientOptions } from 'klasa';
 import { Client } from 'klasa';
 import pLimit from 'p-limit';
 
+import { clientOptions } from '../config/config';
 import { initItemAliases } from '../itemAliases';
 import { piscinaPool } from '../workers';
 
@@ -11,11 +13,18 @@ import('../settings/schemas/ClientSchema');
 import('../settings/schemas/UserSchema');
 import('../settings/schemas/GuildSchema');
 
+const { production } = clientOptions;
+
+if (typeof production !== 'boolean') {
+	throw new Error(`Must provide production boolean.`);
+}
+
 export class OldSchoolBotClient extends Client {
 	public oneCommandAtATimeCache = new Set<string>();
 	public secondaryUserBusyCache = new Set<string>();
 	public queuePromise = pLimit(1);
 	public piscinaPool = piscinaPool;
+	public production = production ?? false;
 
 	public init = async (): Promise<this> => {
 		initItemAliases();
