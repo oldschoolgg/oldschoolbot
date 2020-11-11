fe21aa38d8873f59d9d286802ff68c45dc312310
gc <gc@users.noreply.github.com>
2020-11-11 22:19:20 +1100

    show specific perk tier required

--- src/inhibitors/perkTierCommands.ts
+++ src/inhibitors/perkTierCommands.ts
@@ -11,11 +11,15 @@ export default class extends Inhibitor {
 		if (!command.perkTier) return;
 
 		if (getUsersPerkTier(msg.author) < command.perkTier) {
-			throw `You need to be a tier ${
-				command.perkTier - 1
-			} patron to use this command. You can become this patron tier at https://www.patreon.com/oldschoolbot`;
+			msg.send(
+				`You need to be a ${
+					command.perkTier - 1 > 0
+						? `tier ${command.perkTier - 1} patron`
+						: `tier ${command.perkTier} patron or server booster`
+				} to use this command. You can become this patron tier at https://www.patreon.com/oldschoolbot`
+			);
 		}
 
-		return false;
+		return true;
 	}
 }
