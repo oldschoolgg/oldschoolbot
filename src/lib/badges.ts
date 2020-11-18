6da844e9276c44aedf31cb984c525f1e8a1b7b0f
gc <30398469+gc@users.noreply.github.com>
2020-11-18 11:47:52 +1100

    add new badges and modify existing

--- src/lib/badges.ts
+++ src/lib/badges.ts
@@ -1,8 +1,13 @@
+import { Emoji } from './constants';
+
 const badges: { [key: number]: string } = {
-	0: '<:settings_rs:586330616539381760>',
-	1: '<:booster:666184228748263436>',
-	2: '🦀',
-	3: '<:patreonLogo:679334888792391703>'
+	0: Emoji.Spanner,
+	1: Emoji.PinkGem,
+	2: Emoji.Crab,
+	3: Emoji.OrangeGem,
+	4: Emoji.BigOrangeGem,
+	5: Emoji.GreenGem,
+	6: Emoji.Bug
 };
 
 export default badges;
