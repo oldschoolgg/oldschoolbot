913c7495a8d30ba7ca846fb7ec75b5ba0b28e88d
gc <30398469+gc@users.noreply.github.com>
2020-11-18 12:30:12 +1100

    badges changes

--- src/lib/badges.ts
+++ src/lib/badges.ts
@@ -4,8 +4,8 @@ const badges: { [key: number]: string } = {
 	0: Emoji.Spanner,
 	1: Emoji.PinkGem,
 	2: Emoji.Crab,
-	3: Emoji.OrangeGem,
-	4: Emoji.BigOrangeGem,
+	3: Emoji.BigOrangeGem,
+	4: Emoji.Hammer,
 	5: Emoji.GreenGem,
 	6: Emoji.Bug
 };
