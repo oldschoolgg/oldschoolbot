b3c7bb54900b1fd3ace00ad8f17f66eb0ff251d3
gc <30398469+gc@users.noreply.github.com>
2020-11-29 15:37:47 +1100

    refactor blacklisting

--- src/commands/Owner/blacklist.ts
+++ src/commands/Owner/blacklist.ts
@@ -2,22 +2,23 @@ import { Guild, User } from 'discord.js';
 import { ArrayActions, CommandStore, KlasaMessage } from 'klasa';
 
 import { BotCommand } from '../../lib/BotCommand';
-import { BitField } from '../../lib/constants';
+import { BitField, Channel } from '../../lib/constants';
 import { ClientSettings } from '../../lib/settings/types/ClientSettings';
+import { toTitleCase } from '../../lib/util';
+import { channelIsSendable } from '../../lib/util/channelIsSendable';
 import getSupportGuild from '../../lib/util/getSupportGuild';
 
 export default class extends BotCommand {
-	public terms = ['usersAdded', 'usersRemoved', 'guildsAdded', 'guildsRemoved'];
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			bitfieldsRequired: [BitField.isModerator],
-			usage: '<User:user|Guild:guild|guild:str> [reason:...str]',
+			usage: '<User:user|Guild:guild> [reason:...str]',
 			usageDelim: ' ',
 			guarded: true
 		});
 	}
 
-	async run(msg: KlasaMessage, [userOrGuild]: [User | Guild]) {
+	async run(msg: KlasaMessage, [userOrGuild, reason = '*No reason.*']: [User | Guild, string]) {
 		const type = userOrGuild instanceof User ? 'user' : 'guild';
 		const key = type === 'guild' ? ClientSettings.GuildBlacklist : ClientSettings.UserBlacklist;
 		const entry = this.client.settings.get(key);
@@ -28,11 +29,17 @@ export default class extends BotCommand {
 			arrayAction: alreadyBlacklisted ? ArrayActions.Remove : ArrayActions.Add
 		});
 		const emoji = getSupportGuild(this.client).emojis.random().toString();
+		const name = 'name' in userOrGuild ? userOrGuild.name : userOrGuild.username;
+		const newStatus = `${alreadyBlacklisted ? 'un' : ''}blacklisted`;
 
-		return msg.send(
-			`${emoji} Successfully ${alreadyBlacklisted ? 'un' : ''}blacklisted a ${type}, ${
-				'name' in userOrGuild ? userOrGuild.name : userOrGuild.username
-			}.`
-		);
+		const channel = this.client.channels.get(Channel.BlacklistLogs);
+		if (channelIsSendable(channel)) {
+			channel.send(
+				`${toTitleCase(type)} \`${name}\` was ${newStatus} by ${
+					msg.author.username
+				} for \`${reason}\`.`
+			);
+		}
+		return msg.send(`${emoji} Successfully ${newStatus} ${type} ${name}.`);
 	}
 }
