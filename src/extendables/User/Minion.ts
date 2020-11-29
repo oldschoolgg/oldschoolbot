f73bdc415a4d4e75691df4280b8056e513852542
gc <30398469+gc@users.noreply.github.com>
2020-11-30 01:10:49 +1100

    fix xp multiplier

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -404,15 +404,18 @@ export default class extends Extendable {
 		return this.settings.update(UserSettings.Minion.DailyDuration, newDuration);
 	}
 
-	public async addXP(this: User, skillName: SkillsEnum, amount: number) {
+	public async addXP(this: User, skillName: SkillsEnum, amount: number, multiplier = true) {
 		await this.settings.sync(true);
 		const currentXP = this.settings.get(`skills.${skillName}`) as number;
-		if (currentXP >= 200_000_000) return;
 
 		const skill = Object.values(Skills).find(skill => skill.id === skillName);
 		if (!skill) return;
 
-		const newXP = Math.min(200_000_000, currentXP + amount);
+		if (multiplier) {
+			amount *= 5;
+		}
+
+		let newXP = currentXP + amount;
 
 		// If they reached a XP milestone, send a server notification.
 		for (const XPMilestone of [50_000_000, 100_000_000, 150_000_000, 200_000_000]) {
