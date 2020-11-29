84e996f2dd6353695351c74efa4456beefdd1728
gc <30398469+gc@users.noreply.github.com>
2020-11-30 01:07:20 +1100

    fix

--- src/extendables/User/Bank.ts
+++ src/extendables/User/Bank.ts
@@ -81,18 +81,6 @@ export default class extends Extendable {
 
 	public async addItemsToBank(this: User, _items: ItemBank, collectionLog = false) {
 		await this.settings.sync(true);
-		for (const { scrollID } of clueTiers) {
-			// If they didnt get any of this clue scroll in their loot, continue to next clue tier.
-			if (!_items[scrollID]) continue;
-			const alreadyHasThisScroll = await this.hasItem(scrollID);
-			if (alreadyHasThisScroll) {
-				// If they already have this scroll in their bank, delete it from the loot.
-				delete _items[scrollID];
-			} else {
-				// If they dont have it in their bank, reset the amount to 1 incase they got more than 1 of the clue.
-				_items[scrollID] = 1;
-			}
-		}
 
 		const items = {
 			..._items
