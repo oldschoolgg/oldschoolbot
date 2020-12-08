33a2ced6313c96d9514a3de1cd4659974bbdb901
Andre Da Silva <ard35@njit.edu>
2020-12-08 07:20:11 -0500

    Move chat head images to the appropriate folder

--- src/lib/image/chatHeadImage.ts
+++ src/lib/image/chatHeadImage.ts
@@ -6,7 +6,7 @@ registerFont('./src/lib/resources/osrs-font-quill-8.ttf', { family: 'Regular' })
 
 const textBoxFile = fs.readFileSync('./src/lib/resources/images/textbox.png');
 const mejJalChatHead = fs.readFileSync('./src/lib/resources/images/mejJal.png');
-const janeChatHead = fs.readFileSync('./src/lib/resources/image/jane.png');
+const janeChatHead = fs.readFileSync('./src/lib/resources/images/jane.png');
 
 const chatHeads = {
 	mejJal: mejJalChatHead,
