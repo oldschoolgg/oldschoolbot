b5825165f2f9d72e107b6ab69c8e7be2366d90ab
Andre Da Silva <ard35@njit.edu>
2020-12-08 04:28:57 -0500

    Revert "Rebase Farming Branch to Master"
    This reverts commit d9beb47f95cbfe660ae3c1a8dafbaaed35ce8cd5.

---
+++ src/tasks/minions/minigames/titheFarmActivity.ts
@@ -0,0 +1,136 @@
+import { Task } from 'klasa';
+
+import { Emoji, Events } from '../../../lib/constants';
+import { TitheFarmStats } from '../../../lib/farming/types';
+import { UserSettings } from '../../../lib/settings/types/UserSettings';
+import { SkillsEnum } from '../../../lib/skilling/types';
+import { TitheFarmActivityTaskOptions } from '../../../lib/types/minions';
+import { bankHasItem, roll } from '../../../lib/util';
+import { handleTripFinish } from '../../../lib/util/handleTripFinish';
+import itemID from '../../../lib/util/itemID';
+
+export default class extends Task {
+	async run({ userID, channelID, duration }: TitheFarmActivityTaskOptions) {
+		const baseHarvest = 85;
+		const lootStr: string[] = [];
+		const levelStr: string[] = [];
+
+		const user = await this.client.users.fetch(userID);
+		user.incrementMinionDailyDuration(duration);
+
+		const farmingLvl = user.skillLevel(SkillsEnum.Farming);
+		const titheFarmStats = user.settings.get(UserSettings.Stats.TitheFarmStats);
+		const { titheFarmsCompleted } = titheFarmStats;
+		const { titheFarmPoints } = titheFarmStats;
+		const determineHarvest = baseHarvest + Math.min(15, titheFarmsCompleted);
+		const determinePoints = determineHarvest - 74;
+
+		const updatedTitheFarmStats: TitheFarmStats = {
+			titheFarmsCompleted: titheFarmsCompleted + 1,
+			titheFarmPoints: titheFarmPoints + determinePoints
+		};
+
+		await user.settings.update(UserSettings.Stats.TitheFarmStats, updatedTitheFarmStats);
+
+		let fruit = '';
+		let fruitXp = 0;
+		if (farmingLvl < 54) {
+			fruitXp = 6;
+			fruit = 'golovanova';
+		} else if (farmingLvl < 74) {
+			fruitXp = 14;
+			fruit = 'bologano';
+		} else {
+			fruitXp = 23;
+			fruit = 'logavano';
+		}
+
+		const harvestXp = determineHarvest * fruitXp;
+		const depositXp =
+			74 * 10 * fruitXp + (determineHarvest - 74) * 20 * fruitXp + 250 * fruitXp;
+		const farmingXp = harvestXp + depositXp;
+
+		const harvestStr = `${user} ${user.minionName} successfully harvested ${determineHarvest}x ${fruit} fruit and received ${farmingXp} Farming XP.`;
+		const completedStr = `You have completed the ${
+			Emoji.MinigameIcon
+		} Tithe Farm ${titheFarmsCompleted + 1}x times. You now have ${titheFarmPoints +
+			determinePoints} points to spend.`;
+
+		const userBank = user.settings.get(UserSettings.Bank);
+		let bonusXpMultiplier = 0;
+		let farmersPiecesCheck = 0;
+		if (
+			bankHasItem(userBank, itemID(`Farmer's strawhat`), 1) ||
+			user.hasItemEquippedAnywhere(itemID(`Farmer's strawhat`))
+		) {
+			bonusXpMultiplier += 0.004;
+			farmersPiecesCheck += 1;
+		}
+		if (
+			bankHasItem(userBank, itemID(`Farmer's jacket`), 1) ||
+			bankHasItem(userBank, itemID(`Farmer's shirt`), 1) ||
+			user.hasItemEquippedAnywhere(itemID(`Farmer's jacket`)) ||
+			user.hasItemEquippedAnywhere(itemID(`Farmer's shirt`))
+		) {
+			bonusXpMultiplier += 0.008;
+			farmersPiecesCheck += 1;
+		}
+		if (
+			bankHasItem(userBank, itemID(`Farmer's boro trousers`), 1) ||
+			user.hasItemEquippedAnywhere(itemID(`Farmer's boro trousers`))
+		) {
+			bonusXpMultiplier += 0.006;
+			farmersPiecesCheck += 1;
+		}
+		if (
+			bankHasItem(userBank, itemID(`Farmer's boots`), 1) ||
+			user.hasItemEquippedAnywhere(itemID(`Farmer's boots`))
+		) {
+			bonusXpMultiplier += 0.002;
+			farmersPiecesCheck += 1;
+		}
+		if (farmersPiecesCheck === 4) bonusXpMultiplier += 0.005;
+
+		const bonusXp = farmingXp * bonusXpMultiplier;
+		const totalXp = farmingXp + bonusXp;
+
+		let bonusXpStr = '';
+		if (bonusXp > 0) {
+			bonusXpStr = `You received an additional ${Math.floor(
+				bonusXp
+			)} Bonus XP from your farmer's outfit pieces.`;
+		}
+
+		await user.addXP(SkillsEnum.Farming, Math.floor(totalXp));
+
+		const newFarmingLevel = user.skillLevel(SkillsEnum.Farming);
+
+		if (newFarmingLevel > farmingLvl) {
+			levelStr.push(`\n\n${user.minionName}'s Farming level is now ${newFarmingLevel}!`);
+		}
+
+		if (roll((7_494_389 - user.skillLevel(SkillsEnum.Farming) * 25) / determineHarvest)) {
+			const loot = { [itemID('Tangleroot')]: 1 };
+			lootStr.push('\n\n```diff');
+			lootStr.push(`\n- You have a funny feeling you're being followed...`);
+			lootStr.push('```');
+			this.client.emit(
+				Events.ServerNotification,
+				`${Emoji.Farming} **${user.username}'s** minion, ${
+					user.minionName
+				}, just received a Tangleroot by completing the ${
+					Emoji.MinigameIcon
+				} Tithe Farm on their ${titheFarmsCompleted + 1} run!`
+			);
+
+			await user.addItemsToBank(loot, true);
+		}
+
+		const returnStr = `${harvestStr} ${bonusXpStr}\n\n${completedStr}${levelStr}${lootStr}\n`;
+
+		handleTripFinish(this.client, user, channelID, returnStr, res => {
+			user.log(`attemped another run of the Tithe Farm.`);
+			return this.client.commands.get('tithefarm')!.run(res, []);
+		});
+	}
+}
