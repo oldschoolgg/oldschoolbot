46066f77a34f9a267f98f8d4e4614d5473ae1c96
Lajnux <68188574+Lajnux@users.noreply.github.com>
2020-12-13 22:56:13 +0100

    Wyson mole part exchange (#1102)
    * wyson
    
    * update
    
    * removedConfirm
    
    * removed getRandomNestBox
    
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

---
+++ src/commands/Minion/wyson.ts
@@ -0,0 +1,85 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Bank } from 'oldschooljs';
+import { itemID } from 'oldschooljs/dist/util';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { stringMatches } from '../../lib/util';
+import createReadableItemListFromBank from '../../lib/util/createReadableItemListFromTuple';
+import { NestBoxes } from './../../lib/openables';
+
+interface MolePartItem {
+	name: string;
+	inputItem: number;
+	aliases?: string[];
+}
+
+const MolePartItems: MolePartItem[] = [
+	{
+		name: 'Mole skin',
+		inputItem: itemID('Mole skin'),
+		aliases: ['skin']
+	},
+	{
+		name: 'Mole claw',
+		inputItem: itemID('Mole claw'),
+		aliases: ['claw']
+	}
+];
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			usage: '[quantity:int{1}] <itemName:...string>',
+			usageDelim: ' ',
+			oneAtTime: true,
+			cooldown: 5,
+			altProtection: true,
+			description: 'Allows you to exchange mole parts for Nest boxes.',
+			examples: ['+wyson mole skin', '+wyson 100 mole claw'],
+			categoryFlags: ['minion']
+		});
+	}
+
+	async run(msg: KlasaMessage, [quantity, itemName]: [number, string]) {
+		itemName = itemName.toLowerCase();
+
+		const moleItem = MolePartItems.find(
+			item =>
+				stringMatches(itemName, item.name) ||
+				(item.aliases && item.aliases.some(alias => stringMatches(alias, itemName)))
+		);
+
+		if (!moleItem) {
+			return msg.send(
+				`I don't exchange that item. I exchange the following: ${MolePartItems.map(item => {
+					return item.name;
+				}).join(', ')}.`
+			);
+		}
+
+		if (!quantity) {
+			quantity = 1;
+		}
+
+		await msg.author.settings.sync(true);
+
+		if (msg.author.numItemsInBankSync(moleItem.inputItem) < quantity) {
+			return msg.send(`You don't have enough ${moleItem.name} to exchange!`);
+		}
+
+		await msg.author.removeItemFromBank(moleItem.inputItem, quantity);
+
+		const loot = new Bank();
+		for (let i = 0; i < quantity; i++) {
+			loot.add(NestBoxes.roll());
+		}
+
+		await msg.author.addItemsToBank(loot.values(), true);
+
+		return msg.send(
+			`You exchanged ${quantity}x ${
+				moleItem.name
+			} and received: ${await createReadableItemListFromBank(this.client, loot.bank)}.`
+		);
+	}
+}
