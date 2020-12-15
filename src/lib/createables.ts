618a6551eb961294a154efb38c2c27aa9ddc3317
Andre Da Silva <ard35@njit.edu>
2020-12-15 13:37:38 -0500

    Add ability to cook wine of zam using tfs buyables

--- src/lib/createables.ts
+++ src/lib/createables.ts
@@ -1193,6 +1193,16 @@ const Createables: Createable[] = [
 			"Inquisitor's armour set": 1
 		})
 	},
+	{
+		name: `Zamorak's grapes`,
+		inputItems: resolveNameBank({
+			"Grapes": 1,
+			"Bologa's blessing": 1
+		}),
+		outputItems: {
+			[itemID(`Zamorak's grapes`)]: 1
+		}
+	},
 	...crystalTools,
 	...ornamentKits
 ];
