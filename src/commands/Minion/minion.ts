49aa95a7045f106b38f3ffc0c5593909a13bfe16
gc <30398469+gc@users.noreply.github.com>
2020-12-19 12:00:33 +1100

    fix ironman bug

--- src/commands/Minion/minion.ts
+++ src/commands/Minion/minion.ts
@@ -153,8 +153,7 @@ Type \`confirm\` if you understand the above information, and want to become an 
 				'stats',
 				'skills',
 				'minion',
-				'farmingPatches',
-				'farmingContracts'
+				'farmingPatches'
 			]);
 
 			await msg.author.settings.update([
