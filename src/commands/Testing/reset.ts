cc0c87f36af07b95abc8c82d6190cbfac0a75615
gc <30398469+gc@users.noreply.github.com>
2020-12-23 12:32:08 +1100

    add reset testing cmd

---
+++ src/commands/Testing/reset.ts
@@ -0,0 +1,18 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			testingCommand: true
+		});
+		this.enabled = !this.client.production;
+	}
+
+	async run(msg: KlasaMessage) {
+		await msg.author.settings.reset();
+		await msg.author.settings.update('minion.hasBought', true);
+		return msg.send(`Resetteded all your data.`);
+	}
+}
