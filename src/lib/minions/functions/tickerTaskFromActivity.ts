974906857145327174ee5465a93cf9d930a1b120
GC <30398469+gc@users.noreply.github.com>
2020-12-27 13:06:27 +1100

    Revert christmas event (#1131)

--- src/lib/minions/functions/tickerTaskFromActivity.ts
+++ src/lib/minions/functions/tickerTaskFromActivity.ts
@@ -40,7 +40,6 @@ export function tickerTaskFromActivity(type: Activity): Tasks {
 		case Activity.Zalcano:
 		case Activity.FishingTrawler:
 		case Activity.TitheFarm:
-		case Activity.DeliverPresents:
 		case Activity.BarbarianAssault:
 		case Activity.AgilityArena:
 		case Activity.ChampionsChallenge:
