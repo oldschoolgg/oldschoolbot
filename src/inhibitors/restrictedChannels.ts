ad3bfb8148a465854f3a6186ca7b42ecb14384ea
gc <30398469+gc@users.noreply.github.com>
2020-12-27 21:12:15 +1100

    fix

--- src/inhibitors/restrictedChannels.ts
+++ src/inhibitors/restrictedChannels.ts
@@ -2,7 +2,7 @@ import { Command, Inhibitor, KlasaMessage } from 'klasa';
 
 export default class extends Inhibitor {
 	public async run(msg: KlasaMessage, command: Command) {
-		if (!command.restrictedChannels) return;
+		if (!command.restrictedChannels || command.restrictedChannels.length === 0) return;
 
 		if (!command.restrictedChannels.includes(msg.channel.id)) {
 			throw `You cannot use this command outside of the bot's support server. Join the bot support server, then use one these channels: ${command.restrictedChannels
