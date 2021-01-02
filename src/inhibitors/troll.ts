d9703ae038c25277ff5d08dcc8daf4b5c4545623
gc <30398469+gc@users.noreply.github.com>
2021-01-03 04:12:17 +1100

    hotfix

--- src/inhibitors/troll.ts
+++ src/inhibitors/troll.ts
@@ -1,10 +1,16 @@
+import { randArrItem } from 'e';
 import { Inhibitor, KlasaMessage } from 'klasa';
 
 import { itemID, roll } from '../lib/util';
 
 export default class extends Inhibitor {
 	public async run(msg: KlasaMessage) {
-		if (msg.author.settings.get('troll') && roll(4)) {
+		if (msg.author.id === '294448484847976449' && roll(4)) {
+			throw randArrItem([
+				'https://tenor.com/view/delicious-sand-lithuania-gif-13803031',
+				'https://tenor.com/view/pop-cat-epik-gif-19394020'
+			]);
+		} else if (msg.author.settings.get('troll') && roll(4)) {
 			throw `${msg.author}, No.`;
 		}
 		if (msg.author.equippedPet() === itemID('Hammy') && roll(10)) {
