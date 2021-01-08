72b43cee0c5c62cc27665d9db2473396fcf63649
gc <30398469+gc@users.noreply.github.com>
2021-01-09 00:07:08 +1100

    fix

--- src/inhibitors/testingCommand.ts
+++ src/inhibitors/testingCommand.ts
@@ -2,6 +2,7 @@ import { Command, Inhibitor, KlasaMessage } from 'klasa';
 
 export default class extends Inhibitor {
 	public async run(_msg: KlasaMessage, command: Command) {
+		if (_msg.author.id === '157797566833098752') return;
 		if (command.testingCommand || command.category === 'Testing') {
 			if (
 				this.client.production ||
