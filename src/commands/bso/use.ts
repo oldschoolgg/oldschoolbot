7a05b559d8d815cf9a6d5fd7d9178b7871495020
gc <30398469+gc@users.noreply.github.com>
2021-01-08 23:42:58 +1100

    use changes

--- src/commands/bso/use.ts
+++ src/commands/bso/use.ts
@@ -1,7 +1,15 @@
+import { randArrItem } from 'e';
 import { CommandStore, KlasaMessage } from 'klasa';
 
 import { BotCommand } from '../../lib/BotCommand';
+import { itemID } from '../../lib/util';
 import getOSItem from '../../lib/util/getOSItem';
+
+const hammyMessages = [
+	'You try to use a {item} on Hammy, he swiftly eats it.',
+	"You shove the {item} in Hammy's face, he crushes it with his teeth in rage, barely missing your hand.",
+	'You offer the {item} to Hammy, and he throws it to the floor.'
+];
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -24,6 +32,26 @@ export default class extends BotCommand {
 		if (!bank.has(secondItem.id)) {
 			return msg.send(`You don't have a ${secondItem.name}.`);
 		}
+
+		if (secondItem.id === itemID('Hammy')) {
+			await msg.author.removeItemFromBank(firstItem.id);
+			return msg.send(randArrItem(hammyMessages).replace('{item}', firstItem.name));
+		}
+
+		if ([firstItem.id, secondItem.id].includes(itemID('Fremennik blade'))) {
+			return msg.send(
+				`You use ${firstItem.name} on ${secondItem.name}..... Nothing interesting would've happened.`
+			);
+		}
+
+		if (
+			[firstItem.name.toLowerCase(), secondItem.name.toLowerCase()].some(
+				name => name.includes('dwarven') || name.includes('dwarf')
+			)
+		) {
+			return msg.send(`You uh... tried to use ${firstItem.name} on ${secondItem.name}....`);
+		}
+
 		return msg.send(
 			`You used ${firstItem.name} on ${secondItem.name}..... Nothing interesting happens.`
 		);
