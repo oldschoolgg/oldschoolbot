698514753b1da72731b0384de514b84c20276b73
gc <30398469+gc@users.noreply.github.com>
2021-01-20 23:12:28 +1100

    fix lb using tickets

--- src/commands/Minion/lotterybank.ts
+++ src/commands/Minion/lotterybank.ts
@@ -26,6 +26,12 @@ export default class extends BotCommand {
 	}
 
 	async run(msg: KlasaMessage, [[bankToSell, totalPrice]]: [[Bank, number]]) {
+		if (bankToSell.amount('Lottery ticket')) {
+			bankToSell.remove('Lottery ticket', bankToSell.amount('Lottery ticket'));
+		}
+		if (bankToSell.amount('Bank lottery ticket')) {
+			bankToSell.remove('Bank lottery ticket', bankToSell.amount('Bank lottery ticket'));
+		}
 		let amountOfTickets = Math.floor(totalPrice / 10_000_000);
 
 		if (amountOfTickets < 1) {
