b1a361227f15ed181901ec2a4e6c85748c0c01d8
gc <30398469+gc@users.noreply.github.com>
2021-01-20 22:50:13 +1100

    refactor lotterybank cmd to new system

--- src/commands/Minion/lotterybank.ts
+++ src/commands/Minion/lotterybank.ts
@@ -1,12 +1,10 @@
 import { CommandStore, KlasaMessage } from 'klasa';
-import { Item } from 'oldschooljs/dist/meta/types';
+import { Bank } from 'oldschooljs';
 
 import { BotCommand } from '../../lib/BotCommand';
 import { Events } from '../../lib/constants';
 import { ClientSettings } from '../../lib/settings/types/ClientSettings';
-import { UserSettings } from '../../lib/settings/types/UserSettings';
-import { addItemToBank, calcPercentOfNum, itemID } from '../../lib/util';
-import itemIsTradeable from '../../lib/util/itemIsTradeable';
+import { addBanks, itemID } from '../../lib/util';
 
 const options = {
 	max: 1,
@@ -18,7 +16,7 @@ export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			cooldown: 1,
-			usage: '[quantity:int{1}] [item:...item]',
+			usage: '(items:...TradeableBank)',
 			usageDelim: ' ',
 			oneAtTime: true,
 			categoryFlags: ['minion'],
@@ -27,52 +25,17 @@ export default class extends BotCommand {
 		});
 	}
 
-	async run(msg: KlasaMessage, [quantity, itemArray]: [number | undefined, Item[]]) {
-		if (!itemArray) {
-			return msg.send(`Please specify an item.`);
-		}
-
-		const userBank = msg.author.settings.get(UserSettings.Bank);
-		const osItem = itemArray.find(i => userBank[i.id]);
-
-		if (!osItem) {
-			return msg.send(`You don't have any of this item.`);
-		}
-
-		const numItemsHas = userBank[osItem.id];
-		if (!quantity) {
-			quantity = numItemsHas;
-		}
-
-		if (quantity > numItemsHas) {
-			return msg.send(`You dont have ${quantity}x ${osItem.name}.`);
-		}
-
-		let priceOfItem = itemIsTradeable(osItem.id)
-			? await this.client.fetchItemPrice(osItem.id)
-			: 1;
-
-		const totalPrice = priceOfItem * quantity;
-
+	async run(msg: KlasaMessage, [[bankToSell, totalPrice]]: [[Bank, number]]) {
 		let amountOfTickets = Math.floor(totalPrice / 10_000_000);
 
 		if (amountOfTickets < 1) {
 			return msg.send(`Those items aren't worth enough.`);
 		}
 
-		const wasAlready = Boolean(this.client.settings.get(ClientSettings.BankLottery)[osItem.id]);
-		let bonusTickets = 0;
-		if (!wasAlready) {
-			bonusTickets = Math.floor(calcPercentOfNum(15, amountOfTickets));
-			amountOfTickets += bonusTickets;
-		}
-
 		if (!msg.flagArgs.confirm && !msg.flagArgs.cf) {
-			let str = `${msg.author}, say \`confirm\` to commit ${quantity}x ${osItem.name} to the bank lottery - you'll receive ${amountOfTickets} bank lottery tickets.`;
-			if (bonusTickets > 0) {
-				str += `\n\nYou're getting ${bonusTickets}x Bonus Tickets for sacrificing something that nobody has before.`;
-			}
-			const sellMsg = await msg.channel.send(str);
+			const sellMsg = await msg.channel.send(
+				`${msg.author}, say \`confirm\` to commit ${bankToSell} to the bank lottery - you'll receive **${amountOfTickets} bank lottery tickets**.`
+			);
 
 			try {
 				await msg.channel.awaitMessages(
@@ -82,29 +45,27 @@ export default class extends BotCommand {
 					options
 				);
 			} catch (err) {
-				return sellMsg.edit(
-					`Cancelling bank lottery sacrifice of ${quantity}x ${osItem.name}.`
-				);
+				return sellMsg.edit(`Cancelling bank lottery sacrifice.`);
 			}
 		}
 
 		if (totalPrice > 200_000_000) {
 			this.client.emit(
 				Events.ServerNotification,
-				`${msg.author.username} just committed ${quantity}x ${osItem.name} to the Bank Lottery!`
+				`${msg.author.username} just committed this to the bank lottery: ${bankToSell}`
 			);
 		}
 
 		await msg.author.addItemsToBank({ [itemID('Bank lottery ticket')]: amountOfTickets }, true);
-		await msg.author.removeItemFromBank(osItem.id, quantity);
+		await msg.author.removeItemsFromBank(bankToSell.bank);
 
 		await this.client.settings.update(
 			ClientSettings.BankLottery,
-			addItemToBank(this.client.settings.get(ClientSettings.BankLottery), osItem.id, quantity)
+			addBanks([this.client.settings.get(ClientSettings.BankLottery), bankToSell.bank])
 		);
 
 		return msg.send(
-			`You commited ${quantity}x ${osItem.name} to the bank lottery, and received ${amountOfTickets}x bank lottery tickets.`
+			`You commited ${bankToSell} to the bank lottery, and received ${amountOfTickets}x bank lottery tickets.`
 		);
 	}
 }
