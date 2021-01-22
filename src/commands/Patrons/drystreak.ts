8822e7e496c70b088bd99ab6a5ec7debaf470bc2
GC <30398469+gc@users.noreply.github.com>
2021-01-22 19:18:13 +1100

    Misc updates (#1242)
    * add bucket of water to buyables
    
    * remove stackedVariants
    
    * fix: dont add tickets to cl when buying recolor
    
    * add toads legs creatable
    
    * remove untrimmed agility cape from cl skilling
    
    * minigame support for drystreaks
    
    * track db transactions in analytics
    
    * add economy logging

--- src/commands/Patrons/drystreak.ts
+++ src/commands/Patrons/drystreak.ts
@@ -3,6 +3,7 @@ import { Monsters } from 'oldschooljs';
 
 import { BotCommand } from '../../lib/BotCommand';
 import { PerkTier } from '../../lib/constants';
+import { Minigames } from '../../lib/minions/data/minigames';
 import { stringMatches } from '../../lib/util';
 import getOSItem from '../../lib/util/getOSItem';
 import LeaderboardCommand from '../Minion/leaderboard';
@@ -23,15 +24,17 @@ export default class extends BotCommand {
 
 	async run(msg: KlasaMessage, [monName, itemName]: [string, string]) {
 		const mon = Monsters.find(mon => mon.aliases.some(alias => stringMatches(alias, monName)));
-
-		if (!mon) {
-			return msg.send(`That's not a valid monster.`);
+		const minigame = Minigames.find(min => stringMatches(min.name, monName));
+		if (!mon && !minigame) {
+			return msg.send(`That's not a valid monster or minigame.`);
 		}
 
 		const item = getOSItem(itemName);
 
 		const ironmanPart = msg.flagArgs.im ? 'AND "minion.ironman" = true' : '';
-		const query = `SELECT "id", "monsterScores"->>'${mon.id}' AS "KC" FROM users WHERE "collectionLogBank"->>'${item.id}' IS NULL AND "monsterScores"->>'${mon.id}' IS NOT NULL ${ironmanPart} ORDER BY ("monsterScores"->>'${mon.id}')::int DESC LIMIT 10;`;
+		const key = Boolean(minigame) ? 'minigameScores' : 'monsterScores';
+		const id = minigame?.id ?? mon?.id;
+		const query = `SELECT "id", "${key}"->>'${id}' AS "KC" FROM users WHERE "collectionLogBank"->>'${item.id}' IS NULL AND "${key}"->>'${id}' IS NOT NULL ${ironmanPart} ORDER BY ("${key}"->>'${id}')::int DESC LIMIT 10;`;
 
 		const result = await this.client.query<
 			{
@@ -45,7 +48,7 @@ export default class extends BotCommand {
 		const command = this.client.commands.get('leaderboard') as LeaderboardCommand;
 
 		return msg.send(
-			`**Dry Streaks for ${item.name} from ${mon.name}:**\n${result
+			`**Dry Streaks for ${item.name} from ${(mon || minigame)!.name}:**\n${result
 				.map(
 					({ id, KC }) =>
 						`${command.getUsername(id) as string}: ${parseInt(KC).toLocaleString()}`
