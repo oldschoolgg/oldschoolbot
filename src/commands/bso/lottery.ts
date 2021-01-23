4936bab5988dd37cab74128c71a6c6f897445caa
gc <30398469+gc@users.noreply.github.com>
2021-01-24 04:46:03 +1100

    lottery cmd

---
+++ src/commands/bso/lottery.ts
@@ -0,0 +1,32 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/BotCommand';
+import { PerkTier } from '../../lib/constants';
+import { ClientSettings } from '../../lib/settings/types/ClientSettings';
+import { addArrayOfNumbers, toKMB } from '../../lib/util';
+
+export default class extends BotCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			usage: '<user:user>',
+			perkTier: PerkTier.Four
+		});
+	}
+
+	async run(msg: KlasaMessage) {
+		const tickets = addArrayOfNumbers(
+			await this.client
+				.query<{ qty: number }[]>(
+					`SELECT id, CAST(bank->>'5020' AS INTEGER) as qty FROM users WHERE bank->>'5020' IS NOT NULL; `
+				)
+				.then(res => res.map(i => i.qty))
+		);
+
+		const gp = tickets * 50_000_000;
+
+		return msg.channel.sendBankImage({
+			bank: this.client.settings.get(ClientSettings.BankLottery),
+			content: `**GP Lottery:** ${toKMB(gp)}\n**Bank Lottery:**`
+		});
+	}
+}
