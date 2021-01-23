7c4e71eedf31ad23a5665bc9060ddf1e826e05f4
gc <30398469+gc@users.noreply.github.com>
2021-01-23 23:03:12 +1100

    change

--- src/inhibitors/altProtection.ts
+++ src/inhibitors/altProtection.ts
@@ -14,7 +14,7 @@ export default class extends Inhibitor {
 		if (getUsersPerkTier(msg.author) >= PerkTier.Four) return;
 
 		if (
-			Date.now() - msg.author.createdTimestamp < Time.Year &&
+			Date.now() - msg.author.createdTimestamp < Time.Month * 6 &&
 			!msg.author.settings.get(UserSettings.BitField).includes(BitField.BypassAgeRestriction)
 		) {
 			throw `You cannot use this command as your account is too new. You can ask to be manually verified if you have social media accounts as proof of identity.`;
