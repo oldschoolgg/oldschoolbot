f09f0012c7e16843106c388c7d5d4822e7582dcc
gc <30398469+gc@users.noreply.github.com>
2021-02-09 09:32:51 +1100

    give magic xp in questing

--- src/tasks/minions/questingActivity.ts
+++ src/tasks/minions/questingActivity.ts
@@ -22,9 +22,11 @@ export default class extends Task {
 			qpRecieved = rand(1, 2);
 		}
 
+		const newQP = currentQP + qpRecieved;
+
 		// The minion could be at (MAX_QP - 1) QP, but gain 4 QP here, so we'll trim that down from 4 to 1.
-		if (currentQP + qpRecieved > MAX_QP) {
-			qpRecieved -= currentQP + qpRecieved - MAX_QP;
+		if (newQP > MAX_QP) {
+			qpRecieved -= newQP - MAX_QP;
 		}
 
 		let str = `${user}, ${
@@ -33,16 +35,33 @@ export default class extends Task {
 			currentQP + qpRecieved
 		}.`;
 
-		const hasMaxQP = currentQP + qpRecieved >= MAX_QP;
+		const hasMaxQP = newQP >= MAX_QP;
 		if (hasMaxQP) {
 			str += `\n\nYou have achieved the maximum amount of ${MAX_QP} Quest Points!`;
 		}
 
 		await user.addQP(qpRecieved);
 		const herbLevel = user.skillLevel(SkillsEnum.Herblore);
-		if (herbLevel === 1 && currentQP + qpRecieved > 5 && roll(2)) {
+		if (herbLevel === 1 && newQP > 5 && roll(2)) {
 			await user.addXP(SkillsEnum.Herblore, 250);
 			str += `${Emoji.Herblore} You received 250 Herblore XP for completing Druidic Ritual.`;
+		}
+
+		const magicXP = user.settings.get(UserSettings.Skills.Magic);
+		if (magicXP === 0 && roll(2)) {
+			await user.addXP(SkillsEnum.Magic, 325);
+			str += `${Emoji.Herblore} You received 325 Magic XP for completing Witch's Potion.`;
+		} else if (magicXP < 1000 && newQP > 15 && roll(2)) {
+			await user.addXP(SkillsEnum.Magic, 1000);
+			str += `${Emoji.Herblore} You received 2150 Magic XP for completing Fairytale I - Growing Pains.`;
+		} else if (
+			user.skillLevel(SkillsEnum.Cooking) >= 40 &&
+			newQP > 50 &&
+			magicXP < 2500 &&
+			roll(2)
+		) {
+			await user.addXP(SkillsEnum.Magic, 2150);
+			str += `${Emoji.Herblore} You received 2500 Magic XP for completing Recipe For Disaster (Lumbridge guide subquest).`;
 		}
 
 		handleTripFinish(
