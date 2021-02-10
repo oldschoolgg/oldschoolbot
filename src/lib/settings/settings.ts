b2d913ba232210c4a645faa5ffbe4eaa5ac1404f
gc <30398469+gc@users.noreply.github.com>
2021-02-10 18:28:15 +1100

    add utils for username syncing

--- src/lib/settings/settings.ts
+++ src/lib/settings/settings.ts
@@ -1,10 +1,13 @@
 import { Util } from 'discord.js';
 import { Gateway, Settings } from 'klasa';
+import { getConnection } from 'typeorm';
 
 import { client } from '../..';
 import { MinigameKey } from '../../extendables/User/Minigame';
 import { Emoji } from '../constants';
 import { MinigameTable } from '../typeorm/MinigameTable.entity';
+import { NewUserTable } from '../typeorm/NewUserTable.entity';
+import { parseUsername, toSnakeCase } from '../util';
 
 export async function getUserSettings(userID: string): Promise<Settings> {
 	return (client.gateways.get('users') as Gateway)!
@@ -12,6 +15,33 @@ export async function getUserSettings(userID: string): Promise<Settings> {
 			id: userID
 		})
 		.sync(true);
+}
+
+export async function syncNewUserUsername(id: string, username: string) {
+	let value = await NewUserTable.findOne({ id });
+	if (!value) {
+		value = new NewUserTable();
+		value.id = id;
+		value.username = username;
+		value.save();
+		return;
+	}
+	value.username = username;
+	value.save();
+}
+
+export async function batchSyncNewUserUsernames(arr: [string, string][]) {
+	console.log(`Syncing ${JSON.stringify(arr)}`);
+	await getConnection()
+		.createQueryBuilder()
+		.insert()
+		.into(NewUserTable)
+		.values(arr.map(arr => ({ id: arr[0], username: parseUsername(arr[1]) })))
+		.orUpdate({
+			conflict_target: ['id'],
+			overwrite: ['username']
+		})
+		.execute();
 }
 
 export async function getMinigameEntity(userID: string): Promise<MinigameTable> {
