46b2c3ee67f7f53f2ba47233d5fd54e6965bd100
gc <30398469+gc@users.noreply.github.com>
2021-02-10 18:28:56 +1100

    remove default values from minigame table

--- src/lib/typeorm/MinigameTable.entity.ts
+++ src/lib/typeorm/MinigameTable.entity.ts
@@ -6,32 +6,32 @@ export class MinigameTable extends BaseEntity {
 	public userID!: string;
 
 	@Column({ name: 'tithe_farm', type: 'int', nullable: false, default: 0 })
-	public TitheFarm = 0;
+	public TitheFarm!: number;
 
 	@Column({ name: 'wintertodt', type: 'int', nullable: false, default: 0 })
-	public Wintertodt = 0;
+	public Wintertodt!: number;
 
 	@Column({ name: 'sepulchre', type: 'int', nullable: false, default: 0 })
-	public Sepulchre = 0;
+	public Sepulchre!: number;
 
 	@Column({ name: 'fishing_trawler', type: 'int', nullable: false, default: 0 })
-	public FishingTrawler = 0;
+	public FishingTrawler!: number;
 
 	@Column({ name: 'barb_assault', type: 'int', nullable: false, default: 0 })
-	public BarbarianAssault = 0;
+	public BarbarianAssault!: number;
 
 	@Column({ name: 'pyramid_plunder', type: 'int', nullable: false, default: 0 })
-	public PyramidPlunder = 0;
+	public PyramidPlunder!: number;
 
 	@Column({ name: 'agility_arena', type: 'int', nullable: false, default: 0 })
-	public AgilityArena = 0;
+	public AgilityArena!: number;
 
 	@Column({ name: 'champions_challenge', type: 'int', nullable: false, default: 0 })
-	public ChampionsChallenge = 0;
+	public ChampionsChallenge!: number;
 
 	@Column({ name: 'mahogany_homes', type: 'int', nullable: false, default: 0 })
-	public MahoganyHomes = 0;
+	public MahoganyHomes!: number;
 
 	@Column({ name: 'gnome_restaurant', type: 'int', nullable: false, default: 0 })
-	public GnomeRestaurant = 0;
+	public GnomeRestaurant!: number;
 }
