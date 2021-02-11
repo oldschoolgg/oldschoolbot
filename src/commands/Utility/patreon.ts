bbfd69b5ffaa5c5f4105316d407f516ce4819dcd
gc <30398469+gc@users.noreply.github.com>
2021-02-11 17:25:35 +1100

    add gh sponsors to patreon cmd

--- src/commands/Utility/patreon.ts
+++ src/commands/Utility/patreon.ts
@@ -15,7 +15,7 @@ export default class extends BotCommand {
 
 	async run(msg: KlasaMessage) {
 		return msg.send(
-			`You can become a patron to support me or thank me if you're enjoying the bot, and receive some perks: <https://www.patreon.com/oldschoolbot> ${Emoji.PeepoOSBot}`
+			`You can become a patron to support me or thank me if you're enjoying the bot, and receive some perks. <https://github.com/sponsors/gc> OR <https://www.patreon.com/oldschoolbot> ${Emoji.PeepoOSBot}`
 		);
 	}
 }
