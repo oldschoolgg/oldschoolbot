86e18b345dc04732b26791fa94785e802cbc893d
gc <30398469+gc@users.noreply.github.com>
2021-02-17 22:03:26 +1100

    track gp from skillcape cmd

--- src/commands/Minion/skillcape.ts
+++ src/commands/Minion/skillcape.ts
@@ -1,7 +1,9 @@
 import { CommandStore, KlasaMessage } from 'klasa';
+import { Bank } from 'oldschooljs';
 import { toKMB } from 'oldschooljs/dist/util/util';
 
 import { Time } from '../../lib/constants';
+import { ClientSettings } from '../../lib/settings/types/ClientSettings';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import Skillcapes from '../../lib/skilling/skillcapes';
 import { BotCommand } from '../../lib/structures/BotCommand';
@@ -81,6 +83,13 @@ export default class extends BotCommand {
 
 		await msg.author.removeGP(skillCapeCost);
 		await msg.author.addItemsToBank(itemsToPurchase, true);
+		await this.client.settings.update(
+			ClientSettings.EconomyStats.BuyCostBank,
+			new Bank(this.client.settings.get(ClientSettings.EconomyStats.BuyCostBank)).add(
+				'Coins',
+				skillCapeCost
+			).bank
+		);
 
 		return msg.send(`You purchased ${itemString} for ${toKMB(skillCapeCost)}.`);
 	}
