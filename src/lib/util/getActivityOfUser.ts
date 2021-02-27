56f39757eb55f70d4367f5850f5d845836141743
gc <30398469+gc@users.noreply.github.com>
2021-02-27 15:55:18 +1100

    remove old file

--- src/lib/util/getActivityOfUser.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from 'discord.js';
-
-export default function getActivityOfUser(client: Client, userID: string) {
-	const task = client.minionActivityCache.get(userID);
-	return task ?? null;
-}
