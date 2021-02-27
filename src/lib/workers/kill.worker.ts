7637155bae22eda95a90830ea3e188fe917cfe5e
gc <30398469+gc@users.noreply.github.com>
2021-02-27 16:06:11 +1100

    should fix errors in worker threads

--- src/lib/workers/kill.worker.ts
+++ src/lib/workers/kill.worker.ts
@@ -1,6 +1,8 @@
 import { Misc, Monsters } from 'oldschooljs';
 import { addBanks } from 'oldschooljs/dist/util/bank';
 
+import { initCustomItems } from '../customItems';
+import { initItemAliases } from '../data/itemAliases';
 import abyssalSire from '../data/monsters/abyssalSire';
 import alchemicalHydra from '../data/monsters/alchemicalHydra';
 import brutalBlackDragon from '../data/monsters/brutalBlackDragon';
@@ -30,6 +32,8 @@ export function stringMatches(str: string, str2: string) {
 }
 
 export default ({ quantity, bossName, limit }: KillWorkerArgs) => {
+	initCustomItems();
+	initItemAliases();
 	const osjsMonster = Monsters.find(mon =>
 		mon.aliases.some(alias => stringMatches(alias, bossName))
 	);
