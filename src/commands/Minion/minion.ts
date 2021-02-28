bdcb0447f3b6b362adcdd0b40759a5977a4c676d
gc <30398469+gc@users.noreply.github.com>
2021-02-28 15:26:52 +1100

    fix

--- src/commands/Minion/minion.ts
+++ src/commands/Minion/minion.ts
@@ -171,6 +171,7 @@ Type \`confirm\` if you understand the above information, and want to become an 
 				UserSettings.HighGambles,
 				UserSettings.CarpenterPoints,
 				UserSettings.ZealTokens,
+				UserSettings.OpenableScores,
 				'gear',
 				'stats',
 				'skills',
