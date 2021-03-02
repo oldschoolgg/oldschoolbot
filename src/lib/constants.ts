90861434449c416a3c58aa9a239d9fe0f656f2b3
gc <30398469+gc@users.noreply.github.com>
2021-03-02 22:43:51 +1100

    fix rp check

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -319,7 +319,9 @@ export const BitFieldData: Partial<Record<BitField, BitFieldData>> = {
 	[BitField.isModerator]: { name: 'Moderator' },
 	[BitField.isContributor]: { name: 'Contributor' },
 	[BitField.BypassAgeRestriction]: { name: 'Bypassed Age Restriction' },
-	[BitField.HasHosidiusWallkit]: { name: 'Hosidius Wall Kit Unlocked' }
+	[BitField.HasHosidiusWallkit]: { name: 'Hosidius Wall Kit Unlocked' },
+	[BitField.HasPermanentEventBackgrounds]: { name: 'Permanent Event Backgrounds' },
+	[BitField.HasPermanentTierOne]: { name: 'Permanent Tier 1' }
 } as const;
 
 export const enum PatronTierID {
