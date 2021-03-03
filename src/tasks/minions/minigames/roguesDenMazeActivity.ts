8afb57754df75b69369742a963981750e278ca5b
Corey <cpotts97@live.co.uk>
2021-03-03 23:38:21 +0000

    Added rogue outfit loot to activity

--- src/tasks/minions/minigames/roguesDenMazeActivity.ts
+++ src/tasks/minions/minigames/roguesDenMazeActivity.ts
@@ -1,9 +1,11 @@
+import { randInt } from 'e';
 import { Task } from 'klasa';
 import { Bank } from 'oldschooljs';
 
+import { rogueOutfit } from '../../../lib/data/collectionLog';
 import { getMinionName, incrementMinigameScore } from '../../../lib/settings/settings';
 import { RoguesDenMazeTaskOptions } from '../../../lib/types/minions';
-import { incrementMinionDailyDuration } from '../../../lib/util';
+import { incrementMinionDailyDuration, randomItemFromArray } from '../../../lib/util';
 import { handleTripFinish } from '../../../lib/util/handleTripFinish';
 
 export default class extends Task {
@@ -18,8 +20,29 @@ export default class extends Task {
 
 		let str = `<@${userID}>, ${minionName} finished completing ${quantity}x laps of the Rogues' Den Maze.`;
 
+		for (let i = 0; i < quantity; i++) {
+			if (randInt(1, 8) <= 5) {
+				loot.add(randomItemFromArray(rogueOutfit));
+			}
+		}
+
+		const gotLoot = loot.length > 0;
+		if (!gotLoot) {
+			str += '\n**Your minion failed to find any Rogue outfit!**';
+		}
+
 		const user = await this.client.users.fetch(userID);
 		await user.addItemsToBank(loot.bank, true);
+
+		const image = await this.client.tasks.get('bankImage')!.generateBankImage(
+			loot.bank,
+			`Loot From ${quantity}x Rogues' Den maze`,
+			false,
+			{
+				showNewCL: 1
+			},
+			user
+		);
 
 		handleTripFinish(
 			this.client,
@@ -30,9 +53,9 @@ export default class extends Task {
 				user.log(`continued rogues den`);
 				return this.client.commands.get('roguesden')!.run(res, []);
 			},
-			undefined,
+			gotLoot ? image : undefined,
 			data,
-			null
+			loot.bank
 		);
 	}
 }
