15170dbce5178387018effe00c50a1e730a0f602
gc <30398469+gc@users.noreply.github.com>
2021-03-18 11:46:21 +1100

    dont allow duplicate training styles

--- src/commands/Minion/train.ts
+++ src/commands/Minion/train.ts
@@ -1,3 +1,4 @@
+import { uniqueArr } from 'e';
 import { CommandStore, KlasaMessage } from 'klasa';
 
 import { AttackStyles } from '../../lib/minions/functions';
@@ -48,8 +49,15 @@ export default class extends BotCommand {
 					)}, the available styles are: Shared, Attack, Strength, Defence, Magic, Ranged.`
 			);
 		}
-		const parsed = _styles.split(' ').map(i => i.trim());
-		if (_styles !== 'shared' && !parsed.every(isValidAttackStyle)) {
+		const parsed = _styles
+			.toLowerCase()
+			.split(' ')
+			.map(i => i.trim());
+
+		if (
+			uniqueArr(parsed).length !== parsed.length ||
+			(_styles !== 'shared' && !parsed.every(isValidAttackStyle))
+		) {
 			return msg.channel.send(
 				`That is not a valid attack style, the available styles are: Shared, Attack, Strength, Defence, Magic, Ranged.`
 			);
