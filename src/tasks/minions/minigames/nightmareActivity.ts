3d48b69d8a322439e92501c31b2dbc50e827aa33
Lajnux <lajnuxxx@gmail.com>
2021-03-21 00:57:01 +0100

    yarn lint

--- src/tasks/minions/minigames/nightmareActivity.ts
+++ src/tasks/minions/minigames/nightmareActivity.ts
@@ -74,7 +74,7 @@ export default class extends Task {
 		for (const [userID, loot] of Object.entries(teamsLoot)) {
 			const user = await this.client.users.fetch(userID).catch(noOp);
 			if (!user) continue;
-			await addMonsterXP(user, NIGHTMARE_ID, Math.ceil(quantity/users.length), duration);
+			await addMonsterXP(user, NIGHTMARE_ID, Math.ceil(quantity / users.length), duration);
 			totalLoot.add(loot);
 			await user.addItemsToBank(loot, true);
 			const kcToAdd = kcAmounts[user.id];
