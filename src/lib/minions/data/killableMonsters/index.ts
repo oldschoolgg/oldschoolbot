cb04db11364c694be8c8bbb75f4c0d34f6921e6b
6enny6 <76587427+6enny6@users.noreply.github.com>
2021-05-09 16:25:39 +0100

    Update index.ts

--- src/lib/minions/data/killableMonsters/index.ts
+++ src/lib/minions/data/killableMonsters/index.ts
@@ -362,7 +362,7 @@ const killableMonsters: KillableMonster[] = [
 		notifyDrops: resolveItems(['Sraracha', 'Jar of eyes']),
 		qpRequired: 0,
 		itemInBankBoosts: [
-			{ [itemID('Dragon claws')]: 5 }
+			{·[itemID('Dragon·claws')]:·5·}
 		],
 		levelRequirements: {
 			prayer: 43
