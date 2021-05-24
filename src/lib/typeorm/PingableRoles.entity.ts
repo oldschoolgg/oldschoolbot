ce7d796e9f3316eeaa0e9f440b54b5e4b77467ee
gc <30398469+gc@users.noreply.github.com>
2021-05-24 17:23:21 +1000

    add pingable roles system

---
+++ src/lib/typeorm/PingableRoles.entity.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'pingable_roles' })
+export class PingableRolesTable extends BaseEntity {
+	@PrimaryGeneratedColumn('increment')
+	public id!: number;
+
+	@Column('varchar', { length: 19, nullable: false, name: 'role_id', unique: true })
+	public roleID!: string;
+
+	@Column('varchar', { length: 32, nullable: false, name: 'name' })
+	public name!: string;
+}
