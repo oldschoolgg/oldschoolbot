5357cba61ab51d1dccd9a44a61deb9fd3178ea20
gc <30398469+gc@users.noreply.github.com>
2021-06-02 09:14:45 +1000

    fix vasa having kg alias

--- src/commands/bso/vasa.ts
+++ src/commands/bso/vasa.ts
@@ -18,8 +18,7 @@ export default class extends BotCommand {
 			usageDelim: ' ',
 			oneAtTime: true,
 			altProtection: true,
-			requiredPermissions: ['ADD_REACTIONS', 'ATTACH_FILES'],
-			aliases: ['kg']
+			requiredPermissions: ['ADD_REACTIONS', 'ATTACH_FILES']
 		});
 	}
 
