93aa76e682b5b0388738c098b280f4d7b171b14d
gc <30398469+gc@users.noreply.github.com>
2021-06-05 06:06:39 +1000

    add SOTW cmd

---
+++ src/commands/bso/sotw.ts
@@ -0,0 +1,39 @@
+import { MessageEmbed } from 'discord.js';
+import { KlasaMessage } from 'klasa';
+
+import { BotCommand } from '../../lib/structures/BotCommand';
+
+export default class extends BotCommand {
+	async run(msg: KlasaMessage) {
+		const res = await this.client.orm.query(
+			`SELECT "new_user".username, sum(xp) as total_xp
+FROM xp_gains
+INNER JOIN "new_users" "new_user" ON xp_gains.user_id = "new_user"."id"
+WHERE date > ('2021-06-05 07:00:00'::timestamp)
+AND date < ('2021-06-12 07:00:00'::timestamp)
+AND skill = 'construction'
+GROUP BY "new_user".username
+ORDER BY total_xp DESC
+LIMIT 15;`
+		);
+
+		if (res.length === 0) {
+			return msg.channel.send(`No results found.`);
+		}
+
+		const embed = new MessageEmbed()
+			.setTitle(`#1 BSO SOTW - Construction`)
+			.setDescription(
+				res
+					.map(
+						(i: any) =>
+							`**${i.username ?? 'Unknown'}** ${Number(
+								i.total_xp
+							).toLocaleString()} XP`
+					)
+					.join('\n')
+			);
+
+		return msg.send(embed);
+	}
+}
