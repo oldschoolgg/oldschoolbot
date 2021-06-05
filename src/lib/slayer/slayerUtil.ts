8c01fbe11a73c2374527b5bb931a060255e83a4b
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-04 20:25:51 -0700

    No points until 5th task in a row.

--- src/lib/slayer/slayerUtil.ts
+++ src/lib/slayer/slayerUtil.ts
@@ -14,6 +14,9 @@ export function calculateSlayerPoints(currentStreak: number, master: SlayerMaste
 	const streaks = [1000, 250, 100, 50, 10];
 	const multiplier = [50, 35, 25, 15, 5];
 
+	if (currentStreak < 5) {
+		return 0;
+	}
 	for (let i = 0; i < streaks.length; i++) {
 		if (currentStreak >= streaks[i] && currentStreak % streaks[i] === 0) {
 			return master.basePoints * multiplier[i];
