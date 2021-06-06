941c2a1e06f5299a40bc7e21033f66217b5f56f1
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-06 15:50:20 -0700

    Only boss tasks with unlock v2

--- src/lib/slayer/tasks/nieveTasks.ts
+++ src/lib/slayer/tasks/nieveTasks.ts
@@ -1,7 +1,6 @@
 import { Monsters } from 'oldschooljs';
 
 import { AssignableSlayerTask } from '../types';
-import { bossTasks } from './bossTasks';
 
 export const nieveTasks: AssignableSlayerTask[] = [
 	{
