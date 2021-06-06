1ce76a92c07f007f930af40b5c826f85f3822c5b
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-06 09:24:13 -0700

    bug fixes - think we got it

--- src/commands/Minion/slayertask.ts
+++ src/commands/Minion/slayertask.ts
@@ -202,9 +202,13 @@ You've done ${totalTasksDone} tasks. Your current streak is ${msg.author.setting
 		if (myUnlocks) {
 			SlayerRewardsShop.filter(srs => { return srs.extendID !== undefined; })
 				.forEach(srsf => {
-					if (srsf.extendID!.includes(newSlayerTask.currentTask.monsterID)) {
+					if (
+						myUnlocks.includes(srsf.id)
+							&& srsf.extendID!.includes(newSlayerTask.currentTask.monsterID)
+					) {
 						console.log(`Extending... previous: ${newSlayerTask.currentTask.quantity}`);
-						newSlayerTask.currentTask.quantity *= srsf.extendMult!;
+						newSlayerTask.currentTask.quantity =
+							Math.ceil(newSlayerTask.currentTask.quantity * srsf.extendMult!);
 						console.log(`New: ${newSlayerTask.currentTask.quantity}`);
 						newSlayerTask.currentTask.save();
 					}
