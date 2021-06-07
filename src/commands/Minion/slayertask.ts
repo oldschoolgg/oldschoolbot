b0cf9498e2c8f3ccc3508baaea78a67764dbe450
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-06 19:20:51 -0700

    slayer task options cleanup 4 bug fix

--- src/commands/Minion/slayertask.ts
+++ src/commands/Minion/slayertask.ts
@@ -232,7 +232,7 @@ export default class extends BotCommand {
 			if (currentTask && assignedTask) {
 				const altMobs = assignedTask.monsters;
 				altMobs.forEach(m => {
-					const monster = killableMonsters.find(mon => mon.id === m && m !== assignedTask.monsterID);
+					const monster = killableMonsters.find(mon => mon.id === m && m !== assignedTask.monster.id);
 					alternateMonsters.push(monster!.name);
 				});
 				monsterList = alternateMonsters.length > 1
