38e8fb2bd205458c3fd0c32953f03d010d7fb4e6
gc <30398469+gc@users.noreply.github.com>
2021-06-07 15:41:31 +1000

    make spawnbox use new challenge system

--- src/commands/bso/spawnbox.ts
+++ src/commands/bso/spawnbox.ts
@@ -1,15 +1,12 @@
-import { MessageEmbed } from 'discord.js';
+import { randArrItem } from 'e';
 import { CommandStore, KlasaMessage } from 'klasa';
-import { Items } from 'oldschooljs';
-import { Item } from 'oldschooljs/dist/meta/types';
-import { cleanString } from 'oldschooljs/dist/util';
+import { Bank } from 'oldschooljs';
 
-import { BitField, Color, PerkTier } from '../../lib/constants';
+import { BitField, PerkTier } from '../../lib/constants';
 import { getRandomMysteryBox } from '../../lib/data/openables';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import { BotCommand } from '../../lib/structures/BotCommand';
-import { itemID, roll } from '../../lib/util';
-import getOSItem from '../../lib/util/getOSItem';
+import { itemChallenge, reactChallenge, triviaChallenge } from '../../monitors/boxSpawns';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -32,43 +29,19 @@ export default class extends BotCommand {
 		if (msg.author.id !== '157797566833098752' && msg.channel.id !== '732207379818479756') {
 			return msg.send(`You can only use this in the BSO channel.`);
 		}
-
-		const randomItem = Items.filter(i => (i as Item).tradeable_on_ge).random() as Item;
-
-		const embed = new MessageEmbed()
-			.setColor(Color.Orange)
-			.setThumbnail(`https://static.runelite.net/cache/item/icon/${randomItem.id}.png`)
-			.setTitle(
-				`${msg.author.username} has spawned a Mystery Box. Tell me what this item is called, for a Mystery Box!`
-			)
-			.setDescription(randomItem.examine);
-
-		await msg.channel.send(embed);
-
-		try {
-			const collected = await msg.channel.awaitMessages(
-				_msg =>
-					cleanString(_msg.content.toLowerCase()) ===
-						cleanString(randomItem.name.toLowerCase()) && !_msg.author.isIronman,
-				{
-					max: 1,
-					time: 14_000,
-					errors: ['time']
-				}
-			);
-
-			const col = collected.first();
-			if (!col) return;
-			const winner = col.author!;
-			const box = roll(10) ? getRandomMysteryBox() : itemID('Mystery box');
-			await winner.addItemsToBank({ [box]: 1 });
-			return msg.channel.send(
-				`Congratulations, ${winner}! You got it. I've given you: **1x ${
-					getOSItem(box).name
-				}**.`
-			);
-		} catch (err) {
-			return msg.channel.send(`Nobody got it! :(`);
+		const item = randArrItem([
+			itemChallenge,
+			itemChallenge,
+			itemChallenge,
+			itemChallenge,
+			reactChallenge,
+			triviaChallenge
+		]);
+		const winner = await item(msg);
+		if (winner) {
+			const loot = new Bank().add(getRandomMysteryBox());
+			await winner.addItemsToBank(loot);
+			return msg.channel.send(`Congratulations, ${winner}! You received: **${loot}**.`);
 		}
 	}
 }
