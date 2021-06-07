0a42a8810a80ca56f8af86d278ca30b726ef3f2f
gc <30398469+gc@users.noreply.github.com>
2021-06-07 15:17:54 +1000

    increase dice limit to 10b

--- src/commands/Minion/dice.ts
+++ src/commands/Minion/dice.ts
@@ -34,8 +34,8 @@ export default class extends BotCommand {
 		} else {
 			if (msg.author.isIronman) return msg.send(`You're an ironman and you cant play dice.`);
 
-			if (amount < 20_000_000 || amount > 400_000_000) {
-				return msg.send(`You must dice atleast 20m and less than 400m.`);
+			if (amount < 20_000_000 || amount > 10_000_000_000) {
+				return msg.send(`You must dice atleast 20m and less than 10b.`);
 			}
 
 			await msg.author.settings.sync(true);
