6ae9086b5f3daa7f2958a6164abc3c16050a88b5
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-06-07 17:52:14 -1000

    ran yarn lint

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,56 +1,47 @@
-import getUserFoodFromBank from "../src/lib/minions/functions/getUserFoodFromBank";
-import { stripEmojis } from "../src/lib/util";
-import getOSItem from "../src/lib/util/getOSItem";
-import itemID from "../src/lib/util/itemID";
+import getUserFoodFromBank from '../src/lib/minions/functions/getUserFoodFromBank';
+import { stripEmojis } from '../src/lib/util';
+import getOSItem from '../src/lib/util/getOSItem';
+import itemID from '../src/lib/util/itemID';
 
-describe("util", () => {
-	test("stripEmojis", () => {
-		expect(stripEmojis("b👏r👏u👏h")).toEqual("bruh");
+describe('util', () => {
+	test('stripEmojis', () => {
+		expect(stripEmojis('b👏r👏u👏h')).toEqual('bruh');
 	});
 
-	test("getOSItem", () => {
-		expect(getOSItem("Twisted bow").id).toEqual(20997);
+	test('getOSItem', () => {
+		expect(getOSItem('Twisted bow').id).toEqual(20997);
 		expect(getOSItem(20997).id).toEqual(20997);
-		expect(getOSItem("20997").id).toEqual(20997);
-		expect(getOSItem("3rd age platebody").id).toEqual(10348);
+		expect(getOSItem('20997').id).toEqual(20997);
+		expect(getOSItem('3rd age platebody').id).toEqual(10348);
 
-		expect(() => getOSItem("Non-existant item")).toThrowError(
-			"That item doesn't exist."
-		);
+		expect(() => getOSItem('Non-existant item')).toThrowError("That item doesn't exist.");
 	});
 
-	test("getUserFoodFromBank", () => {
-		expect(
-			getUserFoodFromBank({ [itemID("Shark")]: 1 }, 500)
-		).toStrictEqual(false);
-		expect(
-			getUserFoodFromBank({ [itemID("Shark")]: 100 }, 500)
-		).toStrictEqual({
-			[itemID("Shark")]: 25,
+	test('getUserFoodFromBank', () => {
+		expect(getUserFoodFromBank({ [itemID('Shark')]: 1 }, 500)).toStrictEqual(false);
+		expect(getUserFoodFromBank({ [itemID('Shark')]: 100 }, 500)).toStrictEqual({
+			[itemID('Shark')]: 25
 		});
 		expect(
-			getUserFoodFromBank(
-				{ [itemID("Shark")]: 30, [itemID("Tuna")]: 20 },
-				750
-			)
+			getUserFoodFromBank({ [itemID('Shark')]: 30, [itemID('Tuna')]: 20 }, 750)
 		).toStrictEqual({
-			[itemID("Tuna")]: 20,
-			[itemID("Shark")]: 28,
+			[itemID('Tuna')]: 20,
+			[itemID('Shark')]: 28
 		});
 		expect(
 			getUserFoodFromBank(
 				{
-					[itemID("Shark")]: 100,
-					[itemID("Lobster")]: 20,
-					[itemID("Shrimps")]: 50,
-					[itemID("Coal")]: 1,
+					[itemID('Shark')]: 100,
+					[itemID('Lobster')]: 20,
+					[itemID('Shrimps')]: 50,
+					[itemID('Coal')]: 1
 				},
 				1600
 			)
 		).toStrictEqual({
-			[itemID("Lobster")]: 20,
-			[itemID("Shark")]: 68,
-			[itemID("Shrimps")]: 50,
+			[itemID('Lobster')]: 20,
+			[itemID('Shark')]: 68,
+			[itemID('Shrimps')]: 50
 		});
 	});
 });
