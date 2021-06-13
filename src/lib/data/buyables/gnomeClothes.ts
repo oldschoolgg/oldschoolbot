9f88ad5f5724237f1c78471afa42bebc050dfb05
gc <30398469+gc@users.noreply.github.com>
2021-06-13 20:23:32 +1000

    buyables cleanup/refactoring

--- src/lib/data/buyables/gnomeClothes.ts
+++ src/lib/data/buyables/gnomeClothes.ts
@@ -4,37 +4,22 @@ import { Buyable } from './buyables';
 export const gnomeClothes: Buyable[] = [
 	{
 		name: 'Pink boots',
-		outputItems: resolveNameBank({
-			'Pink boots': 1
-		}),
 		gpCost: 100_000
 	},
 	{
 		name: 'Green boots',
-		outputItems: resolveNameBank({
-			'Green boots': 1
-		}),
 		gpCost: 100_000
 	},
 	{
 		name: 'Blue boots',
-		outputItems: resolveNameBank({
-			'Blue boots': 1
-		}),
 		gpCost: 100_000
 	},
 	{
 		name: 'Cream boots',
-		outputItems: resolveNameBank({
-			'Cream boots': 1
-		}),
 		gpCost: 100_000
 	},
 	{
 		name: 'Turquoise boots',
-		outputItems: resolveNameBank({
-			'Turquoise boots': 1
-		}),
 		gpCost: 100_000
 	}
 ];
