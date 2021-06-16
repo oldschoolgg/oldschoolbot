c0351b6b29b74cfeb94f62f0f0a4a96fbd4b94c6
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-16 11:27:39 -0700

    Fix OSB Daily reminder message.

--- src/tasks/dailyReminder.ts
+++ src/tasks/dailyReminder.ts
@@ -1,15 +1,31 @@
 import { Task } from 'klasa';
 
+import { client } from '../index';
 import { PerkTier, Time } from '../lib/constants';
 import { UserSettings } from '../lib/settings/types/UserSettings';
 import { noOp } from '../lib/util';
 import getUsersPerkTier from '../lib/util/getUsersPerkTier';
 
 export default class extends Task {
+	async init() {
+		if (this.client.dailyReminderTicker) {
+			clearInterval(this.client.dailyReminderTicker);
+		}
+		this.client.dailyReminderTicker = setInterval(this.dailyReminderTick.bind(this), Time.Second * 60);
+	}
+
 	async run() {
+		this.dailyReminderTick();
+	}
+
+	async dailyReminderTick() {
 		const currentDate = Date.now();
 
-		for (const user of this.client.users.cache.values()) {
+		const result = await client.query<{ id: string }[]>('SELECT id FROM users;');
+
+		if (result.length === 0) return null;
+		for (const userID of result.values()) {
+			const user = await client.users.fetch(userID.id);
 			if (getUsersPerkTier(user) < PerkTier.Two) continue;
 			const lastVoteDate = user.settings.get(UserSettings.LastDailyTimestamp);
 			if (lastVoteDate === -1) continue;
