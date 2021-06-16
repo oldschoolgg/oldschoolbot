e851b930e580d0fb3a4f0b577b6fb381fe68dc69
6enny6 <76587427+6enny6@users.noreply.github.com>
2021-06-16 19:09:43 +0100

    Update filterables.ts

--- src/lib/data/filterables.ts
+++ src/lib/data/filterables.ts
@@ -780,7 +780,7 @@ const diango = resolveItems([
 	'White toy horsey',
 	'Black toy horsey',
 	'Grey toy horsey'
-]) as number['11705', '11706'];
+])
 
 const food = resolveItems(Eatables.map(food => food.name));
 
