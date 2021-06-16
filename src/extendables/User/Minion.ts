c6fb61e187a7a6f84da7ba64e25d05f27f0e0687
gc <30398469+gc@users.noreply.github.com>
2021-06-16 22:54:59 +1000

    add status for chompy hunting

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -559,6 +559,10 @@ export default class extends Extendable {
 			case Activity.MageArena2: {
 				return `${this.minionName} is currently attempting the Mage Arena II. ${formattedDuration}`;
 			}
+
+			case Activity.BigChompyBirdHunting: {
+				return `${this.minionName} is currently hunting Chompy Birds! ${formattedDuration}`;
+			}
 		}
 	}
 
