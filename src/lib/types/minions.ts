915924c2e1588b9c60132ede3e3bf929f58c4c3e
GC <30398469+gc@users.noreply.github.com>
2021-06-17 21:51:31 +1000

    Dark altar (#2089)
    * wip
    
    * commit wip
    
    * fix
    
    * remove logging
    
    * remove logging
    
    * add pet drop to dark altar
    
    * fixes

--- src/lib/types/minions.ts
+++ src/lib/types/minions.ts
@@ -27,6 +27,12 @@ export interface WealthChargingActivityTaskOptions extends ActivityTaskOptions {
 	quantity: number;
 }
 
+export interface DarkAltarOptions extends ActivityTaskOptions {
+	quantity: number;
+	hasElite: boolean;
+	rune: 'blood' | 'soul';
+}
+
 export interface AgilityActivityTaskOptions extends ActivityTaskOptions {
 	courseID: string;
 	quantity: number;
