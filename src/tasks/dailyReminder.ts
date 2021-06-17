f5cd9f27523d3fb5ce5e2a25029577d12a34fe08
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-17 01:11:18 -0700

    Do it the smart way this time

--- src/tasks/dailyReminder.ts
+++ src/tasks/dailyReminder.ts
@@ -1,17 +1,19 @@
 import { Task } from 'klasa';
 
 import { client } from '../index';
-import { PerkTier, Time } from '../lib/constants';
+import { Time } from '../lib/constants';
 import { UserSettings } from '../lib/settings/types/UserSettings';
 import { noOp } from '../lib/util';
-import getUsersPerkTier from '../lib/util/getUsersPerkTier';
+
+const DailyInterval = 12 * 60 * 60;
+const DailyTickInterval = Time.Second * 60;
 
 export default class extends Task {
 	async init() {
 		if (this.client.dailyReminderTicker) {
 			clearInterval(this.client.dailyReminderTicker);
 		}
-		this.client.dailyReminderTicker = setInterval(this.dailyReminderTick.bind(this), Time.Second * 60);
+		this.client.dailyReminderTicker = setInterval(this.dailyReminderTick.bind(this), DailyTickInterval);
 	}
 
 	async run() {
@@ -21,20 +23,17 @@ export default class extends Task {
 	async dailyReminderTick() {
 		const currentDate = Date.now();
 
-		const result = await client.query<{ id: string }[]>('SELECT id FROM users;');
+		const dailyReady = currentDate - DailyInterval;
+		const result = await client.query<{ id: string }[]>(
+			`SELECT id FROM users WHERE bitfield && '{2,3,4,5,6}'::int[] AND "lastDailyTimestamp" != -1 AND "lastDailyTimestamp" < ${dailyReady};`
+		);
 
 		if (result.length === 0) return null;
 		for (const userID of result.values()) {
 			const user = await client.users.fetch(userID.id);
-			if (getUsersPerkTier(user) < PerkTier.Two) continue;
-			const lastVoteDate = user.settings.get(UserSettings.LastDailyTimestamp);
-			if (lastVoteDate === -1) continue;
 
-			const difference = currentDate - lastVoteDate;
-			if (difference >= Time.Hour * 12) {
-				await user.settings.update(UserSettings.LastDailyTimestamp, -1);
-				await user.send('Your daily is ready!').catch(noOp);
-			}
+			await user.settings.update(UserSettings.LastDailyTimestamp, -1);
+			await user.send('Your daily is ready!').catch(noOp);
 		}
 	}
 }
