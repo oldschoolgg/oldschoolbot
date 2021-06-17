a80fd8b54af81dddeda65251714bbb27c6caf09e
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-17 05:06:26 -0700

    Review

--- src/tasks/dailyReminder.ts
+++ src/tasks/dailyReminder.ts
@@ -5,15 +5,15 @@ import { Time } from '../lib/constants';
 import { UserSettings } from '../lib/settings/types/UserSettings';
 import { noOp } from '../lib/util';
 
-const DailyInterval = 12 * 60 * 60;
-const DailyTickInterval = Time.Second * 60;
+const dailyInterval = 12 * 60 * 60;
+const dailyTickInterval = Time.Second * 60;
 
 export default class extends Task {
 	async init() {
 		if (this.client.dailyReminderTicker) {
 			clearInterval(this.client.dailyReminderTicker);
 		}
-		this.client.dailyReminderTicker = setInterval(this.dailyReminderTick.bind(this), DailyTickInterval);
+		this.client.dailyReminderTicker = setInterval(this.dailyReminderTick.bind(this), dailyTickInterval);
 	}
 
 	async run() {
@@ -23,14 +23,13 @@ export default class extends Task {
 	async dailyReminderTick() {
 		const currentDate = Date.now();
 
-		const dailyReady = currentDate - DailyInterval;
+		const dailyReady = currentDate - dailyInterval;
 		const result = await client.query<{ id: string }[]>(
 			`SELECT id FROM users WHERE bitfield && '{2,3,4,5,6}'::int[] AND "lastDailyTimestamp" != -1 AND "lastDailyTimestamp" < ${dailyReady};`
 		);
 
-		if (result.length === 0) return null;
-		for (const userID of result.values()) {
-			const user = await client.users.fetch(userID.id);
+		for (const row of result.values()) {
+			const user = await client.users.fetch(row.id);
 
 			await user.settings.update(UserSettings.LastDailyTimestamp, -1);
 			await user.send('Your daily is ready!').catch(noOp);
