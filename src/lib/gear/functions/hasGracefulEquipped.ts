5f6ca85128e3d1370421f2c10516940a2666a65c
gc <30398469+gc@users.noreply.github.com>
2021-06-23 05:05:00 +1000

    add trailblazer graceful

--- src/lib/gear/functions/hasGracefulEquipped.ts
+++ src/lib/gear/functions/hasGracefulEquipped.ts
@@ -9,7 +9,8 @@ export const gracefulHoods = [
 	'Hosidius graceful hood',
 	'Kourend graceful hood',
 	'Brimhaven graceful hood',
-	'Dark graceful hood'
+	'Dark graceful hood',
+	'Trailblazer graceful hood'
 ];
 
 export const gracefulTops = [
@@ -21,7 +22,8 @@ export const gracefulTops = [
 	'Hosidius graceful top',
 	'Kourend graceful top',
 	'Brimhaven graceful top',
-	'Dark graceful top'
+	'Dark graceful top',
+	'Trailblazer graceful top'
 ];
 
 export const gracefulLegs = [
@@ -33,7 +35,8 @@ export const gracefulLegs = [
 	'Hosidius graceful legs',
 	'Kourend graceful legs',
 	'Brimhaven graceful legs',
-	'Dark graceful legs'
+	'Dark graceful legs',
+	'Trailblazer graceful legs'
 ];
 
 export const gracefulFeet = [
@@ -45,7 +48,8 @@ export const gracefulFeet = [
 	'Hosidius graceful boots',
 	'Kourend graceful boots',
 	'Brimhaven graceful boots',
-	'Dark graceful boots'
+	'Dark graceful boots',
+	'Trailblazer graceful boots'
 ];
 
 export const gracefulHands = [
@@ -57,7 +61,8 @@ export const gracefulHands = [
 	'Hosidius graceful gloves',
 	'Kourend graceful gloves',
 	'Brimhaven graceful gloves',
-	'Dark graceful gloves'
+	'Dark graceful gloves',
+	'Trailblazer graceful gloves'
 ];
 
 export const gracefulCapes = [
@@ -70,6 +75,7 @@ export const gracefulCapes = [
 	'Kourend graceful cape',
 	'Brimhaven graceful cape',
 	'Dark graceful cape',
+	'Trailblazer graceful cape',
 	'Agility cape',
 	'Agility cape (t)',
 	'Max cape'
