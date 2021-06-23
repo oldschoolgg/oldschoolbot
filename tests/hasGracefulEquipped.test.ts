b5911a357d0718bc33c217f2f025bc1dcf85d003
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-22 21:22:31 -0700

    Fix graceful tests

--- tests/hasGracefulEquipped.test.ts
+++ tests/hasGracefulEquipped.test.ts
@@ -32,9 +32,9 @@ describe('hasGracefulEquipped', () => {
 		expect(hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Agility cape' }))).toEqual(true);
 		expect(
 			hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Agility cape', head: 'Agility hood' }))
-		).toEqual(true);
+		).toEqual(false);
 		expect(hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Agility cape(t)' }))).toEqual(true);
 		expect(hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Max cape' }))).toEqual(true);
-		expect(hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Max cape', head: 'Max hood' }))).toEqual(true);
+		expect(hasGracefulEquipped(constructGearSetup({ ...base, cape: 'Max cape', head: 'Max hood' }))).toEqual(false);
 	});
 });
