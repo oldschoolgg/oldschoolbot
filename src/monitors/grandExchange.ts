9a9594982e63788ac3e401e782de4488090148a2
Lily <Lilylicious@users.noreply.github.com>
2021-06-26 21:02:04 +0200

    Minor fix

--- src/monitors/grandExchange.ts
+++ src/monitors/grandExchange.ts
@@ -25,16 +25,21 @@ export default class extends Monitor {
 			)
 		) {
 			errorStr +=
-				'Your message was automatically removed from the grand exchange channel because it did not include one of the mandatory words(buying, selling, trading, trade, swap, swapping).';
+				'Your message was automatically removed from the grand exchange channel because it did not include one of the mandatory words(buying, selling, trading, trade, swap, swapping)';
 		}
 
-		if (msg.content.split(/\r\n|\r|\n/).length > 10 || msg.cleanContent.length > 450)
-			errorStr +=
-				'Your message was automatically removed from the grand exchange channel because it was either over 10 lines long OR over 450 characters long.';
+		if (msg.content.split(/\r\n|\r|\n/).length > 10 || msg.cleanContent.length > 450) {
+			if (errorStr !== '') {
+				errorStr += ' and because it was either over 10 lines long OR over 450 characters long';
+			} else {
+				errorStr +=
+					'Your message was automatically removed from the grand exchange channel because it was either over 10 lines long OR over 450 characters long';
+			}
+		}
 
 		if (errorStr !== '') {
 			errorStr +=
-				' Please take a second to read the rules here: https://discordapp.com/channels/342983479501389826/682996313209831435/706772870923288618';
+				'. Please take a second to read the rules here: https://discordapp.com/channels/342983479501389826/682996313209831435/706772870923288618';
 			await msg.delete();
 			await msg.author.send(errorStr);
 		}
