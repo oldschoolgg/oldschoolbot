0ec33166080d44295e5ecdf48ea8e3bfdd04de23
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-27 08:55:45 -0700

    Swapped a test with a better one

--- tests/parseStringBank.test.ts
+++ tests/parseStringBank.test.ts
@@ -201,7 +201,7 @@ describe('Bank Parsers', () => {
 		const bank = new Bank().add('Steel arrow').add('Bones').add('Coal', 500).add('Clue scroll (easy)');
 		expect(parseBank({ inputBank: bank, inputStr: '1 Portrait' }).toString()).toEqual('No items');
 		expect(parseBank({ inputBank: bank, inputStr: '1 666' }).toString()).toEqual('No items');
-		expect(parseBank({ inputBank: bank, inputStr: '5 Coal' }).toString()).toEqual('5x Coal');
+		expect(parseBank({ inputBank: bank, inputStr: '526' }).toString()).toEqual('1x Bones');
 		expect(parseBank({ inputBank: bank, inputStr: '0 Coal' }).toString()).toEqual('500x Coal');
 	});
 });
