13ab875c18a70c9f312be25b0cbfabcb80743f2b
gc <30398469+gc@users.noreply.github.com>
2021-07-06 18:13:12 +1000

    move jmods file contents

--- src/events/redditPosts.ts
+++ src/events/redditPosts.ts
@@ -5,10 +5,33 @@ import { CommentStream, SubmissionStream } from 'snoostorm';
 import Snoowrap from 'snoowrap';
 
 import { redditAppConfig } from '../config';
-import JagexMods from '../lib/data/jagexMods';
 import { GuildSettings } from '../lib/settings/types/GuildSettings';
 import { JMod } from '../lib/types';
 import { sendToChannelID } from '../lib/util/webhook';
+
+const JagexMods: JMod[] = [
+	{ redditUsername: 'jagexjd', formattedName: 'Mod JD' },
+	{ redditUsername: 'mod_kieren', formattedName: 'Mod Kieren' },
+	{ redditUsername: 'jagexsween', formattedName: 'Mod Sween' },
+	{ redditUsername: 'jagexbruno', formattedName: 'Mod Bruno' },
+	{ redditUsername: 'mod_archie', formattedName: 'Mod Archie' },
+	{ redditUsername: 'jagexroq', formattedName: 'Mod Roq' },
+	{ redditUsername: 'mod_west', formattedName: 'Mod West' },
+	{ redditUsername: 'mod_ed', formattedName: 'Mod Ed' },
+	{ redditUsername: 'mod_ry', formattedName: 'Mod Ry' },
+	{ redditUsername: 'jagexash', formattedName: 'Mod Ash' },
+	{ redditUsername: 'mod_curse', formattedName: 'Mod Curse' },
+	{ redditUsername: 'jagexgee', formattedName: 'Mod Gee' },
+	{ redditUsername: 'jagexlenny', formattedName: 'Mod Lenny' },
+	{ redditUsername: 'jagexlottie', formattedName: 'Mod Lottie' },
+	{ redditUsername: 'jagexmunro', formattedName: 'Mod Munro' },
+	{ redditUsername: 'jagexacorn', formattedName: 'Mod Acorn' },
+	{ redditUsername: 'jagex_weath', formattedName: 'Mod Weath' },
+	{ redditUsername: 'jagextide', formattedName: 'Mod Tide' },
+	{ redditUsername: 'jagexflippy', formattedName: 'Mod Flippy' },
+	{ redditUsername: 'jagexbolton', formattedName: 'Mod Bolton' },
+	{ redditUsername: 'jagexarcane', formattedName: 'Mod Arcane' }
+];
 
 const jmodAccounts = JagexMods.filter(jmod => jmod.redditUsername).map(jmod => jmod.redditUsername);
 
