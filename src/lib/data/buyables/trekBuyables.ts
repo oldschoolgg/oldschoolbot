f7747b88cd9af771e58a9e51a0e7f475f652177b
Lily <Lilylicious@users.noreply.github.com>
2021-07-09 15:12:06 +0200

    Fix trekshop (#2309)
    * You can now actually buy lobster and watermelon seeds.

--- src/lib/data/buyables/trekBuyables.ts
+++ src/lib/data/buyables/trekBuyables.ts
@@ -5,7 +5,6 @@ interface TrekShopItem {
 	easyRange: [number, number];
 	medRange: [number, number];
 	hardRange: [number, number];
-	possibleResults: string[];
 	aliases?: string[];
 }
 
@@ -15,62 +14,54 @@ const TrekShopItems: TrekShopItem[] = [
 		easyRange: [50, 150],
 		medRange: [120, 380],
 		hardRange: [370, 660],
-		aliases: ['pure', 'essence'],
-		possibleResults: ['Pure essence']
+		aliases: ['pure', 'essence']
 	},
 	{
 		name: 'Bow string',
 		easyRange: [100, 200],
 		medRange: [150, 250],
 		hardRange: [200, 300],
-		aliases: ['bow', 'string'],
-		possibleResults: ['Bow string']
+		aliases: ['bow', 'string']
 	},
 	{
 		name: 'Silver bar',
 		easyRange: [5, 50],
 		medRange: [50, 100],
 		hardRange: [100, 150],
-		aliases: ['silver', 'bar'],
-		possibleResults: ['Silver bar']
+		aliases: ['silver', 'bar']
 	},
 	{
 		name: 'Herbs',
 		easyRange: [5, 15],
 		medRange: [10, 20],
-		hardRange: [15, 30],
-		possibleResults: ['Tarromin', 'Harralander', 'Toadflax']
+		hardRange: [15, 30]
 	},
 	{
 		name: 'Ore',
 		easyRange: [21, 72],
 		medRange: [66, 165],
-		hardRange: [150, 216],
-		possibleResults: ['Coal', 'Iron ore']
+		hardRange: [150, 216]
 	},
 	{
-		name: 'Watermelon seeds',
+		name: 'Watermelon seed',
 		easyRange: [10, 20],
 		medRange: [15, 30],
 		hardRange: [20, 40],
-		aliases: ['water', 'watermelon', 'seeds'],
-		possibleResults: ['Watermelon seeds']
+		aliases: ['water', 'watermelon', 'seeds', 'watermelon seeds']
 	},
 	{
-		name: 'Raw lobsters',
+		name: 'Raw lobster',
 		easyRange: [2, 35],
 		medRange: [30, 65],
 		hardRange: [60, 80],
-		aliases: ['water', 'watermelon', 'seeds'],
-		possibleResults: ['Raw lobsters']
+		aliases: ['lobster', 'raw lobsters']
 	},
 	{
 		name: 'Experience',
 		easyRange: [1100, 1650],
 		medRange: [2035, 3025],
 		hardRange: [4015, 5005],
-		aliases: ['xp'],
-		possibleResults: []
+		aliases: ['xp']
 	}
 ];
 
