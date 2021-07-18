2843eec2d745a0856b693ab4cb09b8df539e02cf
Lily <Lilylicious@users.noreply.github.com>
2021-07-18 14:12:49 +0200

    Added clarity to GE error message

--- src/monitors/grandExchange.ts
+++ src/monitors/grandExchange.ts
@@ -17,17 +17,31 @@ export default class extends Monitor {
 			return;
 		}
 
+		let errorStr = '';
+
 		if (
 			['buying', 'selling', 'trading', 'trade', 'swap', 'swapping'].every(
 				str => !msg.content.toLowerCase().includes(str)
-			) ||
-			msg.content.split(/\r\n|\r|\n/).length > 10 ||
-			msg.cleanContent.length > 450
+			)
 		) {
+			errorStr +=
+				'Your message was automatically removed from the grand exchange channel because it did not include one of the mandatory words(buying, selling, trading, trade, swap, swapping)';
+		}
+
+		if (msg.content.split(/\r\n|\r|\n/).length > 10 || msg.cleanContent.length > 450) {
+			if (errorStr !== '') {
+				errorStr += ' and because it was either over 10 lines long OR over 450 characters long';
+			} else {
+				errorStr +=
+					'Your message was automatically removed from the grand exchange channel because it was either over 10 lines long OR over 450 characters long';
+			}
+		}
+
+		if (errorStr !== '') {
+			errorStr +=
+				'. Please take a second to read the rules here: https://discordapp.com/channels/342983479501389826/682996313209831435/706772870923288618';
 			await msg.delete();
-			await msg.author.send(
-				'Your message was automatically removed from the grand exchange channel, because it was either over 10 lines long OR over 450 characters long. Please take a second to read the rules here: https://discordapp.com/channels/342983479501389826/682996313209831435/706772870923288618'
-			);
+			await msg.author.send(errorStr);
 		}
 	}
 }
