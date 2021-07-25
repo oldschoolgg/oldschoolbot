25a707272bc3f7686588539d003f61d86cfab6f6
Munken <dm@me.ondiscord>
2021-07-25 15:39:14 +0300

    Fix up messages and lint

--- src/commands/Minion/fightcaves.ts
+++ src/commands/Minion/fightcaves.ts
@@ -156,7 +156,7 @@ export default class extends BotCommand {
 		// 15% boost for on task
 		if (isOnTask && msg.author.hasItemEquippedOrInBank('Black mask (i)')) {
 			duration *= 0.85;
-			debugStr = debugStr === '' ? '15% on Task with Black mask (i)' : ', 15% on Task with Black mask (i)';
+			debugStr += ', 15% on Task with Black mask (i)';
 		}
 
 		await addSubTaskToActivityTask<FightCavesActivityTaskOptions>({
