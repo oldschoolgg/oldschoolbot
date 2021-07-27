30e586e065d3ab7dfdac4d9530a16878001ba1ec
gc <30398469+gc@users.noreply.github.com>
2021-07-27 18:45:04 +1000

    bonus superior rolls for slayer master cape

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -107,6 +107,7 @@ export const enum Emoji {
 	Dungeoneering = '<:dungeoneering:828683755198873623>',
 	Gear = '<:gear:835314891950129202>',
 	Slayer = '<:slayer:630911040560824330>',
+	SlayerMasterCape = '<:slayerMasterCape:869497600284459008>',
 	// Badges,
 	BigOrangeGem = '<:bigOrangeGem:778418736188489770>',
 	GreenGem = '<:greenGem:778418736495067166>',
