ea365477af620bd5ec76baf93fd9cb14d8f1d5d6
6enny6 <76587427+6enny6@users.noreply.github.com>
2021-07-29 08:18:27 +0100

    Add to MTA message (#2463)

--- src/commands/Minion/mta.ts
+++ src/commands/Minion/mta.ts
@@ -91,7 +91,7 @@ ${buyables
 	.map(i => `${i.item.name} - ${i.cost} pts - ${formatDuration((i.cost / pizazzPointsPerHour) * (Time.Minute * 60))}`)
 	.join('\n')}
 
-Hint: Magic Training Arena is combined into 1 room, and 1 set of points - rewards take approximately the same amount of time to get.`);
+Hint: Magic Training Arena is combined into 1 room, and 1 set of points - rewards take approximately the same amount of time to get. To get started use **+mta train**`);
 	}
 
 	@requiresMinion
