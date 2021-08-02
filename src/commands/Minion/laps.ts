4643ce2a5f3a5349aa8b29381cb6a471f32e4a18
Kevin Santiago Mendes <mbr.ark@gmail.com>
2021-08-02 13:28:04 -0300

    Fix strange message when the user doest have enough runes to alch

--- src/commands/Minion/laps.ts
+++ src/commands/Minion/laps.ts
@@ -60,6 +60,8 @@ function alching(msg: KlasaMessage, tripLength: number) {
 		bankToRemove.add('Fire rune', maxCasts * 5);
 	}
 
+	if (maxCasts === 0 || bankToRemove.items().length === 0) return null;
+
 	return {
 		maxCasts,
 		bankToRemove,
