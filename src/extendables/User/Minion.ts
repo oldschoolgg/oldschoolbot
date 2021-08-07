949e20bba2f462ea62eaa592053cc704528b921b
Lajnux <68188574+Lajnux@users.noreply.github.com>
2021-08-07 22:38:40 +0200

    add driftnet (#2490)
    * add driftnet
    
    * fix requested changes
    
    * yarn lint
    
    * fix typos
    
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -437,6 +437,10 @@ export default class extends Extendable {
 				return `${this.minionName} is currently aerial fishing. ${formattedDuration}`;
 			}
 
+			case Activity.DriftNet: {
+				return `${this.minionName} is currently drift net fishing. ${formattedDuration}`;
+			}
+
 			case Activity.Construction: {
 				const data = currentTask as ConstructionActivityTaskOptions;
 				return `${this.minionName} is currently building ${data.quantity}x ${itemNameFromID(
