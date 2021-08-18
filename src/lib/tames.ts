aa88b4e5d539162a3d1283f5920df68383e96e57
Kevin S. [Gidedin] <mbr.ark@gmail.com>
2021-08-18 04:15:12 -0300

    Fix ate a ori typo and ceil Ori effect instead of floor (#2647)
    * Fix ate a ori
    
    * Ceil ori effect

--- src/lib/tames.ts
+++ src/lib/tames.ts
@@ -99,7 +99,7 @@ export async function runTameTask(activity: TameActivityTable) {
 			// If less than 8 kills, roll 25% chance per kill
 			if (hasOri) {
 				if (killQty >= 8) {
-					killQty = Math.floor(killQty * 1.25);
+					killQty = Math.ceil(killQty * 1.25);
 				} else {
 					for (let i = 0; i < quantity; i++) {
 						if (roll(4)) killQty++;
@@ -112,7 +112,7 @@ export async function runTameTask(activity: TameActivityTable) {
 			let str = `${user}, ${activity.tame.name} finished killing ${quantity}x ${fullMonster.name}.`;
 			const boosts = [];
 			if (hasOri) {
-				boosts.push('25% extra loot (ate a Ori)');
+				boosts.push('25% extra loot (ate an Ori)');
 			}
 			if (boosts.length > 0) {
 				str += `\n\n**Boosts:** ${boosts.join(', ')}.`;
