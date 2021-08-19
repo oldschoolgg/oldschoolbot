01d88a9bca56759101f13a040e5ba7ecf2868a28
gc <30398469+gc@users.noreply.github.com>
2021-08-19 18:37:27 +1000

    bingo xp improvements

--- src/lib/bingo.ts
+++ src/lib/bingo.ts
@@ -1,8 +1,22 @@
 import { KlasaUser } from 'klasa';
+import { SkillsEnum } from 'oldschooljs/dist/constants';
 
 export interface BingoInstance {
 	start: Date;
 	finish: Date;
+}
+
+async function getXPGainsInBingo(user: KlasaUser, bingo: BingoInstance, skills?: SkillsEnum[]) {
+	const query = `SELECT COALESCE(sum(xp), 0) as total_xp
+FROM xp_gains
+WHERE date > to_timestamp(${bingo.start.getTime()} / 1000)::date
+AND date < to_timestamp(${bingo.finish.getTime()} / 1000)::date
+AND artificial IS NULL
+${skills !== undefined ? `AND skill IN (${skills.map(s => `'${s}'`).join(', ')})` : ''}
+AND user_id = '${user.id}';`;
+	console.log(query);
+	const xpGains = await user.client.query<{ total_xp: string }[]>(query);
+	return parseInt(xpGains[0].total_xp);
 }
 
 export interface BingoItem {
@@ -14,30 +28,42 @@ export interface BingoItem {
 export const bingoItems: BingoItem[] = [
 	{
 		id: 1,
-		description: 'Gain 5 million XP in any skills',
+		description: 'Gain 3 million XP in any skills',
 		check: async (user, bingo) => {
-			const query = `SELECT COALESCE(sum(xp), 0) as total_xp
-FROM xp_gains
-WHERE date > to_timestamp(${bingo.start.getTime()} / 1000)::date
-AND date < to_timestamp(${bingo.finish.getTime()} / 1000)::date
-AND artificial IS NULL
-AND user_id = '${user.id}';`;
-			const xpGains = await user.client.query<{ total_xp: string }[]>(query);
-			return parseInt(xpGains[0].total_xp) > 5_000_000;
+			const xpGains = await getXPGainsInBingo(user, bingo);
+			return xpGains > 5_000_000;
 		}
 	},
 	{
 		id: 2,
-		description: 'Gain 5 million XP in any skills',
+		description: 'Gain 1 million XP in Artisan skills',
 		check: async (user, bingo) => {
-			const query = `SELECT COALESCE(sum(xp), 0) as total_xp
-FROM xp_gains
-WHERE date > to_timestamp(${bingo.start.getTime()} / 1000)::date
-AND date < to_timestamp(${bingo.finish.getTime()} / 1000)::date
-AND artificial IS NULL
-AND user_id = '${user.id}';`;
-			const xpGains = await user.client.query<{ total_xp: string }[]>(query);
-			return parseInt(xpGains[0].total_xp) > 5_000_000;
+			const xpGains = await getXPGainsInBingo(user, bingo, [
+				SkillsEnum.Cooking,
+				SkillsEnum.Smithing,
+				SkillsEnum.Fletching,
+				SkillsEnum.Firemaking,
+				SkillsEnum.Herblore,
+				SkillsEnum.Crafting,
+				SkillsEnum.Runecraft,
+				SkillsEnum.Runecraft,
+				SkillsEnum.Construction
+			]);
+			return xpGains > 1_000_000;
+		}
+	},
+	{
+		id: 3,
+		description: 'Gain 1 million XP in Gathering skills',
+		check: async (user, bingo) => {
+			const xpGains = await getXPGainsInBingo(user, bingo, [
+				SkillsEnum.Mining,
+				SkillsEnum.Fishing,
+				SkillsEnum.Woodcutting,
+				SkillsEnum.Hunter,
+				SkillsEnum.Farming
+			]);
+			return xpGains > 1_000_000;
 		}
 	}
 ];
