ce34247046940902fd0f499f815b675e102de370
Kevin Santiago Mendes <mbr.ark@gmail.com>
2021-08-27 16:18:31 -0300

    Changes it to allow for name/alias

--- src/commands/Minion/amrod.ts
+++ src/commands/Minion/amrod.ts
@@ -2,27 +2,37 @@ import { CommandStore, KlasaMessage } from 'klasa';
 import { Bank } from 'oldschooljs';
 
 import { BotCommand } from '../../lib/structures/BotCommand';
-import getOSItem from '../../lib/util/getOSItem';
+import { stringMatches } from '../../lib/util';
 import itemID from '../../lib/util/itemID';
 
 const amrodTradeableList = [
 	{
+		name: 'Crystal weapon seed',
+		alias: ['cws'],
 		require: itemID('Crystal weapon seed'),
 		output: 10
 	},
 	{
+		name: 'Crystal tool seed',
+		alias: ['cts'],
 		require: itemID('Crystal tool seed'),
 		output: 100
 	},
 	{
+		name: 'Enhanced crystal teleport seed',
+		alias: ['ects'],
 		require: itemID('Enhanced crystal teleport seed'),
 		output: 150
 	},
 	{
+		name: 'Crystal armour seed',
+		alias: ['cas'],
 		require: itemID('Crystal armour seed'),
 		output: 250
 	},
 	{
+		name: 'Enhanced crystal weapon seed',
+		alias: ['ecws'],
 		require: itemID('Enhanced crystal weapon seed'),
 		output: 1500
 	}
@@ -44,25 +54,24 @@ export default class extends BotCommand {
 
 	async run(msg: KlasaMessage, [quantity = 1, itemName]: [number, string]) {
 		await msg.author.settings.sync(true);
-		const toTradeItem = getOSItem(itemName);
-		const amrodTrade = amrodTradeableList.find(a => a.require === toTradeItem?.id);
-		if (!toTradeItem || !amrodTrade) {
+		const amrodTrade = amrodTradeableList.find(
+			a => stringMatches(itemName, a.name) || a.alias.some(al => stringMatches(al, itemName))
+		);
+		if (!amrodTrade) {
 			return msg.channel.send(
 				`This is not a valid item. Valid items that can be traded are: ${amrodTradeableList
-					.map(a => getOSItem(a.require).name)
+					.map(a => `${a.name} (${a.alias})`)
 					.join(', ')}`
 			);
 		}
-		const toTradeBank = new Bank().add(toTradeItem.id, quantity);
+		const toTradeBank = new Bank().add(amrodTrade.require, quantity);
 		if (!msg.author.bank().has(toTradeBank.bank)) {
 			return msg.channel.send(`You do not have **${toTradeBank}** to trade. Plese, come back when you do.`);
 		}
 
 		const toReceiveBank = new Bank().add('Crystal shard', amrodTrade.output * quantity);
 
-		await msg.confirm(
-			`Do you want to trade **${quantity.toLocaleString()}x ${toTradeItem.name}** for **${toReceiveBank}**?`
-		);
+		await msg.confirm(`Do you want to trade **${toTradeBank}** for **${toReceiveBank}**?`);
 
 		await msg.author.removeItemsFromBank(toTradeBank);
 		await msg.author.addItemsToBank(toReceiveBank);
