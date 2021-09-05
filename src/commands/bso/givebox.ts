bd679795607c97751d99e1ded66c635662e361f0
Gidedin <mbr.ark@gmail.com>
2021-09-05 01:25:02 -0300

    Refactor =givebox (#2715)
    * Refactor the code a bit, Allow empty command to show time till next box.
    
    * Move the two owner checks to its own variable

--- src/commands/bso/givebox.ts
+++ src/commands/bso/givebox.ts
@@ -1,43 +1,46 @@
 import { Time } from 'e';
 import { CommandStore, KlasaMessage, KlasaUser } from 'klasa';
+import { Bank } from 'oldschooljs';
 
 import { PerkTier } from '../../lib/constants';
 import { getRandomMysteryBox } from '../../lib/data/openables';
 import { UserSettings } from '../../lib/settings/types/UserSettings';
 import { BotCommand } from '../../lib/structures/BotCommand';
 import { formatDuration, itemID, roll } from '../../lib/util';
-import getOSItem from '../../lib/util/getOSItem';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
-			usage: '<user:user>',
+			usage: '[user:user]',
 			perkTier: PerkTier.One,
 			restrictedChannels: ['732207379818479756', '792691343284764693']
 		});
 	}
 
 	async run(msg: KlasaMessage, [user]: [KlasaUser]) {
-		if (user.id === msg.author.id) {
-			return msg.channel.send("You can't give boxes to yourself!");
-		}
-		if (user.isIronman) return;
 		const currentDate = Date.now();
 		const lastDate = msg.author.settings.get(UserSettings.LastGivenBox);
 		const difference = currentDate - lastDate;
+		const timeLimit = Time.Hour * 24;
+		const isOwner = this.client.owners.has(msg.author);
 
-		if (difference < Time.Hour * 24 && msg.author.id !== '157797566833098752') {
-			const duration = formatDuration(Date.now() - (lastDate + Time.Hour * 24));
-			return msg.channel.send(`You can give another box in ${duration}.`);
+		// If no user or not an owner and can not send one yet, show time till next box.
+		if (!user || (difference < timeLimit && !isOwner)) {
+			if (difference >= timeLimit || isOwner) {
+				return msg.channel.send('You can give another box!');
+			}
+			return msg.channel.send(`You can give another box in ${formatDuration(timeLimit - difference)}`);
 		}
+		// Disable box to self or irons
+		if (user.id === msg.author.id) return msg.channel.send("You can't give boxes to yourself!");
+		if (user.isIronman) return msg.channel.send("You can't give boxes to ironmans!");
+
 		await msg.author.settings.update(UserSettings.LastGivenBox, currentDate);
 
-		const box = roll(10) ? getRandomMysteryBox() : itemID('Mystery box');
+		const boxToReceive = new Bank().add(roll(10) ? getRandomMysteryBox() : itemID('Mystery box'));
 
-		await user.addItemsToBank({ [box]: 1 });
+		await user.addItemsToBank(boxToReceive);
 
-		return msg.channel.send(
-			`Gave ${[19_939].includes(box) ? 'an' : 'a'} **${getOSItem(box).name}** to ${user.username}.`
-		);
+		return msg.channel.send(`Gave **${boxToReceive}** to ${user.username}.`);
 	}
 }
