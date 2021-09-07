9537c066c583c150e310a7da583b15de2e80afe0
Gidedin <mbr.ark@gmail.com>
2021-09-07 10:14:08 -0300

    Add missing Monkey Rumble Activity to minion status

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -76,6 +76,7 @@ import {
 	HunterActivityTaskOptions,
 	MinigameActivityTaskOptions,
 	MiningActivityTaskOptions,
+	MonkeyRumbleOptions,
 	MonsterActivityTaskOptions,
 	OfferingActivityTaskOptions,
 	PickpocketActivityTaskOptions,
@@ -626,6 +627,11 @@ export default class extends Extendable {
 			case Activity.VolcanicMine: {
 				const data = currentTask as VolcanicMineActivityTaskOptions;
 				return `${this.minionName} is currently doing ${data.quantity} games of Volcanic Mine. ${formattedDuration}`;
+			}
+
+			case Activity.MonkeyRumble: {
+				const data = currentTask as MonkeyRumbleOptions;
+				return `${this.minionName} is currently playing ${data.quantity} games of Monkey Rumble. ${formattedDuration}`;
 			}
 		}
 	}
