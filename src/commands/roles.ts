9fa0ae1d815cf1083ef5ccb52ea2c07b1ca27d85
Gidedin <mbr.ark@gmail.com>
2021-09-07 00:02:46 -0300

    Rename comment

--- src/commands/roles.ts
+++ src/commands/roles.ts
@@ -45,8 +45,8 @@ export default class extends BotCommand {
 			!msg.guild ||
 			msg.guild.id !== SupportServer ||
 			!role ||
-			// 346238402737340416 = Bot Role
-			// 590527706974781470 = Skira user?
+			// 346238402737340416 = Bot role
+			// 590527706974781470 = Skira role
 			[Roles.Moderator, '590527706974781470', '346238402737340416'].includes(role.id) ||
 			!msg.member ||
 			!msg.member!.roles.cache.has(Roles.Moderator)
