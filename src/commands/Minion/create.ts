1fbef72e182cda40a6d5f67a965a3984526c10ce
Gidedin <mbr.ark@gmail.com>
2021-09-08 11:50:35 -0300

    Rework +create (#2692)
    * Rework +create and categorize revert/fix/unpack/create into their own command alias.
    
    * Add noCL to reverting GS
    
    * Force noCL for reverts/unpack/fix;
    Add dragon full helm g
    
    * Add dragon platebody g
    
    * Add missing ornaments
    
    * Fix deepscan
    
    * Substitute msg.commandText to cmd
    
    * Improves noCl if
    
    * Add reverts for the bowfa and blade of saeldor
    
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

--- src/commands/Minion/create.ts
+++ src/commands/Minion/create.ts
@@ -9,12 +9,20 @@ import { SkillsEnum } from '../../lib/skilling/types';
 import { SlayerTaskUnlocksEnum } from '../../lib/slayer/slayerUnlocks';
 import { hasSlayerUnlock } from '../../lib/slayer/slayerUtil';
 import { BotCommand } from '../../lib/structures/BotCommand';
-import { addBanks, bankHasAllItemsFromBank, itemNameFromID, removeBankFromBank, stringMatches } from '../../lib/util';
+import {
+	addBanks,
+	bankHasAllItemsFromBank,
+	itemNameFromID,
+	removeBankFromBank,
+	stringMatches,
+	toTitleCase
+} from '../../lib/util';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			usage: '[quantity:int{1}] [itemName:...string]',
+			aliases: ['revert', 'fix', 'unpack'],
 			usageDelim: ' ',
 			oneAtTime: true,
 			cooldown: 5,
@@ -26,47 +34,82 @@ export default class extends BotCommand {
 	}
 
 	async run(msg: KlasaMessage, [quantity, itemName]: [number, string]) {
-		if (msg.flagArgs.items) {
+		const cmd = msg.commandText!.toLowerCase();
+		// No items to fix on OSB for now
+		if (cmd === 'fix') return;
+		if (msg.flagArgs.items || !itemName) {
+			let content = `This are the items that you can ${cmd}:`;
 			const creatableTable = table([
-				['Item Name', 'Input Items', 'GP Cost', 'Skills Required', 'QP Required'],
-				...Createables.map(i => [
-					i.name,
-					`${Object.entries(i.inputItems)
-						.map(entry => `${entry[1]} ${itemNameFromID(parseInt(entry[0]))}`)
-						.join('\n')}`,
-					`${i.GPCost ?? 0}`,
-					`${
-						i.requiredSkills === undefined
-							? ''
-							: Object.entries(i.requiredSkills)
-									.map(entry => `${entry[0]}: ${entry[1]}`)
-									.join('\n')
-					}`,
-					`${i.QPRequired ?? ''}`
-				])
+				['Item Name', 'Input Items', 'Output Items', 'GP Cost', 'Skills Required', 'QP Required'],
+				...Createables.filter(f => {
+					return cmd.toLowerCase() === 'create'
+						? !msg.command!.aliases.some(s => f.name.toLowerCase().startsWith(s))
+						: f.name.toLowerCase().startsWith(cmd.toLowerCase());
+				}).map(i => {
+					return [
+						cmd.toLowerCase() === 'create'
+							? i.name
+							: toTitleCase(
+									i.name
+										.split(' ')
+										.filter(m => {
+											return msg.command!.aliases.some(s => stringMatches(s, m)) ? '' : m;
+										})
+										.join(' ')
+										.toLowerCase()
+										.trim()
+							  ),
+						`${Object.entries(i.inputItems)
+							.map(entry => `${entry[1]}x ${itemNameFromID(parseInt(entry[0]))}`)
+							.join('\n')}`,
+						`${Object.entries(i.outputItems)
+							.map(entry => `${entry[1]}x ${itemNameFromID(parseInt(entry[0]))}`)
+							.join('\n')}`,
+						`${i.GPCost ?? 0}`,
+						`${
+							i.requiredSkills === undefined
+								? ''
+								: Object.entries(i.requiredSkills)
+										.map(entry => `${entry[0]}: ${entry[1]}`)
+										.join('\n')
+						}`,
+						`${i.QPRequired ?? ''}`
+					];
+				})
 			]);
-			return msg.channel.send({ files: [new MessageAttachment(Buffer.from(creatableTable), 'Creatables.txt')] });
+			return msg.channel.send({
+				content,
+				files: [new MessageAttachment(Buffer.from(creatableTable), 'Creatables.txt')]
+			});
 		}
-		if (itemName === undefined) {
-			throw 'Item name is a required argument.';
-		}
+
 		itemName = itemName.toLowerCase();
 
-		const createableItem = Createables.find(item => stringMatches(item.name, itemName));
-		if (!createableItem) throw "That's not a valid item you can create.";
+		if (cmd.toLowerCase() === 'revert') itemName = `revert ${itemName}`;
+		if (cmd.toLowerCase() === 'fix') itemName = `fix ${itemName}`;
+		if (cmd.toLowerCase() === 'unpack') itemName = `unpack ${itemName}`;
+
+		const createableItem = Createables.find(
+			item =>
+				stringMatches(item.name, itemName) &&
+				(cmd.toLowerCase() === 'create'
+					? !msg.command!.aliases.some(s => item.name.toLowerCase().startsWith(s))
+					: item.name.toLowerCase().startsWith(cmd.toLowerCase()))
+		);
+		if (!createableItem) throw `That's not a valid item you can ${cmd}.`;
 
 		if (!quantity || createableItem.cantHaveItems) {
 			quantity = 1;
 		}
 
 		if (createableItem.QPRequired && msg.author.settings.get(UserSettings.QP) < createableItem.QPRequired) {
-			throw `You need ${createableItem.QPRequired} QP to create this item.`;
+			throw `You need ${createableItem.QPRequired} QP to ${cmd} this item.`;
 		}
 
 		if (createableItem.requiredSkills) {
 			for (const [skillName, lvl] of Object.entries(createableItem.requiredSkills)) {
 				if (msg.author.skillLevel(skillName as SkillsEnum) < lvl) {
-					throw `You need ${lvl} ${skillName} to create this item.`;
+					throw `You need ${lvl} ${skillName} to ${cmd} this item.`;
 				}
 			}
 		}
@@ -78,18 +121,18 @@ export default class extends BotCommand {
 				createableItem.requiredSlayerUnlocks
 			);
 			if (!success) {
-				throw `You don't have the required Slayer Unlocks to create this item.\n\nRequired: ${errors}`;
+				throw `You don't have the required Slayer Unlocks to ${cmd} this item.\n\nRequired: ${errors}`;
 			}
 		}
 
 		if (createableItem.GPCost && msg.author.settings.get(UserSettings.GP) < createableItem.GPCost * quantity) {
-			throw `You need ${createableItem.GPCost.toLocaleString()} coins to create this item.`;
+			throw `You need ${createableItem.GPCost.toLocaleString()} coins to ${cmd} this item.`;
 		}
 
 		if (createableItem.cantBeInCL) {
 			const cl = new Bank(msg.author.settings.get(UserSettings.CollectionLogBank));
 			if (Object.keys(createableItem.outputItems).some(itemID => cl.amount(Number(itemID)) > 0)) {
-				return msg.channel.send('You can only create this item once!');
+				return msg.channel.send(`You can only ${cmd} this item once!`);
 			}
 		}
 
@@ -104,7 +147,7 @@ export default class extends BotCommand {
 
 		// Ensure they have the required items to create the item.
 		if (!bankHasAllItemsFromBank(userBank, inItems.bank)) {
-			throw `You don't have the required items to create this item. You need: ${inputItemsString}${
+			throw `You don't have the required items to ${cmd} this item. You need: ${inputItemsString}${
 				createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
 			}.`;
 		}
@@ -114,18 +157,28 @@ export default class extends BotCommand {
 			for (const [itemID, qty] of Object.entries(createableItem.cantHaveItems)) {
 				const numOwned = msg.author.numOfItemsOwned(parseInt(itemID));
 				if (numOwned >= qty) {
-					throw `You can't create this item, because you have ${new Bank(
+					throw `You can't ${cmd} this item, because you have ${new Bank(
 						createableItem.cantHaveItems
 					)} in your bank.`;
 				}
 			}
 		}
 
-		await msg.confirm(
-			`${msg.author}, please confirm that you want to create **${outputItemsString}** using ${inputItemsString}${
-				createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
-			}.`
-		);
+		if (cmd === 'revert') {
+			await msg.confirm(
+				`${msg.author}, please confirm that you want to revert **${inputItemsString}${
+					createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
+				}** into ${outputItemsString}.`
+			);
+		} else {
+			await msg.confirm(
+				`${
+					msg.author
+				}, please confirm that you want to ${cmd} **${outputItemsString}** using ${inputItemsString}${
+					createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
+				}.`
+			);
+		}
 
 		await msg.author.settings.update(
 			UserSettings.Bank,
@@ -136,8 +189,12 @@ export default class extends BotCommand {
 			await msg.author.removeGP(createableItem.GPCost);
 		}
 
-		if (!createableItem.noCl) await msg.author.addItemsToCollectionLog(outItems.bank);
+		// Only allow +create to add items to CL
+		if (!createableItem.noCl && cmd === 'create') await msg.author.addItemsToCollectionLog(outItems.bank);
 
-		return msg.channel.send(`You created ${outputItemsString}.`);
+		if (cmd === 'revert') {
+			return msg.channel.send(`You reverted ${inputItemsString} into ${outputItemsString}.`);
+		}
+		return msg.channel.send(`You ${cmd}${cmd === 'create' ? 'd' : 'ed'} ${outputItemsString}.`);
 	}
 }
