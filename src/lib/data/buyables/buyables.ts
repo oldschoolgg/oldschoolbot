c2caa37667ccbc9355b65c64100abcca947b7927
gc <30398469+gc@users.noreply.github.com>
2021-09-17 12:00:25 +1000

    add ironman prices to buy

--- src/lib/data/buyables/buyables.ts
+++ src/lib/data/buyables/buyables.ts
@@ -26,6 +26,7 @@ export interface Buyable {
 	skillsNeeded?: Skills;
 	restockTime?: number;
 	minigameScoreReq?: [MinigameKey, number];
+	ironmanPrice?: number;
 	customReq?: (user: KlasaUser) => Promise<[true] | [false, string]>;
 }
 
@@ -578,7 +579,8 @@ const Buyables: Buyable[] = [
 	{
 		name: 'Fishing Bait',
 		aliases: ['fishing bait'],
-		gpCost: 20
+		gpCost: 20,
+		ironmanPrice: 3
 	},
 	{
 		name: 'Jug of Water',
@@ -589,7 +591,8 @@ const Buyables: Buyable[] = [
 	{
 		name: 'Feather',
 		aliases: ['feather'],
-		gpCost: 50
+		gpCost: 50,
+		ironmanPrice: 2
 	},
 	{
 		name: 'Shield right half',
@@ -606,17 +609,20 @@ const Buyables: Buyable[] = [
 	{
 		name: 'Eye of newt',
 		aliases: ['eye of newt', 'newt eye'],
-		gpCost: 300
+		gpCost: 300,
+		ironmanPrice: 3
 	},
 	{
 		name: 'Vial of water',
 		aliases: ['vial of water'],
-		gpCost: 60
+		gpCost: 60,
+		ironmanPrice: 3
 	},
 	{
 		name: 'Vial',
 		aliases: ['vial'],
-		gpCost: 30
+		gpCost: 30,
+		ironmanPrice: 2
 	},
 	{
 		name: 'Cup of hot water',
