2e919fdd4d8fd7ed59dccea18575e6f7441284c6
gc <30398469+gc@users.noreply.github.com>
2021-11-07 11:58:10 +1100

    more death reduction from gora

--- src/commands/Minion/inferno.ts
+++ src/commands/Minion/inferno.ts
@@ -450,17 +450,17 @@ AND (data->>'diedPreZuk')::boolean = false;`)
 		}
 
 		if (isEmergedZuk) {
-			const meleeGora = meleeGear.hasEquipped(gorajanWarriorOutfit, true);
-			const rangeGora = rangeGear.hasEquipped(gorajanArcherOutfit, true);
-			const mageGora = mageGear.hasEquipped(gorajanOccultOutfit, true);
+			const meleeGora = meleeGear.hasEquipped(gorajanWarriorOutfit, true, true);
+			const rangeGora = rangeGear.hasEquipped(gorajanArcherOutfit, true, true);
+			const mageGora = mageGear.hasEquipped(gorajanOccultOutfit, true, true);
 			for (const [name, has] of [
 				['melee', meleeGora],
 				['range', rangeGora],
 				['mage', mageGora]
 			] as const) {
-				preZukDeathChance.add(has, -1.5, `Gorajan ${name}`);
-				zukDeathChance.add(has, -1.5, `Gorajan ${name}`);
-				emergedZukDeathChance.add(has, -3, `Gorajan ${name}`);
+				preZukDeathChance.add(has, -3.5, `Gorajan ${name}`);
+				zukDeathChance.add(has, -3.5, `Gorajan ${name}`);
+				emergedZukDeathChance.add(has, -5, `Gorajan ${name}`);
 				duration.add(has, 5, `Gorajan ${name}`);
 			}
 		}
