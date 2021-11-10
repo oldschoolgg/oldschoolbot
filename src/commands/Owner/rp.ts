2c2aaedce77db55c435bd8003ecb9f89bbdab5a5
gc <30398469+gc@users.noreply.github.com>
2021-11-10 14:17:41 +1100

    move checkbank to rp

--- src/commands/Owner/rp.ts
+++ src/commands/Owner/rp.ts
@@ -20,6 +20,60 @@ import getUsersPerkTier from '../../lib/util/getUsersPerkTier';
 import { sendToChannelID } from '../../lib/util/webhook';
 import PatreonTask from '../../tasks/patreon';
 
+async function checkBank(msg: KlasaMessage) {
+	const bank = msg.author.settings.get(UserSettings.Bank);
+	const CLBank = msg.author.settings.get(UserSettings.CollectionLogBank);
+
+	const brokenBank = [];
+	for (const id of [...Object.keys(bank), ...Object.keys(CLBank)].map(key => parseInt(key))) {
+		const item = Items.get(id);
+		if (!item) {
+			brokenBank.push(id);
+		}
+	}
+
+	const favorites = msg.author.settings.get(UserSettings.FavoriteItems);
+	const brokenFavorites: number[] = [];
+	for (const id of favorites) {
+		const item = Items.get(id);
+		if (!item) {
+			brokenFavorites.push(id);
+		}
+	}
+
+	if (msg.flagArgs.fix) {
+		let str = '';
+		const newFavs = favorites.filter(i => !brokenFavorites.includes(i));
+		await msg.author.settings.update(UserSettings.FavoriteItems, newFavs, {
+			arrayAction: 'overwrite'
+		});
+		str += `Removed ${favorites.length - newFavs.length} from favorites\n`;
+
+		const newBank = { ...bank };
+		const newCL = { ...CLBank };
+		for (const id of brokenBank) {
+			str += `Removed ${newBank[id]}x of item ID ${id} from bank\n`;
+			delete newBank[id];
+			delete newCL[id];
+		}
+		await msg.author.settings.update(UserSettings.Bank, newBank);
+		await msg.author.settings.update(UserSettings.CollectionLogBank, newCL);
+		return msg.channel.send(str);
+	}
+
+	return msg.channel.send(
+		[
+			`You have ${brokenBank.length} broken items in your bank. `,
+			`You have ${brokenBank.length} broken items in your favorites. `,
+			'You can use `=checkbank --fix` to try to automatically remove the broken items.',
+			`Check here to potentially see what they are: <https://chisel.weirdgloop.org/moid/item_id.html#${[
+				...brokenBank,
+				...brokenFavorites
+			].join(',`')}`
+		].join('\n')
+	);
+}
+
 function generateReadyThings(user: KlasaUser) {
 	const readyThings = [];
 	for (const [cooldown, setting, name] of userTimers) {
@@ -104,6 +158,9 @@ export default class extends BotCommand {
 		const isOwner = this.client.owners.has(msg.author);
 
 		switch (cmd.toLowerCase()) {
+			case 'checkbank': {
+				return checkBank(msg);
+			}
 			case 'givetgb': {
 				if (!(input instanceof KlasaUser)) return;
 				if (!isMod && !isContributor) return;
