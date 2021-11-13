e1325c03d293a15876f3cd2ea357970dd1f5b4d7
6enny6 <76587427+6enny6@users.noreply.github.com>
2021-11-13 08:59:08 +0000

    Update Collections.ts

--- src/lib/data/Collections.ts
+++ src/lib/data/Collections.ts
@@ -1050,6 +1050,7 @@ export const allCollectionLogs: ICollection = {
 					'Medium pouch',
 					'Large pouch',
 					'Giant pouch',
+					'Abyssal pouch',
 					'Elder pouch',
 					'Crystal pickaxe',
 					'Crystal axe',
