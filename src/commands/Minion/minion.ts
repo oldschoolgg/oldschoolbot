d2a0caaeced0dd9af48ee0ac2b94a4aed882f6f7
gc <30398469+gc@users.noreply.github.com>
2021-11-13 22:44:55 +1100

    add aliases to m commands

--- src/commands/Minion/minion.ts
+++ src/commands/Minion/minion.ts
@@ -66,7 +66,10 @@ const subCommands = [
 	'equippet',
 	'unequippet',
 	'autofarm',
-	'activities'
+	'activities',
+	'af',
+	'ep',
+	'uep'
 ];
 
 export default class MinionCommand extends BotCommand {
@@ -159,6 +162,18 @@ export default class MinionCommand extends BotCommand {
 
 	async equippet(msg: KlasaMessage, [input = '']: [string | undefined]) {
 		return equipPet(msg, input);
+	}
+
+	async uep(msg: KlasaMessage) {
+		return unequipPet(msg);
+	}
+
+	async ep(msg: KlasaMessage, [input = '']: [string | undefined]) {
+		return equipPet(msg, input);
+	}
+
+	async af(msg: KlasaMessage) {
+		return autoFarm(msg);
 	}
 
 	async autofarm(msg: KlasaMessage) {
