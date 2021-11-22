13c3ab4e63448285acb9ddfe1b9727c2b64a316f
Oliver Whitwam <oliverwhitwam@yahoo.com>
2021-11-22 10:01:40 +0000

    Multiply the yield of the fixed output by alive plants

--- src/tasks/minions/farmingActivity.ts
+++ src/tasks/minions/farmingActivity.ts
@@ -243,7 +243,7 @@ export default class extends Task {
 					);
 				} else if (plantToHarvest.fixedOutput) {
 					if (!plantToHarvest.fixedOutputAmount) return;
-					cropYield = plantToHarvest.fixedOutputAmount;
+					cropYield = plantToHarvest.fixedOutputAmount * alivePlants;
 				} else {
 					const plantChanceFactor =
 						Math.floor(
