e9c0ca76a64e95a405d3e747a01c1abfa213322e
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-11-24 20:34:42 -0800

    Fix casting issues

--- src/commands/Utility/checkmasses.ts
+++ src/commands/Utility/checkmasses.ts
@@ -2,7 +2,7 @@ import { CommandStore, KlasaMessage } from 'klasa';
 
 import { convertStoredActivityToFlatActivity, prisma } from '../../lib/settings/prisma';
 import { BotCommand } from '../../lib/structures/BotCommand';
-import { formatDuration } from '../../lib/util';
+import { formatDuration, isGroupActivity, isRaidsActivity } from '../../lib/util';
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -19,19 +19,19 @@ export default class extends BotCommand {
 		const channelIDs = msg.guild.channels.cache.filter(c => c.type === 'text').map(c => c.id);
 
 		const masses = (
-			(
-				await prisma.activity.findMany({
-					where: {
-						completed: false,
-						group_activity: true,
-						channel_id: { in: channelIDs }
-					},
-					orderBy: {
-						finish_date: 'asc'
-					}
-				})
-			).map(convertStoredActivityToFlatActivity) as any[]
-		).filter(m => m.users.length > 1);
+			await prisma.activity.findMany({
+				where: {
+					completed: false,
+					group_activity: true,
+					channel_id: { in: channelIDs }
+				},
+				orderBy: {
+					finish_date: 'asc'
+				}
+			})
+		)
+			.map(convertStoredActivityToFlatActivity)
+			.filter(m => isRaidsActivity(m) || isGroupActivity(m));
 
 		if (masses.length === 0) {
 			return msg.channel.send('There are no active masses in this server.');
@@ -39,9 +39,11 @@ export default class extends BotCommand {
 		const now = Date.now();
 		const massStr = masses
 			.map(m => {
-				return `${m.type}${m.challengeMode ? ' CM' : ''}: ${m.users.length} users returning to <#${
-					m.channelID
-				}> in ${formatDuration(m.finishDate - now)}`;
+				if (isGroupActivity(m)) {
+					return `${m.type}${isRaidsActivity(m) && m.challengeMode ? ' CM' : ''}: ${
+						m.users.length
+					} users returning to <#${m.channelID}> in ${formatDuration(m.finishDate - now)}`;
+				}
 			})
 			.join('\n');
 		return msg.channel.send(`**Masses in this server:**
