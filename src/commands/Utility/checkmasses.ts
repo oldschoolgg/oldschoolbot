83f5e40442bd5db6ebd1b3ee9e3877974dd592c5
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-11-24 19:57:13 -0800

    Clean up

--- src/commands/Utility/checkmasses.ts
+++ src/commands/Utility/checkmasses.ts
@@ -18,20 +18,20 @@ export default class extends BotCommand {
 		if (!msg.guild) return null;
 		const channelIDs = msg.guild.channels.cache.filter(c => c.type === 'text').map(c => c.id);
 
-		let masses: any[] = (
-			await prisma.activity.findMany({
-				where: {
-					completed: false,
-					group_activity: true,
-					channel_id: { in: channelIDs }
-				},
-				orderBy: {
-					finish_date: 'asc'
-				}
-			})
-		).map(convertStoredActivityToFlatActivity);
-
-		masses = masses.filter(m => m.users.length > 1);
+		const masses = (
+			(
+				await prisma.activity.findMany({
+					where: {
+						completed: false,
+						group_activity: true,
+						channel_id: { in: channelIDs }
+					},
+					orderBy: {
+						finish_date: 'asc'
+					}
+				})
+			).map(convertStoredActivityToFlatActivity) as any[]
+		).filter(m => m.users.length > 1);
 
 		if (masses.length === 0) {
 			return msg.channel.send('There are no active masses in this server.');
@@ -39,8 +39,6 @@ export default class extends BotCommand {
 		const now = Date.now();
 		const massStr = masses
 			.map(m => {
-				console.log(m);
-				console.log(m.finish_date);
 				return `${m.type}${m.challengeMode ? ' CM' : ''}: ${m.users.length} users returning to <#${
 					m.channelID
 				}> in ${formatDuration(m.finishDate - now)}`;
