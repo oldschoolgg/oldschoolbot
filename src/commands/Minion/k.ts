5d05cf3b211ef12752dbc2225cf87cde8e24c795
gc <30398469+gc@users.noreply.github.com>
2021-11-28 12:14:05 +1100

    remove test code

--- src/commands/Minion/k.ts
+++ src/commands/Minion/k.ts
@@ -111,9 +111,6 @@ export default class extends BotCommand {
 	@minionNotBusy
 	async run(msg: KlasaMessage, [quantity, name = '', method = '']: [null | number | string, string, string]) {
 		const { minionName } = msg.author;
-		if (1 > 0) {
-			throw new Error();
-		}
 
 		let boosts = [];
 		let messages: string[] = [];
