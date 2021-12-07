5318ce73d47df0fa16b5dcdcee434cdce96b8541
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-12-06 22:49:39 -0800

    Avoid potential sql injection in xpgains (#3247)

--- src/commands/Minion/xpgains.ts
+++ src/commands/Minion/xpgains.ts
@@ -34,7 +34,7 @@ export default class extends BotCommand {
 		const res: any =
 			await prisma.$queryRawUnsafe(`SELECT user_id AS user, sum(xp) AS total_xp, max(date) AS lastDate
 FROM xp_gains
-WHERE date > now() - INTERVAL '1 ${interval}'
+WHERE date > now() - INTERVAL '1 ${interval.toLowerCase() === 'day' ? 'day' : 'week'}'
 ${skillObj ? `AND skill = '${skillObj.id}'` : ''}
 GROUP BY user_id
 ORDER BY total_xp DESC, lastDate ASC
