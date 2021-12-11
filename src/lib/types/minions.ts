97367abf0cd1289855a23791d408d5407dbb611f
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-12-10 16:54:52 -0800

    Lint

--- src/lib/types/minions.ts
+++ src/lib/types/minions.ts
@@ -1,5 +1,4 @@
 import { Peak } from '../../tasks/WildernessPeakInterval';
-import { TOBRoom } from '../data/tob';
 import { IPatchData } from '../minions/farming/types';
 import { MinigameName } from '../settings/minigames';
 import { KourendFavour } from './../minions/data/kourendFavour';
