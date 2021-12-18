d0ff4b1b3eeca00adc1e8ff3af30e5c04ed93729
gc <30398469+gc@users.noreply.github.com>
2021-12-18 15:17:51 +1100

    significant price reduction on presents for irons

--- src/commands/Minion/buy.ts
+++ src/commands/Minion/buy.ts
@@ -140,7 +140,7 @@ export default class extends BotCommand {
 			quantity = 1;
 			const previouslyBought = msg.author.cl().amount('Festive present');
 			if (msg.author.isIronman) {
-				gpCost = Math.floor(10_000_000 * (previouslyBought + 1) * ((previouslyBought + 1) / 3));
+				gpCost = Math.floor(10_000_000 * (previouslyBought + 1) * ((previouslyBought + 1) / 6));
 			} else {
 				gpCost = Math.floor(100_000_000 * (previouslyBought + 1) * ((previouslyBought + 1) / 3));
 			}
