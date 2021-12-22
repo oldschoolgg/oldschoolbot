3bdd745d8fe60886d359b0868d0ff266e5d08d68
Oliver Whitwam <oliverwhitwam@yahoo.com>
2021-12-22 22:33:34 +0000

    Move Castable interface to index
    Add new fields, consolidate others

--- src/lib/skilling/skills/magic/index.ts
+++ src/lib/skilling/skills/magic/index.ts
@@ -1,5 +1,31 @@
 import { Emoji } from '../../../constants';
 import { SkillsEnum } from '../../types';
+import { Bank } from 'oldschooljs';
+import Standard from './standard';
+import Arceuus from './arceuss';
+import Lunar from './lunar';
+
+type EnumKeys = keyof typeof SkillsEnum;
+type EnumKeyFields = {[key in EnumKeys]?: number};
+
+export enum MagicTypes {
+	"Combat", "Curse", "Enchant", "Alchemy", "Skilling", "Teleport"
+}
+
+export interface Castable {
+	input: Bank;
+	output?: Bank | null;
+	name: string;
+	alias?: string[];
+	ticks: number;
+	qpRequired?: number;
+	gpCost?: number;
+	xp: EnumKeyFields;
+	levels: EnumKeyFields;
+	category: MagicTypes;
+}
+
+export const castables: Castable[] = [...Standard, ...Arceuus, ...Lunar];
 
 const Magic = {
 	aliases: ['mage', 'magic'],
