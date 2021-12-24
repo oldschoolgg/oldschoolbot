f296d525a0f6923e90b1162c731b99a6864166b9
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2021-12-24 09:13:06 +0000

    Lint

--- src/lib/data/CollectionsExport.ts
+++ src/lib/data/CollectionsExport.ts
@@ -1974,7 +1974,16 @@ export const gracefulCL = resolveItems([
 	...gracefulFeet,
 	...gracefulHands,
 	...gracefulCapes
-]).filter(id => !resolveItems(['Max cape', 'Agility cape', 'Agility cape(t)', 'Trailblazer graceful gloves', 'Trailblazer graceful boots', 'Trailblazer graceful legs', 'Trailblazer graceful cape', 'Trailblazer graceful body']).includes(id));
+]).filter(id => !resolveItems([
+	'Max cape',
+	'Agility cape',
+	'Agility cape(t)',
+	'Trailblazer graceful gloves',
+	'Trailblazer graceful boots',
+	'Trailblazer graceful legs',
+	'Trailblazer graceful cape', 
+	'Trailblazer graceful body'
+]).includes(id));
 
 export const allPetIDs = [
 	...allPetsCL,
