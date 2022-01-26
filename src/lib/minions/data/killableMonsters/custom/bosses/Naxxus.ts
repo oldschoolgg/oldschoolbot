b8e9ff4b0996948d3b8b2ce0ca3b58ea6692288b
Oliver Whitwam <oliverwhitwam@yahoo.com>
2022-01-26 14:30:45 +0000

    Update Naxxus monster details, still needs CL and Droptable

--- src/lib/minions/data/killableMonsters/custom/bosses/Naxxus.ts
+++ src/lib/minions/data/killableMonsters/custom/bosses/Naxxus.ts
@@ -1,9 +1,11 @@
-import { Monsters } from 'oldschooljs';
+import { Time } from 'e';
 import RareDropTable from 'oldschooljs/dist/simulation/subtables/RareDropTable';
 import LootTable from 'oldschooljs/dist/structures/LootTable';
+import { GearStat } from '../../../../../gear';
+import { KillableMonster } from '../../../../../minions/types';
+import { makeKillTable } from '../../../../../util/setCustomMonster';
 
 import { MysteryBoxes } from '../../../../../data/openables';
-import setCustomMonster from '../../../../../util/setCustomMonster';
 
 export const NaxxusLootTable = new LootTable()
 	.add('Rune pickaxe', [1, 9])
@@ -25,10 +27,35 @@ export const NaxxusLootTable = new LootTable()
 	.tertiary(350, 'Tattered robes of Vasa')
 	.tertiary(9, 'Magical artifact');
 
-setCustomMonster(294_820, 'Naxxus', NaxxusLootTable, Monsters.GeneralGraardor, {
+export const Naxxus: KillableMonster = {
 	id: 294_820,
 	name: 'Naxxus',
-	aliases: ['naxx']
-});
-
-export const Naxxus = Monsters.find(mon => mon.name === 'Naxxus')!;
+	aliases: ['nax'],
+	timeToFinish: Time.Minute * 25,
+	// notifyDrops: nexCL,
+	table: makeKillTable(NaxxusLootTable),
+	emoji: '',
+	wildy: false,
+	difficultyRating: 10,
+	qpRequired: 0,
+	groupKillable: false,
+	respawnTime: Time.Second * 10,
+	levelRequirements: {
+		slayer: 110,
+		magic: 120
+	},
+	healAmountNeeded: 100 * 20,
+	minimumGearRequirements: {
+		mage: {
+			[GearStat.AttackMagic]: 250,
+			[GearStat.DefenceMagic]: 250
+		},
+		melee: {
+			[GearStat.AttackStab]: 250,
+			[GearStat.DefenceCrush]: 250,
+			[GearStat.DefenceRanged]: 250,
+			[GearStat.DefenceSlash]: 250,
+			[GearStat.DefenceStab]: 250
+		}
+	}
+};
