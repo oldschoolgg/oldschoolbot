154b869089e6ea864a6d5030c1a491cc5e3b25a4
Oliver Whitwam <oliverwhitwam@yahoo.com>
2022-01-27 13:21:00 +0000

    Revert "Add skill requirements for creating void staff"
    This reverts commit 6c4b0edd138525d4848ba74fc370e305dec6c37c.

--- src/lib/data/creatables/bsoItems.ts
+++ src/lib/data/creatables/bsoItems.ts
@@ -744,7 +744,7 @@ const bsoItems: Createable[] = [
 		}
 	},
 	{
-		name: 'Void Staff',
+		name: 'Void Staff (u)',
 		inputItems: resolveNameBank({
 			'Kodai Wand': 1,
 			'Virtus Wand': 1,
@@ -752,13 +752,8 @@ const bsoItems: Createable[] = [
 			'Dark Animica': 75
 		}),
 		outputItems: resolveNameBank({
-			'Void Staff': 1
-		}),
-		requiredSkills: {
-			magic: 110,
-			runecrafting: 110,
-			crafting: 110
-		}
+			'Void Staff (u)': 1
+		})
 	},
 	{
 		name: 'Abyssal Tome',
