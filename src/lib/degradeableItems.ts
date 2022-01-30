44cbd78abcc192d422e6bf13c5071cf631f5eaa7
Oliver Whitwam <oliverwhitwam@yahoo.com>
2022-01-30 15:54:35 +0000

    Magic master cape can now be anywhere for boost

--- src/lib/degradeableItems.ts
+++ src/lib/degradeableItems.ts
@@ -60,11 +60,11 @@ export const degradeableItems: DegradeableItem[] = [
 			totalHP = totalHP;
 			const mageGear = user.getGear('mage');
 			const minutesDuration = Math.ceil(duration / Time.Minute);
-			if ( mageGear.hasEquipped('Vasa cloak') ) {
-				return Math.ceil(minutesDuration / 2);
-			} else if ( mageGear.hasEquipped('Magic master cape') ) {
+			if ( user.hasItemEquippedAnywhere('Magic master cape') ) {
 				return Math.ceil(minutesDuration / 3);
-			}
+			} else if ( mageGear.hasEquipped('Vasa cloak') ) {
+				return Math.ceil(minutesDuration / 2);
+			};
 			return minutesDuration;
 		}
 	}
