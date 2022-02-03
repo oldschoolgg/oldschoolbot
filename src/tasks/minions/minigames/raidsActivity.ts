74c466b514ab9f8fbe8f8b0c40b0289dc0782994
ianfights <ianfights@gmail.com>
2022-02-02 19:01:31 -0500

    Added an emoj and some text for when you get a cluescroll at raids

--- src/tasks/minions/minigames/raidsActivity.ts
+++ src/tasks/minions/minigames/raidsActivity.ts
@@ -95,7 +95,10 @@ export default class extends Task {
 			}
 			const str = isPurple ? `${emote} ||${itemsAdded}||` : itemsAdded.toString();
 			const deathStr = deaths === 0 ? '' : new Array(deaths).fill(Emoji.Skull).join(' ');
-
+			const clue = items.some(([item]) => item.name.toLowerCase().includes('clue scroll'));
+			if (clue) {
+				resultMessage += `${Emoji.Clue} You have received a clue scroll!`;
+			}
 			resultMessage += `\n${deathStr} **${user}** received: ${str} (${personalPoints?.toLocaleString()} pts, ${
 				Emoji.Skull
 			}${deathChance.toFixed(0)}%)`;
