df8ca0855c22381986216357d20468b2a88a99e8
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-02-18 05:07:03 -0800

    Add Mr. E to tame feedables for 2x loot chance

--- src/lib/tames.ts
+++ src/lib/tames.ts
@@ -179,6 +179,17 @@ export async function addDurationToTame(tame: Tame, duration: number) {
 	return `Your tame has grown ${percentToAdd.toFixed(2)}%!`;
 }
 
+function doubleLootCheck(tame: Tame, loot: Bank) {
+	const hasMrE = tameHasBeenFed(tame, 'Mr. E');
+	let doubleLootMsg = '';
+	if (hasMrE && roll(12)) {
+		loot.multiply(2);
+		doubleLootMsg = '\n**2x Loot from Mr. E**';
+	}
+
+	return { loot, doubleLootMsg };
+}
+
 export interface Species {
 	id: number;
 	type: TameType;
@@ -305,6 +316,8 @@ export async function runTameTask(activity: TameActivity, tame: Tame) {
 			if (boosts.length > 0) {
 				str += `\n\n**Boosts:** ${boosts.join(', ')}.`;
 			}
+			const { doubleLootMsg } = doubleLootCheck(tame, loot);
+			str += doubleLootMsg;
 			const { itemsAdded } = await user.addItemsToBank({ items: loot, collectionLog: false });
 			await trackLoot({
 				duration: activity.duration,
@@ -331,6 +344,8 @@ export async function runTameTask(activity: TameActivity, tame: Tame) {
 			let str = `${user}, ${tameName(tame)} finished collecting ${totalQuantity}x ${
 				collectable.item.name
 			}. (${Math.round((totalQuantity / (activity.duration / Time.Minute)) * 60).toLocaleString()}/hr)`;
+			const { doubleLootMsg } = doubleLootCheck(tame, loot);
+			str += doubleLootMsg;
 			const { itemsAdded } = await user.addItemsToBank({ items: loot, collectionLog: false });
 			handleFinish({
 				loot: itemsAdded,
