c5f7a9ebac84d92acf82d6258a97b4c76a17b21c
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-02-19 01:49:48 -0800

    Add minionNotBusy inhibitor (#3600)
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

--- src/mahoji/commands/lms.ts
+++ src/mahoji/commands/lms.ts
@@ -226,6 +226,9 @@ export const lmsCommand: OSBMahojiCommand = {
 			return `You spent ${cost} points to buy ${loot}. You now have ${newUser.lms_points} LMS points.`;
 		}
 
+		if (user.minionIsBusy) {
+			return 'Your minion must not be busy to do an LMS trip';
+		}
 		const durationPerGame = Time.Minute * 5.5;
 		const quantity = Math.floor(user.maxTripLength('LastManStanding') / durationPerGame);
 		const duration = randomVariation(quantity * durationPerGame, 5);
