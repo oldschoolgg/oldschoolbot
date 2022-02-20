c38b7f33a2f05d50feb1a58d17d484b684070f64
gc <30398469+gc@users.noreply.github.com>
2022-02-20 18:05:21 +1100

    fix checkmasses deprecated cmd

--- src/commands/deprecated/checkmasses.ts
+++ src/commands/deprecated/checkmasses.ts
@@ -1,10 +1,9 @@
 import { KlasaMessage } from 'klasa';
 
-import { COMMAND_BECAME_SLASH_COMMAND_MESSAGE } from '../../lib/constants';
 import { BotCommand } from '../../lib/structures/BotCommand';
 
 export default class extends BotCommand {
 	async run(msg: KlasaMessage) {
-		return msg.channel.send(COMMAND_BECAME_SLASH_COMMAND_MESSAGE(msg));
+		return msg.channel.send(`This command has been moved to \`${msg.cmdPrefix}rp checkmasses\``);
 	}
 }
