4426d63373dc42c21d2c23e221abc259d016fbab
gc <30398469+gc@users.noreply.github.com>
2022-02-21 02:04:11 +1100

    fix deprecated commands

--- src/commands/deprecated/achievementdiary.ts
+++ src/commands/deprecated/achievementdiary.ts
@@ -5,6 +5,6 @@ import { BotCommand } from '../../lib/structures/BotCommand';
 
 export default class extends BotCommand {
 	async run(msg: KlasaMessage) {
-		return msg.channel.send(COMMAND_BECAME_SLASH_COMMAND_MESSAGE(msg));
+		return msg.channel.send(COMMAND_BECAME_SLASH_COMMAND_MESSAGE(msg, 'minion achievementdiary'));
 	}
 }
