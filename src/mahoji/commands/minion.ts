b07c76409ba59e36b248b9e196cb3bbe0e1e2a47
gc <30398469+gc@users.noreply.github.com>
2022-02-20 17:38:31 +1100

    move achievementdiary/bankbg to minion slash cmd

--- src/mahoji/commands/minion.ts
+++ src/mahoji/commands/minion.ts
@@ -2,9 +2,16 @@ import { Time } from 'e';
 import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 
 import { client } from '../..';
+import { diaries } from '../../lib/diaries';
 import { ActivityTaskOptions } from '../../lib/types/minions';
 import addSubTaskToActivityTask from '../../lib/util/addSubTaskToActivityTask';
 import { minionStatsEmbed } from '../../lib/util/minionStatsEmbed';
+import BankImageTask from '../../tasks/bankImage';
+import {
+	achievementDiaryCommand,
+	claimAchievementDiaryCommand
+} from '../lib/abstracted_commands/achievementDiaryCommand';
+import { bankBgCommand } from '../lib/abstracted_commands/bankBgCommand';
 import { OSBMahojiCommand } from '../lib/util';
 
 export const minionCommand: OSBMahojiCommand = {
@@ -20,9 +27,62 @@ export const minionCommand: OSBMahojiCommand = {
 			type: ApplicationCommandOptionType.Subcommand,
 			name: 'stats',
 			description: 'Check the stats of your minion.'
+		},
+		{
+			type: ApplicationCommandOptionType.Subcommand,
+			name: 'achievementdiary',
+			description: 'Manage your achievement diary.',
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: 'diary',
+					description: 'The achievement diary name.',
+					required: false,
+					choices: diaries.map(i => ({ name: i.name, value: i.name }))
+				},
+				{
+					type: ApplicationCommandOptionType.Boolean,
+					name: 'claim',
+					description: 'Claim your rewards?',
+					required: false
+				}
+			]
+		},
+		{
+			type: ApplicationCommandOptionType.Subcommand,
+			name: 'bankbg',
+			description: 'Change your bank background.',
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: 'name',
+					description: 'The name of the bank background you want.',
+					autocomplete: async value => {
+						const bankImages = (client.tasks.get('bankImage') as BankImageTask).backgroundImages;
+						return bankImages
+							.filter(bg => (!value ? true : bg.available))
+							.map(i => {
+								const name = i.perkTierNeeded
+									? `${i.name} (Tier ${i.perkTierNeeded + 1} patrons)`
+									: i.name;
+								return { name, value: i.name };
+							});
+					}
+				}
+			]
 		}
 	],
-	run: async ({ userID, options, channelID }: CommandRunOptions<{ birthdayevent?: {}; stats?: {} }>) => {
+	run: async ({
+		userID,
+		options,
+		channelID,
+		interaction
+	}: CommandRunOptions<{
+		birthdayevent?: {};
+		stats?: {};
+		achievementdiary?: { diary?: string; claim?: boolean };
+		bankbg?: { name?: string };
+	}>) => {
 		const user = await client.fetchUser(userID.toString());
 
 		if (options.birthdayevent) {
@@ -38,6 +98,17 @@ export const minionCommand: OSBMahojiCommand = {
 
 		if (options.stats) {
 			return { embeds: [await minionStatsEmbed(user)] };
+		}
+
+		if (options.achievementdiary) {
+			if (options.achievementdiary.claim) {
+				return claimAchievementDiaryCommand(user, options.achievementdiary.diary ?? '');
+			}
+			return achievementDiaryCommand(user, options.achievementdiary.diary ?? '');
+		}
+
+		if (options.bankbg) {
+			return bankBgCommand(interaction, user, options.bankbg.name ?? '');
 		}
 
 		return 'Unknown command';
