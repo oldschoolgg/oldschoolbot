498770ff9cd7ad0cba5cdf0344fdc503dde060d6
gc <30398469+gc@users.noreply.github.com>
2022-04-03 23:42:38 +1000

    improve/fix selling code

--- src/commands/Minion/sell.ts
+++ src/commands/Minion/sell.ts
@@ -1,5 +1,7 @@
-import { CommandStore, KlasaMessage } from 'klasa';
+import { reduceNumByPercent } from 'e';
+import { CommandStore, KlasaClient, KlasaMessage } from 'klasa';
 import { Bank, Util } from 'oldschooljs';
+import { Item } from 'oldschooljs/dist/meta/types';
 
 import { ClientSettings } from '../../lib/settings/types/ClientSettings';
 import { BotCommand } from '../../lib/structures/BotCommand';
@@ -18,6 +20,16 @@ const specialSoldItems = new Map([
 	[id('Ancient relic'), 16_000_000]
 ]);
 
+export function sellPriceOfItem(client: KlasaClient, item: Item, taxRate = 20): { price: number; basePrice: number } {
+	if (!item.price || !item.tradeable) return { price: 0, basePrice: 0 };
+	const customPrices = client.settings.get(ClientSettings.CustomPrices);
+	let basePrice = customPrices[item.id] ?? item.price;
+	let price = basePrice;
+	price = reduceNumByPercent(price, taxRate);
+	price = Math.floor(price);
+	return { price, basePrice };
+}
+
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
@@ -30,14 +42,15 @@ export default class extends BotCommand {
 
 	async run(msg: KlasaMessage, [[bankToSell]]: [[Bank, number]]) {
 		let totalPrice = 0;
-		const customPrices = this.client.settings.get(ClientSettings.CustomPrices);
+		const taxRatePercent = 20;
+
 		for (const [item, qty] of bankToSell.items()) {
 			const specialPrice = specialSoldItems.get(item.id);
 			if (specialPrice) {
 				totalPrice += Math.floor(specialPrice * qty);
 			} else {
 				if (msg.author.isIronman) return msg.channel.send("Iron players can't sell items.");
-				const price = customPrices[item.id] ?? item.price;
+				const { price } = sellPriceOfItem(this.client, item, taxRatePercent);
 				totalPrice += price * qty;
 			}
 		}
@@ -50,8 +63,6 @@ export default class extends BotCommand {
 			)}).`
 		);
 
-		const tax = Math.ceil((totalPrice / 0.8) * 0.2);
-
 		await Promise.all([msg.author.removeItemsFromBank(bankToSell.bank), msg.author.addGP(totalPrice)]);
 
 		updateGPTrackSetting(this.client, ClientSettings.EconomyStats.GPSourceSellingItems, totalPrice);
@@ -60,9 +71,9 @@ export default class extends BotCommand {
 		msg.author.log(`sold ${JSON.stringify(bankToSell.bank)} for ${totalPrice}`);
 
 		return msg.channel.send(
-			`Sold ${bankToSell} for **${totalPrice.toLocaleString()}gp (${Util.toKMB(totalPrice)})**. Tax: ${Util.toKMB(
-				tax
-			)}`
+			`Sold ${bankToSell} for **${totalPrice.toLocaleString()}gp (${Util.toKMB(
+				totalPrice
+			)})** (${taxRatePercent}% below market price).`
 		);
 	}
 }
