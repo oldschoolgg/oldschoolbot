33387d1229f070c2adf95730ed3afbc1b0e911d8
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2022-04-06 15:01:29 +0100

    Fix barb assault buy cmd (#3649)
    Enables you to buy items again using your honour points, if no quantity is provided quantity is set as 1
    
    Co-authored-by: GC <30398469+gc@users.noreply.github.com>

--- src/mahoji/commands/minigames.ts
+++ src/mahoji/commands/minigames.ts
@@ -4,10 +4,12 @@ import { client } from '../..';
 import TrekShopItems from '../../lib/data/buyables/trekBuyables';
 import { LMSBuyables } from '../../lib/data/CollectionsExport';
 import {
+	barbAssaultBuyCommand,
 	barbAssaultGambleCommand,
 	barbAssaultLevelCommand,
 	barbAssaultStartCommand,
 	barbAssaultStatsCommand,
+	BarbBuyables,
 	GambleTiers
 } from '../lib/abstracted_commands/barbAssault';
 import { castleWarsStartCommand, castleWarsStatsCommand } from '../lib/abstracted_commands/castleWarsCommand';
@@ -69,6 +71,32 @@ export const minigamesCommand: OSBMahojiCommand = {
 					type: ApplicationCommandOptionType.Subcommand,
 					name: 'start',
 					description: 'Start a Barbarian Assault trip.'
+				},
+				{
+					type: ApplicationCommandOptionType.Subcommand,
+					name: 'buy',
+					description: 'Purchase items with Honour points.',
+					options: [
+						{
+							type: ApplicationCommandOptionType.String,
+							name: 'name',
+							description: 'What item you wish to buy.',
+							required: true,
+							autocomplete: async (value: string) => {
+								return BarbBuyables.filter(i =>
+									!value ? true : i.item.name.toLowerCase().includes(value.toLowerCase())
+								).map(i => ({ name: i.item.name, value: i.item.name }));
+							}
+						},
+						{
+							type: ApplicationCommandOptionType.Integer,
+							name: 'quantity',
+							description: 'The quantity you want to purchase.',
+							required: false,
+							min_value: 1,
+							max_value: 1000
+						}
+					]
 				},
 				{
 					type: ApplicationCommandOptionType.Subcommand,
@@ -654,6 +682,7 @@ export const minigamesCommand: OSBMahojiCommand = {
 	}: CommandRunOptions<{
 		barb_assault?: {
 			start?: { quantity?: number };
+			buy?: { name: string; quantity?: number };
 			level?: {};
 			gamble?: { tier: string; quantity: number };
 			stats?: {};
@@ -702,6 +731,15 @@ export const minigamesCommand: OSBMahojiCommand = {
 		 */
 		if (options.barb_assault?.start) {
 			return barbAssaultStartCommand(channelID, user, klasaUser);
+		}
+		if (options.barb_assault?.buy) {
+			return barbAssaultBuyCommand(
+				interaction,
+				klasaUser,
+				user,
+				options.barb_assault.buy.name,
+				options.barb_assault.buy.quantity
+			);
 		}
 		if (options.barb_assault?.level) {
 			return barbAssaultLevelCommand(user);
