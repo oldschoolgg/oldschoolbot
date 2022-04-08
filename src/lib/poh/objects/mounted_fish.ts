e0f5751490705eac5abb6a442df7d42ed0fe0884
Matthew Degnan <38664414+discordwolf-bots@users.noreply.github.com>
2022-04-08 18:28:51 +0100

    Mounted fish refunds in POH
    Mounted Bass/Swordfish/Shark will refund the "big fish"

--- src/lib/poh/objects/mounted_fish.ts
+++ src/lib/poh/objects/mounted_fish.ts
@@ -2,26 +2,40 @@ import { Bank } from 'oldschooljs';
 
 import { PoHObject } from '..';
 
-export const MountedFish: PoHObject[] = [
+const baseBankBass = () => new Bank().add('Oak plank', 2);
+export const MountedFishBass: PoHObject[] = [
 	{
 		id: 13_488,
 		name: 'Mounted bass',
 		slot: 'mounted_fish',
 		level: 36,
-		itemCost: new Bank().add('Big bass').add('Oak plank', 2)
-	},
+		itemCost: baseBankBass().add('Big bass'),
+		refundItems: true
+	}
+];
+
+const baseBankSwordfish = () => new Bank().add('Teak plank', 2);
+export const MountedFishSwordfish: PoHObject[] = [
 	{
 		id: 13_489,
 		name: 'Mounted swordfish',
 		slot: 'mounted_fish',
 		level: 56,
-		itemCost: new Bank().add('Big swordfish').add('Teak plank', 2)
-	},
+		itemCost: baseBankSwordfish().add('Big swordfish'),
+		refundItems: true
+	}
+	
+];
+
+const baseBankShark = () => new Bank().add('Mahogany plank', 2);
+export const MountedFishShark: PoHObject[] = [
 	{
 		id: 13_490,
 		name: 'Mounted shark',
 		slot: 'mounted_fish',
 		level: 76,
-		itemCost: new Bank().add('Big shark').add('Mahogany plank', 2)
+		itemCost: baseBankShark().add('Big shark'),
+		refundItems: true
 	}
 ];
+
