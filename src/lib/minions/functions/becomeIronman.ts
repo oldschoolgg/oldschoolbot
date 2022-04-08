12a5a6ceb2deac4053bdb385343d1f2924493c2a
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-04-08 09:46:23 -0700

    Current idea

--- src/lib/minions/functions/becomeIronman.ts
+++ src/lib/minions/functions/becomeIronman.ts
@@ -51,23 +51,29 @@ Type \`confirm permanent ironman\` if you understand the above information, and 
 
 		await msg.author.settings.reset();
 
-		await prisma.slayerTask.deleteMany({ where: { user_id: msg.author.id } });
-		await prisma.playerOwnedHouse.delete({ where: { user_id: msg.author.id } }).catch();
-		await prisma.minigame.delete({ where: { user_id: msg.author.id } });
-		await prisma.xPGain.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
-		await prisma.newUser.delete({ where: { id: msg.author.id } });
-		await prisma.activity.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
-		await prisma.tameActivity.deleteMany({ where: { user_id: msg.author.id } });
-		await prisma.tame.deleteMany({ where: { user_id: msg.author.id } });
-		await prisma.fishingContestCatch.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
+		try {
+			await prisma.slayerTask.deleteMany({ where: { user_id: msg.author.id } });
+			prisma.playerOwnedHouse.delete({ where: { user_id: msg.author.id } }).catch(_err => null);
+			prisma.minigame.delete({ where: { user_id: msg.author.id } }).catch(_err => null);
+			await prisma.xPGain.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
+			prisma.newUser.delete({ where: { id: msg.author.id } }).catch(_err => null);
+			await prisma.activity.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
+			await prisma.tameActivity.deleteMany({ where: { user_id: msg.author.id } });
+			await prisma.tame.deleteMany({ where: { user_id: msg.author.id } });
+			await prisma.fishingContestCatch.deleteMany({ where: { user_id: BigInt(msg.author.id) } });
 
-		await msg.author.settings.update([
-			[UserSettings.Minion.Ironman, true],
-			[UserSettings.Minion.HasBought, true],
-			[UserSettings.BitField, BitField.BypassAgeRestriction]
-		]);
-		return msg.channel.send('You are now an ironman.');
+			await msg.author.settings.update([
+				[UserSettings.Minion.Ironman, true],
+				[UserSettings.Minion.HasBought, true],
+				[UserSettings.BitField, BitField.BypassAgeRestriction]
+			]);
+			return msg.channel.send('You are now an ironman.');
+		} catch (e) {
+			// Actual database error
+			return msg.channel.send('We are in an unknown state. Likely inconsistent.');
+		}
 	} catch (err) {
+		console.log(err);
 		return msg.channel.send('Cancelled ironman swap.');
 	}
 }
