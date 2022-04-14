2ecc81439e136062cf9086de3e9ece3679ca733e
gc <30398469+gc@users.noreply.github.com>
2022-04-14 10:05:16 +1000

    fix

--- src/commands/bso/bingo.ts
+++ src/commands/bso/bingo.ts
@@ -11,8 +11,8 @@ const end = start + (Time.Day * 7) * 2
 export default class extends BotCommand {
 	async run(msg: KlasaMessage) {
         const str = `**#1 - BSO Bingo**
-**Start:** ${time(start)}
-**Finish:** ${time(end)}`
+**Start:** ${time(start / 1000)}
+**Finish:** ${time(end / 1000)}`
 		return msg.channel.send(str);
 	}
 }
