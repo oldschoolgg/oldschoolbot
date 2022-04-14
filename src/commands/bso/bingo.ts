db62a15d3d957f282386a5e085a97f0dca2f1b3c
gc <30398469+gc@users.noreply.github.com>
2022-04-14 10:39:48 +1000

    add submit helper

--- src/commands/bso/bingo.ts
+++ src/commands/bso/bingo.ts
@@ -13,6 +13,15 @@ export default class extends BotCommand {
         const str = `**#1 - BSO Bingo**
 **Start:** ${time(start / 1000)}
 **Finish:** ${time(end / 1000)}`
+		if (msg.channel.id === '963954209768763433') {
+			const mentions = msg.mentions.users.array();
+			
+			if (mentions.length !== 3) return msg.channel.send(`You didn't mention 3 people.`);
+			const withoutTicket = mentions.find(u => !u.cl().has('Bingo ticket'))
+			if (withoutTicket) return msg.channel.send(`${withoutTicket} hasn't bought a ticket.`)
+			return msg.channel.send(`**Team Submission:**
+${mentions.map(u => `${u.username}#${u.discriminator}[${u.id}]`).join('\n')}`)
+		}
 		return msg.channel.send(str);
 	}
 }
