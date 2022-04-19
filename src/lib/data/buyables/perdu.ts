fa9be81b914eef60d82bfdb94cb2c7b0f022518b
TastyPumPum <james.selfe@hotmail.co.uk>
2022-04-19 20:56:04 +0100

    Achievement Diary Rewards - Buyable/CL
    Adds the achievement diary rewards to the buy command provided they have done the original claim.
    Adds a cl to track the achievement diary rewards (in custom and uncounted)
    
    Closes #3781 (This might not be the ideal way but I'm open to feedback)

---
+++ src/lib/data/buyables/perdu.ts
@@ -0,0 +1,293 @@
+import resolveItems from '../../util/resolveItems';
+import { Buyable } from './buyables';
+
+export const perduBuyables: Buyable[] = [
+	{
+		name: 'Karamja gloves 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Karamja gloves 1'])
+	},
+	{
+		name: 'Karamja gloves 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Karamja gloves 2'])
+	},
+	{
+		name: 'Karamja gloves 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Karamja gloves 3'])
+	},
+	{
+		name: 'Karamja gloves 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Karamja gloves 4'])
+	},
+	{
+		name: 'Ardougne cloak 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Ardougne cloak 1'])
+	},
+	{
+		name: 'Ardougne cloak 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Ardougne cloak 2'])
+	},
+	{
+		name: 'Ardougne cloak 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Ardougne cloak 3'])
+	},
+	{
+		name: 'Ardougne cloak 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Ardougne cloak 4'])
+	},
+	{
+		name: 'Falador shield 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Falador shield 1'])
+	},
+	{
+		name: 'Falador shield 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Falador shield 2'])
+	},
+	{
+		name: 'Falador shield 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Falador shield 3'])
+	},
+	{
+		name: 'Falador shield 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Falador shield 4'])
+	},
+	{
+		name: 'Fremennik sea boots 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Fremennik sea boots 1'])
+	},
+	{
+		name: 'Fremennik sea boots 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Fremennik sea boots 2'])
+	},
+	{
+		name: 'Fremennik sea boots 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Fremennik sea boots 3'])
+	},
+	{
+		name: 'Fremennik sea boots 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Fremennik sea boots 4'])
+	},
+	{
+		name: 'Kandarin headgear 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Kandarin headgear 1'])
+	},
+	{
+		name: 'Kandarin headgear 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Kandarin headgear 2'])
+	},
+	{
+		name: 'Kandarin headgear 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Kandarin headgear 3'])
+	},
+	{
+		name: 'Kandarin headgear 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Kandarin headgear 4'])
+	},
+	{
+		name: 'Desert amulet 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Desert amulet 1'])
+	},
+	{
+		name: 'Desert amulet 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Desert amulet 2'])
+	},
+	{
+		name: 'Desert amulet 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Desert amulet 3'])
+	},
+	{
+		name: 'Desert amulet 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Desert amulet 4'])
+	},
+	{
+		name: "Explorer's ring 1",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Explorer's ring 1"])
+	},
+	{
+		name: "Explorer's ring 2",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Explorer's ring 2"])
+	},
+	{
+		name: "Explorer's ring 3",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Explorer's ring 3"])
+	},
+	{
+		name: "Explorer's ring 4",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Explorer's ring 4"])
+	},
+	{
+		name: 'Morytania legs 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Morytania legs 1'])
+	},
+	{
+		name: 'Morytania legs 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Morytania legs 2'])
+	},
+	{
+		name: 'Morytania legs 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Morytania legs 3'])
+	},
+	{
+		name: 'Morytania legs 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Morytania legs 4'])
+	},
+	{
+		name: 'Varrock armour 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Varrock armour 1'])
+	},
+	{
+		name: 'Varrock armour 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Varrock armour 2'])
+	},
+	{
+		name: 'Varrock armour 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Varrock armour 3'])
+	},
+	{
+		name: 'Varrock armour 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Varrock armour 4'])
+	},
+	{
+		name: 'Wilderness sword 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Wilderness sword 1'])
+	},
+	{
+		name: 'Wilderness sword 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Wilderness sword 2'])
+	},
+	{
+		name: 'Wilderness sword 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Wilderness sword 3'])
+	},
+	{
+		name: 'Wilderness sword 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Wilderness sword 4'])
+	},
+	{
+		name: 'Western banner 1',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Western banner 1'])
+	},
+	{
+		name: 'Western banner 2',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Western banner 2'])
+	},
+	{
+		name: 'Western banner 3',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Western banner 3'])
+	},
+	{
+		name: 'Western banner 4',
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(['Western banner 4'])
+	},
+	{
+		name: "Rada's blessing 1",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Rada's blessing 1"])
+	},
+	{
+		name: "Rada's blessing 2",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Rada's blessing 2"])
+	},
+	{
+		name: "Rada's blessing 3",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Rada's blessing 3"])
+	},
+	{
+		name: "Rada's blessing 4",
+		gpCost: 1000,
+		ironmanPrice: 200,
+		collectionLogReqs: resolveItems(["Rada's blessing 4"])
+	}
+];
