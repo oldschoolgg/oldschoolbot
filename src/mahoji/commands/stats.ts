bdd2b5b9075c04ecf5e77923a43ce4915f4b73f9
TastyPumPum <james.selfe@hotmail.co.uk>
2022-04-19 19:52:27 +0100

    Lint

--- src/mahoji/commands/stats.ts
+++ src/mahoji/commands/stats.ts
@@ -42,7 +42,7 @@ export const statsCommand: OSBMahojiCommand = {
 	run: async ({ options }: CommandRunOptions<{ username: string; type?: AccountType; virtual?: boolean }>) => {
 		try {
 			if (!options.type) {
-				options.type = AccountType.Normal
+				options.type = AccountType.Normal;
 			}
 			const player = await Hiscores.fetch(options.username, {
 				type: options.type,
