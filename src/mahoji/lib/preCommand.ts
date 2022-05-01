7f884f100b68bf18cbc706bd0364feb80a0e7938
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-05-01 16:05:36 -0700

    pre-cache linked users so patreon perks work properly.

--- src/mahoji/lib/preCommand.ts
+++ src/mahoji/lib/preCommand.ts
@@ -1,8 +1,22 @@
 import { TextChannel } from 'discord.js';
 
 import { client } from '../..';
+import { mahojiUsersSettingsFetch } from '../mahojiSettings';
 import { AbstractCommand, runInhibitors } from './inhibitors';
 
+async function cacheLinkedUsers(user_id: bigint | string) {
+	const user = await mahojiUsersSettingsFetch(user_id);
+	let main = user.main_account;
+	const allAccounts: string[] = [...user.ironman_alts];
+	if (main) {
+		allAccounts.push(main);
+	}
+	// Ensure linked users are cached
+	const allAccountsMap = await Promise.all(
+		allAccounts.map(async id => client.users.cache.get(id) ?? client.fetchUser(id))
+	);
+	return allAccountsMap.length;
+}
 export async function preCommand({
 	abstractCommand,
 	userID,
@@ -25,6 +39,7 @@ export async function preCommand({
 	const guild = guildID ? client.guilds.cache.get(guildID) : null;
 	const member = guild?.members.cache.get(userID);
 	const channel = client.channels.cache.get(channelID) as TextChannel;
+	await cacheLinkedUsers(userID);
 	const inhibitResult = await runInhibitors({
 		user,
 		guild: guild ?? null,
