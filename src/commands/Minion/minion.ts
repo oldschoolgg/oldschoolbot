89d783ed26f679d6a53339ff7ab82d2f025b7114
gc <30398469+gc@users.noreply.github.com>
2022-05-12 09:42:58 +1000

    fix m buy error, track start time

--- src/commands/Minion/minion.ts
+++ src/commands/Minion/minion.ts
@@ -37,6 +37,7 @@ import Skills from '../../lib/skilling/skills';
 import Agility from '../../lib/skilling/skills/agility';
 import { BotCommand } from '../../lib/structures/BotCommand';
 import { convertLVLtoXP, isValidNickname, stringMatches } from '../../lib/util';
+import { mahojiUserSettingsUpdate } from '../../mahoji/mahojiSettings';
 
 const patMessages = [
 	'You pat {name} on the head.',
@@ -513,7 +514,10 @@ Type \`confirm\` if you understand the above information, and want to become an 
 	async buy(msg: KlasaMessage) {
 		if (msg.author.hasMinion) return msg.channel.send('You already have a minion!');
 
-		await msg.author.settings.update(UserSettings.Minion.HasBought, true);
+		await mahojiUserSettingsUpdate(this.client, msg.author.id, {
+			minion_hasBought: true,
+			minion_bought_date: new Date()
+		});
 		return msg.channel.send({
 			embeds: [
 				new MessageEmbed().setTitle('Your minion is now ready to use!').setDescription(
