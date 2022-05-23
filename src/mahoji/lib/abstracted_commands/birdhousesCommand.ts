11dff016ae96831f5a36cfe22160fa5f06cc8362
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-05-22 23:30:19 -0700

    Fix birdhouse limit on BSO

--- src/mahoji/lib/abstracted_commands/birdhousesCommand.ts
+++ src/mahoji/lib/abstracted_commands/birdhousesCommand.ts
@@ -7,7 +7,14 @@ import { ClientSettings } from '../../../lib/settings/types/ClientSettings';
 import birdhouses, { Birdhouse } from '../../../lib/skilling/skills/hunter/birdHouseTrapping';
 import defaultBirdhouseTrap, { BirdhouseData } from '../../../lib/skilling/skills/hunter/defaultBirdHouseTrap';
 import { BirdhouseActivityTaskOptions } from '../../../lib/types/minions';
-import { formatDuration, itemID, itemNameFromID, stringMatches, updateBankSetting } from '../../../lib/util';
+import {
+	birdhouseLimit,
+	formatDuration,
+	itemID,
+	itemNameFromID,
+	stringMatches,
+	updateBankSetting
+} from '../../../lib/util';
 import addSubTaskToActivityTask from '../../../lib/util/addSubTaskToActivityTask';
 import { minionName } from '../../../lib/util/minionUtils';
 import { getSkillsOfMahojiUser, mahojiUsersSettingsFetch } from '../../mahojiSettings';
@@ -159,6 +166,8 @@ export async function birdhouseHarvestCommand(
 	const infoStr: string[] = [];
 	const boostStr: string[] = [];
 
+	const birdHouses = birdhouseLimit(user);
+
 	const existingBirdhouse = await calculateBirdhouseDetails(mahojiUser.id);
 	if (!existingBirdhouse.isReady && existingBirdhouse.raw.lastPlaced) return birdhouseCheckCommand(mahojiUser);
 
@@ -197,7 +206,7 @@ export async function birdhouseHarvestCommand(
 	}
 	let gotCraft = false;
 	let removeBank = new Bank();
-	const premadeBankCost = birdhouseToPlant.houseItemReq.clone().multiply(4);
+	const premadeBankCost = birdhouseToPlant.houseItemReq.clone().multiply(birdHouses);
 	if (user.owns(premadeBankCost)) {
 		removeBank.add(premadeBankCost);
 	} else {
@@ -205,16 +214,18 @@ export async function birdhouseHarvestCommand(
 			return `${user.minionName} needs ${birdhouseToPlant.craftLvl} Crafting to make ${birdhouseToPlant.name} during the run or write \`/activities birdhouse run --nocraft\`.`;
 		}
 		gotCraft = true;
-		removeBank.add(birdhouseToPlant.craftItemReq.clone().multiply(4));
+		removeBank.add(birdhouseToPlant.craftItemReq.clone().multiply(birdHouses));
 	}
 
 	let canPay = false;
 	for (const currentSeed of birdhouseSeedReq) {
-		if (userBank.amount(currentSeed.itemID) >= currentSeed.amount * 4) {
+		if (userBank.amount(currentSeed.itemID) >= currentSeed.amount * birdHouses) {
 			infoStr.push(
-				`You baited the birdhouses with ${currentSeed.amount * 4}x ${itemNameFromID(currentSeed.itemID)}.`
+				`You baited the birdhouses with ${currentSeed.amount * birdHouses}x ${itemNameFromID(
+					currentSeed.itemID
+				)}.`
 			);
-			removeBank.add(currentSeed.itemID, currentSeed.amount * 4);
+			removeBank.add(currentSeed.itemID, currentSeed.amount * birdHouses);
 			canPay = true;
 			break;
 		}
@@ -230,10 +241,10 @@ export async function birdhouseHarvestCommand(
 
 	// If user does not have something already placed, just place the new birdhouses.
 	if (!existingBirdhouse.raw.birdhousePlaced) {
-		infoStr.unshift(`${user.minionName} is now placing 4x ${birdhouseToPlant.name}.`);
+		infoStr.unshift(`${user.minionName} is now placing ${birdHouses}x ${birdhouseToPlant.name}.`);
 	} else {
 		infoStr.unshift(
-			`${user.minionName} is now collecting 4x ${existingBirdhouse.raw.lastPlaced}, and then placing 4x ${birdhouseToPlant.name}.`
+			`${user.minionName} is now collecting ${birdHouses}x ${existingBirdhouse.raw.lastPlaced}, and then placing ${birdHouses}x ${birdhouseToPlant.name}.`
 		);
 	}
 
