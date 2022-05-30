752a9bfd58af57d51aab6db6d428a45c2f901fde
gc <30398469+gc@users.noreply.github.com>
2022-05-31 00:51:03 +1000

    remove twitter feature

--- src/lib/settings/schemas/GuildSchema.ts
+++ src/lib/settings/schemas/GuildSchema.ts
@@ -3,6 +3,5 @@ import { Client } from 'klasa';
 Client.defaultGuildSchema
 	.add('jmodComments', 'textchannel', { default: null })
 	.add('petchannel', 'textchannel', { default: null })
-	.add('tweetchannel', 'textchannel', { default: null })
 	.add('levelUpMessages', 'textchannel', { default: null })
 	.add('staffOnlyChannels', 'textchannel', { array: true, default: [] });
