35fcab18eec0df6f8115c19431b651422b3d5f9a
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2022-06-07 21:38:27 +0100

    Remove Hallowed Sack from CL (#3994)
    Following adding the hallowed sack as an openable and buyable, I incorrectly added it to the CL to. 
    
    This removes it from the CL as per the osrs cl - https://oldschool.runescape.wiki/w/Collection_log#Hallowed_Sepulchre

--- src/lib/data/CollectionsExport.ts
+++ src/lib/data/CollectionsExport.ts
@@ -1137,7 +1137,6 @@ export const gnomeRestaurantCL = resolveItems(['Grand seed pod', 'Gnome scarf', 
 export const hallowedSepulchreCL = resolveItems([
 	'Hallowed mark',
 	'Hallowed token',
-	'Hallowed sack',
 	'Hallowed grapple',
 	'Hallowed focus',
 	'Hallowed symbol',
