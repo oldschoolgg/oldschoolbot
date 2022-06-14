b739e84fa2bb895bef1759d284ff9bb436833799
GC <30398469+gc@users.noreply.github.com>
2022-06-15 08:50:37 +1000

    Bso slash commands (#4025)
    - Add 'repeat last tame trip' button to m command
    - Move fishingcontest, mmr, ods, to bsominigames cmd
    - Move tames cmd to slash cmd
    - Move vasa to k command
    - Monkey rumble buyables now in the normal buy command

--- src/tasks/minions/bso/vasaMagusActivity.ts
+++ src/tasks/minions/bso/vasaMagusActivity.ts
@@ -112,6 +112,6 @@ export default class extends Task {
 
 		updateBankSetting(this.client, ClientSettings.EconomyStats.VasaLoot, loot);
 
-		handleTripFinish(user, channelID, resultStr, ['vasa', [quantity], true], image!, data, itemsAdded);
+		handleTripFinish(user, channelID, resultStr, ['k', { name: 'vasa' }, true], image!, data, itemsAdded);
 	}
 }
