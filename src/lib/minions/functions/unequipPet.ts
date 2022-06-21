c73d826935958824b62096d42e8b36958cb94ed4
gc <30398469+gc@users.noreply.github.com>
2022-06-21 18:36:44 +1000

    move pet equip/unequip to gear cmd

--- src/lib/minions/functions/unequipPet.ts
+++ src/lib/minions/functions/unequipPet.ts
@@ -1,28 +1,26 @@
-import { KlasaMessage } from 'klasa';
+import { KlasaUser } from 'klasa';
 import { Bank } from 'oldschooljs';
 
 import { UserSettings } from '../../settings/types/UserSettings';
 import { itemNameFromID } from '../../util';
 import { logError } from '../../util/logError';
 
-export async function unequipPet(msg: KlasaMessage) {
-	const equippedPet = msg.author.settings.get(UserSettings.Minion.EquippedPet);
-	if (!equippedPet) return msg.channel.send("You don't have a pet equipped.");
+export async function unequipPet(user: KlasaUser) {
+	const equippedPet = user.settings.get(UserSettings.Minion.EquippedPet);
+	if (!equippedPet) return "You don't have a pet equipped.";
 
 	const loot = new Bank().add(equippedPet);
 
 	try {
-		await msg.author.addItemsToBank({ items: loot, collectionLog: false });
+		await user.addItemsToBank({ items: loot, collectionLog: false });
 	} catch (e) {
 		logError(new Error('Failed to add pet to bank'), {
-			user_id: msg.author.id,
+			user_id: user.id,
 			pet_to_unequip: equippedPet.toString()
 		});
-		return msg.channel.send('Error removing pet, ask for help in the support server.');
+		return 'Error removing pet, ask for help in the support server.';
 	}
-	await msg.author.settings.update(UserSettings.Minion.EquippedPet, null);
+	await user.settings.update(UserSettings.Minion.EquippedPet, null);
 
-	return msg.channel.send(
-		`${msg.author.minionName} picks up their ${itemNameFromID(equippedPet)} pet and places it back in their bank.`
-	);
+	return `${user.minionName} picks up their ${itemNameFromID(equippedPet)} pet and places it back in their bank.`;
 }
--- src/mahoji/commands/gear.ts
+++ src/mahoji/commands/gear.ts
@@ -1,10 +1,14 @@
 import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 
+import { allPetIDs } from '../../lib/data/CollectionsExport';
 import { GearSetupType, GearStat } from '../../lib/gear';
+import { equipPet } from '../../lib/minions/functions/equipPet';
+import { unequipPet } from '../../lib/minions/functions/unequipPet';
+import { itemNameFromID } from '../../lib/util';
 import { gearEquipCommand, gearStatsCommand, gearUnequipCommand } from '../lib/abstracted_commands/gearCommands';
 import { equippedItemOption, gearPresetOption, gearSetupOption, ownedItemOption } from '../lib/mahojiCommandOptions';
 import { OSBMahojiCommand } from '../lib/util';
-import { mahojiUsersSettingsFetch } from '../mahojiSettings';
+import { getMahojiBank, mahojiUsersSettingsFetch } from '../mahojiSettings';
 
 export const gearCommand: OSBMahojiCommand = {
 	name: 'gear',
@@ -80,6 +84,33 @@ export const gearCommand: OSBMahojiCommand = {
 					required: true
 				}
 			]
+		},
+		{
+			type: ApplicationCommandOptionType.Subcommand,
+			name: 'pet',
+			description: 'Equip or unequip a pet.',
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: 'equip',
+					description: 'Equip a pet.',
+					required: false,
+					autocomplete: async (value, user) => {
+						const bank = getMahojiBank(await mahojiUsersSettingsFetch(user.id));
+						return allPetIDs
+							.filter(i => bank.has(i))
+							.map(i => itemNameFromID(i)!)
+							.filter(i => (!value ? true : i.toLowerCase().includes(value.toLowerCase())))
+							.map(i => ({ name: i, value: i }));
+					}
+				},
+				{
+					type: ApplicationCommandOptionType.Boolean,
+					name: 'unequip',
+					description: 'Unequip your pet.',
+					required: false
+				}
+			]
 		}
 	],
 	run: async ({
@@ -90,6 +121,7 @@ export const gearCommand: OSBMahojiCommand = {
 		equip?: { gear_setup: GearSetupType; item?: string; preset?: string; quantity?: number; auto?: string };
 		unequip?: { gear_setup: GearSetupType; item?: string; all?: boolean };
 		stats?: { gear_setup: string };
+		pet?: { equip?: string; unequip?: string };
 	}>) => {
 		const klasaUser = await globalClient.fetchUser(userID);
 		const mahojiUser = await mahojiUsersSettingsFetch(userID);
@@ -116,6 +148,9 @@ export const gearCommand: OSBMahojiCommand = {
 			);
 		}
 		if (options.stats) return gearStatsCommand(mahojiUser, options.stats.gear_setup);
+		if (options.pet?.equip) return equipPet(klasaUser, options.pet.equip);
+		if (options.pet?.unequip) return unequipPet(klasaUser);
+
 		return 'Invalid command.';
 	}
 };
