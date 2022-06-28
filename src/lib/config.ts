491cc5c89d6d754f615bec45aae522a785405b54
gc <30398469+gc@users.noreply.github.com>
2022-06-28 22:20:06 +1000

    move the slash command finish date closer

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -32,7 +32,7 @@ export const clientOptions: KlasaClientOptions = {
 		const deprecatedCommands = client.commands.filter(c => c.path.toLowerCase().includes('deprecated')).size;
 		const totalCmds = client.commands.size;
 		const commandsLeft = totalCmds - deprecatedCommands;
-		const endOfTheWorld = new Date('2022-08-31');
+		const endOfTheWorld = new Date('2022-08-14');
 		const diff = endOfTheWorld.getTime() - Date.now();
 		const daysUntil = diff / Time.Day;
 		const migrationStr = `There are ${
