ad564cac984d0eb249ce2f6642c37b5f24bdc7c7
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-06-29 04:11:18 -0700

    Fix PoH item mounting (#4026)

--- src/mahoji/commands/poh.ts
+++ src/mahoji/commands/poh.ts
@@ -7,9 +7,11 @@ import {
 	makePOHImage,
 	pohBuildCommand,
 	pohDestroyCommand,
+	pohMountItemCommand,
 	pohWallkitCommand,
 	pohWallkits
 } from '../lib/abstracted_commands/pohCommand';
+import { ownedItemOption } from '../lib/mahojiCommandOptions';
 import { OSBMahojiCommand } from '../lib/util';
 import { getSkillsOfMahojiUser, mahojiUsersSettingsFetch } from '../mahojiSettings';
 
@@ -85,6 +87,19 @@ export const pohCommand: OSBMahojiCommand = {
 					}
 				}
 			]
+		},
+		{
+			type: ApplicationCommandOptionType.Subcommand,
+			name: 'mount_item',
+			description: 'Mount an item into your PoH.',
+			options: [
+				{
+					...ownedItemOption(),
+					name: 'name',
+					description: 'The item you want to mount.',
+					required: true
+				}
+			]
 		}
 	],
 	run: async ({
@@ -96,6 +111,7 @@ export const pohCommand: OSBMahojiCommand = {
 		wallkit?: { name: string };
 		build?: { name: string };
 		destroy?: { name: string };
+		mount_item?: { name: string };
 	}>) => {
 		const user = await globalClient.fetchUser(userID);
 		const mahojiUser = await mahojiUsersSettingsFetch(userID);
@@ -112,6 +128,9 @@ export const pohCommand: OSBMahojiCommand = {
 		}
 		if (options.destroy) {
 			return pohDestroyCommand(user, options.destroy.name);
+		}
+		if (options.mount_item) {
+			return pohMountItemCommand(user, options.mount_item.name);
 		}
 		return 'Invalid command.';
 	}
