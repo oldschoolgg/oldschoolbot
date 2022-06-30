45407e31b4b44fc7b3f4d0942aec8cdd1cdad42f
gc <30398469+gc@users.noreply.github.com>
2022-07-01 01:00:29 +1000

    commit

--- src/lib/util/farmingHelpers.ts
+++ src/lib/util/farmingHelpers.ts
@@ -1,43 +1,15 @@
 import { time } from '@discordjs/builders';
-import { User } from '@prisma/client';
+import { FarmingPatchName } from '@prisma/client';
 
 import { Emoji } from '../constants';
 import { IPatchData, IPatchDataDetailed } from '../minions/farming/types';
 import Farming from '../skilling/skills/farming';
 import { stringMatches } from './cleanString';
 
-export const farmingPatchNames = [
-	'herb',
-	'fruit_tree',
-	'tree',
-	'allotment',
-	'hops',
-	'cactus',
-	'bush',
-	'spirit',
-	'hardwood',
-	'seaweed',
-	'vine',
-	'calquat',
-	'redwood',
-	'crystal',
-	'celastrus',
-	'hespori',
-	'flower',
-	'mushroom',
-	'belladonna'
-] as const;
-
-export type FarmingPatchName = typeof farmingPatchNames[number];
+export const farmingPatchNames = Object.values(FarmingPatchName);
 
 export function isPatchName(name: string): name is FarmingPatchName {
 	return farmingPatchNames.includes(name as FarmingPatchName);
-}
-
-export const farmingKeys: (keyof User)[] = farmingPatchNames.map(i => `farmingPatches_${i}` as const);
-
-export function getFarmingKeyFromName(name: FarmingPatchName): keyof User {
-	return `farmingPatches_${name}`;
 }
 
 export function findPlant(lastPlanted: IPatchData['lastPlanted']) {
