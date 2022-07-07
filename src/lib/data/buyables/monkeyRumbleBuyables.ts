3dd424d9f66a7822fb91ba5078980e61badb86ef
gc <30398469+gc@users.noreply.github.com>
2022-07-08 09:17:09 +1000

    fix greegree sacrifice issue

--- src/lib/data/buyables/monkeyRumbleBuyables.ts
+++ src/lib/data/buyables/monkeyRumbleBuyables.ts
@@ -35,6 +35,9 @@ for (const tier of monkeyTiers) {
 		skillsNeeded: {
 			strength: tier.strengthLevelReq
 		},
-		itemCost: index === 0 ? undefined : new Bank().add('Rumble token', Math.floor((index + 1) * 8.5))
+		itemCost:
+			index === 0
+				? new Bank().add('Coins', 50_000)
+				: new Bank().add('Rumble token', Math.floor((index + 1) * 8.5))
 	});
 }
