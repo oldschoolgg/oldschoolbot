b84ebf15211dd72238ed0742018bd37b4e146069
TastyPumPum <james.selfe@hotmail.co.uk>
2022-07-10 15:16:23 +0100

    Add Mystic Staves to createables
    Adds the mystic staves to createables.
    Fixes a bug with create not deducting the right amount of GP if creating multiple quantity of items.
    Changes the lms mystic staff createables so there is a difference between the commands.

--- src/mahoji/commands/create.ts
+++ src/mahoji/commands/create.ts
@@ -162,7 +162,7 @@ export const createCommand: OSBMahojiCommand = {
 		const inItems = new Bank(createableItem.inputItems).multiply(quantity);
 
 		if (createableItem.GPCost) {
-			inItems.add('Coins', createableItem.GPCost);
+			inItems.add('Coins', createableItem.GPCost * quantity);
 		}
 
 		await user.settings.sync(true);
@@ -184,14 +184,14 @@ export const createCommand: OSBMahojiCommand = {
 			await handleMahojiConfirmation(
 				interaction,
 				`${user}, please confirm that you want to revert **${inItems}${
-					createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
+					createableItem.GPCost ? ` ` : ''
 				}** into ${outItems}.`
 			);
 		} else {
 			await handleMahojiConfirmation(
 				interaction,
 				`${user}, please confirm that you want to ${action} **${outItems}** using ${inItems}${
-					createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
+					createableItem.GPCost ? ` ` : ''
 				}.`
 			);
 		}
@@ -199,7 +199,7 @@ export const createCommand: OSBMahojiCommand = {
 		// Ensure they have the required items to create the item.
 		if (!user.owns(inItems)) {
 			return `You don't have the required items to ${action} this item. You need: ${inItems}${
-				createableItem.GPCost ? ` and ${(createableItem.GPCost * quantity).toLocaleString()} GP` : ''
+				createableItem.GPCost ? ` ` : ''
 			}.`;
 		}
 
