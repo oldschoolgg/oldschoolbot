fcdd590b79c2e919b6d106d1da6a789880ae34c1
gc <30398469+gc@users.noreply.github.com>
2022-07-25 08:13:54 +1000

    commit

---
+++ src/lib/blacklists.ts
@@ -0,0 +1,16 @@
+import { Time } from 'e';
+
+export const BLACKLISTED_USERS = new Set<string>();
+export const BLACKLISTED_GUILDS = new Set<string>();
+
+export async function syncBlacklists() {
+	const blacklistedEntities = await roboChimpClient.blacklistedEntity.findMany();
+	BLACKLISTED_USERS.clear();
+	BLACKLISTED_GUILDS.clear();
+	for (const entity of blacklistedEntities) {
+		const set = entity.type === 'guild' ? BLACKLISTED_GUILDS : BLACKLISTED_USERS;
+		set.add(entity.id.toString());
+	}
+}
+
+setInterval(syncBlacklists, Time.Minute * 10);
