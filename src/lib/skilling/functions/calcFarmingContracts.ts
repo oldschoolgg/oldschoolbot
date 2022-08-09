86283be8d483c9b14aeac05502ef0f83574b1699
Dark <96085008+DarkWorldsArtist@users.noreply.github.com>
2022-08-09 13:53:57 -0400

    Update calcFarmingContracts.ts

--- src/lib/skilling/functions/calcFarmingContracts.ts
+++ src/lib/skilling/functions/calcFarmingContracts.ts
@@ -17,7 +17,7 @@ export function openSeedPack(seedTier: number): Bank {
 	if (seedTier > 2) {
 		tempTable.tertiary(10, new LootTable().every('Mysterious seed', [1, 3]))
 			.tertiary(10, new LootTable().every('Korulsi seed', [1, 3]))
-			.tertiary(15, new LootTable().every('Grand crystal acorn', [1, 3]));
+			.tertiary(35, new LootTable().every('Grand crystal acorn', [1, 3]));
 	}
 
 	// Roll amount variables
