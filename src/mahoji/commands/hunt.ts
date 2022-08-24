43288da05204d5d20b058bd42dbc2696783dad6e
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-08-24 10:02:11 -0700

    revert hunt clarity fix because its probably in master

--- src/mahoji/commands/hunt.ts
+++ src/mahoji/commands/hunt.ts
@@ -172,7 +172,9 @@ export const huntCommand: OSBMahojiCommand = {
 			}
 			const { hasEnough } = brewRestoreSupplyCalc(user, 10, 5);
 			if (!hasEnough) {
-				return `To hunt ${creature.name} in the wilderness you need to have 10x Saradomin brew(4) and 5x Super restore(4) for safety.`;
+				if (userBank.amount('Saradomin brew(4)') < 10 || userBank.amount('Super restore(4)') < 5) {
+					return `To hunt ${creature.name} in the wilderness you need to have 10x Saradomin brew(4) and 5x Super restore(4) for safety.`;
+				}
 			}
 		}
 
