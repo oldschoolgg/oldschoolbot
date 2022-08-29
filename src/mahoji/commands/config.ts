8bd3c1a54d174feafb37dee6138a586b8280169a
alex <67626131+alexpvpmindustry@users.noreply.github.com>
2022-08-29 23:18:53 +0800

    fixed typo in command (#4390)

--- src/mahoji/commands/config.ts
+++ src/mahoji/commands/config.ts
@@ -429,7 +429,7 @@ async function handleCombatOptions(user: KlasaUser, command: 'add' | 'remove' | 
 	if (!command || (command && command === 'list')) {
 		// List enabled combat options:
 		const cbOpts = settings.combat_options.map(o => CombatOptionsArray.find(coa => coa!.id === o)!.name);
-		return `Your current combat options are:\n${cbOpts.join('\n')}\n\nTry: \`/config user command_options help\``;
+		return `Your current combat options are:\n${cbOpts.join('\n')}\n\nTry: \`/config user combat_options help\``;
 	}
 
 	if (command === 'help' || !option || !['add', 'remove'].includes(command)) {
