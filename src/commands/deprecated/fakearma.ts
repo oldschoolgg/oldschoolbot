ac136cf6521ab9d3b45ea1b6bc591df3dcb3522f
gc <30398469+gc@users.noreply.github.com>
2022-08-31 15:58:15 +1000

    remove all deprecated commands

--- src/commands/deprecated/fakearma.ts
+++
@@ -1,9 +0,0 @@
-import { KlasaMessage } from 'klasa';
-
-import { BotCommand } from '../../lib/structures/BotCommand';
-
-export default class extends BotCommand {
-	async run(msg: KlasaMessage) {
-		return msg.channel.send('This command is now in the `fake` command, e.g. `+fake bandos/zammy/arma, Magnaboy`');
-	}
-}
