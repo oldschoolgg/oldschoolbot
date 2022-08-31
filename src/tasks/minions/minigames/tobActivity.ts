8d37d962cd9cee43b59d00e5a8025e0558413f37
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-08-30 20:10:36 -0700

    Solo ToB for BSO along with some boosts

--- src/tasks/minions/minigames/tobActivity.ts
+++ src/tasks/minions/minigames/tobActivity.ts
@@ -14,7 +14,6 @@ import { TheatreOfBloodTaskOptions } from '../../../lib/types/minions';
 import { convertPercentChance, updateBankSetting } from '../../../lib/util';
 import { formatOrdinal } from '../../../lib/util/formatOrdinal';
 import { handleTripFinish } from '../../../lib/util/handleTripFinish';
-import { sendToChannelID } from '../../../lib/util/webhook';
 import { allItemsOwned } from '../../../mahoji/mahojiSettings';
 
 export default class extends Task {
@@ -61,11 +60,30 @@ export default class extends Task {
 		// GIVE XP HERE
 		// 100k tax if they wipe
 		if (wipedRoom !== null) {
-			sendToChannelID(channelID, {
-				content: `${allTag} Your team wiped in the Theatre of Blood, in the ${TOBRooms[wipedRoom].name} room!${
-					diedToMaiden ? ' The team died very early, and nobody learnt much from this raid.' : ''
-				}`
-			});
+			const resultMessage = `${allTag} Your team wiped in the Theatre of Blood, in the ${
+				TOBRooms[wipedRoom].name
+			} room!${diedToMaiden ? ' The team died very early, and nobody learnt much from this raid.' : ''}`;
+			handleTripFinish(
+				allUsers[0],
+				channelID,
+				resultMessage,
+				[
+					'raid',
+					{
+						tob: {
+							start: {
+								hard_mode: hardMode,
+								max_team_size: allUsers.length,
+								solo: allUsers.length === 1
+							}
+						}
+					},
+					true
+				],
+				undefined,
+				data,
+				null
+			);
 			// They each paid 100k tax, it doesn't get refunded, so track it in economy stats.
 			await updateBankSetting(
 				this.client,
@@ -159,7 +177,8 @@ Unique chance: ${result.percentChanceOfUnique.toFixed(2)}% (1 in ${convertPercen
 					tob: {
 						start: {
 							hard_mode: hardMode,
-							max_team_size: allUsers.length
+							max_team_size: allUsers.length,
+							solo: allUsers.length === 1
 						}
 					}
 				},
