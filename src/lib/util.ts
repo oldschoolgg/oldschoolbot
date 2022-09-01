0c92b8ba08612e03ddfbe1b532c82636353e28d7
Lajnux <68188574+Lajnux@users.noreply.github.com>
2022-09-01 19:42:09 +0200

    Mining rework (#3774)

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -158,10 +158,6 @@ export function convertXPtoLVL(xp: number, cap = 99) {
 	return cap;
 }
 
-export function determineScaledOreTime(xp: number, respawnTime: number, lvl: number) {
-	const t = xp / (lvl / 4 + 0.5) + ((100 - lvl) / 100 + 0.75);
-	return Math.floor((t + respawnTime) * 1000) * 1.2;
-}
 export function determineScaledLogTime(xp: number, respawnTime: number, lvl: number) {
 	const t = xp / (lvl / 4 + 0.5) + ((100 - lvl) / 100 + 0.75);
 	return Math.floor((t + respawnTime) * 1000) * 1.2;
