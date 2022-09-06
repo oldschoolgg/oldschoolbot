35be67092894fc330e1fde538bbe1748c9ba1a5d
TastyPumPum <james.selfe@hotmail.co.uk>
2022-09-06 20:03:23 +0100

    Changes

--- src/tasks/minions/miningActivity.ts
+++ src/tasks/minions/miningActivity.ts
@@ -1,6 +1,6 @@
 import { roll, Time } from 'e';
 import { Task } from 'klasa';
-import { Bank } from 'oldschooljs';
+import { Bank, LootTable } from 'oldschooljs';
 
 import { Emoji, Events } from '../../lib/constants';
 import { FaladorDiary, userhasDiaryTier } from '../../lib/diaries';
@@ -8,14 +8,14 @@ import addSkillingClueToLoot from '../../lib/minions/functions/addSkillingClueTo
 import Mining from '../../lib/skilling/skills/mining';
 import { SkillsEnum } from '../../lib/skilling/types';
 import { MiningActivityTaskOptions } from '../../lib/types/minions';
-import { percentChance, rand } from '../../lib/util';
+import { rand } from '../../lib/util';
 import { calcMaxTripLength } from '../../lib/util/calcMaxTripLength';
 import { handleTripFinish } from '../../lib/util/handleTripFinish';
 
 export default class extends Task {
 	async run(data: MiningActivityTaskOptions) {
 		const { oreID, userID, channelID, duration, powermine } = data;
-		let { quantity } = data;
+		let quantity = 100_000;
 		const user = await this.client.fetchUser(userID);
 
 		const ore = Mining.Ores.find(ore => ore.id === oreID)!;
@@ -123,46 +123,37 @@ export default class extends Task {
 				}
 				loot.add(ore.id, daeyaltQty);
 			} else if (ore.name === 'Motherlode mine') {
-				let nugget = 0;
-				let runite = 0;
-				let adamantite = 0;
-				let mithril = 0;
-				let gold = 0;
-				let coal = 0;
-				let nuggetChance = 2.73;
-				let runiteChance = 0.080_71 * currentLevel - 5.721;
-				let adamantiteChance = 0.5187 * currentLevel - 32.39;
-				let mithrilChance = 0.2521 * currentLevel + 2.705;
-				let goldChance = 0.2211 * currentLevel + 2.807;
+				const nuggetChance = 273;
+				let runiteChance = currentLevel >= 85 ? Math.round(100 * (0.080_71 * currentLevel - 5.721)) : 0;
+				let adamantiteChance = currentLevel >= 70 ? Math.round(100 * (0.5187 * currentLevel - 32.39)) : 0;
+				let mithrilChance = currentLevel >= 55 ? Math.round(100 * (0.2521 * currentLevel + 2.705)) : 0;
+				let goldChance = currentLevel >= 40 ? Math.round(100 * (0.2211 * currentLevel + 2.807)) : 0;
 
 				// Check for falador elite diary for increased ore rates
 				const [hasEliteDiary] = await userhasDiaryTier(user, FaladorDiary.elite);
 				if (hasEliteDiary) {
-					runiteChance += 1;
-					adamantiteChance += 1;
-					mithrilChance += 1;
-					goldChance += 1;
+					if (currentLevel >= 85) runiteChance += 100;
+					else if (currentLevel >= 70) adamantiteChance += 100;
+					else if (currentLevel >= 55) mithrilChance += 100;
+					else if (currentLevel >= 40) goldChance += 100;
 				}
-				console.log(nuggetChance / 100);
-				console.log(runiteChance / 100);
-				console.log(adamantiteChance / 100);
-				console.log(mithrilChance / 100);
-				console.log(goldChance / 100);
 
-				for (let i = 0; i < quantity; i++) {
-					if (percentChance(nuggetChance)) nugget++;
-					else if (currentLevel >= 85 && percentChance(runiteChance)) runite++;
-					else if (currentLevel >= 70 && percentChance(adamantiteChance)) adamantite++;
-					else if (currentLevel >= 55 && percentChance(mithrilChance)) mithril++;
-					else if (currentLevel >= 40 && percentChance(goldChance)) gold++;
-					else coal++;
-				}
-				loot.add('Golden nugget', nugget);
-				loot.add('Runite ore', runite);
-				loot.add('Adamantite ore', adamantite);
-				loot.add('Mithril ore', mithril);
-				loot.add('Gold ore', gold);
-				loot.add('Coal', coal);
+				const coalChance =
+					10_000 - (nuggetChance + runiteChance + adamantiteChance + mithrilChance + goldChance);
+
+				console.log(
+					`nuggets: ${nuggetChance}\nrunite:${runiteChance}\naddy: ${adamantiteChance}\nmith: ${mithrilChance}\ngold: ${goldChance}\ncoal: ${coalChance}`
+				);
+
+				const table = new LootTable()
+					.add('Golden nugget', 1, nuggetChance)
+					.add('Runite ore', 1, runiteChance)
+					.add('Adamantite ore', 1, adamantiteChance)
+					.add('Mithril ore', 1, mithrilChance)
+					.add('Gold ore', 1, goldChance)
+					.add('Coal', 1, coalChance);
+
+				loot.add(table.roll(quantity));
 			} else {
 				loot.add(ore.id, quantity);
 			}
