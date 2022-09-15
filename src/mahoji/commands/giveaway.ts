e172922a70288756555102c6ab66dc6c555f6723
GC <30398469+gc@users.noreply.github.com>
2022-09-16 04:32:37 +1000

    Use buttons for giveaways (#4459)

--- src/mahoji/commands/giveaway.ts
+++ src/mahoji/commands/giveaway.ts
@@ -1,17 +1,39 @@
-import { time } from '@discordjs/builders';
 import { Duration } from '@sapphire/time-utilities';
-import { AttachmentBuilder, PermissionsBitField } from 'discord.js';
-import { Time } from 'e';
+import {
+	ActionRowBuilder,
+	AttachmentBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	MessageOptions,
+	PermissionsBitField
+} from 'discord.js';
+import { randInt, Time } from 'e';
 import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 
 import { addToGPTaxBalance, prisma } from '../../lib/settings/prisma';
-import { channelIsSendable, getSupportGuild } from '../../lib/util';
+import { channelIsSendable } from '../../lib/util';
+import { generateGiveawayContent } from '../../lib/util/giveaway';
 import { logError } from '../../lib/util/logError';
 import { makeBankImage } from '../../lib/util/makeBankImage';
 import { parseBank } from '../../lib/util/parseStringBank';
 import { filterOption } from '../lib/mahojiCommandOptions';
 import { OSBMahojiCommand } from '../lib/util';
 import { handleMahojiConfirmation, mahojiUsersSettingsFetch } from '../mahojiSettings';
+
+function makeGiveawayButtons(giveawayID: number): MessageOptions['components'] {
+	return [
+		new ActionRowBuilder<ButtonBuilder>().addComponents([
+			new ButtonBuilder()
+				.setCustomId(`GIVEAWAY_ENTER_${giveawayID}`)
+				.setLabel('Join Giveaway')
+				.setStyle(ButtonStyle.Primary),
+			new ButtonBuilder()
+				.setCustomId(`GIVEAWAY_LEAVE_${giveawayID}`)
+				.setLabel('Leave Giveaway')
+				.setStyle(ButtonStyle.Secondary)
+		])
+	];
+}
 
 export const giveawayCommand: OSBMahojiCommand = {
 	name: 'giveaway',
@@ -53,7 +75,8 @@ export const giveawayCommand: OSBMahojiCommand = {
 		userID,
 		guildID,
 		interaction,
-		channelID
+		channelID,
+		user: apiUser
 	}: CommandRunOptions<{ start?: { duration: string; items?: string; filter?: string; search?: string } }>) => {
 		const user = await mUserFetch(userID);
 		if (user.isIronman) return 'You cannot do giveaways!';
@@ -86,7 +109,7 @@ export const giveawayCommand: OSBMahojiCommand = {
 				excludeItems: mUser.favoriteItems,
 				user,
 				search: options.start.search,
-				filters: [options.start.filter],
+				filters: [options.start.filter, 'tradeables'],
 				maxSize: 70
 			});
 
@@ -94,27 +117,21 @@ export const giveawayCommand: OSBMahojiCommand = {
 				return "You don't own those items.";
 			}
 
-			await handleMahojiConfirmation(
-				interaction,
-				`Are you sure you want to do a giveaway with these items? You cannot cancel the giveaway or retrieve the items back afterwards: ${bank}`
-			);
+			if (bank.items().some(i => !i[0].tradeable)) {
+				return "You can't giveaway untradeable items.";
+			}
+
+			if (interaction) {
+				await handleMahojiConfirmation(
+					interaction,
+					`Are you sure you want to do a giveaway with these items? You cannot cancel the giveaway or retrieve the items back afterwards: ${bank}`
+				);
+			}
 
 			const duration = new Duration(options.start.duration);
 			const ms = duration.offset;
 			if (!ms || ms > Time.Day * 7 || ms < Time.Second * 5) {
 				return 'Your giveaway cannot last longer than 7 days, or be faster than 5 seconds.';
-			}
-
-			const supportServer = getSupportGuild();
-			if (!supportServer) return "Couldn't find support server!";
-
-			// fetch() always calls the API when no id is specified, so only do it sparingly or if needed.
-			if (supportServer.emojis.cache.size === 0) {
-				await supportServer.fetch();
-			}
-			const reaction = supportServer.emojis.cache.random();
-			if (!reaction || !reaction.id) {
-				return "Couldn't retrieve emojis for this guild, ensure you have some emojis and try again.";
 			}
 
 			await user.sync();
@@ -125,36 +142,23 @@ export const giveawayCommand: OSBMahojiCommand = {
 			if (bank.length === 0) {
 				return 'You cannot have a giveaway with no items in it.';
 			}
+
+			const giveawayID = randInt(1, 100_000_000);
+
 			const message = await channel.send({
-				content: `You created a giveaway that will finish at ${time(duration.fromNow, 'F')} (${time(
-					duration.fromNow,
-					'R'
-				)}), the winner will receive these items.
-			
-React to this messsage with ${reaction} to enter.`,
+				content: generateGiveawayContent(user.id, duration.fromNow, []),
 				files: [
 					new AttachmentBuilder(
 						(
 							await makeBankImage({
 								bank,
-								title: `${user.usernameOrMention}'s Giveaway`
+								title: `${apiUser.username}'s Giveaway`
 							})
 						).file.buffer
 					)
-				]
+				],
+				components: makeGiveawayButtons(giveawayID)
 			});
-
-			const dbData = {
-				channel_id: channelID.toString(),
-				start_date: new Date(),
-				finish_date: duration.fromNow,
-				completed: false,
-				loot: bank.bank,
-				user_id: user.id,
-				reaction_id: reaction.id,
-				duration: duration.offset,
-				message_id: message.id
-			};
 
 			try {
 				await user.removeItemsFromBank(bank);
@@ -167,22 +171,27 @@ React to this messsage with ${reaction} to enter.`,
 
 			try {
 				await prisma.giveaway.create({
-					data: dbData
+					data: {
+						id: giveawayID,
+						channel_id: channelID.toString(),
+						start_date: new Date(),
+						finish_date: duration.fromNow,
+						completed: false,
+						loot: bank.bank,
+						user_id: user.id,
+						duration: duration.offset,
+						message_id: message.id,
+						users_entered: []
+					}
 				});
 			} catch (err: any) {
 				logError(err, {
 					user_id: user.id,
-					giveaway_data: JSON.stringify(dbData)
+					giveaway: bank.toString()
 				});
-				return 'Error starting giveaway. Please report this error so you can get refunded.';
+				return 'Error starting giveaway.';
 			}
 
-			try {
-				await message.react(reaction);
-			} catch (err: any) {
-				logError(err, { user_id: user.id, emoji_id: reaction.id, guild_id: guildID.toString() });
-				return 'Unknown error. You will be refunded when the giveaway ends if the giveaway fails.';
-			}
 			return 'Giveaway started.';
 		}
 	}
