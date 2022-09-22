1aa2555660891a1c05869a25dea1f8bcf3a19d9e
Lajnux <lajnuxxx@gmail.com>
2022-09-22 18:09:51 +0200

    remove lost bag and amulet dye options

--- src/lib/data/creatables/guardiansOfTheRiftCreatables.ts
+++ src/lib/data/creatables/guardiansOfTheRiftCreatables.ts
@@ -5,28 +5,6 @@ import { Createable } from '../createables';
 export const guardiansOfTheRiftCreatables: Createable[] = [
 	// Red Recolours
 	{
-		name: 'Amulet of the eye (red)',
-		inputItems: new Bank({ 'Amulet of the eye': 1, 'Abyssal red dye': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye (red)': 1 })
-	},
-	{
-		name: 'Revert Amulet of the eye (red)',
-		inputItems: new Bank({ 'Amulet of the eye (red)': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye': 1 }),
-		noCl: true
-	},
-	{
-		name: 'Lost bag (red)',
-		inputItems: new Bank({ 'Lost bag': 1, 'Abyssal red dye': 1 }),
-		outputItems: new Bank({ 'Lost bag (red)': 1 })
-	},
-	{
-		name: 'Revert Lost bag (red)',
-		inputItems: new Bank({ 'Lost bag (red)': 1 }),
-		outputItems: new Bank({ 'Lost bag': 1 }),
-		noCl: true
-	},
-	{
 		name: 'Hat of the eye (red)',
 		inputItems: new Bank({ 'Hat of the eye': 1, 'Abyssal red dye': 1 }),
 		outputItems: new Bank({ 'Hat of the eye (red)': 1 })
@@ -61,28 +39,6 @@ export const guardiansOfTheRiftCreatables: Createable[] = [
 	},
 	// Green Recolours
 	{
-		name: 'Amulet of the eye (green)',
-		inputItems: new Bank({ 'Amulet of the eye': 1, 'Abyssal green dye': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye (green)': 1 })
-	},
-	{
-		name: 'Revert Amulet of the eye (green)',
-		inputItems: new Bank({ 'Amulet of the eye (green)': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye': 1 }),
-		noCl: true
-	},
-	{
-		name: 'Lost bag (green)',
-		inputItems: new Bank({ 'Lost bag': 1, 'Abyssal green dye': 1 }),
-		outputItems: new Bank({ 'Lost bag (green)': 1 })
-	},
-	{
-		name: 'Revert Lost bag (green)',
-		inputItems: new Bank({ 'Lost bag (green)': 1 }),
-		outputItems: new Bank({ 'Lost bag': 1 }),
-		noCl: true
-	},
-	{
 		name: 'Hat of the eye (green)',
 		inputItems: new Bank({ 'Hat of the eye': 1, 'Abyssal green dye': 1 }),
 		outputItems: new Bank({ 'Hat of the eye (green)': 1 })
@@ -116,28 +72,6 @@ export const guardiansOfTheRiftCreatables: Createable[] = [
 		noCl: true
 	},
 	// Blue Recolours
-	{
-		name: 'Amulet of the eye (blue)',
-		inputItems: new Bank({ 'Amulet of the eye': 1, 'Abyssal blue dye': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye (blue)': 1 })
-	},
-	{
-		name: 'Revert Amulet of the eye (blue)',
-		inputItems: new Bank({ 'Amulet of the eye (blue)': 1 }),
-		outputItems: new Bank({ 'Amulet of the eye': 1 }),
-		noCl: true
-	},
-	{
-		name: 'Lost bag (blue)',
-		inputItems: new Bank({ 'Lost bag': 1, 'Abyssal blue dye': 1 }),
-		outputItems: new Bank({ 'Lost bag (blue)': 1 })
-	},
-	{
-		name: 'Revert Lost bag (blue)',
-		inputItems: new Bank({ 'Lost bag (blue)': 1 }),
-		outputItems: new Bank({ 'Lost bag': 1 }),
-		noCl: true
-	},
 	{
 		name: 'Hat of the eye (blue)',
 		inputItems: new Bank({ 'Hat of the eye': 1, 'Abyssal blue dye': 1 }),
