34054b57c39d48e6ba6c9d86f6ebfd71e8209d39
gc <30398469+gc@users.noreply.github.com>
2022-09-23 14:30:09 +1000

    fix reboot command

--- src/mahoji/commands/admin.ts
+++ src/mahoji/commands/admin.ts
@@ -33,7 +33,7 @@ import {
 } from '../../lib/util';
 import { getItem } from '../../lib/util/getOSItem';
 import getUsersPerkTier from '../../lib/util/getUsersPerkTier';
-import { deferInteraction } from '../../lib/util/interactionReply';
+import { deferInteraction, interactionReply } from '../../lib/util/interactionReply';
 import { logError } from '../../lib/util/logError';
 import { makeBankImage } from '../../lib/util/makeBankImage';
 import { parseBank } from '../../lib/util/parseStringBank';
@@ -802,7 +802,7 @@ LIMIT 10;
 				if (ticker.timer) clearTimeout(ticker.timer);
 			}
 			await sleep(Time.Second * 20);
-			await interaction.reply({
+			await interactionReply(interaction, {
 				content: 'https://media.discordapp.net/attachments/357422607982919680/1004657720722464880/freeze.gif'
 			});
 			process.exit();
