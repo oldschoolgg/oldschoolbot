92895e913e1c2dadb90a2704fad85044042347cc
TastyPumPum <james.selfe@hotmail.co.uk>
2022-09-28 17:27:06 +0100

    Lint

--- src/tasks/minions/runecraftActivity.ts
+++ src/tasks/minions/runecraftActivity.ts
@@ -47,18 +47,18 @@ export const runecraftTask: MinionTask = {
 			});
 
 			const { petDropRate } = skillingPetDropRate(user, SkillsEnum.Runecraft, 1_795_758);
-		if (roll(petDropRate / essenceQuantity)) {
-			loot.add('Rift guardian');
-			str += "\nYou have a funny feeling you're being followed...";
-			globalClient.emit(
-				Events.ServerNotification,
-				`${Emoji.Runecraft} **${user.usernameOrMention}'s** minion, ${
-					user.minionName
-				}, just received a Rift guardian while crafting ${rune.name}s at level ${user.skillLevel(
-					SkillsEnum.Runecraft
-				)} Runecrafting!`
-			);
-		}
+			if (roll(petDropRate / essenceQuantity)) {
+				loot.add('Rift guardian');
+				str += "\nYou have a funny feeling you're being followed...";
+				globalClient.emit(
+					Events.ServerNotification,
+					`${Emoji.Runecraft} **${user.usernameOrMention}'s** minion, ${
+						user.minionName
+					}, just received a Rift guardian while crafting ${rune.name}s at level ${user.skillLevel(
+						SkillsEnum.Runecraft
+					)} Runecrafting!`
+				);
+			}
 			str += `\n\nYou received: ${loot}.`;
 
 			await transactItems({
