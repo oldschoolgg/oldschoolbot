7cd114faa1a772bf3d6770ae7adb19d3e61b5872
themrrobert <10122432+themrrobert@users.noreply.github.com>
2022-10-02 03:45:41 -0700

    Add ability to  equip multiple items at once (#4164)

--- tests/utils.ts
+++ tests/utils.ts
@@ -3,7 +3,7 @@ import { Bank } from 'oldschooljs';
 import { convertLVLtoXP } from 'oldschooljs/dist/util';
 
 import { BitField } from '../src/lib/constants';
-import { PartialGearSetup } from '../src/lib/gear';
+import { filterGearSetup, GearSetup, PartialGearSetup } from '../src/lib/gear';
 import { MUserClass } from '../src/lib/MUser';
 import { Gear } from '../src/lib/structures/Gear';
 
@@ -28,8 +28,15 @@ export function mockArgument(arg: any) {
 interface MockUserArgs {
 	bank?: Bank;
 	cl?: Bank;
-	meleeGear?: PartialGearSetup;
+	meleeGear?: GearSetup | PartialGearSetup;
 	skills_agility?: number;
+	skills_attack?: number;
+	skills_strength?: number;
+	skills_ranged?: number;
+	skills_magic?: number;
+	skills_defence?: number;
+	skills_hitpoints?: number;
+	skills_prayer?: number;
 	GP?: number;
 	premium_balance_tier?: number;
 	premium_balance_expiry_date?: number;
@@ -37,10 +44,11 @@ interface MockUserArgs {
 }
 
 export const mockUser = (overrides?: MockUserArgs): User => {
+	const gearMelee = filterGearSetup(overrides?.meleeGear);
 	return {
 		gear_fashion: new Gear().raw() as Prisma.JsonValue,
 		gear_mage: new Gear().raw() as Prisma.JsonValue,
-		gear_melee: new Gear(overrides?.meleeGear).raw() as Prisma.JsonValue,
+		gear_melee: new Gear(gearMelee).raw() as Prisma.JsonValue,
 		gear_misc: new Gear().raw() as Prisma.JsonValue,
 		gear_other: new Gear().raw() as Prisma.JsonValue,
 		gear_range: new Gear().raw() as Prisma.JsonValue,
@@ -57,20 +65,20 @@ export const mockUser = (overrides?: MockUserArgs): User => {
 		skills_firemaking: 0,
 		skills_runecraft: 0,
 		skills_crafting: 0,
-		skills_prayer: 0,
+		skills_prayer: overrides?.skills_prayer ?? 0,
 		skills_fletching: 0,
 		skills_thieving: 0,
 		skills_farming: 0,
 		skills_herblore: 0,
 		skills_hunter: 0,
 		skills_construction: 0,
-		skills_magic: 0,
-		skills_ranged: 0,
-		skills_attack: 0,
-		skills_strength: 0,
-		skills_defence: 0,
+		skills_magic: overrides?.skills_magic ?? 0,
+		skills_ranged: overrides?.skills_ranged ?? 0,
+		skills_attack: overrides?.skills_attack ?? 0,
+		skills_strength: overrides?.skills_strength ?? 0,
+		skills_defence: overrides?.skills_defence ?? 0,
 		skills_slayer: 0,
-		skills_hitpoints: convertLVLtoXP(10),
+		skills_hitpoints: overrides?.skills_hitpoints ?? convertLVLtoXP(10),
 		GP: overrides?.GP,
 		premium_balance_tier: overrides?.premium_balance_tier,
 		premium_balance_expiry_date: overrides?.premium_balance_expiry_date,
