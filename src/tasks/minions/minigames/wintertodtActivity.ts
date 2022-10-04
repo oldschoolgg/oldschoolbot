07ec60323319053d5f0a67aef8251b87bc640460
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2022-10-04 04:07:49 +0100

    Tidy grammar in Wintertodt activity (#4546)

--- src/tasks/minions/minigames/wintertodtActivity.ts
+++ src/tasks/minions/minigames/wintertodtActivity.ts
@@ -116,8 +116,8 @@ export const wintertodtTask: MinionTask = {
 			}
 		}
 
-		xpStr += ` ${await user.addXP({ skillName: SkillsEnum.Woodcutting, amount: wcXpToGive })}`;
-		xpStr += ` ${await user.addXP({ skillName: SkillsEnum.Firemaking, amount: fmXpToGive })}`;
+		xpStr += `, ${await user.addXP({ skillName: SkillsEnum.Woodcutting, amount: wcXpToGive })}`;
+		xpStr += `, ${await user.addXP({ skillName: SkillsEnum.Firemaking, amount: fmXpToGive })}`;
 		const newLevel = user.skillLevel(SkillsEnum.Firemaking);
 
 		const { itemsAdded, previousCL } = await transactItems({
@@ -133,9 +133,7 @@ export const wintertodtTask: MinionTask = {
 			previousCL
 		});
 
-		let output = `${user}, ${
-			user.minionName
-		} finished subduing Wintertodt ${quantity}x times. You got ${xpStr} Firemaking XP, ${wcXpToGive.toLocaleString()}, you cut ${numberOfRoots}x Bruma roots.`;
+		let output = `${user}, ${user.minionName} finished subduing Wintertodt ${quantity}x times. ${xpStr}, you cut ${numberOfRoots}x Bruma roots.`;
 
 		if (fmBonusXP > 0) {
 			output += `\n\n**Firemaking Bonus XP:** ${fmBonusXP.toLocaleString()}`;
