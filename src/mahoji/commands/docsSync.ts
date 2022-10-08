fd3ddebc41657fdbc8eeecf2ab766d806fb3f1d9
Tom <t.w.campbell92@gmail.com>
2022-10-08 13:32:56 +0100

    Revert "remove options on sync"
    This reverts commit 237d1ceae5fa804de04fdd2ee595cdf97faedba6.

--- src/mahoji/commands/docsSync.ts
+++ src/mahoji/commands/docsSync.ts
@@ -1,4 +1,4 @@
-import { CommandRunOptions } from 'mahoji';
+import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 
 import { docArticles } from '../../lib/docsHelper';
 import { stringMatches } from '../../lib/util/cleanString';
@@ -7,7 +7,14 @@ import { OSBMahojiCommand } from '../lib/util';
 export const docsSyncCommand: OSBMahojiCommand = {
 	name: 'docs',
 	description: 'Search the BSO wiki.',
-	options: [],
+	options: [
+		{
+			type: ApplicationCommandOptionType.Boolean,
+			name: 'update',
+			description: 'Your search query.',
+			required: true
+		}
+	],
 	run: async ({ options }: CommandRunOptions<{ query: string }>) => {
 		const foundArticle = docArticles.find(item => stringMatches(item.name, options.query));
 		console.log(foundArticle);
