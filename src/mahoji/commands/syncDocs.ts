332b9d80a600640ab02f472ccb04cf078e6e9aab
Tom <t.w.campbell92@gmail.com>
2022-10-09 14:54:11 +0100

    Add deferinteraction to stop timeout

--- src/mahoji/commands/syncDocs.ts
+++ src/mahoji/commands/syncDocs.ts
@@ -1,12 +1,14 @@
 import { syncDocs } from '../../lib/docsActivity';
+import { deferInteraction } from '../../lib/util/interactionReply';
 import { OSBMahojiCommand } from '../lib/util';
 
 export const syncDocsCommand: OSBMahojiCommand = {
 	name: 'sync_docs',
 	description: 'Sync wiki articles',
 	options: [],
-	run: async () => {
-		syncDocs();
+	run: async ({ interaction }) => {
+		await deferInteraction(interaction);
+		await syncDocs();
 		return 'Docs updated';
 	}
 };
