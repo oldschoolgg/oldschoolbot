6752a786e1adb14724c0ffd1a03f20c605b5c28f
GC <30398469+gc@users.noreply.github.com>
2022-10-23 21:57:39 +1100

    Add clue finishing simulating (#4628)

--- src/lib/workers/index.ts
+++ src/lib/workers/index.ts
@@ -2,6 +2,8 @@ import { Bank } from 'oldschooljs';
 import { resolve } from 'path';
 import Piscina from 'piscina';
 
+import { ItemBank } from '../types';
+
 export interface CasketWorkerArgs {
 	clueTierID: number;
 	quantity: number;
@@ -22,11 +24,24 @@ export interface KillWorkerReturn {
 	content?: string;
 }
 
-export const piscinaPool = new Piscina();
+export interface FinishWorkerArgs {
+	name: string;
+}
+
+export type FinishWorkerReturn =
+	| {
+			loot: ItemBank;
+			kcBank: ItemBank;
+			kc: number;
+	  }
+	| string;
+
+export const finishWorker = new Piscina({ filename: resolve(__dirname, 'finish.worker.js') });
+export const killWorker = new Piscina({ filename: resolve(__dirname, 'kill.worker.js') });
+export const casketWorker = new Piscina({ filename: resolve(__dirname, 'casket.worker.js') });
 
 export const Workers = {
-	casketOpen: (args: CasketWorkerArgs): Promise<[Bank, string]> =>
-		piscinaPool.runTask(args, resolve(__dirname, 'casket.worker.js')),
-	kill: (args: KillWorkerArgs): Promise<KillWorkerReturn> =>
-		piscinaPool.runTask(args, resolve(__dirname, 'kill.worker.js'))
+	casketOpen: (args: CasketWorkerArgs): Promise<[Bank, string]> => casketWorker.run(args),
+	kill: (args: KillWorkerArgs): Promise<KillWorkerReturn> => killWorker.run(args),
+	finish: (args: FinishWorkerArgs): Promise<FinishWorkerReturn> => finishWorker.run(args)
 };
