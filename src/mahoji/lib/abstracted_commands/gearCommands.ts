7c94f0b3644270c0ee2a0985b57754b078584bbd
gc <30398469+gc@users.noreply.github.com>
2022-10-28 23:39:58 +1100

    handle gear error

--- src/mahoji/lib/abstracted_commands/gearCommands.ts
+++ src/mahoji/lib/abstracted_commands/gearCommands.ts
@@ -14,7 +14,7 @@ import { prisma } from '../../../lib/settings/prisma';
 import { Gear } from '../../../lib/structures/Gear';
 import { assert, formatSkillRequirements, isValidGearSetup, stringMatches, toTitleCase } from '../../../lib/util';
 import { gearEquipMultiImpl } from '../../../lib/util/equipMulti';
-import getOSItem, { getItem } from '../../../lib/util/getOSItem';
+import { getItem } from '../../../lib/util/getOSItem';
 import getUsersPerkTier from '../../../lib/util/getUsersPerkTier';
 import { minionIsBusy } from '../../../lib/util/minionIsBusy';
 import { handleMahojiConfirmation, mahojiParseNumber } from '../../mahojiSettings';
@@ -357,8 +357,8 @@ export async function autoEquipCommand(user: MUser, gearSetup: GearSetupType, eq
 export async function gearStatsCommand(user: MUser, input: string): CommandResponse {
 	const gear = { ...defaultGear };
 	for (const name of input.split(',')) {
-		const item = getOSItem(name);
-		if (item.equipment) {
+		const item = getItem(name);
+		if (item && item.equipment) {
 			gear[item.equipment.slot] = { item: item.id, quantity: 1 };
 		}
 	}
