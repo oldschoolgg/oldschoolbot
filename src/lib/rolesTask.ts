6d2a51d880d530e04f7477897033feeb335fc523
gc <30398469+gc@users.noreply.github.com>
2022-10-28 23:28:01 +1100

    remove bosses/rolepets from top collector roles

--- src/lib/rolesTask.ts
+++ src/lib/rolesTask.ts
@@ -20,18 +20,7 @@ function addToUserMap(userMap: Record<string, string[]>, id: string, reason: str
 
 const minigames = Minigames.map(game => game.column).filter(i => i !== 'tithe_farm');
 
-const collections = [
-	'rolepets',
-	'skilling',
-	'clues',
-	'bosses',
-	'minigames',
-	'raids',
-	'Dyed Items',
-	'slayer',
-	'other',
-	'custom'
-];
+const collections = ['skilling', 'clues', 'minigames', 'raids', 'Dyed Items', 'slayer', 'other'];
 
 for (const cl of collections) {
 	const items = getCollectionItems(cl);
