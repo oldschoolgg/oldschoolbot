3d6f4d959399c014913e24c494002afa12c377db
TastyPumPum <james.selfe@hotmail.co.uk>
2022-11-02 19:23:24 +0000

    Update taskGroupFromActivity.ts

--- src/lib/util/taskGroupFromActivity.ts
+++ src/lib/util/taskGroupFromActivity.ts
@@ -15,7 +15,6 @@ export function taskGroupFromActivity(type: activity_type_enum): ActivityGroup {
 		case 'Cooking':
 		case 'Firemaking':
 		case 'Mining':
-		case 'MotherlodeMining':
 		case 'Questing':
 		case 'Runecraft':
 		case 'Smelting':
