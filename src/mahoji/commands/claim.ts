0a1ddd7ffb394c369d8abe7bc8423f79e346ca2d
gc <30398469+gc@users.noreply.github.com>
2022-11-15 21:17:53 +1100

    commit

---
+++ src/mahoji/commands/claim.ts
@@ -0,0 +1,65 @@
+import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
+
+import { BitField, Channel } from '../../lib/constants';
+import { roboChimpUserFetch } from '../../lib/roboChimp';
+import { stringMatches } from '../../lib/util';
+import { sendToChannelID } from '../../lib/util/webhook';
+import { OSBMahojiCommand } from '../lib/util';
+
+const claimables = [
+	{
+		name: 'Free T1 Perks',
+		hasRequirement: async (user: MUser) => {
+			const roboChimpUser = await roboChimpUserFetch(user.id);
+			return roboChimpUser.osb_total_level === 2277 && roboChimpUser.bso_total_level === 3000;
+		},
+		action: async (user: MUser) => {
+			if (user.bitfield.includes(BitField.BothBotsMaxedFreeTierOnePerks)) {
+				return 'You already claimed this!';
+			}
+			sendToChannelID(Channel.General, {
+				content: `${user.mention} just claimed free T1 patron perks for being maxed in both bots!`
+			});
+			await user.update({
+				bitfield: {
+					push: BitField.BothBotsMaxedFreeTierOnePerks
+				}
+			});
+			return 'You claimed free T1 patron perks in OSB for being maxed in both bots. You can claim this on BSO too for free patron perks on BSO.';
+		}
+	}
+];
+
+export const claimCommand: OSBMahojiCommand = {
+	name: 'claim',
+	description: 'Claim prizes, rewards and other things.',
+	options: [
+		{
+			type: ApplicationCommandOptionType.String,
+			name: 'name',
+			description: 'The thing you want to claim.',
+			required: true,
+			autocomplete: async (_, u) => {
+				const user = await mUserFetch(u.id);
+				return (await Promise.all(claimables.filter(i => i.hasRequirement(user)))).map(i => ({
+					name: i.name,
+					value: i.name
+				}));
+			}
+		}
+	],
+	run: async ({ options, userID }: CommandRunOptions<{ name: string }>) => {
+		const user = await mUserFetch(userID);
+		const claimable = claimables.find(i => stringMatches(i.name, options.name));
+		if (!claimable) {
+			return 'Invalid thing to claim.';
+		}
+
+		if (!(await claimable.hasRequirement(user))) {
+			return 'You are not elligible for this reward.';
+		}
+
+		const result = await claimable.action(user);
+		return result;
+	}
+};
