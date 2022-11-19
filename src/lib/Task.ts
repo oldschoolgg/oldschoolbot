6879b26c33f13aa63b9a099e111d734a47bec819
gc <30398469+gc@users.noreply.github.com>
2022-11-19 19:11:59 +1100

    remove hween event

--- src/lib/Task.ts
+++ src/lib/Task.ts
@@ -17,7 +17,6 @@ import { fishingTask } from '../tasks/minions/fishingActivity';
 import { fletchingTask } from '../tasks/minions/fletchingActivity';
 import { gloryChargingTask } from '../tasks/minions/gloryChargingActivity';
 import { groupoMonsterTask } from '../tasks/minions/groupMonsterActivity';
-import { halloweenTask } from '../tasks/minions/halloweenActivity';
 import { herbloreTask } from '../tasks/minions/herbloreActivity';
 import { aerialFishingTask } from '../tasks/minions/HunterActivity/aerialFishingActivity';
 import { birdHouseTask } from '../tasks/minions/HunterActivity/birdhouseActivity';
@@ -159,8 +158,7 @@ export const tasks: MinionTask[] = [
 	shootingStarTask,
 	guardiansOfTheRiftTask,
 	butlerTask,
-	tiaraRunecraftTask,
-	halloweenTask
+	tiaraRunecraftTask
 ];
 
 export async function syncActivityCache() {
