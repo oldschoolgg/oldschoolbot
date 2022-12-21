18f4574b0027818dadebb6ee3fad9837b4ca5df6
gc <30398469+gc@users.noreply.github.com>
2022-12-21 18:36:04 +1100

    dont shuffle buttons

--- src/lib/util/handleTripFinish.ts
+++ src/lib/util/handleTripFinish.ts
@@ -1,6 +1,6 @@
 import { activity_type_enum } from '@prisma/client';
 import { AttachmentBuilder, ButtonBuilder, MessageCollector } from 'discord.js';
-import { percentChance, randInt, roll, shuffleArr, Time } from 'e';
+import { percentChance, randInt, roll, Time } from 'e';
 import { Bank } from 'oldschooljs';
 
 import { calculateBirdhouseDetails } from '../../mahoji/lib/abstracted_commands/birdhousesCommand';
@@ -173,6 +173,6 @@ export async function handleTripFinish(
 	sendToChannelID(channelID, {
 		content: message,
 		image: attachment,
-		components: components.length > 0 ? makeComponents(shuffleArr(components)) : undefined
+		components: components.length > 0 ? makeComponents(components) : undefined
 	});
 }
