8a6fcbd599864b5947cf19c1b0159337f23c0005
Mart <m4rtvs@gmail.com>
2023-01-21 18:20:30 +0100

    Fixed visual boost percentage for tob and cox

--- src/mahoji/lib/abstracted_commands/tobCommand.ts
+++ src/mahoji/lib/abstracted_commands/tobCommand.ts
@@ -311,7 +311,7 @@ export async function tobStartCommand(user: MUser, channelID: string, isHardMode
 
 	const {
 		duration,
-		totalReduction,
+		maxUserReduction,
 		reductions,
 		wipedRoom: _wipedRoom,
 		deathDuration,
@@ -359,7 +359,7 @@ export async function tobStartCommand(user: MUser, channelID: string, isHardMode
 			}
 			debugStr += `**- ${u.usernameOrMention}** (${Emoji.Gear}${total.toFixed(1)}% ${
 				Emoji.CombatSword
-			} ${calcWhatPercent(reductions[u.id], totalReduction).toFixed(1)}%) used ${realCost}\n\n`;
+			} ${calcWhatPercent(reductions[u.id], maxUserReduction).toFixed(1)}%) used ${realCost}\n\n`;
 			return {
 				userID: u.id,
 				effectiveCost
