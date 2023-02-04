e6759eeef2132b8dffdfdeca166062bb241761f4
gc <30398469+gc@users.noreply.github.com>
2023-02-04 18:22:24 +1100

    commit

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		name: 'Old School Bot',
+		include: ['tests/*.test.ts'],
+		coverage: {
+			provider: 'c8',
+			reporter: 'text-summary'
+		},
+		setupFiles: 'tests/setup.ts'
+	}
+});
