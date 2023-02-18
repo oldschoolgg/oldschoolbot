d6791efe43db28cc267e1afedfb020253e2a28ce
gc <30398469+gc@users.noreply.github.com>
2023-02-18 23:54:24 +1100

    undo changes to global handler

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ import { assert, getInteractionTypeName, runTimedLoggedFn } from './lib/util';
 import { CACHED_ACTIVE_USER_IDS, syncActiveUserIDs } from './lib/util/cachedUserIDs';
 import { interactionHook } from './lib/util/globalInteractions';
 import { handleInteractionError } from './lib/util/interactionReply';
-import { logError } from './lib/util/logError';
 import { sendToChannelID } from './lib/util/webhook';
 import { onStartup } from './mahoji/lib/events';
 import { postCommand } from './mahoji/lib/postCommand';
@@ -234,9 +233,6 @@ async function main() {
 	await runTimedLoggedFn('Log In', () => client.login(botToken));
 }
 
-process.on('uncaughtException', err => {
-	console.error(err);
-	logError(err);
-});
+process.on('uncaughtException', console.error);
 
 main();
