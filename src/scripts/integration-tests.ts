c66207db3ab7f6b12917e2547dc84bb42e77f603
gc <30398469+gc@users.noreply.github.com>
2023-03-17 16:03:42 +1100

    changes

--- src/scripts/integration-tests.ts
+++ src/scripts/integration-tests.ts
@@ -6,7 +6,9 @@ async function main() {
 
 		execSync('dotenv -e .env.example -- prisma db push', { stdio: 'inherit' });
 
-		execSync('yarn test:integration:run', { stdio: 'inherit' });
+		execSync('vitest run --config vitest.integration.config.ts', { stdio: 'inherit' });
+	} catch (err) {
+		throw new Error(`Failed to run integration tests: ${err}`);
 	} finally {
 		execSync('docker-compose down', { stdio: 'inherit' });
 	}
