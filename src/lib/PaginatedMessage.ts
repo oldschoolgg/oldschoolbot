eb721dc65fc0a8831d35538d1ab988acd11c2a2a
gc <30398469+gc@users.noreply.github.com>
2023-04-20 14:11:13 +1000

    dont show buttons on paginatedmessages if only 1 page

--- src/lib/PaginatedMessage.ts
+++ src/lib/PaginatedMessage.ts
@@ -58,7 +58,7 @@ type PaginatedPages =
 
 export class PaginatedMessage {
 	public index = 0;
-	public pages: PaginatedPages;
+	public pages!: PaginatedPages;
 	public channel: TextChannel;
 	public totalPages: number;
 
@@ -78,22 +78,26 @@ export class PaginatedMessage {
 	}
 
 	async render(): Promise<MessageEditOptions | string> {
+		const numberOfPages = Array.isArray(this.pages) ? this.pages.length : this.pages.numPages;
 		try {
 			const rawPage = !Array.isArray(this.pages)
 				? await this.pages.generate({ currentPage: this.index })
 				: this.pages[this.index];
 			return {
 				...rawPage,
-				components: [
-					new ActionRowBuilder<ButtonBuilder>().addComponents(
-						controlButtons.map(i =>
-							new ButtonBuilder()
-								.setStyle(ButtonStyle.Secondary)
-								.setCustomId(i.customId)
-								.setEmoji(i.emoji)
-						)
-					)
-				]
+				components:
+					numberOfPages === 1
+						? []
+						: [
+								new ActionRowBuilder<ButtonBuilder>().addComponents(
+									controlButtons.map(i =>
+										new ButtonBuilder()
+											.setStyle(ButtonStyle.Secondary)
+											.setCustomId(i.customId)
+											.setEmoji(i.emoji)
+									)
+								)
+						  ]
 			};
 		} catch (err) {
 			if (typeof err === 'string') return err;
