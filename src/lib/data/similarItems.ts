500ea75c773a68050d6d7b78103179e4278d7e74
gc <30398469+gc@users.noreply.github.com>
2023-05-31 21:12:19 +1000

    add 5% master clue boost for music cape

--- src/lib/data/similarItems.ts
+++ src/lib/data/similarItems.ts
@@ -329,7 +329,8 @@ const source: [string, (string | number)[]][] = [
 	["Karil's leatherskirt", ['Armadyl chainskirt', 'Masori chaps (f)', 'Masori chaps']],
 	['Armadyl helmet', ['Masori mask (f)', 'Masori mask']],
 	['Armadyl chestplate', ['Armadyl chestplate', 'Masori body (f)', 'Masori body']],
-	['Armadyl chainskirt', ['Masori chaps (f)', 'Masori chaps']]
+	['Armadyl chainskirt', ['Masori chaps (f)', 'Masori chaps']],
+	['Music cape', ['Music cape (t)']]
 ];
 
 export const similarItems: Map<number, number[]> = new Map(
