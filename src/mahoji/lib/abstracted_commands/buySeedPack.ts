19f892d4172397576019a76dc9f3cdad9682c1f0
LuxrayElite <12814878+LuxrayElite@users.noreply.github.com>
2023-06-22 15:00:15 +0100

    Linting

--- src/mahoji/lib/abstracted_commands/buySeedPack.ts
+++ src/mahoji/lib/abstracted_commands/buySeedPack.ts
@@ -8,41 +8,41 @@ import { handleMahojiConfirmation } from '../../../lib/util/handleMahojiConfirma
 import { makeBankImage } from '../../../lib/util/makeBankImage';
 
 export async function buySeedPack(user: MUser, interaction: ChatInputCommandInteraction, quantity: number) {
-    const cost = new Bank().add('Spirit seed', 1).multiply(quantity);
-    if (user.minionIsBusy) {
-        return 'Your minion is busy.';
-    }
+	const cost = new Bank().add('Spirit seed', 1).multiply(quantity);
+	if (user.minionIsBusy) {
+		return 'Your minion is busy.';
+	}
 
-    if (!gotFavour(user, Favours.Hosidius, 60)[0]) {
-        return 'Trading in spirit seeds for seed packs requires 60% Hosidius Favour.';
-    }
+	if (!gotFavour(user, Favours.Hosidius, 60)[0]) {
+		return 'Trading in spirit seeds for seed packs requires 60% Hosidius Favour.';
+	}
 
-    if (user.skillLevel(SkillsEnum.Farming) < 45) return 'You require atleast 45 farming to trade in spirit seeds';
+	if (user.skillLevel(SkillsEnum.Farming) < 45) return 'You require atleast 45 farming to trade in spirit seeds';
 
-    if (!user.owns(cost)) {
-        return "You don't have enough spirit seeds for that.";
-    }
+	if (!user.owns(cost)) {
+		return "You don't have enough spirit seeds for that.";
+	}
 
-    await handleMahojiConfirmation(
-        interaction,
-        `${user}, please confirm that you want to buy ${quantity}x Seed pack${quantity > 1 ? 's' : ''} for: ${cost}${quantity > 1 ? 's' : ''
-        }.`
-    );
+	await handleMahojiConfirmation(
+		interaction,
+		`${user}, please confirm that you want to buy ${quantity}x Seed pack${quantity > 1 ? 's' : ''} for: ${cost}${quantity > 1 ? 's' : ''
+		}.`
+	);
 
-    let loot = new Bank();
-    for (let i = 0; i < quantity; i++) {
-        loot.add(openSeedPack(5));
-    }
+	let loot = new Bank();
+	for (let i = 0; i < quantity; i++) {
+		loot.add(openSeedPack(5));
+	}
 
-    await transactItems({ userID: user.id, itemsToRemove: cost, itemsToAdd: loot, collectionLog: true });
+	await transactItems({ userID: user.id, itemsToRemove: cost, itemsToAdd: loot, collectionLog: true });
 
-    if (quantity < 5) {
-        return `You recieved ${loot}.`;
-    }
+	if (quantity < 5) {
+		return `You recieved ${loot}.`;
+	}
 
-    const image = await makeBankImage({
-        title: `Loot from ${quantity} Seed pack${quantity > 1 ? 's' : ''}`,
-        bank: loot
-    });
-    return { files: [image.file] };
+	const image = await makeBankImage({
+		title: `Loot from ${quantity} Seed pack${quantity > 1 ? 's' : ''}`,
+		bank: loot
+	});
+	return { files: [image.file] };
 }
