49d1169c92e5a17311109e64f9f3e40dca4f2603
themrrobert <10122432+themrrobert@users.noreply.github.com>
2023-06-28 09:06:32 -0700

    Remove doubled code (#5183)

--- src/tasks/minions/woodcuttingActivity.ts
+++ src/tasks/minions/woodcuttingActivity.ts
@@ -109,10 +109,6 @@ export const woodcuttingTask: MinionTask = {
 			str += "\nYour strung rabbit foot necklace increases the chance of receiving bird's eggs and rings.";
 		}
 
-		if (strungRabbitFoot && !log.clueNestsOnly) {
-			str += "\nYour strung rabbit foot necklace increases the chance of receiving bird's eggs and rings.";
-		}
-
 		// Roll for pet
 		if (log.petChance) {
 			const { petDropRate } = skillingPetDropRate(user, SkillsEnum.Woodcutting, log.petChance);
