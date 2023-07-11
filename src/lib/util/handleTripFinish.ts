1e855042be75f6dba738e2aa4537b376dbb33f6c
gc <30398469+gc@users.noreply.github.com>
2023-07-11 13:42:47 +1000

    Make bugs more common

--- src/lib/util/handleTripFinish.ts
+++ src/lib/util/handleTripFinish.ts
@@ -312,7 +312,7 @@ const tripFinishEffects: TripFinishEffect[] = [
 			const minutes = Math.floor(data.duration / Time.Minute);
 			for (let i = 0; i < minutes; i++) {
 				// Find a bug every 2 hours
-				if (roll(80)) {
+				if (roll(60)) {
 					await transactItems({
 						userID: user.id,
 						collectionLog: false,
