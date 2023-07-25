04069a9b3c5a35c9799a9461ea0f77b17c35989c
themrrobert <10122432+themrrobert@users.noreply.github.com>
2023-07-25 07:52:46 -0700

    Dye rework

--- src/lib/paintColors.ts
+++ src/lib/paintColors.ts
@@ -18,12 +18,15 @@ export const paintColors: PaintColor[] = [
 	{ itemId: 72_452, name: 'Gilded Gold', rgb: [255, 221, 0], paintCanItem: null as any as Item },
 	{ itemId: 72_453, name: 'Vorkath Blue', rgb: [0, 251, 255], paintCanItem: null as any as Item },
 	{ itemId: 72_454, name: 'Sapphire Blue', rgb: [0, 26, 255], paintCanItem: null as any as Item },
-	{ itemId: 72_455, name: 'Pretty Pink', rgb: [252, 89, 255], paintCanItem: null as any as Item },
+	{ itemId: 72_455, name: 'Pretty Pink', rgb: [252, 150, 255], paintCanItem: null as any as Item },
 	{ itemId: 72_456, name: 'Zamorak Red', rgb: [128, 5, 28], paintCanItem: null as any as Item },
 	{ itemId: 72_457, name: 'BSO Blurple', rgb: [85, 0, 255], paintCanItem: null as any as Item },
 	{ itemId: 72_458, name: 'Abyssal Purple', rgb: [209, 58, 255], paintCanItem: null as any as Item },
 	{ itemId: 72_459, name: 'Amethyst Purple', rgb: [165, 120, 255], paintCanItem: null as any as Item },
-	{ itemId: 72_460, name: 'Ruby Red', rgb: [255, 0, 0], paintCanItem: null as any as Item }
+	{ itemId: 72_460, name: 'Ruby Red', rgb: [255, 0, 0], paintCanItem: null as any as Item },
+	{ itemId: 72_461, name: 'White Lily White', rgb: [255, 255, 255], paintCanItem: null as any as Item },
+	{ itemId: 72_462, name: 'Balfrug Kreeyath Black', rgb: [0, 0, 0], paintCanItem: null as any as Item },
+	{ itemId: 72_463, name: 'Inversion', rgb: [1, 2, 3], paintCanItem: null as any as Item }
 ];
 
 for (const paintColor of paintColors) {
@@ -64,10 +67,24 @@ export const applyPaintToItemIcon = async (img: Image, tintColor: [number, numbe
 			continue;
 		}
 
-		// Apply tint only to non-black (with tolerance) pixels
-		imageData.data[i + 0] = originalRed + (r - originalRed) / 2.5;
-		imageData.data[i + 1] = originalGreen + (g - originalGreen) / 2.5;
-		imageData.data[i + 2] = originalBlue + (b - originalBlue) / 2.5;
+		// Special case for inversion paint:
+		if ([1, 2, 3].every(n => tintColor.includes(n))) {
+			imageData.data[i + 0] = 255 - originalRed;
+			imageData.data[i + 1] = 255 - originalGreen;
+			imageData.data[i + 2] = 255 - originalBlue;
+		} else {
+			// Strength increases amount of dye color
+			const strength = 1.7;
+			// Darkness helps preserve darkness / detail. Higher numbers preserve more darkness (very subtle)
+			const darkness = 1.5;
+			const rF = 1 + strength * (originalRed / 255) ** darkness;
+			const gF = 1 + strength * (originalGreen / 255) ** darkness;
+			const bF = 1 + strength * (originalBlue / 255) ** darkness;
+			// Apply tint only to non-black (with tolerance) pixels
+			imageData.data[i + 0] = Math.floor((originalRed + rF * r) / (1 + rF));
+			imageData.data[i + 1] = Math.floor((originalGreen + gF * g) / (1 + gF));
+			imageData.data[i + 2] = Math.floor((originalBlue + bF * b) / (1 + bF));
+		}
 	}
 
 	ctx.putImageData(imageData, 0, 0);
@@ -90,4 +107,7 @@ export const PaintBoxTable = new LootTable()
 	.add('Zamorak Red paint can')
 	.add('Abyssal Purple paint can')
 	.add('Amethyst Purple paint can')
-	.add('Ruby Red paint can');
+	.add('Ruby Red paint can')
+	.add('White Lily White paint can')
+	.add('Balfrug Kreeyath Black paint can')
+	.add('Inversion paint can');
