33999d174c7a13df2222e7de0848a3b37ea54558
gc <30398469+gc@users.noreply.github.com>
2023-09-16 13:14:58 +1000

    Make smithed dwarven not add to cl in a bingo

--- src/tasks/minions/smithingActivity.ts
+++ src/tasks/minions/smithingActivity.ts
@@ -1,10 +1,12 @@
 import { Bank } from 'oldschooljs';
 
 import { BlacksmithOutfit } from '../../lib/bsoOpenables';
+import { dwarvenOutfit } from '../../lib/data/CollectionsExport';
 import Smithing from '../../lib/skilling/skills/smithing/';
 import { SkillsEnum } from '../../lib/skilling/types';
 import type { SmithingActivityTaskOptions } from '../../lib/types/minions';
 import { handleTripFinish } from '../../lib/util/handleTripFinish';
+import { findBingosWithUserParticipating } from '../../mahoji/lib/bingo/BingoManager';
 
 export const smithingTask: MinionTask = {
 	type: 'Smithing',
@@ -34,9 +36,15 @@ export const smithingTask: MinionTask = {
 			str += '\n**10%** Bonus XP For Blacksmith Outfit';
 		}
 
+		let collectionLog = true;
+		const bingos = await findBingosWithUserParticipating(user.id);
+		if (bingos.some(bingo => bingo.isActive()) && dwarvenOutfit.includes(smithedItem.id)) {
+			collectionLog = false;
+		}
+
 		await transactItems({
 			userID: user.id,
-			collectionLog: true,
+			collectionLog,
 			itemsToAdd: loot
 		});
 
