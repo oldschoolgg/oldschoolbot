88ed9c13a8023a7a7b88f415e7d76330a6583c4b
themrrobert <10122432+themrrobert@users.noreply.github.com>
2023-09-21 14:06:56 -0700

    Remove dead code

--- src/lib/util/tradePlayerItems.ts
+++ src/lib/util/tradePlayerItems.ts
@@ -5,23 +5,7 @@ import { prisma } from '../settings/prisma';
 import { logError } from './logError';
 import { userQueueFn } from './userQueues';
 
-// export const activeTradeCache = new Map();
-
-function timeout(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// export function tradePlayerItemsImpl(sender: MUser, recipient: MUser, lootSent: Bank, lootReceived: Bank) {}
 export async function tradePlayerItems(sender: MUser, recipient: MUser, _itemsToSend?: Bank, _itemsToReceive?: Bank) {
-	/*
-	if (activeTradeCache.get(sender.id) || activeTradeCache.get(recipient.id)) {
-		return { success: false, message: 'Only one trade per player can be active at a time!' };
-	}
-	activeTradeCache.set(sender.id, true);
-	activeTradeCache.set(recipient.id, true);
-
-	 */
-	// Check if user is busy. This check can probably be removed.
 	if (recipient.isBusy) {
 		return { success: false, message: `${recipient.usernameOrMention} is busy.` };
 	}
@@ -34,7 +18,6 @@ export async function tradePlayerItems(sender: MUser, recipient: MUser, _itemsTo
 	// Queue the primary trade function: (Clears cache on completion/failure)
 	const processTradePromise = userQueueFn(sender.id, async () => {
 		try {
-			await timeout(5000);
 			await Promise.all([sender.sync(), recipient.sync()]);
 			if (!sender.owns(itemsToSend)) {
 				return { success: false, message: `${sender.usernameOrMention} doesn't own all items.` };
@@ -100,10 +83,6 @@ export async function tradePlayerItems(sender: MUser, recipient: MUser, _itemsTo
 		} finally {
 			modifyBusyCounter(sender.id, -1);
 			modifyBusyCounter(recipient.id, -1);
-			/*
-			activeTradeCache.delete(sender.id);
-			activeTradeCache.delete(recipient.id);
-			 */
 		}
 	});
 
