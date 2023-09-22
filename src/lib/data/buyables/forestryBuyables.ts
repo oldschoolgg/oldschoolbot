5a866f5daf153b002154e95331a50f0c58d64003
Jonesy <69014816+nwjgit@users.noreply.github.com>
2023-09-21 20:59:01 -0500

    Update forestryBuyables.ts (#5363)
    * Update forestryBuyables.ts
    
    Fixes the costs of various items in the forestry shop
    
    * banksnap test
    
    ---------
    
    Co-authored-by: ghosts-are-spooky <tommeheu@gmail.com>

--- src/lib/data/buyables/forestryBuyables.ts
+++ src/lib/data/buyables/forestryBuyables.ts
@@ -15,84 +15,49 @@ export const forestryBuyables: Buyable[] = [
 	},
 	{
 		name: 'Secateurs blade',
-		itemCost: new Bank().add('Anima-infused bark', 20).add('Oak logs', 10).add('Willow logs', 5).add('Iron bar'),
-		skillsNeeded: {
-			woodcutting: 35,
-			smithing: 35
-		}
+		itemCost: new Bank().add('Anima-infused bark', 20).add('Oak logs', 10).add('Willow logs', 5)
 	},
 	{
 		name: 'Ritual mulch',
-		itemCost: new Bank().add('Anima-infused bark', 150).add('Maple logs', 10).add('Dwarf weed'),
-		skillsNeeded: {
-			woodcutting: 68,
-			farming: 50
-		}
+		itemCost: new Bank().add('Anima-infused bark', 150).add('Maple logs', 10)
 	},
 	{
 		name: 'Clover insignia',
-		itemCost: new Bank().add('Anima-infused bark', 200).add('Oak logs', 15).add('Emerald').add('Ball of wool'),
-		skillsNeeded: {
-			woodcutting: 70,
-			farming: 35,
-			crafting: 70
-		}
+		itemCost: new Bank().add('Anima-infused bark', 200).add('Oak logs', 15)
 	},
 	{
 		name: 'Powdered pollen',
-		itemCost: new Bank().add('Anima-infused bark', 300).add('Willow logs', 15).add('Logs').add('Ball of wool'),
-		skillsNeeded: {
-			woodcutting: 35,
-			hunter: 50
-		}
+		itemCost: new Bank().add('Anima-infused bark', 300).add('Willow logs', 15)
 	},
 	{
 		name: 'Log brace',
-		itemCost: new Bank()
-			.add('Anima-infused bark', 3000)
-			.add('Maple logs', 300)
-			.add('Yew logs', 300)
-			.add('Steel nails', 45)
-			.add('Rope', 2)
-			.add('Adamantite bar', 3),
-		skillsNeeded: {
-			smithing: 75,
-			woodcutting: 75
-		}
+		itemCost: new Bank().add('Anima-infused bark', 3000).add('Maple logs', 300).add('Yew logs', 300)
 	},
 	{
 		name: 'Clothes pouch blueprint',
-		itemCost: new Bank()
-			.add('Anima-infused bark', 10_000)
-			.add('Willow logs', 300)
-			.add('Maple logs', 300)
-			.add('Thread')
-			.add('Leather'),
-		skillsNeeded: {
-			crafting: 50,
-			woodcutting: 50
-		}
+		itemCost: new Bank().add('Anima-infused bark', 10_000).add('Willow logs', 300).add('Maple logs', 300)
 	},
 	{
 		name: 'Log basket',
-		itemCost: new Bank().add('Anima-infused bark', 5000).add('Willow logs', 300).add('Maple logs', 300)
+		itemCost: new Bank().add('Anima-infused bark', 5000).add('Oak logs', 300).add('Willow logs', 300)
 	},
 	{
 		name: 'Funky shaped log',
 		itemCost: new Bank()
 			.add('Anima-infused bark', 15_000)
 			.add('Oak logs', 500)
-			.add('Maple logs', 500)
 			.add('Willow logs', 500)
+			.add('Teak logs', 500)
+			.add('Maple logs', 500)
 			.add('Mahogany logs', 500)
 			.add('Arctic pine logs', 500)
 			.add('Yew logs', 500)
-			.add('Redwood logs', 500)
 			.add('Magic logs', 500)
+			.add('Redwood logs', 500)
 	},
 	{
 		name: 'Lumberjack boots',
-		itemCost: new Bank().add('Anima-infused bark', 1000).add('Yew logs', 200).add('Maple logs', 100)
+		itemCost: new Bank().add('Anima-infused bark', 1000).add('Maple logs', 100).add('Yew logs', 200)
 	},
 	{
 		name: 'Lumberjack hat',
@@ -100,7 +65,7 @@ export const forestryBuyables: Buyable[] = [
 	},
 	{
 		name: 'Lumberjack legs',
-		itemCost: new Bank().add('Anima-infused bark', 1300).add('Yew logs', 160).add('Maple logs', 140)
+		itemCost: new Bank().add('Anima-infused bark', 1300).add('Yew logs', 160).add('Magic logs', 140)
 	},
 	{
 		name: 'Lumberjack top',
@@ -116,8 +81,9 @@ export const forestryBuyables: Buyable[] = [
 			.add('Anima-infused bark', 1250)
 			.add('Lumberjack boots')
 			.add('Oak logs', 60)
-			.add('Maple logs', 60)
 			.add('Willow logs', 60)
+			.add('Teak logs', 60)
+			.add('Maple logs', 60)
 			.add('Mahogany logs', 60)
 			.add('Arctic pine logs', 60)
 			.add('Yew logs', 60)
@@ -130,8 +96,9 @@ export const forestryBuyables: Buyable[] = [
 			.add('Anima-infused bark', 1250)
 			.add('Lumberjack hat')
 			.add('Oak logs', 60)
-			.add('Maple logs', 60)
 			.add('Willow logs', 60)
+			.add('Teak logs', 60)
+			.add('Maple logs', 60)
 			.add('Mahogany logs', 60)
 			.add('Arctic pine logs', 60)
 			.add('Yew logs', 60)
@@ -144,8 +111,9 @@ export const forestryBuyables: Buyable[] = [
 			.add('Anima-infused bark', 1250)
 			.add('Lumberjack legs')
 			.add('Oak logs', 60)
-			.add('Maple logs', 60)
 			.add('Willow logs', 60)
+			.add('Teak logs', 60)
+			.add('Maple logs', 60)
 			.add('Mahogany logs', 60)
 			.add('Arctic pine logs', 60)
 			.add('Yew logs', 60)
@@ -158,8 +126,9 @@ export const forestryBuyables: Buyable[] = [
 			.add('Anima-infused bark', 1250)
 			.add('Lumberjack top')
 			.add('Oak logs', 60)
-			.add('Maple logs', 60)
 			.add('Willow logs', 60)
+			.add('Teak logs', 60)
+			.add('Maple logs', 60)
 			.add('Mahogany logs', 60)
 			.add('Arctic pine logs', 60)
 			.add('Yew logs', 60)
