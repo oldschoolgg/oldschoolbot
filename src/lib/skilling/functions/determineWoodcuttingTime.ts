8c8108987b6d8a12d3bf44f3b15a0dd4a7e2837a
nwjgit <nwjgit@gmail.com>
2023-10-04 12:51:07 -0500

    minor spelling correction

--- src/lib/skilling/functions/determineWoodcuttingTime.ts
+++ src/lib/skilling/functions/determineWoodcuttingTime.ts
@@ -35,7 +35,7 @@ export function determineWoodcuttingTime({
 	let newQuantity = 0;
 
 	let maxTripLength = calcMaxTripLength(user, 'Woodcutting');
-	if (!powerchopping && user.hasEquippedOrInBank('Log Basket')) {
+	if (!powerchopping && user.hasEquippedOrInBank('Log basket')) {
 		maxTripLength += Time.Minute * 5;
 	}
 	let userMaxTripTicks = maxTripLength / (Time.Second * 0.6);
