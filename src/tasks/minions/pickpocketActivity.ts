b2ac6c803151d31b082a37346eefeeabe5248caa
TastyPumPum <james.selfe@hotmail.co.uk>
2023-10-13 23:01:56 +0100

    Changes

--- src/tasks/minions/pickpocketActivity.ts
+++ src/tasks/minions/pickpocketActivity.ts
@@ -60,7 +60,7 @@ export const pickpocketTask: MinionTask = {
 		if (obj.type === 'pickpockable') {
 			for (let i = 0; i < successfulQuantity; i++) {
 				const lootItems = obj.table.roll(1, {
-					tertiaryItemPercentageChanges: user.buildCATertiaryItemChanges()
+					tertiaryItemPercentageChanges: user.buildTertiaryItemChanges( false )
 				});
 				// TODO: Remove Rocky from loot tables in oldschoolJS
 				if (lootItems.has('Rocky')) lootItems.remove('Rocky');
