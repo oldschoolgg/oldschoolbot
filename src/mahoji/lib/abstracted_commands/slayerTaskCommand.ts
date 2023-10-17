f5bce679452b81ccc138c45cb78f13077e525d2b
nwjgit <nwjgit@gmail.com>
2023-10-17 08:15:02 -0500

    slayer fix

--- src/mahoji/lib/abstracted_commands/slayerTaskCommand.ts
+++ src/mahoji/lib/abstracted_commands/slayerTaskCommand.ts
@@ -202,6 +202,7 @@ async function returnSuccess(channelID: string, user: MUser, content: string) {
 					interaction: selection,
 					...options
 				});
+				return;
 			}
 		}
 	} catch (err: unknown) {
@@ -469,6 +470,7 @@ export async function slayerSkipTaskCommand({
 			});
 		}
 		interactionReply(interaction, resultMessage);
+		return;
 	} catch (e) {
 		logError(e, {
 			user_id: user.id.toString(),
