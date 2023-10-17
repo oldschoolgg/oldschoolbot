1b6767622302d16f5c4a22a791fc84e92183b732
nwjgit <nwjgit@gmail.com>
2023-10-17 16:15:47 -0500

    few grammar fixes

--- src/mahoji/lib/abstracted_commands/camdozaalCommand.ts
+++ src/mahoji/lib/abstracted_commands/camdozaalCommand.ts
@@ -15,7 +15,7 @@ import { minionName } from '../../../lib/util/minionUtils';
 async function miningCommand(user: MUser, channelID: string, quantity: number | undefined) {
 	let miningLevel = user.skillLevel(SkillsEnum.Mining);
 	if (miningLevel < 14) {
-		return 'You need at least level 14 Mining to fish in the Ruins of Camdozaal.';
+		return 'You need at least level 14 Mining to mine in the Ruins of Camdozaal.';
 	}
 
 	const boosts = [];
@@ -86,7 +86,7 @@ async function miningCommand(user: MUser, channelID: string, quantity: number | 
 
 async function smithingCommand(user: MUser, channelID: string, quantity: number | undefined) {
 	if (user.skillLevel(SkillsEnum.Smithing) < 14) {
-		return 'You need at least level 14 Smithing to smith in the Ruins of Camdozaal';
+		return 'You need at least level 14 Smithing to smith in the Ruins of Camdozaal.';
 	}
 
 	const maxTripLength = calcMaxTripLength(user, 'CamdozaalSmithing');
@@ -130,7 +130,7 @@ async function smithingCommand(user: MUser, channelID: string, quantity: number 
 
 async function fishingCommand(user: MUser, channelID: string, quantity: number | undefined) {
 	if (user.skillLevel(SkillsEnum.Fishing) < 7) {
-		return 'You need at least level 7 Fishing to fish in the Ruins of Camdozaal';
+		return 'You need at least level 7 Fishing to fish in the Ruins of Camdozaal.';
 	}
 
 	const maxTripLength = calcMaxTripLength(user, 'CamdozaalFishing');
@@ -165,7 +165,7 @@ async function fishingCommand(user: MUser, channelID: string, quantity: number |
 export async function camdozaalCommand(user: MUser, channelID: string, choice: string, quantity: number | undefined) {
 	const qp = user.QP;
 	if (qp <= 16) {
-		return "You haven't completed enough quests to enter the Ruins of Camdozaal, Return when you have at least 17 quest points.";
+		return "You haven't completed enough quests to enter the Ruins of Camdozaal, return when you have at least 17 quest points.";
 	}
 	if (choice === 'mining') {
 		return miningCommand(user, channelID, quantity);
