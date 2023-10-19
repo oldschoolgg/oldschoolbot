732b832fa1149644ce6d92119e713f065cd623c1
gc <30398469+gc@users.noreply.github.com>
2023-10-19 18:33:50 +1100

    commit

---
+++ src/mahoji/commands/redeem.ts
@@ -0,0 +1,70 @@
+import { bold } from '@discordjs/builders';
+import { products } from '@oldschoolgg/toolkit';
+import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
+
+import { roboChimpSyncData } from '../../lib/roboChimp';
+import { OSBMahojiCommand } from '../lib/util';
+
+export const redeemCommand: OSBMahojiCommand = {
+	name: 'redeem',
+	description: 'Redeem a code you received.',
+	attributes: {
+		cooldown: 10
+	},
+	options: [
+		{
+			type: ApplicationCommandOptionType.String,
+			name: 'code',
+			description: 'The code to redeem.',
+			required: true
+		}
+	],
+	run: async ({ options, userID }: CommandRunOptions<{ code: string }>) => {
+		const user = await mUserFetch(userID);
+		const code = await roboChimpClient.storeCode.findFirst({
+			where: {
+				code: options.code
+			}
+		});
+		if (!code) {
+			return 'That code is invalid.';
+		}
+		if (code.redeemed_at) {
+			return 'That code has already been redeemed.';
+		}
+
+		const product = products.find(p => p.id === code.product_id);
+		if (!product) {
+			throw new Error('Invalid product ID.');
+		}
+		await roboChimpSyncData(user);
+		if (product.type === 'bit' && user.user.store_bitfield.includes(product.id)) {
+			return 'You already have this, redeeming it again would be a waste!';
+		}
+
+		await roboChimpClient.$transaction([
+			roboChimpClient.storeCode.update({
+				where: {
+					code: options.code
+				},
+				data: {
+					redeemed_at: new Date(),
+					redeemed_by_user_id: user.id
+				}
+			}),
+			roboChimpClient.user.update({
+				where: {
+					id: BigInt(userID)
+				},
+				data: {
+					store_bitfield: {
+						push: product.bit
+					}
+				}
+			})
+		]);
+		await roboChimpSyncData(user);
+
+		return `You have redeemed: ${bold(product.name)}!`;
+	}
+};
