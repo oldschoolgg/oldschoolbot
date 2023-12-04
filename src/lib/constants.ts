df29a40063b4796580fc0842cf59b7995b613598
gc <30398469+gc@users.noreply.github.com>
2023-12-05 04:07:12 +1100

    more secret items

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -694,7 +694,17 @@ export const chompyHats = [
 	[getOSItem('Chompy bird hat (expert dragon archer)'), 4000]
 ] as const;
 
-export const secretItems: number[] = resolveItems(['Burnt butter', 'Grinch santa hat']);
+export const secretItems: number[] = resolveItems([
+	'Burnt butter',
+	'Grinch santa hat',
+	'Hairy banana-butter',
+	'Milk with spoon',
+	'Fresh rat milk',
+	'Metallic chocolate dust',
+	'Scorched rat milk',
+	'Dodgy bread',
+	'Ginger root'
+]);
 
 export const toaPurpleItems = resolveItems([
 	"Tumeken's guardian",
