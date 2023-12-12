b9569290920c8caa16be5ecfd87c76890887ff76
TURBO <turbolaceup@gmail.com>
2023-12-11 22:09:24 -0600

    Add uncharged subtext for sang staff/scythe vitur (#5499)

--- src/lib/bankImage.ts
+++ src/lib/bankImage.ts
@@ -225,7 +225,14 @@ const forcedShortNameMap = new Map<number, string>([
 	[i('Yew logs'), 'Yew'],
 	[i('Magic logs'), 'Magic'],
 	[i('Redwood logs'), 'Redwood'],
-	...XPLamps.map(lamp => [lamp.itemID, toKMB(lamp.amount)] as const)
+	...XPLamps.map(lamp => [lamp.itemID, toKMB(lamp.amount)] as const),
+
+	// Uncharged
+	[i('Holy sanguinesti staff (uncharged)'), 'Unch.'],
+	[i('Sanguinesti staff (uncharged)'), 'Unch.'],
+	[i('Scythe of vitur (uncharged)'), 'Unch.'],
+	[i('Holy scythe of vitur (uncharged)'), 'Unch.'],
+	[i('Sanguine scythe of vitur (uncharged)'), 'Unch.']
 ]);
 
 function drawTitle(ctx: SKRSContext2D, title: string, canvas: Canvas) {
