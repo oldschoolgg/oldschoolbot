cece93f765a58bde6da8381b9cacdcdb9504af61
themrrobert <10122432+themrrobert@users.noreply.github.com>
2023-12-19 06:04:58 -0800

    Fix /use bug

--- src/mahoji/lib/abstracted_commands/useCommand.ts
+++ src/mahoji/lib/abstracted_commands/useCommand.ts
@@ -623,7 +623,7 @@ export async function useCommand(user: MUser, _firstItem: string, _secondItem?: 
 	for (const i of items) checkBank.add(i.id);
 	if (!bank.has(checkBank)) return `You don't own ${checkBank}.`;
 
-	const usable = usables.find(i => i.items.length === items.length && items.every(t => i.items.includes(t)));
+	const usable = usables.find(i => i.items.length === items.length && i.items.every(t => items.includes(t)));
 	if (!usable) return "That's not a usable item.";
 	return usable.run(user);
 }
