ba64b91720b58299b7e34ea25b2a5f52f4450e70
nwjgit <nwjgit@gmail.com>
2024-01-10 18:44:47 -0600

    change /sacrifice

--- src/mahoji/commands/sacrifice.ts
+++ src/mahoji/commands/sacrifice.ts
@@ -2,7 +2,7 @@ import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 import { Bank } from 'oldschooljs';
 import { Item } from 'oldschooljs/dist/meta/types';
 
-import { Events } from '../../lib/constants';
+import { Emoji, Events } from '../../lib/constants';
 import { cats } from '../../lib/growablePets';
 import minionIcons from '../../lib/minions/data/minionIcons';
 import { ItemBank } from '../../lib/types';
@@ -77,12 +77,19 @@ export const sacrificeCommand: OSBMahojiCommand = {
 		options,
 		interaction
 	}: CommandRunOptions<{ items?: string; filter?: string; search?: string }>) => {
-		if (!options.filter && !options.items && !options.search) {
-			return "You didn't provide any items, filter or search.";
-		}
-
-		deferInteraction(interaction);
 		const user = await mUserFetch(userID);
+		const currentIcon = user.user.minion_icon;
+		const sacVal = Number(user.user.sacrificedValue);
+		const { sacrificed_bank: sacrificedBank } = await user.fetchStats({ sacrificed_bank: true });
+		const sacUniqVal = sacrificedBank !== null ? Object.keys(sacrificedBank).length : 0;
+
+		// Show user sacrifice stats if no options are given for /sacrifice
+		if (!options.filter && !options.items && !options.search) {
+			return `${Emoji.Incinerator} **Your Sacrifice Stats** ${
+				Emoji.Incinerator
+			}\n\n**Current Minion Icon:** ${currentIcon}\n**Sacrificed Value:** ${sacVal.toLocaleString()} GP\n**Unique Items Sacrificed:** ${sacUniqVal.toLocaleString()} items`;
+		}
+		deferInteraction(interaction);
 
 		const bankToSac = parseBank({
 			inputStr: options.items,
@@ -94,8 +101,6 @@ export const sacrificeCommand: OSBMahojiCommand = {
 			maxSize: 70,
 			noDuplicateItems: true
 		});
-
-		const sacVal = Number(user.user.sacrificedValue);
 
 		if (!user.owns(bankToSac)) {
 			return `You don't own ${bankToSac}.`;
@@ -162,7 +167,7 @@ export const sacrificeCommand: OSBMahojiCommand = {
 		await trackSacBank(user, bankToSac);
 
 		let str = '';
-		const currentIcon = user.user.minion_icon;
+
 		// Ignores notifying the user/server if the user is using a custom icon
 		if (!currentIcon || minionIcons.find(m => m.emoji === currentIcon)) {
 			for (const icon of minionIcons) {
