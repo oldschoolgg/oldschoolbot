871afe5ace2420f4845283d7f751c7f525c7d0a0
Jonesy <69014816+nwjgit@users.noreply.github.com>
2024-02-01 20:18:47 -0600

    Fixes and additions to /kill command (#5638)

--- src/lib/simulation/simulatedKillables.ts
+++ src/lib/simulation/simulatedKillables.ts
@@ -1,19 +1,24 @@
-import { randInt } from 'e';
-import { Bank } from 'oldschooljs';
+import { randArrItem, randInt } from 'e';
+import { Bank, Misc } from 'oldschooljs';
 
 import { DOANonUniqueTable } from '../../tasks/minions/bso/doaActivity';
+import { nexUniqueDrops } from '../data/CollectionsExport';
 import { chanceOfDOAUnique, pickUniqueToGiveUser } from '../depthsOfAtlantis';
+import { MoktangLootTable } from '../minions/data/killableMonsters/custom/bosses/Moktang';
+import { NEX_UNIQUE_DROPRATE, nexLootTable } from '../nex';
 import { roll } from '../util';
 import { WintertodtCrate } from './wintertodt';
 
 interface SimulatedKillable {
 	name: string;
+	isCustom: Boolean;
 	loot: (quantity: number) => Bank;
 }
 
 export const simulatedKillables: SimulatedKillable[] = [
 	{
 		name: 'Wintertodt',
+		isCustom: false,
 		loot: (quantity: number) => {
 			const loot = new Bank();
 			for (let i = 0; i < quantity; i++) {
@@ -40,7 +45,30 @@ export const simulatedKillables: SimulatedKillable[] = [
 		}
 	},
 	{
+		name: 'The Nightmare',
+		isCustom: false,
+		loot: (quantity: number) => {
+			let bank = new Bank();
+			for (let i = 0; i < quantity; i++) {
+				bank.add(Misc.Nightmare.kill({ team: [{ damageDone: 2400, id: 'id' }], isPhosani: false }).id);
+			}
+			return bank;
+		}
+	},
+	{
+		name: "Phosani's Nightmare",
+		isCustom: false,
+		loot: (quantity: number) => {
+			let bank = new Bank();
+			for (let i = 0; i < quantity; i++) {
+				bank.add(Misc.Nightmare.kill({ team: [{ damageDone: 2400, id: 'id' }], isPhosani: true }).id);
+			}
+			return bank;
+		}
+	},
+	{
 		name: 'Depths of Atlantis (DOA) - Solo',
+		isCustom: true,
 		loot: (quantity: number) => {
 			const chanceOfUnique = chanceOfDOAUnique(1, false);
 			const loot = new Bank();
@@ -52,6 +80,27 @@ export const simulatedKillables: SimulatedKillable[] = [
 				}
 			}
 			return loot;
+		}
+	},
+	{
+		name: 'Nex',
+		isCustom: true,
+		loot: (quantity: number) => {
+			let loot = new Bank();
+			for (let i = 0; i < quantity; i++) {
+				if (roll(NEX_UNIQUE_DROPRATE(1))) {
+					loot.add(randArrItem(nexUniqueDrops), 1);
+				}
+				loot.add(nexLootTable.roll());
+			}
+			return loot;
+		}
+	},
+	{
+		name: 'Moktang',
+		isCustom: true,
+		loot: (quantity: number) => {
+			return MoktangLootTable.roll(quantity);
 		}
 	}
 ];
