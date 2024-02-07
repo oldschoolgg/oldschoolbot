5b4027ce5f7929c55be0f9d421735a3cead80451
TURBO <turbolaceup@gmail.com>
2024-02-07 03:51:42 -0600

    removed lowercase search only for cl

--- src/mahoji/commands/cl.ts
+++ src/mahoji/commands/cl.ts
@@ -1,5 +1,10 @@
-import { toTitleCase } from '@oldschoolgg/toolkit';
-import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
+import {
+	toTitleCase
+} from '@oldschoolgg/toolkit';
+import {
+	ApplicationCommandOptionType,
+	CommandRunOptions
+} from 'mahoji';
 
 import {
 	clImageGenerator,
@@ -7,9 +12,15 @@ import {
 	CollectionLogType,
 	collectionLogTypes
 } from '../../lib/collectionLogTask';
-import { allCollectionLogs } from '../../lib/data/Collections';
-import { fetchStatsForCL } from '../../lib/util';
-import { OSBMahojiCommand } from '../lib/util';
+import {
+	allCollectionLogs
+} from '../../lib/data/Collections';
+import {
+	fetchStatsForCL
+} from '../../lib/util';
+import {
+	OSBMahojiCommand
+} from '../lib/util';
 
 export const collectionLogCommand: OSBMahojiCommand = {
 	name: 'cl',
@@ -18,80 +29,92 @@ export const collectionLogCommand: OSBMahojiCommand = {
 		requiresMinion: true,
 		examples: ['/cl name:Boss']
 	},
-	options: [
-		{
-			type: ApplicationCommandOptionType.String,
-			name: 'name',
-			description: 'The log you want to see.',
-			required: true,
-			autocomplete: async (value: string) => {
-				return [
-					{ name: 'Overall (Main Collection Log)', value: 'overall' },
-					{ name: 'Overall+', value: 'overall+' },
-					...Object.entries(allCollectionLogs)
-						.map(i => {
-							return [
-								{ name: `${i[0]} (Group)`, value: i[0] },
-								...Object.entries(i[1].activities).map(act => ({
-									name: `${act[0]} (${act[1].items.length} Items)`,
-									value: act[0]
-								}))
-							];
-						})
-						.flat(3)
-				];
-			}
-		},
-		{
-			type: ApplicationCommandOptionType.String,
-			name: 'type',
-			description: 'The type of log you want to see.',
-			required: false,
-			choices: collectionLogTypes.map(i => ({ name: `${toTitleCase(i.name)} (${i.description})`, value: i.name }))
-		},
-		{
-			type: ApplicationCommandOptionType.String,
-			name: 'flag',
-			description: 'The flag you want to pass.',
-			required: false,
-			choices: CollectionLogFlags.map(i => ({ name: `${toTitleCase(i.name)} (${i.description})`, value: i.name }))
-		},
-		{
-			type: ApplicationCommandOptionType.String,
-			name: 'flag_extra',
-			description: 'An additional flag you want to pass.',
-			required: false,
-			choices: CollectionLogFlags.map(i => ({ name: `${toTitleCase(i.name)} (${i.description})`, value: i.name }))
-		},
-		{
-			type: ApplicationCommandOptionType.Boolean,
-			name: 'all',
-			description: 'Show all items?',
-			required: false
-		}
-	],
-	run: async ({
-		options,
-		userID
-	}: CommandRunOptions<{
-		name: string;
-		type?: CollectionLogType;
-		flag?: string;
-		flag_extra?: string;
-		all?: boolean;
-	}>) => {
-		const user = await mUserFetch(userID);
-		let flags: Record<string, string> = {};
-		if (options.flag) flags[options.flag] = options.flag;
-		if (options.flag_extra) flags[options.flag_extra] = options.flag_extra;
-		if (options.all) flags.all = 'all';
-		const result = await clImageGenerator.generateLogImage({
-			user,
-			type: options.type ?? 'collection',
-			flags,
-			collection: options.name,
-			stats: await fetchStatsForCL(user)
-		});
-		return result;
+	options: [{
+		type: ApplicationCommandOptionType.String,
+		name: 'name',
+		description: 'The log you want to see.',
+		required: true,
+		autocomplete: async (value: string) => {
+			return [{
+				name: 'Overall (Main Collection Log)',
+				value: 'overall'
+			},
+				{
+					name: 'Overall+',
+					value: 'overall+'
+				},
+				...Object.entries(allCollectionLogs)
+				.map(i => {
+					return [{
+						name: `${i[0]} (Group)`, value: i[0]
+					},
+						...Object.entries(i[1].activities).map(act => ({
+							name: `${act[0]} (${act[1].items.length} Items)`,
+							value: act[0]
+						}))
+					];
+				})
+				.flat(3)
+			].filter(i => (!value ? true: i.name.toLowerCase().includes(value.toLowerCase)));
+		];
 	}
+	},
+	{
+		type: ApplicationCommandOptionType.String,
+		name: 'type',
+		description: 'The type of log you want to see.',
+		required: false,
+		choices: collectionLogTypes.map(i => ({
+			name: `${toTitleCase(i.name)} (${i.description})`, value: i.name
+		}))
+	},
+	{
+		type: ApplicationCommandOptionType.String,
+		name: 'flag',
+		description: 'The flag you want to pass.',
+		required: false,
+		choices: CollectionLogFlags.map(i => ({
+			name: `${toTitleCase(i.name)} (${i.description})`, value: i.name
+		}))
+	},
+	{
+		type: ApplicationCommandOptionType.String,
+		name: 'flag_extra',
+		description: 'An additional flag you want to pass.',
+		required: false,
+		choices: CollectionLogFlags.map(i => ({
+			name: `${toTitleCase(i.name)} (${i.description})`, value: i.name
+		}))
+	},
+	{
+		type: ApplicationCommandOptionType.Boolean,
+		name: 'all',
+		description: 'Show all items?',
+		required: false
+	}],
+run: async ({
+	options,
+	userID
+}: CommandRunOptions < {
+	name: string;
+	type?: CollectionLogType;
+	flag?: string;
+	flag_extra?: string;
+	all?: boolean;
+} >) => {
+	const user = await mUserFetch(userID);
+	let flags: Record < string,
+	string > = {};
+	if (options.flag) flags[options.flag] = options.flag;
+	if (options.flag_extra) flags[options.flag_extra] = options.flag_extra;
+	if (options.all) flags.all = 'all';
+	const result = await clImageGenerator.generateLogImage({
+		user,
+		type: options.type ?? 'collection',
+		flags,
+		collection: options.name,
+		stats: await fetchStatsForCL(user)
+	});
+	return result;
+}
 };
