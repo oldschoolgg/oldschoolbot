11cd63d03216fd9ba31cec546d91fbcba74b58a2
Jonesy <69014816+nwjgit@users.noreply.github.com>
2024-02-09 00:47:05 -0600

    Fix CA command being case sensitive (#5671)

--- src/mahoji/commands/ca.ts
+++ src/mahoji/commands/ca.ts
@@ -140,7 +140,7 @@ export const caCommand: OSBMahojiCommand = {
 
 			if (selectedMonster) {
 				const tasksForSelectedMonster = allCombatAchievementTasks.filter(
-					task => task.monster === selectedMonster
+					task => task.monster.toLowerCase() === selectedMonster!.toLowerCase()
 				);
 
 				if (tasksForSelectedMonster.length === 0)
