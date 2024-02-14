e8711a82a184e89a968cbd3f01d33b959c11bcd8
Jonesy <69014816+nwjgit@users.noreply.github.com>
2024-02-13 20:47:48 -0600

    remove any from brimstone

--- src/lib/openables.ts
+++ src/lib/openables.ts
@@ -172,7 +172,7 @@ const osjsOpenables: UnifiedOpenable[] = [
 		}> => {
 			const chest = new BrimstoneChestOpenable(BrimstoneChest);
 			const fishLvl = args.user.skillLevel(SkillsEnum.Fishing);
-			const brimstoneOptions: any = {
+			const brimstoneOptions = {
 				fishLvl
 			};
 			const openLoot: Bank = chest.open(args.quantity, brimstoneOptions);
