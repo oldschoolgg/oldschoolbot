a6a9cd1bd095a9d0cd7bd2ae50256ff6fdb40f30
gc <30398469+gc@users.noreply.github.com>
2024-03-04 12:34:00 +1100

    Dont regenerate creatables file if no changes

--- src/scripts/render-creatables-file.ts
+++ src/scripts/render-creatables-file.ts
@@ -1,20 +1,36 @@
 import '../lib/data/itemAliases';
 
-import { writeFileSync } from 'fs';
+import { Stopwatch } from '@sapphire/stopwatch';
+import { existsSync, writeFileSync } from 'fs';
 import { Bank } from 'oldschooljs';
 import { table } from 'table';
 
 import Createables from '../lib/data/createables';
+import { md5sum } from '../lib/util';
 
-export function makeCreatablesTable() {
-	const creatableTable = table([
+export function main() {
+	const data = [
 		['Item name', 'Input Items', 'Output Items', 'GP Cost'],
 		...Createables.map(i => {
 			return [i.name, `${new Bank(i.inputItems)}`, `${new Bank(i.outputItems)}`, `${i.GPCost ?? 0}`];
 		})
-	]);
+	];
+	const hash = md5sum(JSON.stringify(data)).slice(0, 5);
+	const fileName = `./src/lib/data/creatablesTable_${hash}.txt`;
 
-	return creatableTable;
+	const exists = existsSync(fileName);
+	if (exists) {
+		console.log('No changes to creatables');
+		return;
+	}
+
+	const stopwatch = new Stopwatch();
+	stopwatch.start();
+	const creatableTable = table(data);
+	stopwatch.stop();
+	console.log(`Rendered creatables table in ${stopwatch.toString()}`);
+
+	writeFileSync(fileName, creatableTable);
 }
 
-writeFileSync('./src/lib/data/creatablesTable.txt', makeCreatablesTable());
+main();
