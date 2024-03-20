58a287d2e2798d5689b1240a6f593714886bdc06
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2024-03-20 16:49:06 +0000

    Valamore quests

--- src/mahoji/lib/abstracted_commands/questCommand.ts
+++ src/mahoji/lib/abstracted_commands/questCommand.ts
@@ -8,7 +8,7 @@ import addSubTaskToActivityTask from '../../../lib/util/addSubTaskToActivityTask
 import { minionIsBusy } from '../../../lib/util/minionIsBusy';
 import { userHasGracefulEquipped } from '../../mahojiSettings';
 
-export const MAX_GLOBAL_QP = 298;
+export const MAX_GLOBAL_QP = 303;
 
 interface Quest {
 	id: QuestID;
