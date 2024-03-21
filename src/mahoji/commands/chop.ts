cd303e9e92a469d049108dc8ac44546f3c472357
nwjgit <nwjgit@gmail.com>
2024-03-20 23:28:49 -0500

    forestryUpdate

--- src/mahoji/commands/chop.ts
+++ src/mahoji/commands/chop.ts
@@ -1,7 +1,7 @@
 import { increaseNumByPercent, reduceNumByPercent } from 'e';
 import { ApplicationCommandOptionType, CommandRunOptions } from 'mahoji';
 
-import { IVY_MAX_TRIP_LENGTH_BOOST } from '../../lib/constants';
+import { IVY_MAX_TRIP_LENGTH_BOOST, TwitcherGloves, TWITCHERS_GLOVES } from '../../lib/constants';
 import { InventionID, inventionItemBoost } from '../../lib/invention/inventions';
 import { determineWoodcuttingTime } from '../../lib/skilling/functions/determineWoodcuttingTime';
 import Woodcutting from '../../lib/skilling/skills/woodcutting';
@@ -106,6 +106,19 @@ export const chopCommand: OSBMahojiCommand = {
 			name: 'powerchop',
 			description: 'Set this to true to powerchop. Higher xp/hour, No loot (default false, optional).',
 			required: false
+		},
+		{
+			type: ApplicationCommandOptionType.Boolean,
+			name: 'forestry_events',
+			description: 'Set this to true to participate in forestry events. (default false, optional).',
+			required: false
+		},
+		{
+			type: ApplicationCommandOptionType.String,
+			name: 'twitchers_gloves',
+			description: "Change the settings of your Twitcher's gloves. (default egg, optional)",
+			required: false,
+			choices: TWITCHERS_GLOVES.map(i => ({ name: `${i} nest`, value: i }))
 		}
 	],
 	run: async ({
@@ -116,6 +129,8 @@ export const chopCommand: OSBMahojiCommand = {
 		name: string;
 		quantity?: number;
 		powerchop?: boolean;
+		forestry_events?: boolean;
+		twitchers_gloves?: TwitcherGloves;
 	}>) => {
 		const user = await mUserFetch(userID);
 		const log = Woodcutting.Logs.find(
@@ -127,7 +142,7 @@ export const chopCommand: OSBMahojiCommand = {
 
 		if (!log) return "That's not a valid log to chop.";
 
-		let { quantity, powerchop } = options;
+		let { quantity, powerchop, forestry_events, twitchers_gloves } = options;
 
 		const skills = user.skillsAsLevels;
 
@@ -150,11 +165,19 @@ export const chopCommand: OSBMahojiCommand = {
 		let wcLvl = skills.woodcutting;
 
 		// Invisible wc boost for woodcutting guild, forestry events don't happen in woodcutting guild
-		if (resolveItems(['Redwood logs', 'Logs']).includes(log.id) || log.lootTable) {
+		if (
+			!forestry_events ||
+			resolveItems(['Redwood logs', 'Logs']).includes(log.id) ||
+			log.lootTable ||
+			log.name === 'Ivy'
+		) {
+			forestry_events = false;
 			if (skills.woodcutting >= 60 && log.wcGuild) {
 				boosts.push('+7 invisible WC lvls at the Woodcutting guild');
 				wcLvl += 7;
 			}
+		} else {
+			boosts.push('Participating in Forestry events');
 		}
 
 		// Enable 1.5 tick teaks half way to 99
@@ -164,6 +187,7 @@ export const chopCommand: OSBMahojiCommand = {
 
 		// Default bronze axe, last in the array
 		let axeMultiplier = 1;
+		boosts.push(`**${axeMultiplier}x** success multiplier for Bronze axe`);
 
 		if (user.hasEquippedOrInBank(['Drygore axe'])) {
 			let [predeterminedTotalTime] = determineWoodcuttingTime({
@@ -181,9 +205,11 @@ export const chopCommand: OSBMahojiCommand = {
 			});
 			if (boostRes.success) {
 				axeMultiplier = 10;
+				boosts.pop();
 				boosts.push(`**10x** success multiplier for Drygore axe (${boostRes.messages})`);
 			} else {
 				axeMultiplier = 8;
+				boosts.pop();
 				boosts.push('**8x** success multiplier for Dwarven greataxe');
 			}
 		} else {
@@ -196,7 +222,8 @@ export const chopCommand: OSBMahojiCommand = {
 			}
 		}
 
-		if (log.name === 'Ivy') {
+		// Ivy choping
+		if (!forestry_events && log.name === 'Ivy') {
 			boosts.push(`+${formatDuration(IVY_MAX_TRIP_LENGTH_BOOST, true)} max trip length for Ivy`);
 			powerchop = false;
 		}
@@ -244,6 +271,8 @@ export const chopCommand: OSBMahojiCommand = {
 			quantity: newQuantity,
 			iQty: options.quantity ? options.quantity : undefined,
 			powerchopping: powerchop,
+			forestry: forestry_events,
+			twitchers: twitchers_gloves,
 			duration,
 			fakeDurationMin,
 			fakeDurationMax,
