d4c2e29661bf0da4be37f7c8fbae2bd85ae87696
themrrobert <10122432+themrrobert@users.noreply.github.com>
2024-04-14 19:42:34 -0700

    Fix duplicate untradeable rewards from Elders

--- src/lib/simulation/elderClue.ts
+++ src/lib/simulation/elderClue.ts
@@ -86,8 +86,9 @@ export class ElderClue extends Clue {
 			}
 
 			const untradeableUniques = resolveItems(['Clue bag', 'Inventors tools', 'Elder knowledge']);
+			const currentCl = user.cl.clone();
 			if (roll(100)) {
-				const unowned = untradeableUniques.filter(id => !user.cl.has(id));
+				const unowned = untradeableUniques.filter(id => !currentCl.add(loot).has(id));
 				if (unowned.length > 0) {
 					loot.add(randArrItem(unowned));
 				} else {
