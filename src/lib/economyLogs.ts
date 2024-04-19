758f7a02f5fcbc59b58915119d524697b951ce5c
themrrobert <10122432+themrrobert@users.noreply.github.com>
2024-04-18 17:25:14 -0700

    Dont flush empty log

--- src/lib/economyLogs.ts
+++ src/lib/economyLogs.ts
@@ -5,7 +5,7 @@ let economyLogBuffer: string[] = [];
 
 export async function economyLog(message: string, flush: boolean = false) {
 	economyLogBuffer.push(message);
-	if (flush || economyLogBuffer.length === 10) {
+	if ((flush && economyLogBuffer.length !== 1) || economyLogBuffer.length === 10) {
 		await sendToChannelID(Channel.EconomyLogs, {
 			content: economyLogBuffer.join('\n---------------------------------\n'),
 			allowedMentions: { parse: [], users: [], roles: [] }
