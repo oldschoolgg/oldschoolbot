57f7ca682b4e292a40307d4f690671efa7b0cc6f
themrrobert <10122432+themrrobert@users.noreply.github.com>
2024-05-12 08:54:46 -0700

    Fix Recently Killed

--- src/mahoji/commands/k.ts
+++ src/mahoji/commands/k.ts
@@ -40,11 +40,13 @@ export const autocompleteMonsters = [
 
 async function fetchUsersRecentlyKilledMonsters(userID: string) {
 	const res = await prisma.$queryRawUnsafe<{ mon_id: string }[]>(
-		`SELECT DISTINCT((data->>'monsterID')) AS mon_id
+		`SELECT DISTINCT((data->>'monsterID')) AS mon_id, MAX(start_date)
 FROM activity
 WHERE user_id = $1
 AND type = 'MonsterKilling'
 AND finish_date > now() - INTERVAL '31 days'
+GROUP BY 1
+ORDER BY 2 DESC
 LIMIT 10;`,
 		BigInt(userID)
 	);
