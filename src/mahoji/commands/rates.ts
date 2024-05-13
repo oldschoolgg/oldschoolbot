2aa35d232bc7a34666641c012788ac218b20df02
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2024-05-13 19:31:16 +0100

    Update rates.ts

--- src/mahoji/commands/rates.ts
+++ src/mahoji/commands/rates.ts
@@ -649,7 +649,8 @@ ${zygomiteFarmingSource
 										miningLevel,
 										craftingLevel: 120,
 										strengthLevel: 120,
-										maxTripLength: duration
+										maxTripLength: duration,
+										user
 									});
 									if (typeof result === 'string') continue;
 									const spiritOre = stoneSpirits.find(t => t.ore.id === ore.id);
