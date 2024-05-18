993de0e51049577c3661e11490f0658e708be444
I-am-TURBO <turbolaceup@gmail.com>
2024-05-18 13:30:54 -0500

    subtext, alias, clean while harvest

--- src/lib/bankImage.ts
+++ src/lib/bankImage.ts
@@ -26,9 +26,12 @@ import { logError } from '../lib/util/logError';
 import { XPLamps } from '../mahoji/lib/abstracted_commands/lampCommand';
 import { divinationEnergies } from './bso/divination';
 import { TOBUniques } from './data/tob';
+<<<<<<< HEAD
 import { marketPriceOfBank, marketPriceOrBotPrice } from './marketPrices';
+=======
 import { SkillsEnum } from './skilling/types';
 import { applyCustomItemEffects } from './util/customItemEffects';
+>>>>>>> bsopet
 import resolveItems from './util/resolveItems';
 import { allSlayerMaskHelmsAndMasks, slayerMaskLeaderboardCache } from './util/slayerMaskLeaderboard';
 
@@ -248,21 +251,15 @@ const forcedShortNameMap = new Map<number, string>([
 	[i('Mango seed'), 'mango'],
 	[i('Avocado seed'), 'avocado'],
 	[i('Lychee seed'), 'lychee'],
-	[i('Blood orange seed'), 'b.orange'],
 	[i('Spirit weed seed'), 'spirit.w'],
 	[i('Spirit weed'), 'spirit.w'],
 	[i('Advax berry seed'), 'advax'],
 	[i('Advax berry'), 'advax'],
 	[i('Divination Potion'), 'div'],
-	[i('Elder logs'), 'elder'],
+	[i('Elder logs'), 'Elder'],
 	[i('Clue scroll (grandmaster)'), 'grandmaster'],
 	[i('Reward casket (grandmaster)'), 'grandmaster'],
-	[i('Atomic energy'), 'atomic'],
-	[i('Fruity zygomite spores'), 'fruity'],
-	[i('Barky zygomite spores'), 'barky'],
-	[i('Herbal zygomite spores'), 'herbal'],
-	[i('Clue scroll (elder)'), 'elder'],
-	[i('Reward casket (elder)'), 'elder']
+	[i('Atomic energy'), 'Atomic']
 ]);
 
 for (const energy of divinationEnergies) {
