9beae70b2f1556ca98c50570454c732c67bc287a
I-am-TURBO <turbolaceup@gmail.com>
2024-05-19 07:09:04 -0500

    fixed mistake

--- src/tasks/minions/bso/turaelsTrialsActivity.ts
+++ src/tasks/minions/bso/turaelsTrialsActivity.ts
@@ -5,7 +5,7 @@ import { incrementMinigameScore } from '../../../lib/settings/settings';
 import { XPBank } from '../../../lib/structures/Banks';
 import { TuraelsTrialsOptions } from '../../../lib/types/minions';
 import { handleTripFinish } from '../../../lib/util/handleTripFinish';
-import { trackClientBankStats, userStatsBankUpdate, userStatsUpdate } from '../../../mahoji/mahojiSettings';
+import { trackClientBankStats, userStatsBankUpdate } from '../../../mahoji/mahojiSettings';
 
 export function calculateTuraelsTrialsResult({ quantity, method }: { quantity: number; method: TuraelsTrialsMethod }) {
 	const loot = new Bank();
@@ -38,7 +38,11 @@ export const turaelsTrialsTask: MinionTask = {
 
 		const result = calculateTuraelsTrialsResult({ quantity, method });
 
+<<<<<<< HEAD
 		const { newScore } = await incrementMinigameScore(userID, 'turaels_trials', quantity);
+=======
+		const { newScore } = await incrementMinigameScore(userID, 'TuraelsTrials', quantity);
+>>>>>>> b63059a02 (fixed mistake)
 
 		await user.addItemsToBank({ items: result.loot, collectionLog: true });
 		await trackClientBankStats('turaels_trials_loot_bank', result.loot);
