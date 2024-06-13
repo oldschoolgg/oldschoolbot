600c7153dca1c08314ae1a71249831c6a0075c14
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2024-06-13 20:50:57 +0100

    Update startupScripts.ts

--- src/lib/startupScripts.ts
+++ src/lib/startupScripts.ts
@@ -16,7 +16,6 @@ const arrayColumns = [
 	['users', 'favorite_alchables'],
 	['users', 'favorite_food'],
 	['users', 'favorite_bh_seeds'],
-	['users', 'favorite_skill'],
 	['users', 'attack_style'],
 	['users', 'combat_options'],
 	['users', 'ironman_alts'],
