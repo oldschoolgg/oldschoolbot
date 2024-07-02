7a5c0007cb27f179ce82397742c5ee173279bd62
gc <30398469+gc@users.noreply.github.com>
2024-07-02 11:34:36 +1000

    Fix

--- src/lib/http/routes/index.ts
+++ src/lib/http/routes/index.ts
@@ -1,7 +1,5 @@
 import type { FastifyServer } from '../types';
-import { commandsRoute } from './commands';
 import githubSponsors from './webhooks/githubSponsors';
 import { patreonRoute } from './webhooks/patreon';
 
-export const initRoutes = (server: FastifyServer) =>
-	[githubSponsors, commandsRoute, patreonRoute].map(route => route(server));
+export const initRoutes = (server: FastifyServer) => [githubSponsors, patreonRoute].map(route => route(server));
