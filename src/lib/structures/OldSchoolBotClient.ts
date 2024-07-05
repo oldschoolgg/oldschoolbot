c002157b54c6a88d6030ccc8c12bec751bc6c2f6
gc <30398469+gc@users.noreply.github.com>
2024-07-06 07:37:26 +1000

    changes

--- src/lib/structures/OldSchoolBotClient.ts
+++ src/lib/structures/OldSchoolBotClient.ts
@@ -2,16 +2,10 @@ import type { MahojiClient } from '@oldschoolgg/toolkit';
 import type { User } from 'discord.js';
 import { Client } from 'discord.js';
 
-import { production } from '../../config';
 import type { Peak } from '../tickers';
-
-if (typeof production !== 'boolean') {
-	throw new Error('Must provide production boolean.');
-}
 
 export class OldSchoolBotClient extends Client<true> {
 	public busyCounterCache = new Map<string, number>();
-	public production = production ?? false;
 	public mahojiClient!: MahojiClient;
 	public isShuttingDown = false;
 
