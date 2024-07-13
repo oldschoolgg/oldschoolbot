f204a545e9fc1f2ebde0a8afb31616afbd85f2cb
gc <30398469+gc@users.noreply.github.com>
2024-07-13 12:46:19 +1000

    Typesafety on active user queries

--- src/lib/util/cachedUserIDs.ts
+++ src/lib/util/cachedUserIDs.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { ChannelType } from 'discord.js';
 import { objectEntries } from 'e';
 
@@ -11,14 +12,14 @@ for (const id of OWNER_IDS) CACHED_ACTIVE_USER_IDS.add(id);
 
 export async function syncActiveUserIDs() {
 	const [users, otherUsers] = await prisma.$transaction([
-		prisma.$queryRaw<{ user_id: string }[]>`SELECT DISTINCT(user_id::text)
+		prisma.$queryRaw<{ user_id: string }[]>`SELECT DISTINCT(${Prisma.ActivityScalarFieldEnum.user_id}::text)
 FROM activity
 WHERE finish_date > now() - INTERVAL '48 hours'`,
 		prisma.$queryRaw<{ id: string }[]>`SELECT id
 FROM users
-WHERE main_account IS NOT NULL
-OR CARDINALITY(ironman_alts) > 0
-OR bitfield && ARRAY[2,3,4,5,6,7,8,12,11,21,19];`
+WHERE ${Prisma.UserScalarFieldEnum.main_account} IS NOT NULL
+OR CARDINALITY(${Prisma.UserScalarFieldEnum.ironman_alts}) > 0
+OR ${Prisma.UserScalarFieldEnum.bitfield} && ARRAY[2,3,4,5,6,7,8,12,11,21,19];`
 	]);
 
 	for (const id of [...users.map(i => i.user_id), ...otherUsers.map(i => i.id)]) {
