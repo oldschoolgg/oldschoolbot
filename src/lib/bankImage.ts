f64ff4aa740a4a2800140243fcc9bd91cf095ef9
gc <30398469+gc@users.noreply.github.com>
2024-07-13 17:02:06 +1000

    Remove some debug logs

--- src/lib/bankImage.ts
+++ src/lib/bankImage.ts
@@ -642,8 +642,6 @@ export class BankImageTask {
 
 		let items = bank.items();
 
-		debugLog(`Generating a bank image with ${items.length} items`, { title, userID: user?.id });
-
 		// Sorting
 		const favorites = user?.user.favoriteItems;
 		const weightings = user?.user.bank_sort_weightings as ItemBank;
