5f734cf6a75109463372ff886868bdbf514c9348
gc <30398469+gc@users.noreply.github.com>
2024-07-13 11:37:44 +1000

    Add smol heredit to pets

--- src/lib/data/CollectionsExport.ts
+++ src/lib/data/CollectionsExport.ts
@@ -1546,7 +1546,8 @@ export const allPetsCL = resolveItems([
 	"Lil'viathan",
 	'Butch',
 	'Baron',
-	'Scurry'
+	'Scurry',
+	'Smol heredit'
 ]);
 export const camdozaalCL = resolveItems([
 	'Barronite mace',
