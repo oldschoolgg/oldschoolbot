abdc3a7f7320244a699b6167fa1cc47590fb588f
gc <30398469+gc@users.noreply.github.com>
2024-07-16 11:05:16 +1000

    changes

--- tests/integration/redis.test.ts
+++ tests/integration/redis.test.ts
@@ -11,7 +11,7 @@ function makeSender() {
 	return new TSRedis({ mocked: !globalConfig.redisPort, port: globalConfig.redisPort });
 }
 
-test('Should add patron badge', async () => {
+test.concurrent('Should add patron badge', async () => {
 	const user = await createTestUser();
 	expect(user.user.badges).not.includes(BadgesEnum.Patron);
 	const _redis = makeSender();
@@ -22,12 +22,12 @@ test('Should add patron badge', async () => {
 		old_tier: 0,
 		first_time_patron: false
 	});
-	await sleep(100);
+	await sleep(250);
 	await user.sync();
 	expect(user.user.badges).includes(BadgesEnum.Patron);
 });
 
-test('Should remove patron badge', async () => {
+test.concurrent('Should remove patron badge', async () => {
 	const user = await createTestUser(undefined, { badges: [BadgesEnum.Patron] });
 	expect(user.user.badges).includes(BadgesEnum.Patron);
 	const _redis = makeSender();
@@ -38,12 +38,12 @@ test('Should remove patron badge', async () => {
 		old_tier: 1,
 		first_time_patron: false
 	});
-	await sleep(100);
+	await sleep(250);
 	await user.sync();
 	expect(user.user.badges).not.includes(BadgesEnum.Patron);
 });
 
-test('Should add to cache', async () => {
+test.concurrent('Should add to cache', async () => {
 	const users = [await createTestUser(), await createTestUser(), await createTestUser()];
 	await roboChimpClient.user.createMany({
 		data: users.map(u => ({
@@ -59,7 +59,7 @@ test('Should add to cache', async () => {
 		old_tier: 2,
 		first_time_patron: false
 	});
-	await sleep(100);
+	await sleep(250);
 	for (const user of users) {
 		const cached = roboChimpCache.get(user.id);
 		expect(getPerkTierSync(user.id)).toEqual(5);
@@ -67,7 +67,7 @@ test('Should add to cache', async () => {
 	}
 });
 
-test('Should remove from cache', async () => {
+test.concurrent('Should remove from cache', async () => {
 	const users = [await createTestUser(), await createTestUser(), await createTestUser()];
 	await roboChimpClient.user.createMany({
 		data: users.map(u => ({
@@ -83,7 +83,7 @@ test('Should remove from cache', async () => {
 		old_tier: 5,
 		first_time_patron: false
 	});
-	await sleep(100);
+	await sleep(250);
 	for (const user of users) {
 		expect(getPerkTierSync(user.id)).toEqual(0);
 		const cached = roboChimpCache.get(user.id);
@@ -91,7 +91,7 @@ test('Should remove from cache', async () => {
 	}
 });
 
-test('Should recognize special bitfields', async () => {
+test.concurrent('Should recognize special bitfields', async () => {
 	const users = [
 		await createTestUser(undefined, { bitfield: [BitField.HasPermanentTierOne] }),
 		await createTestUser(undefined, { bitfield: [BitField.BothBotsMaxedFreeTierOnePerks] })
