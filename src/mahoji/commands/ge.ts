117eae1c5f667cffe5f130cd6827118d9077ac12
gc <30398469+gc@users.noreply.github.com>
2024-07-19 01:10:42 +1000

    Fixes

--- src/mahoji/commands/ge.ts
+++ src/mahoji/commands/ge.ts
@@ -10,6 +10,8 @@ import { PerkTier } from '../../lib/constants';
 import { GrandExchange, createGECancelButton } from '../../lib/grandExchange';
 import { marketPricemap } from '../../lib/marketPrices';
 
+import { Bank } from 'oldschooljs';
+import type { ItemBank } from 'oldschooljs/dist/meta/types';
 import { formatDuration, itemNameFromID, makeComponents, returnStringOrFile, toKMB } from '../../lib/util';
 import { createChart } from '../../lib/util/chart';
 import getOSItem from '../../lib/util/getOSItem';
@@ -19,6 +21,7 @@ import itemIsTradeable from '../../lib/util/itemIsTradeable';
 import { cancelGEListingCommand } from '../lib/abstracted_commands/cancelGEListingCommand';
 import { itemOption, tradeableItemArr } from '../lib/mahojiCommandOptions';
 import type { OSBMahojiCommand } from '../lib/util';
+import { mahojiUsersSettingsFetch } from '../mahojiSettings';
 
 export type GEListingWithTransactions = GEListing & {
 	buyTransactions: GETransaction[];
@@ -135,9 +138,10 @@ export const geCommand: OSBMahojiCommand = {
 					description: 'The item you want to sell.',
 					required: true,
 					autocomplete: async (value, { id }) => {
-						const user = await mUserFetch(id);
+						const raw = await mahojiUsersSettingsFetch(id, { bank: true });
+						const bank = new Bank(raw.bank as ItemBank);
 
-						return user.bank
+						return bank
 							.items()
 							.filter(i => i[0].tradeable_on_ge)
 							.filter(i => (!value ? true : i[0].name.toLowerCase().includes(value.toLowerCase())))
