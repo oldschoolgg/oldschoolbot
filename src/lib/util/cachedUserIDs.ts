395234e069c4a60e52faeae218d63d288f139150
gc <30398469+gc@users.noreply.github.com>
2024-07-19 21:04:04 +1000

    Some changes

--- src/lib/util/cachedUserIDs.ts
+++ src/lib/util/cachedUserIDs.ts
@@ -4,28 +4,28 @@ import { objectEntries } from 'e';
 
 import { OWNER_IDS, SupportServer } from '../../config';
 import { globalConfig } from '../constants';
-import { runTimedLoggedFn } from '../util';
+import { logWrapFn, runTimedLoggedFn } from '../util';
 
 export const CACHED_ACTIVE_USER_IDS = new Set();
 CACHED_ACTIVE_USER_IDS.add(globalConfig.clientID);
 for (const id of OWNER_IDS) CACHED_ACTIVE_USER_IDS.add(id);
 
-export async function syncActiveUserIDs() {
-	const users = await prisma.$queryRawUnsafe<
-		{ user_id: string }[]
-	>(`SELECT DISTINCT(${Prisma.ActivityScalarFieldEnum.user_id}::text)
+export const syncActiveUserIDs = logWrapFn('Sync Active User IDs', async () => {
+		const users = await prisma.$queryRawUnsafe<
+			{ user_id: string }[]
+		>(`SELECT DISTINCT(${Prisma.ActivityScalarFieldEnum.user_id}::text)
 FROM activity
 WHERE finish_date > now() - INTERVAL '48 hours'`);
 
-	const perkTierUsers = await roboChimpClient.$queryRawUnsafe<{ id: string }[]>(`SELECT id::text
+		const perkTierUsers = await roboChimpClient.$queryRawUnsafe<{ id: string }[]>(`SELECT id::text
 FROM "user"
 WHERE perk_tier > 0;`);
 
-	for (const id of [...users.map(i => i.user_id), ...perkTierUsers.map(i => i.id)]) {
-		CACHED_ACTIVE_USER_IDS.add(id);
-	}
-	debugLog(`${CACHED_ACTIVE_USER_IDS.size} cached active user IDs`);
-}
+		for (const id of [...users.map(i => i.user_id), ...perkTierUsers.map(i => i.id)]) {
+			CACHED_ACTIVE_USER_IDS.add(id);
+		}
+		debugLog(`${CACHED_ACTIVE_USER_IDS.size} cached active user IDs`);
+});
 
 export function memoryAnalysis() {
 	const guilds = globalClient.guilds.cache.size;
