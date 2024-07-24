80017e44d9a88c9fbdd257a4e5eb41923292c553
GC <30398469+gc@users.noreply.github.com>
2024-07-24 19:56:37 +1000

    Exit handling + test bot status (#5967)

--- src/mahoji/lib/events.ts
+++ src/mahoji/lib/events.ts
@@ -1,6 +1,7 @@
 import type { ItemBank } from 'oldschooljs/dist/meta/types';
 
 import { bulkUpdateCommands } from '@oldschoolgg/toolkit';
+import { ActivityType, bold, time } from 'discord.js';
 import { Channel, META_CONSTANTS, globalConfig } from '../../lib/constants';
 import { initCrons } from '../../lib/crons';
 import { initTickers } from '../../lib/tickers';
@@ -9,6 +10,60 @@ import { mahojiClientSettingsFetch } from '../../lib/util/clientSettings';
 import { sendToChannelID } from '../../lib/util/webhook';
 import { CUSTOM_PRICE_CACHE } from '../commands/sell';
 
+export async function updateTestBotStatus(online = true) {
+	try {
+		if (globalConfig.isProduction) return;
+		const idMap: Record<string, string> = {
+			'829398443821891634': '1265571664142270464',
+			'577488230539067403': '1265582554644217977',
+			'353484579840983042': '1265582554644217977',
+			'897549995446779964': '1265582743970910259',
+			'1158785741028081696': '1265583194108067925'
+		};
+		const catChannelID = idMap[globalConfig.clientID];
+		if (!catChannelID) return;
+		const cat = await globalClient.channels.fetch(catChannelID);
+		if (!cat || !cat.isTextBased() || cat.isDMBased()) {
+			console.log('Could not find status channel');
+			return;
+		}
+
+		const emoji = online ? '🟢' : '🔴';
+		let text = '';
+		if (online) {
+			text = `${emoji} ${globalClient.user.username} is ONLINE ${emoji}
+
+Turned on ${time(new Date(), 'R')}`;
+			text = bold(text);
+		} else {
+			text = `${emoji} ${globalClient.user.username} is offline ${emoji}
+
+Turned off ${time(new Date(), 'R')}`;
+		}
+		const message = await cat.messages
+			.fetch({ limit: 5 })
+			.then(messages => messages.filter(m => m.author.id === globalClient.user!.id))
+			.then(msg => msg.first());
+		if (!message) {
+			await cat.send(text);
+		} else {
+			await message.edit(text);
+		}
+		if (online) {
+			await globalClient.user.setPresence({
+				status: 'online',
+				activities: [
+					{
+						name: `${emoji} ONLINE`,
+						type: ActivityType.Custom
+					}
+				]
+			});
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
 export async function syncCustomPrices() {
 	const clientData = await mahojiClientSettingsFetch({ custom_prices: true });
 	for (const [key, value] of Object.entries(clientData.custom_prices as ItemBank)) {
@@ -17,26 +72,29 @@ export async function syncCustomPrices() {
 }
 
 export const onStartup = logWrapFn('onStartup', async () => {
-	globalClient.application.commands.fetch({
-		guildId: globalConfig.isProduction ? undefined : globalConfig.testingServerID
-	});
-	if (!globalConfig.isProduction) {
-		console.log('Syncing commands locally...');
-		await bulkUpdateCommands({
-			client: globalClient.mahojiClient,
-			commands: Array.from(globalClient.mahojiClient.commands.values()),
-			guildID: globalConfig.testingServerID
-		});
-	}
+	const syncTestBotCommands = globalConfig.isProduction
+		? null
+		: bulkUpdateCommands({
+				client: globalClient.mahojiClient,
+				commands: Array.from(globalClient.mahojiClient.commands.values()),
+				guildID: globalConfig.testingServerID
+			});
 
 	initCrons();
 	initTickers();
 
-	if (globalConfig.isProduction) {
-		sendToChannelID(Channel.GeneralChannel, {
-			content: `I have just turned on!
+	const sendStartupMessage = globalConfig.isProduction
+		? sendToChannelID(Channel.GeneralChannel, {
+				content: `I have just turned on!\n\n${META_CONSTANTS.RENDERED_STR}`
+			}).catch(console.error)
+		: null;
 
-${META_CONSTANTS.RENDERED_STR}`
-		}).catch(console.error);
-	}
+	await Promise.all([
+		globalClient.application.commands.fetch({
+			guildId: globalConfig.isProduction ? undefined : globalConfig.testingServerID
+		}),
+		updateTestBotStatus(),
+		sendStartupMessage,
+		syncTestBotCommands
+	]);
 });
