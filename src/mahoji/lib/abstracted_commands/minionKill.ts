eb0dc0ad30a25e7eed7246a646f57cea36ab8f08
nwjgit <69014816+nwjgit@users.noreply.github.com>
2024-08-06 07:15:39 -0500

    Allow multiple combat options for PVM trips (#5962)

--- src/mahoji/lib/abstracted_commands/minionKill.ts
+++ src/mahoji/lib/abstracted_commands/minionKill.ts
@@ -48,7 +48,7 @@ import type { Consumable } from '../../../lib/minions/types';
 import { calcPOHBoosts } from '../../../lib/poh';
 import { SkillsEnum } from '../../../lib/skilling/types';
 import { SlayerTaskUnlocksEnum } from '../../../lib/slayer/slayerUnlocks';
-import { determineBoostChoice, getUsersCurrentSlayerInfo } from '../../../lib/slayer/slayerUtil';
+import { determineCombatBoosts, getUsersCurrentSlayerInfo } from '../../../lib/slayer/slayerUtil';
 import { maxOffenceStats } from '../../../lib/structures/Gear';
 import type { Peak } from '../../../lib/tickers';
 import type { MonsterActivityTaskOptions } from '../../../lib/types/minions';
@@ -134,7 +134,7 @@ export async function minionKillCommand(
 	channelID: string,
 	name: string,
 	quantity: number | undefined,
-	method: PvMMethod | undefined,
+	method: PvMMethod | PvMMethod[] | undefined,
 	wilderness: boolean | undefined,
 	solo: boolean | undefined
 ): Promise<string | InteractionReplyOptions> {
@@ -206,19 +206,18 @@ export async function minionKillCommand(
 		}
 	}
 
-	// Add jelly check as can barrage in wilderness
+	// Add check for burstable monsters in wilderness
 	const jelly = monster.id === Monsters.Jelly.id;
-	const bloodveld = monster.id === Monsters.Bloodveld.id;
+	const wildyBurst = jelly && isInWilderness;
 
-	const wildyBurst = (jelly || bloodveld) && isInWilderness;
-
-	// Set chosen boost based on priority:
+	// determines what pvm methods the user can use
 	const myCBOpts = user.combatOptions;
-	const boostChoice = determineBoostChoice({
+	const methods = [method] as PvMMethod[];
+	const combatMethods = determineCombatBoosts({
 		cbOpts: myCBOpts as CombatOptionsEnum[],
 		user,
 		monster,
-		method,
+		methods,
 		isOnTask,
 		wildyBurst
 	});
@@ -239,7 +238,7 @@ export async function minionKillCommand(
 
 	const [, osjsMon, attackStyles] = resolveAttackStyles(user, {
 		monsterID: monster.id,
-		boostMethod: boostChoice
+		boostMethod: combatMethods
 	});
 	const [newTime, skillBoostMsg] = applySkillBoost(user, timeToFinish, attackStyles);
 
@@ -457,20 +456,24 @@ export async function minionKillCommand(
 	}
 
 	// Check for stats
-	if (boostChoice === 'barrage' && user.skillLevel(SkillsEnum.Magic) < 94) {
+	if (combatMethods.includes('barrage') && user.skillLevel(SkillsEnum.Magic) < 94) {
 		return `You need 94 Magic to use Ice Barrage. You have ${user.skillLevel(SkillsEnum.Magic)}`;
 	}
-	if (boostChoice === 'burst' && user.skillLevel(SkillsEnum.Magic) < 70) {
+	if (combatMethods.includes('burst') && user.skillLevel(SkillsEnum.Magic) < 70) {
 		return `You need 70 Magic to use Ice Burst. You have ${user.skillLevel(SkillsEnum.Magic)}`;
 	}
-	if (boostChoice === 'chinning' && user.skillLevel(SkillsEnum.Ranged) < 65) {
+	if (combatMethods.includes('chinning') && user.skillLevel(SkillsEnum.Ranged) < 65) {
 		return `You need 65 Ranged to use Chinning method. You have ${user.skillLevel(SkillsEnum.Ranged)}`;
 	}
 
-	// Wildy Monster checks
-	if (isInWilderness === true && boostChoice === 'cannon') {
+	// Wildy monster cannon checks
+	if (isInWilderness === true && combatMethods.includes('cannon')) {
 		if (monster.id === Monsters.HillGiant.id || monster.id === Monsters.MossGiant.id) {
 			usingCannon = isInWilderness;
+		}
+		if (monster.id === Monsters.Spider.id || Monsters.Scorpion.id) {
+			usingCannon = isInWilderness;
+			cannonMulti = isInWilderness;
 		}
 		if (monster.wildySlayerCave) {
 			usingCannon = isInWilderness;
@@ -482,8 +485,9 @@ export async function minionKillCommand(
 		}
 	}
 
+	// Burst/barrage check with wilderness conditions
 	if ((method === 'burst' || method === 'barrage') && !monster?.canBarrage) {
-		if (jelly || bloodveld) {
+		if (jelly) {
 			if (!isInWilderness) {
 				return `${monster.name} can only be barraged or burst in the wilderness.`;
 			}
@@ -496,14 +500,18 @@ export async function minionKillCommand(
 		}
 	}
 
-	if (boostChoice === 'barrage' && attackStyles.includes(SkillsEnum.Magic) && (monster?.canBarrage || wildyBurst)) {
+	if (
+		combatMethods.includes('barrage') &&
+		attackStyles.includes(SkillsEnum.Magic) &&
+		(monster?.canBarrage || wildyBurst)
+	) {
 		consumableCosts.push(iceBarrageConsumables);
 		calculateVirtusBoost();
 		timeToFinish = reduceNumByPercent(timeToFinish, boostIceBarrage + virtusBoost);
 		boosts.push(`${boostIceBarrage + virtusBoost}% for Ice Barrage${virtusBoostMsg}`);
 		burstOrBarrage = SlayerActivityConstants.IceBarrage;
 	} else if (
-		boostChoice === 'burst' &&
+		combatMethods.includes('burst') &&
 		attackStyles.includes(SkillsEnum.Magic) &&
 		(monster?.canBarrage || wildyBurst)
 	) {
@@ -512,13 +520,14 @@ export async function minionKillCommand(
 		timeToFinish = reduceNumByPercent(timeToFinish, boostIceBurst + virtusBoost);
 		boosts.push(`${boostIceBurst + virtusBoost}% for Ice Burst${virtusBoostMsg}`);
 		burstOrBarrage = SlayerActivityConstants.IceBurst;
-	} else if ((boostChoice === 'cannon' && hasCannon && monster?.cannonMulti) || cannonMulti) {
+	}
+	if ((combatMethods.includes('cannon') && hasCannon && monster?.cannonMulti) || cannonMulti) {
 		usingCannon = true;
 		cannonMulti = true;
 		consumableCosts.push(cannonMultiConsumables);
 		timeToFinish = reduceNumByPercent(timeToFinish, boostCannonMulti);
 		boosts.push(`${boostCannonMulti}% for Cannon in multi`);
-	} else if ((boostChoice === 'cannon' && hasCannon && monster?.canCannon) || usingCannon) {
+	} else if ((combatMethods.includes('cannon') && hasCannon && monster?.canCannon) || usingCannon) {
 		usingCannon = true;
 		consumableCosts.push(cannonSingleConsumables);
 		timeToFinish = reduceNumByPercent(timeToFinish, boostCannon);
