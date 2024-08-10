8c2554cad96a39fd82c3da68dea654885a8ad197
gc <30398469+gc@users.noreply.github.com>
2024-08-10 23:10:03 +1000

    Fix

--- src/lib/util/makeBadgeString.ts
+++ src/lib/util/makeBadgeString.ts
@@ -3,6 +3,6 @@ import { badges } from '../constants';
 export function makeBadgeString(user: MUser, badgeIDs: number[] | null | undefined) {
 	const method = user.getRandomizeMethod();
 	const rawBadges: string[] = (badgeIDs ?? []).map(num => badges[num]);
-	rawBadges.push(method!.emoji);
+	rawBadges.push(method?.emoji ?? '');
 	return rawBadges.join(' ').trim();
 }
