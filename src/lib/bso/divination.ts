271838fef8ac97a186ed7f22da578fcc92582647
nwjgit <nwjgit@gmail.com>
2024-08-13 16:02:12 -0500

    fix message

--- src/lib/bso/divination.ts
+++ src/lib/bso/divination.ts
@@ -353,7 +353,7 @@ export const portents: SourcePortent[] = [
 		cost: new Bank().add('Incandescent energy', 1200),
 		chargesPerPortent: 1000,
 		addChargeMessage: portent =>
-			`You used a Spiritual mining portent, your next ${portent.charges_remaining}x stone spirits will grant XP instead of ore.`
+			`You used a Spiritual mining portent, it will turn stone spirits into extra mining XP, instead of ore, in your next ${portent.charges_remaining} minutes of mining.`
 	},
 	{
 		id: PortentID.PacifistPortent,
