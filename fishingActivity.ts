573a6080e4813ca003e576a0f14a9efbe936a022
Badora II <79532596+Arodab@users.noreply.github.com>
2024-08-16 01:26:17 -0300

    Add files via upload

---
+++ fishingActivity.ts
@@ -0,0 +1,211 @@
+//import { calcPercentOfNum, percentChance, randInt } from 'e';
+import { calcPercentOfNum } from 'e';
+import { Bank } from 'oldschooljs';
+import { z } from 'zod';
+//import { Time } from 'e';
+import { Emoji, Events } from '../../lib/constants';
+import addSkillingClueToLoot from '../../lib/minions/functions/addSkillingClueToLoot';
+import Fishing from '../../lib/skilling/skills/fishing';
+import { SkillsEnum } from '../../lib/skilling/types';
+import type { FishingActivityTaskOptions } from '../../lib/types/minions';
+import { roll, skillingPetDropRate } from '../../lib/util';
+import { handleTripFinish } from '../../lib/util/handleTripFinish';
+// itemID from '../../lib/util/itemID';
+import { anglerBoostPercent } from '../../mahoji/mahojiSettings';
+
+const allFishIDs = Fishing.Fishes.flatMap(fish => [fish.id, fish.id2, fish.id3]);
+
+export const fishingTask: MinionTask = {
+	type: 'Fishing',
+	dataSchema: z.object({
+		type: z.literal('Fishing'),
+		fishID: z.number().refine(fishID => allFishIDs.includes(fishID), {
+			message: 'Invalid fish ID'
+		}),
+		quantity: z.number().min(1)
+	}),
+	async run(data: FishingActivityTaskOptions) {
+		let { fishID, userID, channelID, duration, spirit_flakes,
+			Qty1, Qty2 = 0, Qty3 = 0, loot1 = 0, loot2 = 0, loot3 = 0, flakesToRemove } = data;
+
+		spirit_flakes = spirit_flakes ?? false;
+
+		const user = await mUserFetch(userID);
+		const fishLvl = user.skillLevel(SkillsEnum.Fishing);
+
+		const minnowQuantity: { [key: number]: number[] } = {
+			99: [10, 14],
+			95: [11, 13],
+			90: [10, 13],
+			85: [10, 11],
+			1: [10, 10]
+		};
+
+		let baseMinnow = [10, 10];
+		for (const [level, quantities] of Object.entries(minnowQuantity).reverse()) {
+			if (fishLvl >= Number.parseInt(level)) {
+				baseMinnow = quantities;
+				break;
+			}
+		}
+
+		const baseKarambwanji = 1 + Math.floor(fishLvl / 5);
+
+		let xpReceived = 0;
+		let agilityXpReceived = 0;
+		let strengthXpReceived = 0;
+
+		const fish = Fishing.Fishes.find(fish => fish.id === fishID)!;
+
+		// adding xp and loot
+
+		xpReceived += fish.xp * Qty1;
+		if (Qty2 != 0) xpReceived += fish.xp2! * Qty2;
+		if (Qty3 != 0) xpReceived += fish.xp3! * Qty3;
+
+		if (fish.name === 'Barbarian fishing') {
+			agilityXpReceived += 7 * Qty3 + 6 * Qty2 + 5 * Qty1;
+			strengthXpReceived += 7 * Qty3 + 6 * Qty2 + 5 * Qty1;
+		}
+
+
+		// If they have the entire angler outfit, give an extra 0.5% xp bonus
+		let bonusXP = 0;
+		if (
+			user.gear.skilling.hasEquipped(
+				Object.keys(Fishing.anglerItems).map(i => Number.parseInt(i)),
+				true
+			)
+		) {
+			const amountToAdd = Math.floor(xpReceived * (2.5 / 100));
+			xpReceived += amountToAdd;
+			bonusXP += amountToAdd;
+		} else {
+			// For each angler item, check if they have it, give its' XP boost if so.
+			for (const [itemID, bonus] of Object.entries(Fishing.anglerItems)) {
+				if (user.hasEquipped(Number.parseInt(itemID))) {
+					const amountToAdd = Math.floor(xpReceived * (bonus / 100));
+					xpReceived += amountToAdd;
+					bonusXP += amountToAdd;
+				}
+			}
+		}
+
+
+		let xpRes = await user.addXP({
+			skillName: SkillsEnum.Fishing,
+			amount: xpReceived,
+			duration
+		});
+		xpRes +=
+			agilityXpReceived > 0
+				? await user.addXP({
+					skillName: SkillsEnum.Agility,
+					amount: agilityXpReceived,
+					duration
+				})
+				: '';
+		xpRes +=
+			strengthXpReceived > 0
+				? await user.addXP({
+					skillName: SkillsEnum.Strength,
+					amount: strengthXpReceived,
+					duration
+				})
+				: '';
+
+
+		const loot = new Bank();
+		loot.add(fish.id3!, loot3);
+		loot.add(fish.id2!, loot2);
+
+		// handling stackable fish
+		if (fish.name === 'Minnow') {
+			let sum = 0;
+			for (let i = 0; i < loot1; i++) {
+				sum += Math.floor(Math.random() * (baseMinnow[1] - baseMinnow[0] + 1)) + baseMinnow[0];
+			}
+			loot1 = sum;
+		} else if (fish.name === 'Karambwanji') {
+			loot1 *= baseKarambwanji;
+		}
+		loot.add(fish!.id, loot1);
+
+		let str = ''
+
+		const totalCatches = Qty1 + Qty2 + Qty3;
+		str = `${user}, ${user.minionName} finished fishing ${totalCatches} ${fish.name}. ${xpRes}`;
+
+
+		const cost = new Bank();
+		if (spirit_flakes) {
+			cost.add('Spirit flakes', flakesToRemove);
+		}
+
+		if (fish.bait) {
+			cost.add(fish.bait, totalCatches);
+		}
+
+		await user.removeItemsFromBank(cost);
+
+		// Add clue scrolls
+		if (fish.clueScrollChance) {
+			addSkillingClueToLoot(user, SkillsEnum.Fishing, totalCatches, fish.clueScrollChance, loot);
+		}
+
+		const xpBonusPercent = anglerBoostPercent(user);
+		if (xpBonusPercent > 0) {
+			bonusXP += Math.ceil(calcPercentOfNum(xpBonusPercent, xpReceived));
+		}
+
+		if (bonusXP > 0) {
+			str += `\n\n**Bonus XP:** ${bonusXP.toLocaleString()}`;
+		}
+
+		// Roll for pet
+		if (fish.petChance) {
+			const { petDropRate } = skillingPetDropRate(user, SkillsEnum.Fishing, fish.petChance);
+			for (let i = 0; i < totalCatches; i++) {
+				if (roll(petDropRate)) {
+					loot.add('Heron');
+					str += "\nYou have a funny feeling you're being followed...";
+					globalClient.emit(
+						Events.ServerNotification,
+						`${Emoji.Fishing} **${user.badgedUsername}'s** minion, ${user.minionName}, just received a Heron while fishing ${fish.name} at level ${fishLvl} Fishing!`
+					);
+				}
+			}
+		}
+
+		// bigFishQuantity add this 
+		if (fish.bigFishRate && fish.bigFish) {
+			let bigFishQuantity = 0;
+			if (fish.name === 'Shark') {
+				bigFishQuantity = Qty1;
+			}
+			if (fish.name === 'Tuna/Swordfish') {
+				bigFishQuantity = Qty2;
+			}
+			if (fish.name === 'Mackerel/Cod/Bass') {
+				bigFishQuantity = Qty3;
+			}
+			for (let i = 0; i < bigFishQuantity!; i++) {
+				if (roll(fish.bigFishRate)) {
+					loot.add(fish.bigFish);
+				}
+			}
+		}
+
+
+		await transactItems({
+			userID: user.id,
+			collectionLog: true,
+			itemsToAdd: loot
+		});
+
+		str += `\n\nYou received: ${loot}.`;
+
+		handleTripFinish(user, channelID, str, undefined, data, loot);
+	}
+}
+
---
+++ minions.ts
@@ -0,0 +1,622 @@
+import type { CropUpgradeType } from '@prisma/client';
+
+import type { ItemBank } from '.';
+import type { NMZStrategy, TwitcherGloves, UnderwaterAgilityThievingTrainingSkill } from '../constants';
+import type { IPatchData } from '../minions/farming/types';
+import type { MinigameName } from '../settings/minigames';
+import type { RaidLevel } from '../simulation/toa';
+import type { Peak } from '../tickers';
+import type { BirdhouseData } from './../skilling/skills/hunter/defaultBirdHouseTrap';
+
+export interface ActivityTaskOptions {
+	userID: string;
+	duration: number;
+	id: number;
+	finishDate: number;
+	channelID: string;
+}
+
+export interface ActivityTaskOptionsWithNoChanges extends ActivityTaskOptions {
+	type:
+	| 'Questing'
+	| 'Wintertodt'
+	| 'Cyclops'
+	| 'GloryCharging'
+	| 'WealthCharging'
+	| 'BarbarianAssault'
+	| 'AgilityArena'
+	| 'ChampionsChallenge'
+	| 'AerialFishing'
+	| 'DriftNet'
+	| 'SoulWars'
+	| 'RoguesDenMaze'
+	| 'CastleWars'
+	| 'MageArena'
+	| 'MageTrainingArena'
+	| 'BlastFurnace'
+	| 'MageArena2'
+	| 'BigChompyBirdHunting'
+	| 'PestControl'
+	| 'VolcanicMine'
+	| 'TearsOfGuthix'
+	| 'LastManStanding'
+	| 'BirthdayEvent'
+	| 'TroubleBrewing'
+	| 'Easter'
+	| 'ShootingStars'
+	| 'HalloweenEvent'
+	| 'StrongholdOfSecurity'
+	| 'CombatRing';
+}
+
+export interface ActivityTaskOptionsWithQuantity extends ActivityTaskOptions {
+	type:
+	| 'VolcanicMine'
+	| 'Cyclops'
+	| 'ShootingStars'
+	| 'DriftNet'
+	| 'WealthCharging'
+	| 'GloryCharging'
+	| 'AerialFishing'
+	| 'FishingTrawler'
+	| 'CamdozaalFishing'
+	| 'CamdozaalMining'
+	| 'CamdozaalSmithing';
+	quantity: number;
+	// iQty is 'input quantity.' This is the number specified at command time, so we can accurately repeat such trips.
+	iQty?: number;
+}
+
+export interface ShootingStarsOptions extends ActivityTaskOptions {
+	type: 'ShootingStars';
+	size: number;
+	usersWith: number;
+	totalXp: number;
+	lootItems: ItemBank;
+}
+interface ActivityTaskOptionsWithUsers extends ActivityTaskOptions {
+	users: string[];
+}
+
+export interface RunecraftActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Runecraft';
+	runeID: number;
+	essenceQuantity: number;
+	imbueCasts: number;
+	useStaminas?: boolean;
+	daeyaltEssence?: boolean;
+}
+
+export interface TiaraRunecraftActivityTaskOptions extends ActivityTaskOptions {
+	type: 'TiaraRunecraft';
+	tiaraID: number;
+	tiaraQuantity: number;
+}
+
+export interface DarkAltarOptions extends ActivityTaskOptions {
+	type: 'DarkAltar';
+	quantity: number;
+	hasElite: boolean;
+	rune: 'blood' | 'soul';
+}
+
+export interface AgilityActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Agility';
+	courseID: string;
+	quantity: number;
+	alch: {
+		itemID: number;
+		quantity: number;
+	} | null;
+}
+
+export interface CookingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Cooking';
+	cookableID: number;
+	quantity: number;
+}
+
+export interface ConstructionActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Construction';
+	objectID: number;
+	quantity: number;
+}
+
+export interface MonsterActivityTaskOptions extends ActivityTaskOptions {
+	type: 'MonsterKilling';
+	monsterID: number;
+	quantity: number;
+	iQty?: number;
+	usingCannon?: boolean;
+	cannonMulti?: boolean;
+	chinning?: boolean;
+	burstOrBarrage?: number;
+	died?: boolean;
+	pkEncounters?: number;
+	hasWildySupplies?: boolean;
+	isInWilderness?: boolean;
+}
+
+export interface ClueActivityTaskOptions extends ActivityTaskOptions {
+	type: 'ClueCompletion';
+
+	clueID: number;
+	quantity: number;
+	implingID?: number;
+	implingClues?: number;
+}
+
+export interface FishingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Fishing';
+	fishID: number;
+	quantity?: number;
+	Qty1: number;
+	Qty2?: number;
+	Qty3?: number;
+	loot1?: number;
+	loot2?: number;
+	loot3?: number;
+	flakesToRemove?: number;
+	powerfish?: boolean;
+	spirit_flakes?: boolean;
+}
+
+
+export interface MiningActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Mining';
+	fakeDurationMax: number;
+	fakeDurationMin: number;
+	oreID: number;
+	quantity: number;
+	powermine: boolean;
+	iQty?: number;
+}
+
+export interface MotherlodeMiningActivityTaskOptions extends ActivityTaskOptions {
+	type: 'MotherlodeMining';
+	fakeDurationMax: number;
+	fakeDurationMin: number;
+	quantity: number;
+	iQty?: number;
+}
+
+export interface SmeltingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Smelting';
+	barID: number;
+	quantity: number;
+	blastf: boolean;
+}
+
+export interface SmithingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Smithing';
+	smithedBarID: number;
+	quantity: number;
+}
+
+export interface FiremakingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Firemaking';
+	burnableID: number;
+	quantity: number;
+}
+
+export interface WoodcuttingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Woodcutting';
+	fakeDurationMax: number;
+	fakeDurationMin: number;
+	powerchopping: boolean;
+	forestry?: boolean;
+	twitchers?: TwitcherGloves;
+	logID: number;
+	quantity: number;
+	iQty?: number;
+}
+
+export interface CraftingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Crafting';
+	craftableID: number;
+	quantity: number;
+}
+
+export interface FletchingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Fletching';
+	fletchableName: string;
+	quantity: number;
+}
+
+export interface EnchantingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Enchanting';
+	itemID: number;
+	quantity: number;
+}
+
+export interface CastingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Casting';
+	spellID: number;
+	quantity: number;
+}
+export interface PickpocketActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Pickpocket';
+	monsterID: number;
+	quantity: number;
+	xpReceived: number;
+	successfulQuantity: number;
+	damageTaken: number;
+}
+
+export interface BuryingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Burying';
+	boneID: number;
+	quantity: number;
+}
+
+export interface ScatteringActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Scattering';
+	ashID: number;
+	quantity: number;
+}
+
+export interface OfferingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Offering';
+	boneID: number;
+	quantity: number;
+}
+
+export interface AnimatedArmourActivityTaskOptions extends ActivityTaskOptions {
+	type: 'AnimatedArmour';
+	armourID: string;
+	quantity: number;
+}
+
+export interface HerbloreActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Herblore';
+	mixableID: number;
+	quantity: number;
+	zahur: boolean;
+	wesley: boolean;
+}
+
+export interface CutLeapingFishActivityTaskOptions extends ActivityTaskOptions {
+	type: 'CutLeapingFish';
+	fishID: number;
+	quantity: number;
+}
+
+export interface HunterActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Hunter';
+	creatureName: string;
+	quantity: number;
+	usingHuntPotion: boolean;
+	wildyPeak: Peak | null;
+	usingStaminaPotion: boolean;
+}
+
+export interface AlchingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Alching';
+	itemID: number;
+	quantity: number;
+	alchValue: number;
+}
+
+export interface FightCavesActivityTaskOptions extends ActivityTaskOptions {
+	type: 'FightCaves';
+	jadDeathChance: number;
+	preJadDeathChance: number;
+	preJadDeathTime: number | null;
+	fakeDuration: number;
+	quantity: number;
+}
+export interface InfernoOptions extends ActivityTaskOptions {
+	type: 'Inferno';
+	zukDeathChance: number;
+	preZukDeathChance: number;
+	deathTime: number | null;
+	fakeDuration: number;
+	diedZuk: boolean;
+	diedPreZuk: boolean;
+	cost: ItemBank;
+}
+
+export interface FarmingActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Farming';
+	pid?: number;
+	plantsName: string | null;
+	quantity: number;
+	upgradeType: CropUpgradeType | null;
+	payment?: boolean;
+	patchType: IPatchData;
+	planting: boolean;
+	currentDate: number;
+	autoFarmed: boolean;
+}
+
+export interface BirdhouseActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Birdhouse';
+	birdhouseName: string | null;
+	placing: boolean;
+	gotCraft: boolean;
+	birdhouseData: BirdhouseData;
+	currentDate: number;
+}
+
+interface MinigameActivityTaskOptions extends ActivityTaskOptions {
+	minigameID: MinigameName;
+	quantity: number;
+}
+
+export interface MinigameActivityTaskOptionsWithNoChanges extends MinigameActivityTaskOptions {
+	type:
+	| 'Wintertodt'
+	| 'TroubleBrewing'
+	| 'TearsOfGuthix'
+	| 'SoulWars'
+	| 'RoguesDenMaze'
+	| 'MageTrainingArena'
+	| 'LastManStanding'
+	| 'BigChompyBirdHunting'
+	| 'FishingTrawler'
+	| 'PestControl'
+	| 'BarbarianAssault'
+	| 'ChampionsChallenge'
+	| 'CastleWars'
+	| 'AgilityArena'
+	| 'GiantsFoundry';
+}
+
+export interface MahoganyHomesActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'MahoganyHomes';
+	xp: number;
+	quantity: number;
+	points: number;
+	tier: number;
+}
+
+export interface NightmareActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Nightmare';
+	method: 'solo' | 'mass';
+	quantity: number;
+	isPhosani?: boolean;
+}
+
+export interface TemporossActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'Tempoross';
+	quantity: number;
+	rewardBoost: number;
+}
+
+export interface TitheFarmActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'TitheFarm';
+}
+
+export interface SepulchreActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'Sepulchre';
+	floors: number[];
+}
+
+export interface PlunderActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'Plunder';
+	rooms: number[];
+}
+
+export interface ZalcanoActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Zalcano';
+	isMVP: boolean;
+	performance: number;
+	quantity: number;
+}
+
+export interface TempleTrekkingActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'Trekking';
+	difficulty: string;
+}
+
+export interface SawmillActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Sawmill';
+	plankID: number;
+	plankQuantity: number;
+}
+
+export interface ButlerActivityTaskOptions extends ActivityTaskOptions {
+	type: 'Butler';
+	plankID: number;
+	plankQuantity: number;
+}
+
+export interface GnomeRestaurantActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'GnomeRestaurant';
+	gloriesRemoved: number;
+}
+
+export interface GauntletOptions extends ActivityTaskOptions {
+	type: 'Gauntlet';
+	corrupted: boolean;
+	quantity: number;
+}
+
+export interface GroupMonsterActivityTaskOptions extends Omit<MonsterActivityTaskOptions, 'type'> {
+	type: 'GroupMonsterKilling';
+	leader: string;
+	users: string[];
+}
+
+export interface RaidsOptions extends ActivityTaskOptionsWithUsers {
+	type: 'Raids';
+	leader: string;
+	users: string[];
+	challengeMode: boolean;
+	quantity?: number;
+}
+
+export interface TheatreOfBloodTaskOptions extends ActivityTaskOptionsWithUsers {
+	type: 'TheatreOfBlood';
+	leader: string;
+	users: string[];
+	hardMode: boolean;
+	fakeDuration: number;
+	wipedRooms: (null | number)[];
+	deaths: number[][][];
+	quantity: number;
+	solo?: boolean;
+}
+
+export interface ColoTaskOptions extends ActivityTaskOptions {
+	type: 'Colosseum';
+	fakeDuration: number;
+	diedAt?: number;
+	loot?: ItemBank;
+	maxGlory: number;
+}
+
+type UserID = string;
+type Points = number;
+type RoomIDsDiedAt = number[];
+
+type TOAUser = [UserID, Points[], RoomIDsDiedAt[]];
+export interface TOAOptions extends ActivityTaskOptionsWithUsers {
+	type: 'TombsOfAmascut';
+	leader: string;
+	detailedUsers: TOAUser[] | [UserID, Points, RoomIDsDiedAt][][];
+	raidLevel: RaidLevel;
+	fakeDuration: number;
+	wipedRoom: null | number | (number | null)[];
+	quantity: number;
+}
+
+export interface NexTaskOptions extends ActivityTaskOptionsWithUsers {
+	type: 'Nex';
+	quantity: number;
+	leader: string;
+	userDetails: [string, number, number[]][];
+	fakeDuration: number;
+	wipedKill: number | null;
+}
+
+export interface CollectingOptions extends ActivityTaskOptions {
+	type: 'Collecting';
+	collectableID: number;
+	quantity: number;
+	noStaminas?: boolean;
+}
+
+export interface KourendFavourActivityTaskOptions extends ActivityTaskOptions {
+	type: 'KourendFavour';
+	favour: string;
+	quantity: number;
+}
+
+export interface TokkulShopOptions extends ActivityTaskOptions {
+	type: 'TokkulShop';
+	itemID: number;
+	quantity: number;
+}
+
+export interface UnderwaterAgilityThievingTaskOptions extends ActivityTaskOptions {
+	type: 'UnderwaterAgilityThieving';
+	trainingSkill: UnderwaterAgilityThievingTrainingSkill;
+	quantity: number;
+	noStams: boolean;
+}
+
+export interface PuroPuroActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'PuroPuro';
+	quantity: number;
+	darkLure: boolean;
+	implingTier: number | null;
+}
+
+export interface GiantsFoundryActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'GiantsFoundry';
+	alloyID: number;
+	quantity: number;
+	metalScore: number;
+}
+
+export interface GuardiansOfTheRiftActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'GuardiansOfTheRift';
+	minedFragments: number;
+	barrierAndGuardian: number;
+	rolls: number;
+	combinationRunes: boolean;
+}
+
+export interface NightmareZoneActivityTaskOptions extends MinigameActivityTaskOptions {
+	type: 'NightmareZone';
+	strategy: NMZStrategy;
+	quantity: number;
+}
+
+export interface ShadesOfMortonOptions extends MinigameActivityTaskOptions {
+	type: 'ShadesOfMorton';
+	shadeID: string;
+	logID: number;
+}
+export interface SpecificQuestOptions extends ActivityTaskOptions {
+	type: 'SpecificQuest';
+	questID: number;
+}
+
+export type ActivityTaskData =
+	| MonsterActivityTaskOptions
+	| WoodcuttingActivityTaskOptions
+	| CollectingOptions
+	| RaidsOptions
+	| GauntletOptions
+	| CastingActivityTaskOptions
+	| EnchantingActivityTaskOptions
+	| ConstructionActivityTaskOptions
+	| HunterActivityTaskOptions
+	| ZalcanoActivityTaskOptions
+	| SawmillActivityTaskOptions
+	| ButlerActivityTaskOptions
+	| FarmingActivityTaskOptions
+	| HerbloreActivityTaskOptions
+	| FletchingActivityTaskOptions
+	| RunecraftActivityTaskOptions
+	| TempleTrekkingActivityTaskOptions
+	| TemporossActivityTaskOptions
+	| PuroPuroActivityTaskOptions
+	| KourendFavourActivityTaskOptions
+	| AgilityActivityTaskOptions
+	| InfernoOptions
+	| TOAOptions
+	| NexTaskOptions
+	| ZalcanoActivityTaskOptions
+	| TheatreOfBloodTaskOptions
+	| GuardiansOfTheRiftActivityTaskOptions
+	| GiantsFoundryActivityTaskOptions
+	| NightmareZoneActivityTaskOptions
+	| ShadesOfMortonOptions
+	| UnderwaterAgilityThievingTaskOptions
+	| PickpocketActivityTaskOptions
+	| BuryingActivityTaskOptions
+	| ScatteringActivityTaskOptions
+	| OfferingActivityTaskOptions
+	| AnimatedArmourActivityTaskOptions
+	| CookingActivityTaskOptions
+	| CraftingActivityTaskOptions
+	| FiremakingActivityTaskOptions
+	| FishingActivityTaskOptions
+	| MiningActivityTaskOptions
+	| MotherlodeMiningActivityTaskOptions
+	| PlunderActivityTaskOptions
+	| SmithingActivityTaskOptions
+	| SmeltingActivityTaskOptions
+	| TiaraRunecraftActivityTaskOptions
+	| ClueActivityTaskOptions
+	| AlchingActivityTaskOptions
+	| DarkAltarOptions
+	| GroupMonsterActivityTaskOptions
+	| MahoganyHomesActivityTaskOptions
+	| NightmareActivityTaskOptions
+	| TitheFarmActivityTaskOptions
+	| SepulchreActivityTaskOptions
+	| GnomeRestaurantActivityTaskOptions
+	| SpecificQuestOptions
+	| ActivityTaskOptionsWithNoChanges
+	| TokkulShopOptions
+	| BirdhouseActivityTaskOptions
+	| FightCavesActivityTaskOptions
+	| ActivityTaskOptionsWithQuantity
+	| MinigameActivityTaskOptionsWithNoChanges
+	| CutLeapingFishActivityTaskOptions
+	| ColoTaskOptions;
+
