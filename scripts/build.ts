606498b056d3a218ef857787753272a4c3398895
gc <30398469+gc@users.noreply.github.com>
2024-10-02 12:19:59 +1000

    Lint

--- scripts/build.ts
+++ scripts/build.ts
@@ -7,11 +7,11 @@ import fg from 'fast-glob';
 
 import { production } from '../src/config';
 import { BOT_TYPE } from '../src/lib/constants';
+import { customItems } from '../src/lib/customItems/util.js';
 import { getSystemInfo } from '../src/lib/systemInfo';
+import { itemNameFromID } from '../src/lib/util.js';
 import { renderCreatablesFile } from './renderCreatablesFile.js';
 import { execAsync, runTimedLoggedFn } from './scriptUtil.js';
-import { customItems } from '../src/lib/customItems/util.js';
-import { itemNameFromID } from '../src/lib/util.js';
 
 const args = process.argv.slice(2);
 
@@ -127,10 +127,21 @@ async function main() {
 	await runTimedLoggedFn('Typescript Compilation', handleTypescriptCompilation);
 	await runTimedLoggedFn('Post Build', () => Promise.all([handleCommandsJSON()]));
 	renderCreatablesFile();
-	writeFileSync('data/bso_items.json', JSON.stringify(customItems.reduce((acc, id) => {
-	acc[id] = itemNameFromID(id)!;
-	return acc;
-}, {} as Record<number, string>), null, 4), 'utf-8');
+	writeFileSync(
+		'data/bso_items.json',
+		JSON.stringify(
+			customItems.reduce(
+				(acc, id) => {
+					acc[id] = itemNameFromID(id)!;
+					return acc;
+				},
+				{} as Record<number, string>
+			),
+			null,
+			4
+		),
+		'utf-8'
+	);
 }
 
 runTimedLoggedFn('Build', main);
