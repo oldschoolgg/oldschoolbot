9bb3be780ab372e93ce3b3d0b91018ecdbf9df5e
gc <30398469+gc@users.noreply.github.com>
2024-10-02 10:55:48 +1000

    Add authors to docs pages

---
+++ docs/src/plugins/authors.ts
@@ -0,0 +1,27 @@
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+
+const execAsync = promisify(exec);
+interface Author {
+	gitIDs: string[];
+	displayName: string;
+}
+const authors: Author[] = [{ gitIDs: ['GC'], displayName: 'Magnaboy' }];
+const authorsMap = new Map<string, Author>();
+
+for (const author of authors) {
+	for (const gitID of author.gitIDs) {
+		authorsMap.set(gitID, author);
+	}
+}
+
+export async function getAuthors(filePath: string) {
+	const command = `git log --pretty=format:"%an" "${filePath}"`;
+	const result = await execAsync(command, { encoding: 'utf-8' });
+	const authors = result.stdout
+		.split('\n')
+		.filter((v, i, a) => a.indexOf(v) === i)
+		.map(author => authorsMap.get(author)?.displayName ?? author);
+	if (authors.length === 1 && authors[0] === 'Magnaboy') return [];
+	return authors;
+}
