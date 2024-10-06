3fa84174efb8bbcfa9d75c40567e35f1134a9fd8
gc <30398469+gc@users.noreply.github.com>
2024-10-06 17:14:15 +1100

    Various wiki improvements/updates

--- scripts/wiki.ts
+++ scripts/wiki.ts
@@ -248,7 +248,76 @@ async function renderMonstersMarkdown() {
 	handleMarkdownEmbed('monsters', 'osb/monsters.mdx', markdown.toString());
 }
 
+function renderQuestsMarkdown() {
+	const markdown = new Markdown();
+
+	for (const quest of quests.sort((a, b) => a.name.localeCompare(b.name))) {
+		const questMarkdown = new Markdown();
+		questMarkdown.addLine(`## ${quest.name}`);
+		questMarkdown.addLine(
+			`You can send your minion to do this quest using [[/activities quest name:${quest.name}]]`
+		);
+
+		if (quest.skillReqs) {
+			questMarkdown.addLine('### Skill Requirements');
+			questMarkdown.addLine(
+				`- Skills: ${Object.entries(quest.skillReqs)
+					.map(([skill, lvl]) => `[[${skill}:${lvl}]]`)
+					.join(' ')}`
+			);
+		}
+		if (quest.ironmanSkillReqs) {
+			questMarkdown.addLine('### Ironman Skill Requirements');
+			questMarkdown.addLine(
+				`${Object.entries(quest.ironmanSkillReqs)
+					.map(([skill, lvl]) => `[[${skill}:${lvl}]]`)
+					.join(' ')}`
+			);
+		}
+
+		if (quest.prerequisitesQuests) {
+			questMarkdown.addLine('### Required Quests');
+			for (const req of quest.prerequisitesQuests) {
+				questMarkdown.addLine(`- Must have finished ${quests.find(q => q.id === req)!.name}`);
+			}
+		}
+
+		if (quest.combatLevelReq || quest.qpReq) {
+			questMarkdown.addLine('### Other requirements');
+			if (quest.combatLevelReq) {
+				questMarkdown.addLine(`- Combat Level requirement: ${quest.combatLevelReq}`);
+			}
+			if (quest.qpReq) {
+				questMarkdown.addLine(`- Quest Points requirement: [[qp:${quest.qpReq}]]`);
+			}
+		}
+
+		if (quest.rewards) {
+			questMarkdown.addLine('### Item Rewards');
+			questMarkdown.addLine(
+				quest.rewards
+					.items()
+					.map(([item]) => `[[${item.id}]]`)
+					.join(' ')
+			);
+		}
+		if (quest.skillsRewards) {
+			questMarkdown.addLine('### XP Rewards');
+			questMarkdown.addLine(
+				Object.entries(quest.skillsRewards)
+					.map(([skill, xp]) => `[[${skill}:${xp.toLocaleString()}]]`)
+					.join(' ')
+			);
+		}
+
+		markdown.add(questMarkdown);
+	}
+
+	handleMarkdownEmbed('quests', 'osb/quests.mdx', markdown.toString());
+}
+
 async function wiki() {
+	renderQuestsMarkdown();
 	await Promise.all([renderCAMarkdown(), renderMonstersMarkdown()]);
 }
 
