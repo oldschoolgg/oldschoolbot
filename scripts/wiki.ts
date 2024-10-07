780523d40912c8f84b039ec016f7bc4fc27ea9d6
gc <30398469+gc@users.noreply.github.com>
2024-10-07 13:40:44 +1100

    Update cox wiki page

--- scripts/wiki.ts
+++ scripts/wiki.ts
@@ -2,6 +2,7 @@ import { readFileSync, writeFileSync } from 'node:fs';
 
 import '../src/lib/safeglobals';
 import { type CombatAchievement, CombatAchievements } from '../src/lib/combat_achievements/combatAchievements';
+import { COXMaxMageGear, COXMaxMeleeGear, COXMaxRangeGear, itemBoosts } from '../src/lib/data/cox';
 import killableMonsters from '../src/lib/minions/data/killableMonsters';
 import { quests } from '../src/lib/minions/data/quests';
 import { sorts } from '../src/lib/sorts';
@@ -316,8 +317,59 @@ function renderQuestsMarkdown() {
 	handleMarkdownEmbed('quests', 'osb/quests.mdx', markdown.toString());
 }
 
+function rendeCoxMarkdown() {
+	const markdown = new Markdown();
+
+	markdown.addLine('## Gear');
+	markdown.addLine('This is the best-in-slot gear you should use for CoX, substitute the next best items you have. ');
+	for (const gear of [
+		['mage', 'Magic Damage', COXMaxMageGear],
+		['range', 'Ranged Strength', COXMaxRangeGear],
+		['melee', 'Melee Strength', COXMaxMeleeGear]
+	] as const) {
+		markdown.addLine(`### ${toTitleCase(gear[0])}`);
+		markdown.addLine(`For ${gear[0]}, use these items, or the next best '${gear[1]}' gear you have:`);
+		markdown.addLine(
+			`- ${gear[2]
+				.allItems(false)
+				.map(id => `[[${id}]]`)
+				.join(' ')}`
+		);
+	}
+
+	markdown.addLine('## Boosts');
+	markdown.addLine(`Higher Kc makes raids faster. Here is the maximum kc that will give a boost:
+
+| Difficulty | Solo Kc | Mass Kc |
+| ---------- | ------- | ------- |
+| Normal     | 250     | 400     |
+| Challenge  | 75      | 100     |
+
+`);
+	for (const boostSet of itemBoosts) {
+		markdown.addLine(
+			`- ${boostSet
+				.map(boost => {
+					const messages = [];
+					if (!boost.mustBeEquipped) {
+						messages.push('Works from bank');
+					}
+					if (boost.mustBeCharged) {
+						messages.push('Must be charged');
+					}
+					const msgStr = messages.length > 0 ? ` (${messages.join(', ')})` : '';
+					return `${boost.boost}% boost for [[${boost.item.name}]]${msgStr}`;
+				})
+				.join(' or ')}`
+		);
+	}
+
+	handleMarkdownEmbed('cox', 'osb/Raids/cox.mdx', markdown.toString());
+}
+
 async function wiki() {
 	renderQuestsMarkdown();
+	rendeCoxMarkdown();
 	await Promise.all([renderCAMarkdown(), renderMonstersMarkdown()]);
 }
 
