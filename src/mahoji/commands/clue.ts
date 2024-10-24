52588e1a6f1f898c242d7c31a55b09ef5bd6c1b6
Badora II <79532596+Arodab@users.noreply.github.com>
2024-10-23 22:00:13 -0300

    revert function name

--- src/mahoji/commands/clue.ts
+++ src/mahoji/commands/clue.ts
@@ -162,7 +162,7 @@ export const clueCommand: OSBMahojiCommand = {
 		}
 
 		// Global Boosts
-		const clueBoosts = [
+		const globalBoosts = [
 			{
 				condition: isWeekend,
 				boost: '10% for Weekend',
@@ -195,7 +195,7 @@ export const clueCommand: OSBMahojiCommand = {
 			}
 		];
 
-		for (const { condition, boost, durationMultiplier } of clueBoosts) {
+		for (const { condition, boost, durationMultiplier } of globalBoosts) {
 			if (condition()) {
 				boosts.push(boost);
 				duration *= durationMultiplier;
