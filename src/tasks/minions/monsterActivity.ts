907e2ae76795bc327843101d65f60220338e284a
DayV <Ashleyjd22@gmail.com>
2024-11-05 11:22:09 +0000

    lint

--- src/tasks/minions/monsterActivity.ts
+++ src/tasks/minions/monsterActivity.ts
@@ -286,7 +286,8 @@ export function doMonsterTrip(data: newOptions) {
 	});
 
 	const superiorTable = slayerContext.hasSuperiorsUnlocked && monster.superior ? monster.superior : undefined;
-	const isInCatacombs = (!(usingCannon || cannonMulti) ? (monster.existsInCatacombs ?? undefined) : undefined) && !isInWilderness;
+	const isInCatacombs =
+		(!(usingCannon || cannonMulti) ? (monster.existsInCatacombs ?? undefined) : undefined) && !isInWilderness;
 
 	const hasRingOfWealthI = gearBank.gear.wildy.hasEquipped('Ring of wealth (i)') && isInWilderness;
 	if (hasRingOfWealthI) {
