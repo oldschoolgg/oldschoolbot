cfd1e964cef1de757c7695af36faf2b16cde633f
DayV <Ashleyjd22@gmail.com>
2024-11-24 19:54:59 +0000

    lint

--- packages/oldschooljs/src/structures/SimpleMonster.ts
+++ packages/oldschooljs/src/structures/SimpleMonster.ts
@@ -1,17 +1,17 @@
-import { roll } from "e";
+import { roll } from 'e';
 
-import { MonsterSlayerMaster } from "../meta/monsterData";
-import type { CustomKillLogic, MonsterKillOptions, MonsterOptions } from "../meta/types";
+import { MonsterSlayerMaster } from '../meta/monsterData';
+import type { CustomKillLogic, MonsterKillOptions, MonsterOptions } from '../meta/types';
 import {
 	getAncientShardChanceFromHP,
 	getBrimKeyChanceFromCBLevel,
 	getLarranKeyChanceFromCBLevel,
 	getSlayersEnchantmentChanceFromHP,
-	getTotemChanceFromHP,
-} from "../util/util";
-import Bank from "./Bank";
-import LootTable from "./LootTable";
-import Monster from "./Monster";
+	getTotemChanceFromHP
+} from '../util/util';
+import Bank from './Bank';
+import LootTable from './LootTable';
+import Monster from './Monster';
 
 interface SimpleMonsterOptions extends MonsterOptions {
 	table?: LootTable | LootTable[];
@@ -55,7 +55,7 @@ export default class SimpleMonster extends Monster {
 		);
 		const lootTableOptions = {
 			...options.lootTableOptions,
-			targetBank: loot,
+			targetBank: loot
 		};
 		const rollTable = this.table
 			? this.table instanceof LootTable
@@ -76,7 +76,7 @@ export default class SimpleMonster extends Monster {
 		for (let i = 0; i < quantity; i++) {
 			if (canGetBrimKey) {
 				if (roll(getBrimKeyChanceFromCBLevel(this.data.combatLevel))) {
-					loot.add("Brimstone key");
+					loot.add('Brimstone key');
 				}
 			}
 			if (wildySlayer && this.data.hitpoints) {
@@ -89,11 +89,11 @@ export default class SimpleMonster extends Monster {
 			}
 			if (options.inCatacombs && this.data.hitpoints && !wildySlayer) {
 				if (roll(getAncientShardChanceFromHP(this.data.hitpoints))) {
-					loot.add("Ancient shard");
+					loot.add('Ancient shard');
 				}
 				if (roll(getTotemChanceFromHP(this.data.hitpoints))) {
 					// Always drop Dark totem base and bot will transmog accordingly.
-					loot.add("Dark totem base");
+					loot.add('Dark totem base');
 				}
 			}
 			if (options.onSlayerTask) {
