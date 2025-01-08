83c4ba06f39a479f5499d680db5efa58be2b76ed
DayV <69717452+DayV-git@users.noreply.github.com>
2025-01-08 07:26:10 +0000

    Various small 2024 updates (#6275)

--- packages/oldschooljs/src/simulation/monsters/low/a-f/FeverSpider.ts
+++ packages/oldschooljs/src/simulation/monsters/low/a-f/FeverSpider.ts
@@ -1,7 +1,18 @@
 import LootTable from '../../../../structures/LootTable';
 import SimpleMonster from '../../../../structures/SimpleMonster';
+import { GemTable } from '../../../subtables';
 
-const FeverSpiderTable = new LootTable({ limit: 120 }).add('Grimy kwuarm', 10);
+const FeverSpiderTable = new LootTable({ limit: 36 })
+	.add("Red d'hide chaps", 1, 3)
+	.add('Adamant axe', 1, 2)
+	.add('Adamant med helm', 1, 2)
+	.add('Adamant battleaxe', 1, 1)
+	.add('Grimy kwuarm', 6)
+	.add('Coins', [200, 600], 8)
+	.add('Bass', [1, 2], 7)
+	.add('Limpwurt root', 1, 4)
+	.add('Pure essence', [100, 200], 2)
+	.add(GemTable, 1, 1);
 
 export default new SimpleMonster({
 	id: 626,
