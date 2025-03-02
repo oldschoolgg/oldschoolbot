8576c5de5835bd4523d2bf088e4b6f3386c044ed
gc <30398469+gc@users.noreply.github.com>
2025-03-02 12:39:04 +1100

    Remove ascii-table3, move Table to toolkit

--- scripts/renderCreatablesFile.ts
+++ scripts/renderCreatablesFile.ts
@@ -1,17 +1,19 @@
 import { writeFile } from 'node:fs/promises';
+import { Table } from '@oldschoolgg/toolkit';
 import { Bank } from 'oldschooljs';
 import '../src/lib/safeglobals';
 
 import Createables from '../src/lib/data/createables';
-import { makeTable } from '../src/lib/util';
 
 async function renderCreatablesFile() {
-	const headers = ['Item name', 'Input Items', 'Output Items', 'GP Cost'];
-	const rows = Createables.map(i => {
-		return [i.name, `${new Bank(i.inputItems)}`, `${new Bank(i.outputItems)}`, `${i.GPCost ?? 0}`];
-	});
+	const table = new Table();
+	table.addHeader('Item name', 'Input Items', 'Output Items', 'GP Cost');
 
-	await writeFile('./src/lib/data/creatablesTable.txt', makeTable(headers, rows));
+	for (const i of Createables) {
+		table.addRow(i.name, `${new Bank(i.inputItems)}`, `${new Bank(i.outputItems)}`, `${i.GPCost ?? 0}`);
+	}
+
+	await writeFile('./src/lib/data/creatablesTable.txt', table.toString());
 }
 
 renderCreatablesFile();
