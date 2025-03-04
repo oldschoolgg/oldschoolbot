d7b260448950020b40728bf2b1ecff1d281d8f32
gc <30398469+gc@users.noreply.github.com>
2025-03-04 13:59:38 +1100

    Make drygores drop in order, half chitin droprate

--- src/lib/minions/data/killableMonsters/custom/customMonsters.ts
+++ src/lib/minions/data/killableMonsters/custom/customMonsters.ts
@@ -4,6 +4,7 @@ import type { MonsterData } from 'oldschooljs/dist/meta/monsterData';
 import setCustomMonster, { makeKillTable } from '../../../../util/setCustomMonster';
 import type { KillableMonster } from '../../../types';
 import { SunMoonMonsters } from './SunMoon';
+import { KalphiteKingMonster } from './bosses/KalphiteKing.js';
 import { customDemiBosses } from './demiBosses';
 import { MiscCustomMonsters } from './misc';
 import { resourceDungeonMonsters } from './resourceDungeons';
@@ -17,7 +18,7 @@ export interface CustomMonster extends Readonly<Omit<Readonly<KillableMonster>, 
 	isCustom: true;
 }
 
-export const customKillableMonsters: KillableMonster[] = [];
+export const customKillableMonsters: KillableMonster[] = [KalphiteKingMonster];
 
 export const BSOMonsters = {
 	...customDemiBosses,
