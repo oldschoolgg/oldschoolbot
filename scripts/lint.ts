03f40960ea8b3cc2d2356f8c486df1c8dfdc9c53
gc <30398469+gc@users.noreply.github.com>
2025-04-27 13:44:12 +1000

    Simplify/improve lint script

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,41 +1,11 @@
-import { execSync } from 'node:child_process';
+import { execAsync } from './scriptUtil.ts';
 
-const ignoredFiles = ['pnpm-lock.yaml'];
-
-try {
-	const changedFiles = execSync(
-		'git diff --name-only --diff-filter=AM HEAD && git ls-files --others --exclude-standard'
-	)
-		.toString()
-		.trim()
-		.split('\n');
-
-	if (changedFiles.length > 0) {
-		execSync(`biome check ${changedFiles.join(' ')} --write --unsafe --diagnostic-level=error`, {
-			stdio: 'inherit'
-		});
-
-		// Prettier
-		const prettierFiles = changedFiles.filter(f => /\.(md|yaml)$/.exec(f) && !ignoredFiles.includes(f));
-		if (prettierFiles.length) {
-			console.log(`Prettier is formatting: ${prettierFiles.join(', ')}`);
-			execSync(`prettier --use-tabs ${prettierFiles.join(' ')} --write --log-level silent`, {
-				stdio: 'inherit'
-			});
-		} else {
-			console.log('Nothing for prettier to format.');
-		}
-
-		// Prisma
-		if (changedFiles.some(f => f.includes('prisma/schema.prisma'))) {
-			console.log('Formatting Prisma schema files');
-			execSync(
-				'prisma format --schema ./prisma/robochimp.prisma && prisma format --schema ./prisma/schema.prisma',
-				{ stdio: 'inherit' }
-			);
-		}
-	}
-} catch (error) {
-	console.error('Error occurred while running biome lint:', error);
-	process.exit(1);
+async function lintScript() {
+	await Promise.all([
+		execAsync('prettier --use-tabs --write "**/*.json" && biome check --write --unsafe --diagnostic-level=error'),
+		execAsync('prettier --use-tabs --write "**/*.{yaml,yml,css,html,md,mdx}"'),
+		execAsync('prisma format --schema ./prisma/robochimp.prisma && prisma format --schema ./prisma/schema.prisma')
+	]);
 }
+
+lintScript();
