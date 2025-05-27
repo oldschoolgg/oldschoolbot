cdc01df1f82515f22223fd54ac075f0fdd51b2ae
nwjgit <nwjgit@gmail.com>
2025-05-26 21:46:06 -0500

    fix

--- src/lib/data/CollectionsExport.ts
+++ src/lib/data/CollectionsExport.ts
@@ -66,7 +66,7 @@ interface ICollectionActivity {
 		allItems?: number[];
 		kcActivity?: string | IKCActivity;
 		isActivity?: boolean;
-		isRaid?: true;
+		isRaid?: boolean;
 		fmtProg?: FormatProgressFunction;
 	};
 }
