4ef2ab6b23cd3f78551d59103fb106ff9cf27cac
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-06-03 08:10:57 +0100

    Allow Fletching at Sepulchre (#6399)

--- src/lib/skilling/skills/fletching/fletchables/slayer.ts
+++ src/lib/skilling/skills/fletching/fletchables/slayer.ts
@@ -82,4 +82,14 @@ const Slayer: Fletchable[] = [
 	}
 ];
 
-export default Slayer;
+const get = (id: number) => {
+	const found = Slayer.find(f => f.id === id);
+	if (!found) throw new Error(`Slayer fletchable with ID "${id}" not found.`);
+	return found;
+};
+
+const BroadArrows = get(itemID('Broad arrows'));
+const BroadBolts = get(itemID('Broad bolts'));
+const AmethystBroadBolts = get(itemID('Amethyst broad bolts'));
+
+export { Slayer, BroadArrows, BroadBolts, AmethystBroadBolts };
