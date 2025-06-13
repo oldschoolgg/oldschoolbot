bcfd73583c3fba6cfdfbde2d585d5054d1209729
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-06-13 14:14:44 +0100

    Add rune buyables for timed trips

--- src/lib/data/buyables/tripBuyables.ts
+++ src/lib/data/buyables/tripBuyables.ts
@@ -52,6 +52,62 @@ export const tripBuyables: Buyable[] = [
     outputItems: new Bank().add('Coal'),
     gpCost: 67,
     quantityPerHour: 11000
+  },
+  {
+    name: 'Blood rune',
+    outputItems: new Bank().add('Blood rune'),
+    gpCost: 400,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Law rune',
+    outputItems: new Bank().add('Law rune'),
+    gpCost: 240,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Soul rune',
+    outputItems: new Bank().add('Soul rune'),
+    gpCost: 300,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Astral rune',
+    outputItems: new Bank().add('Astral rune'),
+    gpCost: 50,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Death rune',
+    outputItems: new Bank().add('Death rune'),
+    gpCost: 180,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Nature rune',
+    outputItems: new Bank().add('Nature rune'),
+    gpCost: 180,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
+  },
+  {
+    name: 'Chaos rune',
+    outputItems: new Bank().add('Chaos rune'),
+    gpCost: 90,
+    quantityPerHour: 100_000,
+    shopQuantity: 1000,
+    changePer: 0.1
   }
 ];
 
