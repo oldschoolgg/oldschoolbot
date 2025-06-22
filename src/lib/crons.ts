c3adfcf31508c4849dcaa94d0eec3ab07715413d
gc <30398469+gc@users.noreply.github.com>
2025-06-22 23:28:21 +1000

    Explicitly cleanup crons, sonicboom, and sql in exit handler

--- src/lib/crons.ts
+++ src/lib/crons.ts
@@ -4,42 +4,54 @@ import { analyticsTick } from './analytics';
 import { cacheGEPrices } from './marketPrices';
 import { cacheCleanup } from './util/cachedUserIDs';
 
+export const crons = new Set<ReturnType<typeof schedule>>();
+
 export function initCrons() {
 	/**
 	 * Capture economy item data
 	 */
-	schedule('0 */6 * * *', async () => {
-		await prisma.$queryRawUnsafe(`INSERT INTO economy_item
+	crons.add(
+		schedule('0 */6 * * *', async () => {
+			await prisma.$queryRawUnsafe(`INSERT INTO economy_item
 SELECT item_id::integer, SUM(qty)::bigint FROM 
 (
     SELECT id, (jdata).key AS item_id, (jdata).value::text::bigint AS qty FROM (select id, json_each(bank) AS jdata FROM users) AS banks
 )
 AS DATA
 GROUP BY item_id;`);
-	});
+		})
+	);
 
 	/**
 	 * Analytics
 	 */
-	schedule('*/5 * * * *', () => {
-		return analyticsTick();
-	});
+	crons.add(
+		schedule('*/5 * * * *', () => {
+			return analyticsTick();
+		})
+	);
 
 	/**
 	 * prescence
 	 */
-	schedule('0 * * * *', () => {
-		globalClient.user?.setActivity('/help');
-	});
+	crons.add(
+		schedule('0 * * * *', () => {
+			globalClient.user?.setActivity('/help');
+		})
+	);
 
 	/**
 	 * Delete all voice channels
 	 */
-	schedule('0 0 */1 * *', async () => {
-		cacheCleanup();
-	});
+	crons.add(
+		schedule('0 0 */1 * *', async () => {
+			cacheCleanup();
+		})
+	);
 
-	schedule('35 */48 * * *', async () => {
-		await cacheGEPrices();
-	});
+	crons.add(
+		schedule('35 */48 * * *', async () => {
+			await cacheGEPrices();
+		})
+	);
 }
