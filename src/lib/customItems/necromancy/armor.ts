19d8eee33da8fbc0740b0ead0f5c0c6eb70d035d
gc <30398469+gc@users.noreply.github.com>
2025-06-29 21:08:15 +1000

    commit

---
+++ src/lib/customItems/necromancy/armor.ts
@@ -0,0 +1,170 @@
+// 74,101-74,300
+import { setCustomItem } from "../util";
+
+setCustomItem(
+    74_101,
+    'Deathwarden hood (t1)',
+    'Bronze full helm',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_102,
+    'Deathwarden top (t1)',
+    'Bronze platebody',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_103,
+    'Deathwarden legs (t1)',
+    'Bronze platelegs',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_104,
+    'Deathwarden gloves (t1)',
+    'Bronze gloves',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_105,
+    'Deathwarden boots (t1)',
+    'Bronze boots',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+// Deathwarden T2
+
+setCustomItem(
+    74_106,
+    'Deathwarden hood (t2)',
+    'Bronze full helm',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_107,
+    'Deathwarden top (t2)',
+    'Bronze platebody',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_108,
+    'Deathwarden legs (t2)',
+    'Bronze platelegs',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_109,
+    'Deathwarden gloves (t2)',
+    'Bronze gloves',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_110,
+    'Deathwarden boots (t2)',
+    'Bronze boots',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+// Deathwarden T3
+setCustomItem(
+    74_111,
+    'Deathwarden hood (t3)',
+    'Bronze full helm',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_112,
+    'Deathwarden top (t3)',
+    'Bronze platebody',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_113,
+    'Deathwarden legs (t3)',
+    'Bronze platelegs',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_114,
+    'Deathwarden gloves (t3)',
+    'Bronze gloves',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+    74_115,
+    'Deathwarden boots (t3)',
+    'Bronze boots',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
---
+++ src/lib/customItems/necromancy/misc.ts
@@ -0,0 +1,91 @@
+// 74,051-74,100
+import { setCustomItem } from "../util";
+
+setCustomItem(
+    74_051,
+    'Death Monkey',
+    'Herbi',
+    {
+    customItemData: {
+        cantDropFromMysteryBoxes: true,
+        isSuperUntradeable: true
+    }
+});
+
+setCustomItem(
+	74_052,
+	'Necromancy master cape',
+	'Abyssal cape',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	200_000_000
+);
+
+setCustomItem(
+	74_053,
+	'Necromancy cape',
+	'Fire cape',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	20_000
+);
+
+setCustomItem(
+	74_054,
+	'Necromancy cape(t)',
+	'Fire cape',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	20_000
+);
+
+setCustomItem(
+	74_055,
+	'Necromancy hood',
+	'Rune full helm',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	20_000
+);
+
+setCustomItem(
+	74_056,
+	'Necromancy master cape (inverted)',
+	'Abyssal cape',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	200_000_000
+);
+
+setCustomItem(
+	74_057,
+	'Ball and chain',
+	'Coal',
+	{
+		customItemData: {
+			cantDropFromMysteryBoxes: true,
+			isSuperUntradeable: true
+		}
+	},
+	100_000
+);
