8ba0a20566c2c386aec6c4a27956862baae07cff
GC <30398469+gc@users.noreply.github.com>
2025-06-30 13:28:13 +1000

    Add path map (#6472)

--- src/lib/minions/data/killableMonsters/bosses/dt.ts
+++ src/lib/minions/data/killableMonsters/bosses/dt.ts
@@ -1,11 +1,10 @@
 import { Time, roll } from 'e';
-import { Bank, Monsters, deepResolveItems, resolveItems } from 'oldschooljs';
+import { Bank, Monsters, deepResolveItems, itemID, resolveItems } from 'oldschooljs';
 
-import { dukeSucellusCL, theLeviathanCL, theWhispererCL, vardorvisCL } from '../../../../data/CollectionsExport';
-import { GearStat } from '../../../../gear/types';
-import { SkillsEnum } from '../../../../skilling/types';
-import { getOSItem } from '../../../../util/getOSItem';
-import itemID from '../../../../util/itemID';
+import { dukeSucellusCL, theLeviathanCL, theWhispererCL, vardorvisCL } from '@/lib/data/CollectionsExport';
+import { GearStat } from '@/lib/gear/types';
+import { SkillsEnum } from '@/lib/skilling/types';
+import { getOSItem } from '@/lib/util/getOSItem';
 import type { KillableMonster, KillableMonsterEffect } from '../../../types';
 import { QuestID } from '../../quests';
 
