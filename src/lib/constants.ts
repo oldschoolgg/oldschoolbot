33155112fecbd9fc5a180d9ee2d67ae7485132bd
DayV <ashleyjd22@gmail.com>
2025-06-30 12:00:07 +0100

    Remove arrows from ammo in chompyHunt and tidy messages

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -333,6 +333,11 @@ export const projectiles = {
 		savedByAvas: true,
 		weapons: resolveItems(['Twisted bow'])
 	},
+	ogreArrow: {
+		items: resolveItems(['Ogre Arrow']),
+		savedByAvas: true,
+		weapons: resolveItems(['Ogre bow'])
+	},
 	bolt: {
 		items: resolveItems([
 			'Runite bolts',
