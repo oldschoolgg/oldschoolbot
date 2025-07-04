1b2f52b335d72c581c57999f3cecb610ae895ff3
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-07-04 11:19:49 +0100

    Colosseum Furball CA Fix (#6317)

--- src/lib/combat_achievements/elite.ts
+++ src/lib/combat_achievements/elite.ts
@@ -1526,7 +1526,8 @@ export const eliteCombatAchievements: CombatAchievement[] = [
 		rng: {
 			chancePerKill: 12,
 			hasChance: (data, _user, index) =>
-				data.type === 'Colosseum' && (!data.diedAt || (Array.isArray(data.diedAt) && data.diedAt[index]! > 4))
+				data.type === 'Colosseum' &&
+				(!data.diedAt || (Array.isArray(data.diedAt) && (!data.diedAt[index] || data.diedAt[index] > 4)))
 		}
 	},
 	{
