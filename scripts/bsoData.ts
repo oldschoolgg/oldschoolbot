933c524b9ba4d08473d1e6536a1fef939ebe9ea3
gc <30398469+gc@users.noreply.github.com>
2025-07-08 03:07:51 +1000

    changes

--- scripts/bsoData.ts
+++ scripts/bsoData.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'node:fs';
-import { calcNecroLevelForMonster, calcSoulChance } from '@oldschoolgg/necromancy';
+import { calcMonsterStrengthFactor, calcNecroLevelForMonster, calcSoulChance } from '@oldschoolgg/necromancy';
 import { Items } from 'oldschooljs';
 
 import killableMonsters from '@/lib/minions/data/killableMonsters';
@@ -25,13 +25,21 @@ export function renderBsoItemsFile() {
 	const necromancySoulChances: any = {};
 	for (const monster of [...killableMonsters].sort((a, b) => a.timeToFinish - b.timeToFinish)) {
 		necromancySoulChances[monster.name] = {};
+		necromancySoulChances[monster.name].strength_factor = calcMonsterStrengthFactor(monster.timeToFinish);
 		necromancySoulChances[monster.name].necromancy_level_required = calcNecroLevelForMonster({
 			monsterKillTime: monster.timeToFinish
 		});
 		necromancySoulChances[monster.name].chances = [];
 
 		for (const necromancyLevel of [1, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90, 95, 100, 105, 110, 115, 120]) {
-			const chance = calcSoulChance({ monsterKillTime: monster.timeToFinish, necromancyLevel });
+			if (necromancyLevel < necromancySoulChances[monster.name].necromancy_level_required) continue;
+
+			const chance = calcSoulChance({
+				monsterKillTime: monster.timeToFinish,
+				necromancyLevel,
+				requiredNecromancyLevel: necromancySoulChances[monster.name].necromancy_level_required
+			});
+
 			necromancySoulChances[monster.name].chances.push({
 				kill_time_s: monster.timeToFinish / 1000,
 				chance: chance,
