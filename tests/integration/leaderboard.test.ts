e50fef482b6da701dcdcd221087880e3d728f667
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-08-29 11:12:37 +0100

    fix leaderboard integration test syntax

--- tests/integration/leaderboard.test.ts
+++ tests/integration/leaderboard.test.ts
@@ -1,31 +1,32 @@
 import { describe, test } from 'vitest';
 
-import { kcGains } from '../../src/mahoji/commands/tools';
+import { leaderboardCommand } from '../../src/mahoji/commands/leaderboard';
 import { createTestUser } from './util';
 
 describe('Leaderboard', async () => {
-        test('KC Leaderboard', async () => {
-                const user = await createTestUser();
-                await user.runCommand(leaderboardCommand, {
-                        kc: {
-                                monster: 'man'
-                        }
-                });
-        });
+	test('KC Leaderboard', async () => {
+		const user = await createTestUser();
+		await user.runCommand(leaderboardCommand, {
+			kc: {
+				monster: 'man'
+			}
+		});
+	});
 
-       test('KC Leaderboard Tame', async () => {
-               const user = await createTestUser();
-               await user.runCommand(leaderboardCommand, {
-                       kc: {
-                               monster: 'Zulrah',
-                               tame: true
-                       }
-               });
-       });
+	test('KC Leaderboard Tame', async () => {
+		const user = await createTestUser();
+		await user.runCommand(leaderboardCommand, {
+			kc: {
+				monster: 'Zulrah',
+				tame: true
+			}
+		});
+	});
 
-       test('Tames Hatched Leaderboard', async () => {
-               const user = await createTestUser();
-               await user.runCommand(leaderboardCommand, {
-                       tames_hatched: {}
-               });
-       });
+	test('Tames Hatched Leaderboard', async () => {
+		const user = await createTestUser();
+		await user.runCommand(leaderboardCommand, {
+			tames_hatched: {}
+		});
+	});
+});
