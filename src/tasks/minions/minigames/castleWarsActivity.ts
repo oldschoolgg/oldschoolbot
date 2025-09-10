567e4fc932f8604d82795dbff087587e83410d54
nwjgit <nwjgit@gmail.com>
2025-09-09 22:08:33 -0500

    lower ticket weights

--- src/tasks/minions/minigames/castleWarsActivity.ts
+++ src/tasks/minions/minigames/castleWarsActivity.ts
@@ -6,10 +6,10 @@ import { handleTripFinish } from '../../../lib/util/handleTripFinish';
 
 // Assumes always playing on Castle Wars worlds(which give +1 ticket)"
 const ticketTable = new SimpleTable<number>()
-	.add(1, 8) // 2 tickets are earned if the player's team loses.
-	.add(2, 8) // 2 tickets are earned if scores are tied at 0-0.
-	.add(3, 5) // 3 tickets are earned if scores are tied at any number other than 0.
-	.add(3, 5) // 3 tickets are earned for winning if the opposing team has scored.
+	.add(1, 5) // 2 tickets are earned if the player's team loses.
+	.add(2, 5) // 2 tickets are earned if scores are tied at 0-0.
+	.add(3, 3) // 3 tickets are earned if scores are tied at any number other than 0.
+	.add(3, 3) // 3 tickets are earned for winning if the opposing team has scored.
 	.add(4, 2); // 4 tickets are earned for winning if the opposing team had 0 points.
 
 export const castleWarsTask: MinionTask = {
