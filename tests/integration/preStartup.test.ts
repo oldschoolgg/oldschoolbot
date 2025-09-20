ed646de41afaba7f5d9d06ddb8ffbae1c096e702
gc <30398469+gc@users.noreply.github.com>
2025-09-20 17:35:32 +1000

    changes

--- tests/integration/preStartup.test.ts
+++ tests/integration/preStartup.test.ts
@@ -3,7 +3,7 @@ import { test } from 'vitest';
 import { preStartup } from '../../src/lib/preStartup.js';
 import { mockClient } from './util.js';
 
-test.skip('PreStartup', async () => {
+test('PreStartup', async () => {
 	await mockClient();
 	await preStartup();
 });
