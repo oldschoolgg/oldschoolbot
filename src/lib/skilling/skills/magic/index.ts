4e68cfd7e184ecab1603ee6eabde45d5b5854ec8
gc <30398469+gc@users.noreply.github.com>
2025-09-26 21:59:04 +1000

    Add lots more snapshots

--- src/lib/skilling/skills/magic/index.ts
+++ src/lib/skilling/skills/magic/index.ts
@@ -1,12 +1,16 @@
 import { Emoji } from '@oldschoolgg/toolkit/constants';
 
+import { Castables } from '@/lib/skilling/skills/magic/castables.js';
+import { Enchantables } from '@/lib/skilling/skills/magic/enchantables.js';
 import { SkillsEnum } from '../../types';
 
 const Magic = {
 	aliases: ['mage', 'magic'],
 	id: SkillsEnum.Magic,
 	emoji: Emoji.Magic,
-	name: 'Magic'
+	name: 'Magic',
+	Castables,
+	Enchantables
 };
 
 export default Magic;
