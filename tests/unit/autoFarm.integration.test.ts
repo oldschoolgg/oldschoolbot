9268e6064db5d2d0ab75a9036b2bdaf3e063a4a6
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-09-26 09:22:37 +0200

    Ensure auto farm tree test has enough yew seeds

--- tests/unit/autoFarm.integration.test.ts
+++ tests/unit/autoFarm.integration.test.ts
@@ -265,7 +265,7 @@ describe('autoFarm tree clearing fees', () => {
 			gp: 1000,
 			farmingLevel: 99,
 			woodcuttingLevel: 1,
-			bank: new Bank({ 'Yew seed': 1 }),
+			bank: new Bank({ 'Yew seed': 10 }),
 			autoFarmFilter: AutoFarmFilterEnum.AllFarm
 		});
 
