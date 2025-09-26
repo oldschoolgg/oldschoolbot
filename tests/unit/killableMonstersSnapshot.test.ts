92867c31e1d72c78a8f8b6d8c2a70bd0f5a59cbb
gc <30398469+gc@users.noreply.github.com>
2025-09-26 23:27:08 +1000

    Improve snapshots

--- tests/unit/killableMonstersSnapshot.test.ts
+++ tests/unit/killableMonstersSnapshot.test.ts
@@ -1,19 +1,33 @@
-import { LootTable } from 'oldschooljs';
-import { isFunction, omit, omitBy } from 'remeda';
+import { omit } from 'remeda';
 import { expect, it } from 'vitest';
 
 import killableMonsters from '@/lib/minions/data/killableMonsters/index.js';
+import type { KillableMonster } from '@/lib/minions/types.js';
 import { BOT_TYPE } from '../../src/lib/constants.js';
+import { serializeSnapshotItem } from './userutil.js';
 
 it(`${BOT_TYPE} KillableMonsters`, () => {
 	const result = [...killableMonsters]
 		.sort((a, b) => a.name.localeCompare(b.name))
-		.map(m =>
-			omitBy(
-				m,
-				(val, _key) => val instanceof LootTable || isFunction(val) || ['table', 'superior'].includes(_key)
-			)
-		);
+		.map(serializeSnapshotItem)
+		.map((m: KillableMonster) => {
+			const obj: any = {
+				...omit(m, ['equippedItemBoosts', 'itemInBankBoosts', 'aliases'])
+			};
+
+			if (m.itemInBankBoosts) {
+				obj.itemInBankBoosts = m.itemInBankBoosts?.map(ib => Object.entries(ib));
+			}
+			if (m.equippedItemBoosts) {
+				obj.equippedItemBoosts = m.equippedItemBoosts?.map(eb => [
+					eb.gearSetup,
+					eb.required ?? false,
+					eb.items.map(item => Object.values(item))
+				]);
+			}
+
+			return obj;
+		});
 	expect(result).toMatchSnapshot();
 });
 
