61b1d5b1c2a6f799e4245a990e690e107d95be81
gc <30398469+gc@users.noreply.github.com>
2025-10-04 09:20:58 +1000

    Add unobtainable gear itemgroup

---
+++ packages/oldschooljs/src/item-groups/unobtainable.ts
@@ -0,0 +1,245 @@
+import { Items } from "@/structures/Items.js";
+
+const unobtainableGauntletGear = Items.resolveItems([
+	'Corrupted helm (perfected)',
+	'Corrupted body (perfected)',
+	'Corrupted legs (perfected)',
+	'Corrupted bow (perfected)',
+	'Corrupted staff (perfected)',
+	'Corrupted halberd (perfected)',
+
+	'Corrupted helm (attuned)',
+	'Corrupted body (attuned)',
+	'Corrupted legs (attuned)',
+	'Corrupted bow (attuned)',
+	'Corrupted staff (attuned)',
+	'Corrupted halberd (attuned)',
+
+	'Corrupted helm (basic)',
+	'Corrupted body (basic)',
+	'Corrupted legs (basic)',
+	'Corrupted bow (basic)',
+	'Corrupted staff (basic)',
+	'Corrupted halberd (basic)',
+
+
+	'Crystal helm (perfected)',
+	'Crystal body (perfected)',
+	'Crystal legs (perfected)',
+	'Crystal bow (perfected)',
+	'Crystal staff (perfected)',
+	'Crystal halberd (perfected)',
+
+	'Crystal helm (attuned)',
+	'Crystal body (attuned)',
+	'Crystal legs (attuned)',
+	'Crystal bow (attuned)',
+	'Crystal staff (attuned)',
+	'Crystal halberd (attuned)',
+
+	'Crystal helm (basic)',
+	'Crystal body (basic)',
+	'Crystal legs (basic)',
+	'Crystal bow (basic)',
+	'Crystal staff (basic)',
+	'Crystal halberd (basic)',
+]);
+
+const unobtainableEmirsArenaGear = [
+	26674, // Slayer helmet (i)
+	26675, // Black slayer helmet (i)
+	26676, // Green slayer helmet (i)
+	26677, // Red slayer helmet (i)
+	26678, // Purple slayer helmet (i)
+	26679, // Turquoise slayer helmet (i)
+	26680, // Hydra slayer helmet (i)
+	26681, // Twisted slayer helmet (i)
+	26682, // Tztok slayer helmet (i)
+	26683, // Vampyric slayer helmet (i)
+	26684, // Tzkal slayer helmet (i)
+	26685, // Granite ring (i)
+	26763, // Salve amulet(i)
+	26764, // Ring of the gods (i)
+	26765, // Tyrannical ring (i)
+	26766, // Treasonous ring (i)
+	26767, // Seers ring (i)
+	26768, // Archers ring (i)
+	26769, // Warrior ring (i)
+	26770, // Berserker ring (i)
+	26782, // Salve amulet(ei)
+	27086, // Rune pouch
+	29822, // Araxyte slayer helmet (i)
+	26686, // Maoma's med helm (broken)
+	26687, // Maoma's full helm (broken)
+	26688, // Maoma's great helm (broken)
+	26689, // Calamity chest (broken)
+	26690, // Superior calamity chest (broken)
+	26691, // Elite calamity chest (broken)
+	26692, // Calamity breeches (broken)
+	26693, // Superior calamity breeches (broken)
+	26694, // Elite calamity breeches (broken)
+	26698, // Koriff's headband (broken)
+	26699, // Koriff's cowl (broken)
+	26700, // Koriff's coif (broken)
+	26737, // Koriff's headband
+	26738, // Koriff's headband (l)
+	26739, // Koriff's cowl
+	26740, // Koriff's cowl (l)
+	26741, // Koriff's coif
+	26742, // Koriff's coif (l)
+	26743, // Maoma's med helm
+	26744, // Maoma's med helm (l)
+	26745, // Maoma's full helm
+	26746, // Maoma's full helm (l)
+	26747, // Maoma's great helm
+	26748, // Maoma's great helm (l)
+	26749, // Calamity chest
+	26750, // Calamity chest (l)
+	26751, // Superior calamity chest
+	26752, // Superior calamity chest (l)
+	26753, // Elite calamity chest
+	26754, // Elite calamity chest (l)
+	26755, // Calamity breeches
+	26756, // Calamity breeches (l)
+	26757, // Superior calamity breeches
+	26758, // Superior calamity breeches (l)
+	26759, // Elite calamity breeches
+	26760, // Elite calamity breeches (l)
+	27110, // Mithril gloves (wrapped)
+	27111, // Rune gloves (wrapped)
+	27112, // Barrows gloves (wrapped)
+	26696, // Wristbands of the arena (broken)',
+	26697, // Hardened wristbands of the arena (broken)',
+	26723, // Wristbands of the arena',
+	26724, // Wristbands of the arena (l)',
+	26725, // Wristbands of the arena (c)',
+	26726, // Wristbands of the arena (cl)',
+	26727, // Wristbands of the arena (i)',
+	26728, // Wristbands of the arena (il)',
+	26729, // Wristbands of the arena (ic)',
+	26730, // Wristbands of the arena (ilc)'
+	26701, // Saika's hood (broken)
+	26702, // Saika's veil (broken)
+	26703, // Saika's shroud (broken)
+	26731, // Saika's hood
+	26732, // Saika's hood (l)
+	26733, // Saika's veil
+	26734, // Saika's veil (l)
+	26735, // Saika's shroud
+	26736, // Saika's shroud (l)
+];
+
+const unobtainableLeaguesItems = Items.resolveItems([
+	'Amulet of the monarchs',
+	'The dogsword',
+	'Drygore blowpipe',
+	'Emperor ring',
+	"Devil's element",
+	"Nature's reprisal",
+	"Gloves of the damned",
+	"Crystal blessing",
+	"Sunlight spear",
+	"Sunlit bracers",
+	"Thunder khopesh",
+	'Thousand-dragon ward',
+	'Crystal dagger (perfected)',
+	"Nature's reprisal (uncharged)",
+	"Corrupted tumeken's shadow",
+	"Corrupted voidwaker",
+	"Corrupted dragon claws",
+	'Corrupted armadyl godsword',
+	'Corrupted twisted bow',
+	'Corrupted scythe of vitur',
+	'Corrupted scythe of vitur (uncharged)',
+	'Corrupted dark bow',
+	'Corrupted volatile nightmare staff'
+]);
+
+const unobtainableBountyHunterGear = Items.resolveItems([
+	27831, // Vesta's chainbody (bh)
+	27832, // Vesta's plateskirt (bh)
+	27833, // Statius's full helm (bh)
+	27834, // Statius's platebody (bh)
+	27835, // Statius's platelegs (bh)
+	27836, // Morrigan's coif (bh)
+	27837, // Morrigan's leather body (bh)
+	27838, // Morrigan's leather chaps (bh)
+	27839, // Zuriel's hood (bh)
+	27840, // Zuriel's robe top (bh)
+	27841, // Zuriel's robe bottom (bh)
+	27842, // Corrupted vesta's chainbody (bh)
+	27843, // Corrupted vesta's plateskirt (bh)
+	27844, // Corrupted statius's full helm (bh)
+	27845, // Corrupted statius's platebody (bh)
+	27846, // Corrupted statius's platelegs (bh)
+	27847, // Corrupted morrigan's coif (bh)
+	27848, // Corrupted morrigan's leather body (bh)
+	27849, // Corrupted morrigan's leather chaps (bh)
+	27850, // Corrupted zuriel's hood (bh)
+	27851, // Corrupted zuriel's robe top (bh)
+	27852, // Corrupted zuriel's robe bottom (bh)
+	27853, // Dark bow (bh)
+	27855, // Barrelchest anchor (bh)
+	27857, // Dragon mace (bh)
+	27859, // Dragon longsword (bh)
+	27861, // Abyssal dagger (bh)
+	27863, // Abyssal dagger (bh)(p)
+	27865, // Abyssal dagger (bh)(p+)
+	27867, // Abyssal dagger (bh)(p++)
+	27900, // Vesta's spear (bh)
+	27902, // Vesta's spear (bh)(inactive)
+	27904, // Vesta's longsword (bh)
+	27906, // Vesta's longsword (bh)(inactive)
+	27908, // Statius's warhammer (bh)
+	27910, // Statius's warhammer (bh)(inactive)
+	27912, // Morrigan's throwing axe (bh)
+	27914, // Morrigan's throwing axe (bh)(inactive)
+	27916, // Morrigan's javelin (bh)
+	27918, // Morrigan's javelin (bh)(inactive)
+	27920, // Zuriel's staff (bh)
+	27922, // Zuriel's staff (bh)(inactive)
+	27925, // Vesta's chainbody (bh)(inactive)
+	27928, // Vesta's plateskirt (bh)(inactive)
+	27931, // Statius's full helm (bh)(inactive)
+	27934, // Statius's platebody (bh)(inactive)
+	27937, // Statius's platelegs (bh)(inactive)
+	27940, // Morrigan's coif (bh)(inactive)
+	27943, // Morrigan's leather body (bh)(inactive)
+	27946, // Morrigan's leather chaps (bh)(inactive)
+	27949, // Zuriel's hood (bh)(inactive)
+	27952, // Zuriel's robe top (bh)(inactive)
+	27955, // Zuriel's robe bottom (bh)(inactive)
+	27965, // Corrupted vesta's chainbody (bh)(inactive)
+	27968, // Corrupted vesta's plateskirt (bh)(inactive)
+	27971, // Corrupted statius's full helm (bh)(inactive)
+	27974, // Corrupted statius's platebody (bh)(inactive)
+	27977, // Corrupted statius's platelegs (bh)(inactive)
+	27980, // Corrupted morrigan's coif (bh)(inactive)
+	27983, // Corrupted morrigan's leather body (bh)(inactive)
+	27986, // Corrupted morrigan's leather chaps (bh)(inactive)
+	27989, // Corrupted zuriel's hood (bh)(inactive)
+	27992, // Corrupted zuriel's robe top (bh)(inactive)
+	27995, // Corrupted zuriel's robe bottom (bh)(inactive)
+]);
+
+const unobtainableDeadmanModeItems = [
+	22616, // Vesta's chainbody
+	22619, // Vesta's plateskirt
+	22610, // Vesta's spear
+	22613, // Vesta's longsword
+	22622, // Statius's warhammer
+	22625, // Statius's full helm
+	22628, // Statius's platebody
+	22631, // Statius's platelegs
+	22634, // Morrigan's throwing axe
+	22636, // Morrigan's javelin
+	22638, // Morrigan's coif
+	22641, // Morrigan's leather body
+	22644, // Morrigan's leather chaps
+	22647, // Zuriel's staff
+	22650, // Zuriel's hood
+	22653, // Zuriel's robe top
+	22656, // Zuriel's robe bottom
+];
+
+export const allUnobtainableGear = [...unobtainableGauntletGear, ...unobtainableEmirsArenaGear, ...unobtainableLeaguesItems, ...unobtainableBountyHunterGear, ...unobtainableDeadmanModeItems];
