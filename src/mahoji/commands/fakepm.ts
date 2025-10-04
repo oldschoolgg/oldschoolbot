c94e23d8286f3dd00025670f7cdcb9af0354afb0
gc <30398469+gc@users.noreply.github.com>
2025-10-04 11:42:13 +1000

    Lazy load images to improve startup time

--- src/mahoji/commands/fakepm.ts
+++ src/mahoji/commands/fakepm.ts
@@ -1,6 +1,8 @@
+import type { Image } from 'skia-canvas';
+
 import { canvasToBuffer, createCanvas, loadAndCacheLocalImage } from '@/lib/canvas/canvasUtil.js';
 
-const bg = loadAndCacheLocalImage('./src/lib/resources/images/pm-bg.png');
+let bg: Image | null = null;
 
 export const fakepmCommand: OSBMahojiCommand = {
 	name: 'fakepm',
@@ -23,8 +25,11 @@ export const fakepmCommand: OSBMahojiCommand = {
 		const canvas = createCanvas(376, 174);
 		const ctx = canvas.getContext('2d');
 		ctx.font = '16px OSRSFont';
-		const img = await bg;
-		ctx.drawImage(img, 0, 0, img.width, img.height);
+
+		if (!bg) {
+			bg = await loadAndCacheLocalImage('./src/lib/resources/images/pm-bg.png');
+		}
+		ctx.drawImage(bg, 0, 0, bg.width, bg.height);
 
 		ctx.fillStyle = '#000000';
 		ctx.fillText(`From ${options.username}: ${options.message}`, 6, 98);
