862932527d10e40ad00af8fd53d3059f889adada
gc <30398469+gc@users.noreply.github.com>
2025-10-06 10:56:36 +1100

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import './lib/constants.js';
 import './lib/safeglobals.js';
 
-import { createDb } from './lib/globals.js';
 import './lib/MUser.js';
 import './lib/discord/client.js';
 
@@ -9,9 +8,7 @@ import { init } from '@sentry/node';
 import exitHook from 'exit-hook';
 
 import { gitHash, globalConfig } from '@/lib/constants.js';
-import { allCollectionLogsFlat } from '@/lib/data/Collections.js';
 import { preStartup } from '@/lib/preStartup.js';
-import { fetchCLLeaderboard } from '@/lib/util/clLeaderboard.js';
 import { exitCleanup } from '@/mahoji/lib/exitHandler.js';
 
 exitHook(exitCleanup);
@@ -26,25 +23,9 @@ if (globalConfig.sentryDSN) {
 }
 
 async function main() {
-	await createDb();
 	Logging.logDebug(`Starting up after ${process.uptime()}s`);
-
 	await Promise.all([preStartup(), globalClient.login(globalConfig.botToken)]);
-
 	Logging.logDebug(`Logged in as ${globalClient.user.username} after ${process.uptime()}s`);
-	// 	console.log(await prisma.$queryRawUnsafe(`SELECT * FROM users
-	// WHERE "minion.equippedPet"::text::integer = ANY(ARRAY[5]);`));
-	// 	console.log(await prisma.$queryRawUnsafe(`SELECT * FROM users
-	// WHERE "minion.equippedPet"::text::integer @> ARRAY[5];`));
-	const cl = allCollectionLogsFlat[0];
-	console.log(
-		await fetchCLLeaderboard({
-			ironmenOnly: false,
-			items: cl.items,
-			resultLimit: 10,
-			clName: cl.name
-		})
-	);
 }
 
 process.on('uncaughtException', err => {
