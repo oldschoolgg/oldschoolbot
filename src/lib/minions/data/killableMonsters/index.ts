d421fd47f3c740d9f0bfb8156cfa33c786b96a4c
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-10-08 22:41:07 +0100

    Scale boosts

--- src/lib/minions/data/killableMonsters/index.ts
+++ src/lib/minions/data/killableMonsters/index.ts
@@ -226,35 +226,35 @@ const killableMonsters: KillableMonster[] = [
 		qpRequired: 0,
 		itemInBankBoosts: [
 			{
-				[itemID('Armadyl chestplate')]: 2,
-				[itemID('Masori body (f)')]: 4
+				[itemID('Armadyl chestplate')]: 2/3,
+				[itemID('Masori body (f)')]: 4/3
 			},
 			{
-				[itemID('Armadyl chainskirt')]: 2,
-				[itemID('Masori chaps (f)')]: 4
+				[itemID('Armadyl chainskirt')]: 2/3,
+				[itemID('Masori chaps (f)')]: 4/3
 			},
 			{
-				[itemID('Twisted bow')]: 6
+				[itemID('Twisted bow')]: 6/3
 			},
 			{
-				[itemID("Iban's staff")]: 3,
-				[itemID('Warped sceptre (uncharged)')]: 4,
-				[itemID('Harmonised nightmare staff')]: 5
+				[itemID("Iban's staff")]: 3/3,
+				[itemID('Warped sceptre (uncharged)')]: 4/3,
+				[itemID('Harmonised nightmare staff')]: 5/3
 			},
 			{
-				[itemID('Occult necklace')]: 5
+				[itemID('Occult necklace')]: 5/3
 			},
 			{
-				[itemID('Bandos chestplate')]: 2,
-				[itemID('Torva platebody')]: 2
+				[itemID('Bandos chestplate')]: 2/3,
+				[itemID('Torva platebody')]: 2/3
 			},
 			{
-				[itemID('Bandos tassets')]: 2,
-				[itemID('Torva platelegs')]: 2
+				[itemID('Bandos tassets')]: 2/3,
+				[itemID('Torva platelegs')]: 2/3
 			},
 			{
-				[itemID('Saradomin godsword')]: 4,
-				[itemID('Dragon claws')]: 6
+				[itemID('Saradomin godsword')]: 4/3,
+				[itemID('Dragon claws')]: 6/3
 			}
 		],
 		levelRequirements: {
