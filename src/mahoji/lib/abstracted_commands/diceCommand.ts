01500660c0aa4ba12d50a7cc189c25ddc779916d
gc <30398469+gc@users.noreply.github.com>
2025-10-12 11:50:30 +1100

    Fix gamblers bag drop

--- src/mahoji/lib/abstracted_commands/diceCommand.ts
+++ src/mahoji/lib/abstracted_commands/diceCommand.ts
@@ -44,6 +44,13 @@ export async function diceCommand(rng: RNGProvider, user: MUser, interaction: MI
 		await user.removeItemsFromBank(new Bank().add('Coins', amount));
 	}
 
-	return `${user.badgedUsername} rolled **${roll}** on the percentile dice, and you ${won ? 'won' : 'lost'
-		} ${toKMB(amountToAdd)} GP.`;
+	let resultStr = `${user.badgedUsername} rolled **${roll}** on the percentile dice, and you ${
+		won ? 'won' : 'lost'
+	} ${toKMB(amountToAdd)} GP.`;
+	if (amount >= 100_000_000 && won && rng.percentChance(3)) {
+		await user.addItemsToBank({ items: new Bank().add('Gamblers bag'), collectionLog: true });
+		resultStr += `\n\nYou received a **Gamblers Bag**.`;
+	}
+
+	return resultStr;
 }
