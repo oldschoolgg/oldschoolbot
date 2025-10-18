ed970999ee0d0f3a10d67d20ce5633e3726eeae1
gc <30398469+gc@users.noreply.github.com>
2025-10-19 00:33:38 +1100

    Update hunter test for consistency with bso

--- tests/integration/hunter/hunter.test.ts
+++ tests/integration/hunter/hunter.test.ts
@@ -1,19 +1,26 @@
+import { ECreature } from 'oldschooljs';
 import { expect, test } from 'vitest';
 
-import Hunter from '@/lib/skilling/skills/hunter/hunter.js';
 import { minionCommand } from '@/mahoji/commands/minion.js';
 import { createTestUser } from '../util.js';
 
 test('incrementCreatureScore', async () => {
 	const user = await createTestUser();
 
-	await user.incrementCreatureScore(1);
-	expect(await user.getCreatureScore(1)).toEqual(1);
+	await user.incrementCreatureScore(ECreature.BLACK_CHINCHOMPA);
+	expect(await user.getCreatureScore(ECreature.BLACK_CHINCHOMPA)).toEqual(1);
 
-	await user.incrementCreatureScore(1, 10);
-	expect(await user.getCreatureScore(1)).toEqual(11);
+	await user.incrementCreatureScore(ECreature.BLACK_CHINCHOMPA, 10);
+	expect(await user.getCreatureScore(ECreature.BLACK_CHINCHOMPA)).toEqual(11);
 
-	const creatures = Hunter.Creatures.slice(10, 20).map(c => c.id);
+	const creatures = [
+		ECreature.SAPPHIRE_GLACIALIS,
+		ECreature.CERULEAN_TWITCH,
+		ECreature.MANIACAL_MONKEY,
+		ECreature.SWAMP_LIZARD,
+		ECreature.SNOWY_KNIGHT,
+		ECreature.RUBY_HARVEST
+	];
 	await Promise.all([
 		...creatures.map(id => user.incrementCreatureScore(id, 3)),
 		...creatures.map(id => user.incrementCreatureScore(id, 3))
@@ -23,7 +30,7 @@ test('incrementCreatureScore', async () => {
 		expect(await user.getCreatureScore(id)).toEqual(6);
 	}
 
-	expect(await user.getCreatureScore(1)).toEqual(11);
+	expect(await user.getCreatureScore(ECreature.BLACK_CHINCHOMPA)).toEqual(11);
 	const res = await user.runCommand(minionCommand, { kc: { name: 'Sapphire glacialis' } });
 	expect(res).toEqual('Your Sapphire glacialis KC is: 6.');
 });
