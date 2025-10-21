613b2b81f300e97fe462d5cc6e5fc29d7454e6da
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2025-10-21 14:33:25 +0100

    Add Steel ring to Deranged Arch drop table and CL (#6557)

--- src/lib/data/Collections.ts
+++ src/lib/data/Collections.ts
@@ -55,6 +55,7 @@ import {
 	dagannothKingsCL,
 	dailyCL,
 	demonicGorillaCL,
+	derangedArchaeologistCL,
 	diariesCL,
 	dukeSucellusCL,
 	type FormatProgressFunction,
@@ -256,6 +257,12 @@ export const allCollectionLogs: ICollection = {
 				allItems: Monsters.CrazyArchaeologist.allItems,
 				items: crazyArchaeologistCL,
 				fmtProg: kcProg(Monsters.CrazyArchaeologist)
+			},
+			'Deranged Archaeologist': {
+				alias: Monsters.DerangedArchaeologist.aliases,
+				allItems: Monsters.DerangedArchaeologist.allItems,
+				items: derangedArchaeologistCL,
+				fmtProg: kcProg(Monsters.DerangedArchaeologist)
 			},
 			'Dagannoth Kings': {
 				alias: ['dagannoth kings', 'kings', 'dagga', 'dks'],
