fa2324bfcadc1e4d2a32dddd2b29f527d57795c6
gc <30398469+gc@users.noreply.github.com>
2025-11-13 11:37:59 +1100

    Add redis-keys to util package

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,3 +1,4 @@
 export * from './array.js';
 export * from './discord.js';
 export * from './object.js';
+export * from './redis-keys.js';
---
+++ packages/util/src/redis-keys.ts
@@ -0,0 +1,27 @@
+export const RedisKeys = {
+	BlacklistedUsers: 'blacklist:users',
+	BlacklistedGuilds: 'blacklist:guilds',
+	RoboChimpUser: (id: bigint) => `robochimp:user:${id.toString()}`,
+	OSB: {
+		BadgedUsername: (id: string) => `osb:user:${id.toString()}:badged_username`,
+		LockStatus: (id: string) => `osb:user:${id.toString()}:lock_status`,
+		Ratelimit: (id: string, type: string) => `osb:user:${id.toString()}:ratelimit:${type}`,
+		GuildSettings: (id: string) => `osb:guild:${id.toString()}:settings`,
+		DisabledCommands: 'osb:disabled_commands'
+	},
+	BSO: {
+		BadgedUsername: (id: string) => `bso:user:${id.toString()}:badged_username`,
+		LockStatus: (id: string) => `bso:user:${id.toString()}:lock_status`,
+		Ratelimit: (id: string, type: string) => `bso:user:${id.toString()}:ratelimit:${type}`,
+		GuildSettings: (id: string) => `bso:guild:${id.toString()}:settings`,
+		DisabledCommands: 'bso:disabled_commands'
+	},
+	Discord: {
+		Channel: (id: string) => `discord:channel:${id.toString()}`,
+		Guild: (id: string) => `discord:guild:${id.toString()}`,
+		Member: (guildId: string, userId: string) => `discord:guild:${guildId}:member:${userId}`,
+		Role: (guildId: string, roleId: string) => `discord:guild:${guildId}:role:${roleId}`,
+		Emoji: (guildId: string, emojiId: string) => `discord:guild:${guildId}:emoji:${emojiId}`,
+		Username: (id: string) => `discord:user:${id.toString()}:username`
+	}
+} as const;
