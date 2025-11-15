ed0831b0d15fdb360c0a3f24196b3810698b806d
gc <30398469+gc@users.noreply.github.com>
2025-11-15 18:18:37 +1100

    Fix robochimp

--- packages/robochimp/src/discord/RoboChimpBotClient.ts
+++ packages/robochimp/src/discord/RoboChimpBotClient.ts
@@ -1,12 +1,4 @@
-import {
-	type APIApplication,
-	ButtonBuilder,
-	ButtonStyle,
-	collectSingleInteraction,
-	DiscordClient,
-	type DiscordClientOptions
-} from '@oldschoolgg/discord';
-import { Time } from '@oldschoolgg/toolkit';
+import { type APIApplication, DiscordClient, type DiscordClientOptions } from '@oldschoolgg/discord';
 import { RedisKeys } from '@oldschoolgg/util';
 
 import { mentionCommand } from '@/discord/utils.js';
@@ -52,80 +44,4 @@ export class RoboChimpBotClient extends DiscordClient {
 	async handleReadyEvent({ application }: { application: APIApplication }) {
 		console.log(`Logged in as ${application.bot?.username} after ${process.uptime()}s`);
 	}
-
-	// async channelIsSendable(channelId: IChannel | string): Promise<boolean> {
-	// 	const channel = typeof channelId === 'string' ? await Cache.getChannel(channelId) : channelId;
-	// 	if (!channel) return false;
-	// 	if (
-	// 		![ChannelType.DM, ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread].includes(
-	// 			channel.type
-	// 		)
-	// 	) {
-	// 		return false;
-	// 	}
-	// 	return true;
-	// }
-
-	async pickStringWithButtons({
-		options,
-		content,
-		interaction,
-		allowedUsers
-	}: {
-		allowedUsers?: string[];
-		interaction: MInteraction;
-		options: { label?: string; id: string; emoji?: string }[];
-		content: string;
-	}): Promise<{ choice: { label?: string; id: string; emoji?: string }; userId: string } | null> {
-		const CUSTOM_ID_PREFIX = `DYN_PICK_STRING_BUTTON_`;
-		try {
-			const buttons = options.map(opt => {
-				const button = new ButtonBuilder()
-					.setCustomId(`${CUSTOM_ID_PREFIX}${opt.id}`)
-					.setStyle(ButtonStyle.Secondary);
-				if (opt.emoji) {
-					button.setEmoji({ id: opt.emoji });
-				}
-				if (opt.label) {
-					button.setLabel(opt.label);
-				}
-				return button;
-			});
-			allowedUsers ??= [interaction.userId];
-			await interaction.defer();
-			const msg = await interaction.reply({
-				content,
-				components: buttons,
-				withResponse: true
-			});
-			const res = await collectSingleInteraction({
-				interaction,
-				messageId: msg!.id,
-				channelId: msg!.channel_id,
-				users: allowedUsers,
-				timeoutMs: Time.Second * 30
-			});
-			if (!res) return null;
-			res.silentButtonAck();
-			const resId = res.customId!.replace(CUSTOM_ID_PREFIX, '');
-			const choice = options.find(o => o.id === resId)!;
-			return { choice, userId: res.userId };
-		} catch (err) {
-			console.error(err as Error);
-			return null;
-		}
-	}
-
-	// async reactToMsg({
-	// 	channelId,
-	// 	messageId,
-	// 	emojiId
-	// }: {
-	// 	channelId: string;
-	// 	messageId: string;
-	// 	emojiId: keyof typeof ReactEmoji;
-	// }) {
-	// 	const route = Routes.channelMessageOwnReaction(channelId, messageId, encodeURIComponent(ReactEmoji[emojiId]));
-	// 	await this.rest.put(route);
-	// }
 }
