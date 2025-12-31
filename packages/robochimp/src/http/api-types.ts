739e03a84216b60f7120b865590bcb2fd7a5f78b
GC <30398469+gc@users.noreply.github.com>
2025-12-31 19:10:14 +1100

    Robochimp server changes (#6705)

---
+++ packages/robochimp/src/http/api-types.ts
@@ -0,0 +1,28 @@
+import type { ItemBank } from 'oldschooljs';
+
+export type AuthenticatedUser = {
+	id: string;
+	username: string | null;
+	avatar: string | null;
+	global_name: string | null;
+	bits: number[];
+};
+
+type RoboChimpInfo = {
+	osb_total_level: number | null;
+	bso_total_level: number | null;
+	// osb_total_xp: number | null;
+	// bso_total_xp: number | null;
+	osb_cl_percent: number | null;
+	bso_cl_percent: number | null;
+	osb_mastery: number | null;
+	bso_mastery: number | null;
+};
+
+export type FullMinionData = RoboChimpInfo & {
+	gp: number;
+	is_ironman: boolean;
+	qp: number;
+	collection_log_bank: ItemBank;
+	bitfield: number[];
+};
