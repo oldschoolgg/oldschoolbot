3638a094100a9935a70915ee7b0bd022a4adfa3d
TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>
2026-03-24 13:23:25 +0000

    Update Various Tables
    Updated the following tables (mainly for the addition of shark lures)
    Bosses: Zulrah, Phantom Muspah, Vorkath, Grotesque Guardians, Thermonuclear Smoke Devil, Drake.
    Openables: Brimstone Chest, Larran’s Chest (big/small)

--- src/lib/openables.ts
+++ src/lib/openables.ts
@@ -205,7 +205,7 @@ const osjsOpenables: UnifiedOpenable[] = [
 
 			return { bank: openLoot };
 		},
-		allItems: BrimstoneChest.table.allItems
+		allItems: BrimstoneChest.allItems
 	},
 	{
 		name: 'Elven crystal chest',
@@ -290,7 +290,7 @@ const osjsOpenables: UnifiedOpenable[] = [
 
 			return { bank: openLoot };
 		},
-		allItems: LarransChest.table.allItems
+		allItems: LarransChest.allItems
 	},
 	{
 		name: 'Muddy chest',
