61c7c52e7cd32b2faf4d39c9290d286a7add09cf
themrrobert <10122432+themrrobert@users.noreply.github.com>
2026-06-14 15:31:44 -0700

    dont shadowban new players with legit bit

--- src/lib/bso/handleCrateSpawns.ts
+++ src/lib/bso/handleCrateSpawns.ts
@@ -4,17 +4,19 @@ import { reduceNumByPercent, Time } from '@oldschoolgg/toolkit';
 import { roll } from 'node-rng';
 import { Bank, Items } from 'oldschooljs';
 
+import { BitField } from '@/lib/constants.js';
+
 const crateItem = Items.getOrThrow(BSOItem.SUMMER_CRATE_S9);
 
 export function handleCrateSpawns(user: MUser, duration: number, kind: 'trip' | 'tame' = 'trip', _messages?: string[]) {
 	const accountAge = user.accountAgeInDays();
 	let dropratePerMinute = 8 * 60;
 	if (kind === 'tame') dropratePerMinute *= 2;
-	if (accountAge) {
+	if (!user.isIronman && !user.bitfield.includes(BitField.LegitNewPlayer) && accountAge !== null) {
 		if (accountAge < 31) return null;
-		if (user.isIronman) {
-			dropratePerMinute = reduceNumByPercent(dropratePerMinute, 35);
-		}
+	}
+	if (user.isIronman) {
+		dropratePerMinute = reduceNumByPercent(dropratePerMinute, 35);
 	}
 	dropratePerMinute = Math.ceil(dropratePerMinute / 3);
 	dropratePerMinute = Math.ceil(dropratePerMinute / 2);
