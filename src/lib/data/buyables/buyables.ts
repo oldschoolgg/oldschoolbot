abe848fa1485a24e209972ff1966fe87b7141e65
themrrobert <10122432+themrrobert@users.noreply.github.com>
2026-06-21 04:04:27 -0700

    Fix autofarm repeat mess

--- src/lib/data/buyables/buyables.ts
+++ src/lib/data/buyables/buyables.ts
@@ -773,6 +773,12 @@ const noveltyFood: Buyable[] = [
 
 const Buyables: Buyable[] = [
 	{
+		name: 'Pestle and mortar',
+		aliases: ['mortar and pestle', 'pestle & mortar', 'mortar & pestle'],
+		gpCost: 10_000,
+		ironmanPrice: 128
+	},
+	{
 		name: 'Rope',
 		aliases: ['rope'],
 		gpCost: 100,
