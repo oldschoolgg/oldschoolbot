b2c7d4ed29671bbae36a73914a466da7e093efa6
themrrobert <10122432+themrrobert@users.noreply.github.com>
2026-06-24 05:47:20 -0700

    Error logging

--- src/discord/commandHandler.ts
+++ src/discord/commandHandler.ts
@@ -94,12 +94,21 @@ export async function rawCommandHandlerInner({
 		return response;
 	} catch (err) {
 		if ((err as Error).message === SILENT_ERROR) return SpecialResponse.SilentErrorResponse;
+		const date = new Date();
 		Logging.logError({
 			err: err as Error,
-			interaction,
-			context: { command: command.name, options: JSON.stringify(options) }
+			context: {
+				type: 'COMMAND_ERROR',
+				user_id: interaction.userId,
+				command_name: command.name,
+				channel_id: interaction.channelId,
+				guild_id: interaction.guildId,
+				datetime: date.toISOString(),
+				timestamp: date.getTime(),
+				options: JSON.stringify(options)
+			}
 		});
-		if (err instanceof UserError) return SpecialResponse.RespondedManually;
+		if (err instanceof UserError) return { content: `👻 ${err.message}` };
 		return {
 			content: `An error occurred while running this command.`
 		};
