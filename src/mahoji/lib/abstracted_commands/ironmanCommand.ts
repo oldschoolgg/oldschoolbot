086f24da6d215beaae9940a660aa4ceca186bfd4
themrrobert <10122432+themrrobert@users.noreply.github.com>
2026-07-08 15:29:04 -0700

    hopefully last time

--- src/mahoji/lib/abstracted_commands/ironmanCommand.ts
+++ src/mahoji/lib/abstracted_commands/ironmanCommand.ts
@@ -143,6 +143,9 @@ Type \`confirm permanent ironman\` if you understand the above information, and 
 	await prisma.pinnedTrip.deleteMany({ where: { user_id: user.id } });
 	await prisma.farmedCrop.deleteMany({ where: { user_id: user.id } });
 	await prisma.portent.deleteMany({ where: { user_id: user.id } });
+	await prisma.minigame.deleteMany({ where: { user_id: user.id } });
+	await prisma.slayerTask.deleteMany({ where: { user_id: user.id } });
+
 	// Now we can delete the user
 	await prisma.user.deleteMany({
 		where: { id: user.id }
@@ -150,9 +153,7 @@ Type \`confirm permanent ironman\` if you understand the above information, and 
 	await prisma.user.create({
 		data: createOptions
 	});
-	await prisma.slayerTask.deleteMany({ where: { user_id: user.id } });
 	await prisma.playerOwnedHouse.deleteMany({ where: { user_id: user.id } });
-	await prisma.minigame.deleteMany({ where: { user_id: user.id } });
 	await prisma.xPGain.deleteMany({ where: { user_id: BigInt(user.id) } });
 	await prisma.activity.deleteMany({ where: { user_id: BigInt(user.id) } });
 	await prisma.stashUnit.deleteMany({ where: { user_id: BigInt(user.id) } });
