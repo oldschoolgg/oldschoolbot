12d92d78d405fd38ebc563fd7c5dcf5ac95b6b83
DayV <69717452+DayV-git@users.noreply.github.com>
2026-07-26 13:45:02 +0100

    Update Blood rune cost for Sanguinesti staff (#6990)
    Co-authored-by: TastyPumPum <79149170+TastyPumPum@users.noreply.github.com>

--- src/lib/degradeableItems.ts
+++ src/lib/degradeableItems.ts
@@ -78,7 +78,7 @@ export const degradeableItems: DegradeableItem[] = [
 		setup: 'mage',
 		aliases: ['sang', 'sang staff', 'sanguinesti staff', 'sanguinesti'],
 		chargeInput: {
-			cost: new Bank().add('Blood rune', 3).freeze(),
+			cost: new Bank().add('Blood rune', 2).freeze(),
 			charges: 1
 		},
 		unchargedItem: Items.getOrThrow('Sanguinesti staff (uncharged)'),
