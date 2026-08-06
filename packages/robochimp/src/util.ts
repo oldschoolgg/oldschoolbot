22eb4fce99a6cb8dd694772d39f7e2fd53087e5e
themrrobert <10122432+themrrobert@users.noreply.github.com>
2026-08-06 04:41:45 -0700

    Robochimp Branch (#7015)

--- packages/robochimp/src/util.ts
+++ packages/robochimp/src/util.ts
@@ -25,6 +25,7 @@ export enum Bits {
 	CyrPatronTier6 = 22,
 	CyrPatronTier7 = 23,
 	CyrsOriginalPatrons = 24,
+	BonusMinute = 25,
 	PatronTier1 = MagnaPatronTier1,
 	PatronTier2 = MagnaPatronTier2,
 	PatronTier3 = MagnaPatronTier3,
@@ -60,7 +61,8 @@ export const bitsDescriptions: BitDescriptions = {
 	[Bits.CyrPatronTier5]: { description: 'Cyr Tier 5 patron' },
 	[Bits.CyrPatronTier6]: { description: 'Cyr Tier 6 patron' },
 	[Bits.CyrPatronTier7]: { description: 'Cyr Tier 7 patron' },
-	[Bits.CyrsOriginalPatrons]: { description: "Cyr's original patron" }
+	[Bits.CyrsOriginalPatrons]: { description: "Cyr's original patron" },
+	[Bits.BonusMinute]: { description: 'Bonus minute' }
 };
 
 export const CHANNELS = {
