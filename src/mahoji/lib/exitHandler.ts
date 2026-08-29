041b614279d7deea4a45196024d119050aec44dd
DayV <ashleyjd22@gmail.com>
2026-08-29 13:12:45 +0100

    Linting

--- src/mahoji/lib/exitHandler.ts
+++ src/mahoji/lib/exitHandler.ts
@@ -1,6 +1,7 @@
 import { TimerManager } from '@sapphire/timer-manager';
 
 import '@/lib/cache/redis.js';
+
 import { sonicBoom } from '@/lib/util/logger.js';
 
 export async function exitCleanup() {
